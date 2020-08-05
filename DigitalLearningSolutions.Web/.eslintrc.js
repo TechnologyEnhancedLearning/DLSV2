b4ff09e98c8c4a6f98b7c140c7aeab9c0eeb7009
Ethan Mills <ethan.mills@softwire.com>
2020-08-05 09:44:57 +0100

    HEEDLS-82: Use Babel transpilation to achieve IE11 compatibility

--- DigitalLearningSolutions.Web/.eslintrc.js
+++ DigitalLearningSolutions.Web/.eslintrc.js
@@ -1,38 +1,38 @@
 module.exports = {
-    env: {
-        browser: true,
-        es2020: true,
-        jasmine: true
-    },
-    extends: [
-        'airbnb-base',
-    ],
-    parser: '@typescript-eslint/parser',
-    parserOptions: {
-        ecmaVersion: 11,
-        sourceType: 'module',
-    },
-    settings: {
-        'import/resolver': 'webpack'
-    },
-    plugins: [
-        '@typescript-eslint',
-        'jasmine'
-    ],
+  env: {
+    browser: true,
+    es2020: true,
+    jasmine: true,
+  },
+  extends: [
+    'airbnb-base',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 11,
+    sourceType: 'module',
+  },
+  settings: {
+    'import/resolver': 'webpack',
+  },
+  plugins: [
+    '@typescript-eslint',
+    'jasmine',
+  ],
 
-    rules: {
-        "import/extensions": [
-            'error',
-            'ignorePackages',
-            {
-                "js": 'never',
-                "jsx": 'never',
-                "ts": 'never',
-                "tsx": 'never'
-            }
-        ],
-        "linebreak-style": 0,
-        "import/no-extraneous-dependencies": ['error', { "devDependencies": ['Scripts/spec/**/*.ts'] }],
-        "no-use-before-define": ["error", { "functions": false }]
-    }
+  rules: {
+    'import/extensions': [
+      'error',
+      'ignorePackages',
+      {
+        js: 'never',
+        jsx: 'never',
+        ts: 'never',
+        tsx: 'never',
+      },
+    ],
+    'linebreak-style': 0,
+    'import/no-extraneous-dependencies': ['error', { devDependencies: ['Scripts/spec/**/*.ts', './*.js'] }],
+    'no-use-before-define': ['error', { functions: false }],
+  },
 };
--- DigitalLearningSolutions.Web/webpack.config.js
+++ DigitalLearningSolutions.Web/webpack.config.js
@@ -1,42 +1,60 @@
-const glob = require("glob");
-const path = require("path");
+const glob = require('glob');
+const path = require('path');
+const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
 
 const entry = {};
-glob.sync("./Scripts/**/*.ts",
-    {
-        ignore: [
-            "./Scripts/**/*.d.ts",
-            "./Scripts/helpers/**/*.ts",
-            "./Scripts/spec/**/*.*"
-        ]
-    }
-).forEach(file => {
-    const name = file.replace("./Scripts/", "").replace(".ts", "");
-    entry[name] = file;
+glob.sync('./Scripts/**/*.ts',
+  {
+    ignore: [
+      './Scripts/**/*.d.ts',
+      './Scripts/helpers/**/*.ts',
+      './Scripts/spec/**/*.*',
+    ],
+  }).forEach((file) => {
+  const name = file.replace('./Scripts/', '').replace('.ts', '');
+  entry[name] = file;
 });
 
 const config = {
-    entry: entry,
-    mode: "production",
-    output: {
-        path: path.join(__dirname, "./wwwroot/js/"),
-        filename: "[name].js",
-        devtoolModuleFilenameTemplate: info => path.join("../../", info.resourcePath)
-    },
-    module: {
-        rules: [
-            {
-                test: /\.ts$/,
-                loader: "ts-loader",
-                options: {
-                    configFile: "tsconfig.json"
-                }
-            }
-        ]
-    },
-    resolve: {
-        extensions: [".ts", ".js"]
-    }
+  entry,
+  mode: 'production',
+  output: {
+    path: path.join(__dirname, './wwwroot/js/'),
+    filename: '[name].js',
+    devtoolModuleFilenameTemplate: (info) => path.join('../../', info.resourcePath),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.[j,t]s$/,
+        loader: 'babel-loader',
+        options: {
+          presets: [
+            ['@babel/preset-env',
+              {
+                targets: {
+                  ie: '11',
+                },
+              }],
+            ['@babel/preset-typescript',
+              {
+                onlyRemoveTypeImports: true,
+              },
+            ],
+          ],
+          plugins: [
+            'transform-es2015-arrow-functions',
+          ],
+        },
+      },
+    ],
+  },
+  plugins: [
+    new ForkTsCheckerWebpackPlugin(),
+  ],
+  resolve: {
+    extensions: ['.ts', '.js'],
+  },
 };
 
 module.exports = config;
