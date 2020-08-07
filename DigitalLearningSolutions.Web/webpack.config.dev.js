ac982c74bb937ded35f271f3f05c6b1c08aa86e1
Ethan Mills <ethan.mills@softwire.com>
2020-08-07 17:29:26 +0100

    HEEDLS-None: Fix dev:webpack

--- DigitalLearningSolutions.Web/webpack.config.dev.js
+++ DigitalLearningSolutions.Web/webpack.config.dev.js
@@ -2,7 +2,6 @@ const glob = require("glob");
 const config = require("./webpack.config");
 
 config.mode = "development";
-config.module.rules.find(rule => rule.loader === "ts-loader").options.configFile = "tsconfig.dev.json";
 config.devtool = "source-map";
 config.watch = true;
 
