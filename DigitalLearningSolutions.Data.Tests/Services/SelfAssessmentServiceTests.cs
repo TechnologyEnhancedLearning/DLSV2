4fdf23e430c115486a4ca6f6bd453ae98d5f8887
FridaTveit <frida.tveit@softwire.com>
2020-08-10 12:52:15 +0100

    HEEDLS-NONE Add self assessment service

---
+++ DigitalLearningSolutions.Data.Tests/Services/SelfAssessmentServiceTests.cs
@@ -0,0 +1,27 @@
+﻿namespace DigitalLearningSolutions.Data.Tests.Services
+{
+    using DigitalLearningSolutions.Data.Services;
+    using DigitalLearningSolutions.Data.Tests.Helpers;
+    using NUnit.Framework;
+    using FluentAssertions;
+
+    [Parallelizable(ParallelScope.Fixtures)]
+    public class SelfAssessmentServiceTests
+    {
+        private SelfAssessmentService selfAssessmentService;
+
+        [SetUp]
+        public void Setup()
+        {
+            var connection = ServiceTestHelper.GetDatabaseConnection();
+            selfAssessmentService = new SelfAssessmentService(connection);
+        }
+
+        [Test]
+        public void Example_test()
+        {
+            var result = selfAssessmentService.Example();
+            result.Should().Be("");
+        }
+    }
+}
