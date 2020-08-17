7724dff0ea92a63d8974547f69099d49d6cd8ad5
Olaf Davis <olaf.davis@softwire.com>
2020-08-17 15:04:25 +0100

    HEEDLS-49: extracted BaseCourse and BaseCourseViewModel classes
    HEEDLS-49: fix inheritance

--- DigitalLearningSolutions.Data/Models/CompletedCourse.cs
+++ DigitalLearningSolutions.Data/Models/CompletedCourse.cs
@@ -2,19 +2,9 @@
 {
     using System;
 
-    public class CompletedCourse
+    public class CompletedCourse : BaseCourse
     {
-        public string CourseName { get; set; }
-        public int CustomisationID { get; set; }
-        public bool HasDiagnostic { get; set; }
-        public bool HasLearning { get; set; }
-        public bool IsAssessed { get; set; }
-        public DateTime StartedDate { get; set; }
         public DateTime Completed { get; set; }
         public DateTime Evaluated { get; set; }
-        public int? DiagnosticScore { get; set; }
-        public int Passes { get; set; }
-        public int Sections { get; set; }
-        public int ProgressID { get; set; }
     }
 }
