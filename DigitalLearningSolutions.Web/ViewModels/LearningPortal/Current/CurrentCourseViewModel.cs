a15e82caed00d5b6233506fc4abc3135d5e5fd29
Olaf Davis <olaf.davis@softwire.com>
2020-08-18 09:54:51 +0100

    HEEDLS-22: add subdirectories to learningportal view models

---
+++ DigitalLearningSolutions.Web/ViewModels/LearningPortal/Current/CurrentCourseViewModel.cs
@@ -0,0 +1,53 @@
+﻿namespace DigitalLearningSolutions.Web.ViewModels.LearningPortal
+{
+    using System;
+    using DigitalLearningSolutions.Data.Models;
+    using DigitalLearningSolutions.Web.ControllerHelpers;
+    using Microsoft.Extensions.Configuration;
+
+    public class CurrentCourseViewModel : BaseCourseViewModel
+    {
+        public DateTime LastAccessedDate { get; }
+        public DateTime? CompleteByDate { get; }
+        public bool UserIsSupervisor { get; }
+        public bool IsEnrolledWithGroup { get; }
+        public bool IsLocked { get; }
+        public bool SelfEnrolled { get; }
+        public DateValidator.ValidationResult? CompleteByValidationResult { get; set; }
+
+        public CurrentCourseViewModel(CurrentCourse course, IConfiguration config) : base(course, config)
+        {
+            LastAccessedDate = course.LastAccessed;
+            CompleteByDate = course.CompleteByDate;
+            UserIsSupervisor = course.SupervisorAdminId != 0;
+            IsEnrolledWithGroup = course.GroupCustomisationId != 0;
+            SelfEnrolled = course.EnrollmentMethodID == 1;
+            IsLocked = course.PLLocked;
+        }
+
+        public string DateStyle()
+        {
+            if (CompleteByDate < DateTime.Today)
+            {
+                return "overdue";
+            }
+
+            if (CompleteByDate < (DateTime.Today + TimeSpan.FromDays(30)))
+            {
+                return "due-soon";
+            }
+
+            return "";
+        }
+
+        public string DueByDescription()
+        {
+            return DateStyle() switch
+            {
+                "overdue" => "Overdue:",
+                "due-soon" => "Due soon:",
+                _ => ""
+            };
+        }
+    }
+}
--- DigitalLearningSolutions.Web/ViewModels/LearningPortal/SelfAssessmentCardViewModel.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace DigitalLearningSolutions.Web.ViewModels.LearningPortal
-{
-    public class SelfAssessmentCardViewModel : NamedItemViewModel
-    {
-        public string Name { get; set; }
-    }
-}
