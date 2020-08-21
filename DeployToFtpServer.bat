1394a1130970df8672c40408c0b3abe972fd41dd
FridaTveit <frida.tveit@softwire.com>
2020-08-21 17:03:36 +0100

    HEEDLS-NONE Add script for deploy to live

---
+++ DeployToFtpServer.bat
@@ -0,0 +1,25 @@
+@echo off
+IF EXIST WinSCP.log (
+    del WinSCP.log
+)
+
+rem deploy to filesystem
+SET PUBLISH_PROFILE_PATH=
+dotnet build DigitalLearningSolutions.Web/DigitalLearningSolutions.Web.csproj -c Release /p:DeployOnBuild=true /p:PublishProfile=DigitalLearningSolutions.Web/Properties/PublishProfiles/%2.pubxml
+if %ERRORLEVEL% neq 0 goto builderror 
+
+rem ftp upload
+"C:\Program Files (x86)\WinSCP\WinSCP.exe" /log="WinSCP.log" /ini=nul /script="DeployToFtpServer.txt" /parameter %1 %3
+if %ERRORLEVEL% neq 0 goto ftperror 
+
+echo Deployment succeeded
+exit /b 0
+
+:builderror
+echo Publish to folder failed, please run 'npm run build' in the DigitalLearningSolutions.Web folder before trying again
+exit /b 1
+
+:ftperror
+echo Ftp transfer failed
+FOR /F "tokens=* delims=" %%x in (WinSCP.log) DO echo %%x
+exit /b 1
