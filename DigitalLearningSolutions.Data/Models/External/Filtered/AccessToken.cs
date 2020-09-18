96ce20d9909a9e9fcd4dfb82c75fd647345f2225
kevwhitt-hee <67740339+kevwhitt-hee@users.noreply.github.com>
2020-09-18 11:25:17 +0100

    DLSV2-10: Adds the required Filtered data models.

---
+++ DigitalLearningSolutions.Data/Models/External/Filtered/AccessToken.cs
@@ -0,0 +1,10 @@
+﻿namespace DigitalLearningSolutions.Data.Models.External.Filtered
+{
+    public class AccessToken
+    {
+#pragma warning disable IDE1006 // Naming Styles
+        public string jwt_access_token { get; set; }
+        public string refresh_token { get; set; }
+#pragma warning restore IDE1006 // Naming Styles
+    }
+}
---
+++ DigitalLearningSolutions.Data/Models/External/Filtered/Profile.cs
@@ -0,0 +1,11 @@
+﻿namespace DigitalLearningSolutions.Data.Models.External.Filtered
+{
+    public class Profile
+    {
+#pragma warning disable IDE1006 // Naming Styles
+        public string function { get; set; }
+        public string sector { get; set; }
+        public string seniority { get; set; }
+#pragma warning restore IDE1006 // Naming Styles
+    }
+}
