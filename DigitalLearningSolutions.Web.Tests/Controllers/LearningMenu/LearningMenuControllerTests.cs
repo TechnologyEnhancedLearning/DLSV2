be41c3cd66526d89efcf4eb6c4e62ae913b57adc
FridaTveit <frida.tveit@softwire.com>
2020-11-17 14:52:02 +0000

    HEEDLS-201 Add proof of concept content viewer
    Checking that we can show html content in an iframe + SCORM content in an iframe using the
    SCORM player in the old system.

--- DigitalLearningSolutions.Web.Tests/Controllers/LearningMenu/LearningMenuControllerTests.cs
+++ DigitalLearningSolutions.Web.Tests/Controllers/LearningMenu/LearningMenuControllerTests.cs
@@ -8,12 +8,14 @@
     using FluentAssertions.AspNetCore.Mvc;
     using Microsoft.AspNetCore.Http;
     using Microsoft.AspNetCore.Mvc;
+    using Microsoft.Extensions.Configuration;
     using Microsoft.Extensions.Logging;
     using NUnit.Framework;
 
     public class LearningMenuControllerTests
     {
         private LearningMenuController controller;
+        private IConfiguration config;
         private const int CandidateId = 11;
         private const int CentreId = 2;
         private const int CustomisationId = 12;
@@ -22,13 +24,14 @@
         public void SetUp()
         {
             var logger = A.Fake<ILogger<LearningMenuController>>();
+            config = A.Fake<IConfiguration>();
 
             var user = new ClaimsPrincipal(new ClaimsIdentity(new[]
             {
                 new Claim("learnCandidateID", CandidateId.ToString()),
                 new Claim("UserCentreID", CentreId.ToString())
             }, "mock"));
-            controller = new LearningMenuController(logger)
+            controller = new LearningMenuController(logger, config)
             {
                 ControllerContext = new ControllerContext() { HttpContext = new DefaultHttpContext { User = user } }
             };
@@ -42,6 +45,18 @@
 
             // Then
             var expectedModel = new InitialMenuViewModel(CustomisationId);
+            result.Should().BeViewResult()
+                .Model.Should().BeEquivalentTo(expectedModel);
+        }
+
+        [Test]
+        public void ContentViewer_should_render_view()
+        {
+            // When
+            var result = controller.ContentViewer();
+
+            // Then
+            var expectedModel = new ContentViewerViewModel(config);
             result.Should().BeViewResult()
                 .Model.Should().BeEquivalentTo(expectedModel);
         }
--- DigitalLearningSolutions.Web/Controllers/LearningMenuController/LearningMenuController.cs
+++ DigitalLearningSolutions.Web/Controllers/LearningMenuController/LearningMenuController.cs
@@ -5,22 +5,31 @@
     using DigitalLearningSolutions.Web.ViewModels.LearningPortal;
     using Microsoft.AspNetCore.Authorization;
     using Microsoft.AspNetCore.Mvc;
+    using Microsoft.Extensions.Configuration;
     using Microsoft.Extensions.Logging;
 
     [Authorize(Policy = CustomPolicies.UserOnly)]
     public class LearningMenuController : Controller
     {
         private readonly ILogger<LearningMenuController> logger;
+        private readonly IConfiguration config;
 
-        public LearningMenuController(ILogger<LearningMenuController> logger)
+        public LearningMenuController(ILogger<LearningMenuController> logger, IConfiguration config)
         {
             this.logger = logger;
+            this.config = config;
         }
 
         [Route("/LearningMenu/{customisationId:int}")]
         public IActionResult Index(int customisationId)
         {
             var model = new InitialMenuViewModel(customisationId);
+            return View(model);
+        }
+
+        public IActionResult ContentViewer()
+        {
+            var model = new ContentViewerViewModel(config);
             return View(model);
         }
     }
