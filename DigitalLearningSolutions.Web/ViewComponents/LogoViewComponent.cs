97b7d3b5dd14fc31dd410d7182c5a01796fe0e5d
kevwhitt-hee <67740339+kevwhitt-hee@users.noreply.github.com>
2020-11-17 13:49:13 +0000

    DLSV2-104: Moves _layout.cshtml to Views/Shared folder and repurposes for use across all DLS apps. Menu items for a specific app are brought in as a partial view in the Shared folder for that app (e.g. _NavMenuItems.cshtml). Error pages are also moved to Views/Shared/Error and repurposed to make generic (links are to App selector rather than LearningPortal/Current) for example.

---
+++ DigitalLearningSolutions.Web/ViewComponents/LogoViewComponent.cs
@@ -0,0 +1,37 @@
+﻿namespace DigitalLearningSolutions.Web.ViewComponents
+{
+    using System;
+    using DigitalLearningSolutions.Data.Services;
+    using DigitalLearningSolutions.Web.Helpers;
+    using Microsoft.AspNetCore.Mvc;
+    using System.Security.Claims;
+    using DigitalLearningSolutions.Web.ViewModels.Common;
+
+    public class LogoViewComponent : ViewComponent
+    {
+        private readonly ICentresService centresService;
+
+        public LogoViewComponent(ICentresService centresService)
+        {
+            this.centresService = centresService;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            var centreId = UserClaimsPrincipal.GetCustomClaimAsInt(CustomClaimTypes.UserCentreId);
+            if (centreId == null)
+            {
+                return View(new LogoViewModel(null));
+            }
+
+            var customLogo = centresService.GetCentreLogo(centreId.Value);
+            if (customLogo.LogoUrl == null)
+            {
+                return View(new LogoViewModel(null));
+            }
+
+            var model = new LogoViewModel(customLogo);
+            return View(model);
+        }
+    }
+}
