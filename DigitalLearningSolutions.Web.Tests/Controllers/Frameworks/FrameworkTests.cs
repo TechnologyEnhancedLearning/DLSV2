89ce3cbc84f37746c7027a144694397ba7729636
kevwhitt-hee <kevin.whittaker@hee.nhs.uk>
2020-11-26 08:40:19 +0000

    DLSV2-109 Begins to implement web test class

---
+++ DigitalLearningSolutions.Web.Tests/Controllers/Frameworks/FrameworkTests.cs
@@ -0,0 +1,15 @@
+﻿
+
+namespace DigitalLearningSolutions.Web.Tests.Controllers.Frameworks
+{
+    using DigitalLearningSolutions.Web.Tests.TestHelpers;
+    using DigitalLearningSolutions.Web.ViewModels.Frameworks.Dashboard;
+    using FakeItEasy;
+    using FluentAssertions;
+    using FluentAssertions.AspNetCore.Mvc;
+    using NUnit.Framework;
+    public class FrameworkTests
+    {
+       
+    }
+}
