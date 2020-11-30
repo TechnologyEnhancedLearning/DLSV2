1170faccf4ef31db9e858ba483074e5db62f2d5c
Shivam <Shivam.Chandarana@softwire.com>
2020-11-30 10:34:10 +0000

    Update to use backend data

--- DigitalLearningSolutions.Web/ViewModels/LearningMenu/InitialMenuViewModel.cs
+++ DigitalLearningSolutions.Web/ViewModels/LearningMenu/InitialMenuViewModel.cs
@@ -1,14 +1,18 @@
 ﻿namespace DigitalLearningSolutions.Web.ViewModels.LearningMenu
 {
+    using System.Collections.Generic;
+    using System.Linq;
     using DigitalLearningSolutions.Data.Models.CourseContent;
 
     public class InitialMenuViewModel
     {
         public CourseContent CourseContent { get; }
+        public IEnumerable<SectionCardViewModel> Sections { get; }
 
         public InitialMenuViewModel(CourseContent courseContent)
         {
             CourseContent = courseContent;
+            Sections = courseContent.Sections.Select(section => new SectionCardViewModel(section));
         }
     }
 }
---
+++ DigitalLearningSolutions.Web/ViewModels/LearningMenu/SectionCardViewModel.cs
@@ -0,0 +1,23 @@
+﻿namespace DigitalLearningSolutions.Web.ViewModels.LearningMenu
+{
+    using DigitalLearningSolutions.Data.Models.CourseContent;
+
+    public class SectionCardViewModel
+    {
+        public readonly string Title;
+        public readonly bool HasLearning;
+        public readonly double PercentComplete;
+
+        public SectionCardViewModel(CourseSection section)
+        {
+            Title = section.Title;
+            HasLearning = section.HasLearning;
+            PercentComplete = section.PercentComplete;
+        }
+
+        public string GetPercentComplete()
+        {
+            return HasLearning ? $"{PercentComplete}% Complete" : "";
+        }
+    }
+}
