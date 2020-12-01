9b526ecd67836eef0c5f60834cd2ec9f451f571c
Benji Marshall <Benji.Marshall@softwire.com>
2020-12-01 15:47:32 +0000

    Add SessionService (and tests) to track sessions

---
+++ DigitalLearningSolutions.Data.Tests/Helpers/SessionTestHelper.cs
@@ -0,0 +1,45 @@
+﻿namespace DigitalLearningSolutions.Data.Tests.Helpers
+{
+    using System.Collections.Generic;
+    using Dapper;
+    using DigitalLearningSolutions.Data.Models;
+    using FluentAssertions;
+    using Microsoft.Data.SqlClient;
+
+    public class SessionTestHelper
+    {
+        private SqlConnection connection;
+
+        public SessionTestHelper(SqlConnection connection)
+        {
+            this.connection = connection;
+        }
+
+        public Session? GetSession(int sessionId)
+        {
+            return connection.QueryFirstOrDefault<Session>(
+                @"SELECT SessionID, CandidateID, CustomisationID, LoginTime, Duration, Active
+                    FROM Sessions
+                   WHERE SessionID = @sessionId",
+                new { sessionId });
+        }
+
+        public IEnumerable<Session> GetCandidateSessions(int candidateId)
+        {
+            return connection.Query<Session>(
+                @"SELECT SessionID, CandidateID, CustomisationID, LoginTime, Duration, Active
+                    FROM Sessions
+                   WHERE CandidateID = @candidateId",
+                new { candidateId });
+        }
+
+        public static void SessionsShouldBeApproximatelyEquivalent(Session session1, Session session2)
+        {
+            session1.CandidateId.Should().Be(session2.CandidateId);
+            session1.CustomisationId.Should().Be(session2.CustomisationId);
+            session1.Duration.Should().Be(session2.Duration);
+            session1.Active.Should().Be(session2.Active);
+            session1.LoginTime.Should().BeCloseTo(session2.LoginTime, 10000);
+        }
+    }
+}
---
+++ DigitalLearningSolutions.Data.Tests/Services/SessionServiceTests.cs
@@ -0,0 +1,166 @@
+﻿namespace DigitalLearningSolutions.Data.Tests.Services
+{
+    using System;
+    using System.Linq;
+    using System.Transactions;
+    using DigitalLearningSolutions.Data.Models;
+    using DigitalLearningSolutions.Data.Services;
+    using DigitalLearningSolutions.Data.Tests.Helpers;
+    using FluentAssertions;
+    using NUnit.Framework;
+
+    internal class SessionServiceTests
+    {
+        private SessionService sessionService;
+        private SessionTestHelper sessionTestHelper;
+
+        [SetUp]
+        public void Setup()
+        {
+            var connection = ServiceTestHelper.GetDatabaseConnection();
+            sessionService = new SessionService(connection);
+            sessionTestHelper = new SessionTestHelper(connection);
+        }
+
+        [Test]
+        public void StartOrRestartSession_Should_Start_Users_First_Session()
+        {
+            using (new TransactionScope())
+            {
+                // When
+                const int candidateId = 29;
+                const int customisationId = 100;
+                var sessionId = sessionService.StartOrRestartSession(candidateId, customisationId);
+
+                // Then
+                var newSession = sessionTestHelper.GetSession(sessionId);
+
+                newSession.Should().NotBeNull();
+                SessionTestHelper.SessionsShouldBeApproximatelyEquivalent(
+                    newSession!,
+                    new Session(
+                        sessionId,
+                        candidateId,
+                        customisationId,
+                        DateTime.Now,
+                        0,
+                        true
+                    )
+                );
+            }
+        }
+
+        [Test]
+        public void StartOrRestartSession_Should_Start_Subsequent_Session()
+        {
+            using (new TransactionScope())
+            {
+                // When
+                const int candidateId = 101;
+                const int customisationId = 1240;
+                var sessionId = sessionService.StartOrRestartSession(candidateId, customisationId);
+
+                // Then
+                var newSession = sessionTestHelper.GetSession(sessionId);
+
+                newSession.Should().NotBeNull();
+                SessionTestHelper.SessionsShouldBeApproximatelyEquivalent(
+                    newSession!,
+                    new Session(
+                        sessionId,
+                        candidateId,
+                        customisationId,
+                        DateTime.Now,
+                        0,
+                        true
+                    )
+                );
+            }
+        }
+
+        [Test]
+        public void StartOrRestartSession_Should_Stop_Other_Sessions()
+        {
+            using (new TransactionScope())
+            {
+                // When
+                const int candidateId = 101;
+                const int customisationId = 1240;
+                var sessionId = sessionService.StartOrRestartSession(candidateId, customisationId);
+
+                // Then
+                var sessions = sessionTestHelper.GetCandidateSessions(candidateId);
+                var activeSessions = sessions.Where(session => session.Active);
+
+                activeSessions.Should().HaveCount(1);
+                activeSessions.First().SessionId.Should().Be(sessionId);
+            }
+        }
+
+        [Test]
+        public void StopSession_Should_Stop_Candidates_Sessions()
+        {
+            using (new TransactionScope())
+            {
+                // When
+                const int candidateId = 101;
+                sessionService.StopSession(candidateId);
+
+                // Then
+                var sessions = sessionTestHelper.GetCandidateSessions(candidateId);
+                var activeSessions = sessions.Where(session => session.Active);
+
+                activeSessions.Should().BeEmpty();
+            }
+        }
+
+        [Test]
+        public void UpdateSessionDuration_Should_Only_Update_Given_Active_Sessions()
+        {
+            const int twoMinutesInMilliseconds = 120 * 1000;
+
+            using (new TransactionScope())
+            {
+                // When
+                const int candidateId = 9;
+                const int sessionId = 473;
+                var startingSessions = sessionTestHelper.GetCandidateSessions(candidateId);
+
+                sessionService.UpdateSessionDuration(sessionId);
+
+                // Then
+                var newSessions = sessionTestHelper.GetCandidateSessions(candidateId).ToList();
+
+                newSessions
+                    .Where(session => session.SessionId != sessionId)
+                    .Should()
+                    .BeEquivalentTo(startingSessions.Where(session => session.SessionId != sessionId));
+
+                var newSession = newSessions.First(session => session.SessionId == sessionId);
+                DateTime.Now.Should().BeCloseTo(
+                    newSession.LoginTime.AddMinutes(newSession.Duration),
+                    twoMinutesInMilliseconds
+                );
+            }
+        }
+
+        [Test]
+        public void UpdateSessionDuration_Should_Not_Update_Inactive_Session()
+        {
+            using (new TransactionScope())
+            {
+                // When
+                const int candidateId = 9;
+                const int sessionId = 468;
+                var startingSessions = sessionTestHelper.GetCandidateSessions(candidateId);
+
+                sessionService.UpdateSessionDuration(sessionId);
+
+                // Then
+                var newSessions = sessionTestHelper.GetCandidateSessions(candidateId);
+
+                newSessions.Should().BeEquivalentTo(startingSessions);
+            }
+        }
+    }
+}
