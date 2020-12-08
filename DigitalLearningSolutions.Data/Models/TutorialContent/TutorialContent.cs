6db493fbfeebfdcc15fb28e3feef81147b17b70f
Benji Marshall <Benji.Marshall@softwire.com>
2020-12-08 09:24:25 +0000

    Add Tutorial Content Service
    Add service to get the contents of a given tutorial for that user.
    
    Add tests for this service.

---
+++ DigitalLearningSolutions.Data/Models/TutorialContent/TutorialContent.cs
@@ -0,0 +1,59 @@
+﻿namespace DigitalLearningSolutions.Data.Models.TutorialContent
+{
+    public class TutorialContent
+    {
+        public int Id { get; }
+        public int CandidateID { get; }
+        public int SectionID { get; }
+        public int CustomisationID { get; }
+        public string TutorialName { get; }
+        public string Status { get; }
+        public int TimeSpent { get; }
+        public int AverageTutMins { get; }
+        public int TutScore { get; }
+        public int PossScore { get; }
+        public bool DiagStatus { get; }
+        public int DiagAttempts { get; }
+        public string Objectives { get; }
+        public string VideoPath { get; }
+        public string TutorialPath { get; }
+        public string SupportingMatsPath { get; }
+
+        public TutorialContent(
+            int id,
+            int candidateId,
+            int sectionId,
+            int customisationId,
+            string tutorialName,
+            string status,
+            int timeSpent,
+            int averageTutMins,
+            int tutScore,
+            int possScore,
+            bool diagStatus,
+            int diagAttempts,
+            string objectives,
+            string videoPath,
+            string tutorialPath,
+            string supportingMatsPath
+        )
+        {
+            Id = id;
+            CandidateID = candidateId;
+            SectionID = sectionId;
+            CustomisationID = customisationId;
+            TutorialName = tutorialName;
+            Status = status;
+            TimeSpent = timeSpent;
+            AverageTutMins = averageTutMins;
+            TutScore = tutScore;
+            PossScore = possScore;
+            DiagStatus = diagStatus;
+            DiagAttempts = diagAttempts;
+            Objectives = objectives;
+            VideoPath = videoPath;
+            TutorialPath = tutorialPath;
+            SupportingMatsPath = supportingMatsPath;
+        }
+    }
+}
---
+++ DigitalLearningSolutions.Data/Services/TutorialContentService.cs
@@ -0,0 +1,62 @@
+﻿namespace DigitalLearningSolutions.Data.Services
+{
+    using System.Data;
+    using Dapper;
+    using DigitalLearningSolutions.Data.Models.TutorialContent;
+
+    public interface ITutorialContentService
+    {
+        TutorialContent? GetTutorialContent(int candidateId, int customisationId, int sectionId, int tutorialId);
+    }
+
+    public class TutorialContentService : ITutorialContentService
+    {
+        private readonly IDbConnection connection;
+
+        public TutorialContentService(IDbConnection connection)
+        {
+            this.connection = connection;
+        }
+
+        public TutorialContent? GetTutorialContent(int candidateId, int customisationId, int sectionId, int tutorialId)
+        {
+            return connection.QueryFirstOrDefault<TutorialContent?>(
+                @"SELECT Tutorials.TutorialID AS Id,
+                         Progress.CandidateID,
+                         Tutorials.SectionID,
+                         CustomisationTutorials.CustomisationID,
+                         Tutorials.TutorialName,
+                         TutStatus.Status,
+                         aspProgress.TutTime AS TimeSpent,
+                         Tutorials.AverageTutMins,
+                         aspProgress.DiagLast AS TutScore,
+                         Tutorials.DiagAssessOutOf AS PossScore,
+                         CustomisationTutorials.DiagStatus,
+                         aspProgress.DiagAttempts,
+                         Tutorials.Objectives,
+                         Tutorials.VideoPath,
+                         Tutorials.TutorialPath,
+                         Tutorials.SupportingMatsPath
+                    FROM Tutorials
+                         INNER JOIN aspProgress
+                         ON aspProgress.TutorialID = Tutorials.TutorialID
+
+                         INNER JOIN TutStatus
+                         ON aspProgress.TutStat = TutStatus.TutStatusID
+
+                         INNER JOIN CustomisationTutorials
+                         ON CustomisationTutorials.TutorialID = Tutorials.TutorialID
+
+                         INNER JOIN Progress
+                         ON aspProgress.ProgressID = Progress.ProgressID
+                            AND CustomisationTutorials.CustomisationID = Progress.CustomisationID
+                   WHERE Progress.CandidateID = @candidateId
+                     AND CustomisationTutorials.CustomisationID = @customisationId
+                     AND Tutorials.SectionId = @sectionId
+                     AND Tutorials.TutorialID = @tutorialId
+                     AND Progress.RemovedDate IS NULL
+                     AND Progress.SystemRefreshed = 0;",
+            new { candidateId, customisationId, sectionId, tutorialId });
+        }
+    }
+}
