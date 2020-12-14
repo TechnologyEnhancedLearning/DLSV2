959927cdceeb40c47dc593ac266f5ea5fbce1da8
Frank Ma <36454654+kwunkiuma@users.noreply.github.com>
2020-12-14 11:59:26 +0000

    HEEDLS-235 Show times on section cards front end (#84)
    Add time information to view, view model, and tests.
    
    Ran and passed all unit tests and manually tested in Firefox.

--- DigitalLearningSolutions.Web/ViewModels/LearningMenu/SectionCardViewModel.cs
+++ DigitalLearningSolutions.Web/ViewModels/LearningMenu/SectionCardViewModel.cs
@@ -8,6 +8,7 @@
         public int SectionId { get; }
         public string PercentComplete { get; }
         public int CustomisationId { get; }
+        public string TimeInformation { get; }
 
         public SectionCardViewModel(CourseSection section, int customisationId)
         {
@@ -15,6 +16,7 @@
             SectionId = section.Id;
             PercentComplete = section.HasLearning ? $"{section.PercentComplete:f0}% Complete" : "";
             CustomisationId = customisationId;
+            TimeInformation = $"{section.TimeMins}m (average time {section.AverageSectionTime}m)";
         }
     }
 }
