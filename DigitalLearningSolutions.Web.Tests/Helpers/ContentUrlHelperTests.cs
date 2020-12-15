be520994e1061de916360c3579e0260439c192bd
Benji Marshall <Benji.Marshall@softwire.com>
2020-12-15 15:50:27 +0000

    Move Tutorial Video URL parser into helper class

---
+++ DigitalLearningSolutions.Web.Tests/Helpers/ContentUrlHelperTests.cs
@@ -0,0 +1,60 @@
+﻿namespace DigitalLearningSolutions.Web.Tests.Helpers
+{
+    using DigitalLearningSolutions.Web.Helpers;
+    using FakeItEasy;
+    using FluentAssertions;
+    using Microsoft.Extensions.Configuration;
+    using NUnit.Framework;
+
+    class ContentUrlHelperTests
+    {
+        private IConfiguration config;
+        private const string BaseUrl = "https://example.com";
+
+        [SetUp]
+        public void SetUp()
+        {
+            config = A.Fake<IConfiguration>();
+            A.CallTo(() => config["CurrentSystemBaseUrl"]).Returns(BaseUrl);
+        }
+
+        [Test]
+        public void GetContentPath_should_parse_absolute_url()
+        {
+            // Given
+            const string videoPath = "https://example.com/testVideo.mp4";
+
+            // When
+            var parsedPath = ContentUrlHelper.GetContentPath(config, videoPath);
+
+            // Then
+            parsedPath.Should().Be(videoPath);
+        }
+
+        [Test]
+        public void GetContentPath_should_parse_protocol_relative_url()
+        {
+            // Given
+            const string videoPath = "example.com/testVideo.mp4";
+
+            // When
+            var parsedPath = ContentUrlHelper.GetContentPath(config, videoPath);
+
+            // Then
+            parsedPath.Should().Be($"https://{videoPath}");
+        }
+
+        [Test]
+        public void GetContentPath_should_parse_relative_path()
+        {
+            // Given
+            const string videoPath = "/testVideo.mp4";
+
+            // When
+            var parsedPath = ContentUrlHelper.GetContentPath(config, videoPath);
+
+            // Then
+            parsedPath.Should().Be(BaseUrl + videoPath);
+        }
+    }
+}
---
+++ DigitalLearningSolutions.Web/Helpers/ContentUrlHelper.cs
@@ -0,0 +1,23 @@
+﻿namespace DigitalLearningSolutions.Web.Helpers
+{
+    using System;
+    using Microsoft.Extensions.Configuration;
+
+    public static class ContentUrlHelper
+    {
+        public static string GetContentPath(IConfiguration config, string videoPath)
+        {
+            if (Uri.IsWellFormedUriString(videoPath, UriKind.Absolute))
+            {
+                return videoPath;
+            }
+
+            var urlWithProtocol = $"https://{videoPath}";
+            if (Uri.IsWellFormedUriString(urlWithProtocol, UriKind.Absolute))
+            {
+                return urlWithProtocol;
+            }
+            return config["CurrentSystemBaseUrl"] + videoPath;
+        }
+    }
+}
