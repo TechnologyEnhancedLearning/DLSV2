c01cc388f6608022e4632ed364b76c5fc0ccf140
Shivam <Shivam.Chandarana@softwire.com>
2020-12-17 16:55:40 +0000

    HEEDLS-NONE- Add Tutorial ID to Order By

--- DigitalLearningSolutions.Data/Services/SectionContentService.cs
+++ DigitalLearningSolutions.Data/Services/SectionContentService.cs
@@ -83,7 +83,7 @@
                         AND Sections.SectionID = @sectionId
                         AND (Sections.ArchivedDate IS NULL)
                         AND (CustomisationTutorials.DiagStatus = 1 OR Customisations.IsAssessed = 1 OR CustomisationTutorials.Status = 1)
-                    ORDER BY Tutorials.OrderByNumber",
+                    ORDER BY Tutorials.OrderByNumber, Tutorials.TutorialID",
                     (section, tutorial) =>
                 {
                     if (sectionContent == null)
