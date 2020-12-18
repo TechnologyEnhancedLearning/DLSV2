929f9bed38e08e0c3f70219defb03d76ba041f14
Benji Marshall <Benji.Marshall@softwire.com>
2020-12-18 15:27:25 +0000

    Remove times from section cards in initial menu
    Tidy up the view model and tests.

--- DigitalLearningSolutions.Web/ViewModels/LearningMenu/SectionCardViewModel.cs
+++ DigitalLearningSolutions.Web/ViewModels/LearningMenu/SectionCardViewModel.cs
@@ -8,7 +8,6 @@
         public int SectionId { get; }
         public string PercentComplete { get; }
         public int CustomisationId { get; }
-        public string TimeInformation { get; }
 
         public SectionCardViewModel(CourseSection section, int customisationId)
         {
@@ -16,7 +15,6 @@
             SectionId = section.Id;
             PercentComplete = section.HasLearning ? $"{section.PercentComplete:f0}% Complete" : "";
             CustomisationId = customisationId;
-            TimeInformation = $"{section.TimeMins}m (average time {section.AverageSectionTime}m)";
         }
     }
 }
