4d51e9d0d59f9f9f091efff9fbedacb5ab18cd32
Benji Marshall <benji.marshall@softwire.com>
2021-01-08 16:00:07 +0000

    HEEDLS-281 Fix tutorial IDs on diagnostic page (#137)
    Change way tutorial IDs are selected for diagnostic page, and update
    criteria for choosing diagnostic tutorials.
    
    Rewrite and add tests for diagnostic assessment service.

--- DigitalLearningSolutions.Data.Tests/Services/DiagnosticAssessmentServiceTests.cs
+++ DigitalLearningSolutions.Data.Tests/Services/DiagnosticAssessmentServiceTests.cs
@@ -95,31 +95,6 @@
         }
 
         [Test]
-        public void Get_diagnostic_assessment_should_return_assessment_if_only_is_assessed_is_true()
-        {
-            // Given
-            const int customisationId = 2684;
-            const int candidateId = 196;
-            const int sectionId = 74;
-
-            // When
-            var result = diagnosticAssessmentService.GetDiagnosticAssessment(customisationId, candidateId, sectionId);
-
-            // Then
-            var expectedDiagnosticAssessment = new DiagnosticAssessment(
-                "Level 2 - Microsoft Word 2007",
-                "Styles and Working with References",
-                "Working with documents",
-                0,
-                0,
-                18,
-                "https://www.dls.nhs.uk/tracking/MOST/Word07Core/Assess/L2_Word_2007_Diag_1.dcr",
-                true
-            );
-            result.Should().BeEquivalentTo(expectedDiagnosticAssessment);
-        }
-
-        [Test]
         public void Get_diagnostic_assessment_can_return_no_tutorials()
         {
             // Given
@@ -165,7 +140,7 @@
         }
 
         [Test]
-        public void Get_diagnostic_assessment_should_return_null_if_archived_date_is_not_null()
+        public void Get_diagnostic_assessment_should_return_null_if_section_archived_date_is_not_null()
         {
             // When
             const int customisationId = 14212;
@@ -178,12 +153,25 @@
         }
 
         [Test]
-        public void Get_diagnostic_assessment_should_return_null_if_diag_status_and_status_and_is_assessed_are_false()
+        public void Get_diagnostic_assessment_should_return_null_if_section_diagAssessPath_is_null()
+        {
+            // When
+            const int customisationId = 22206;
+            const int candidateId = 210962;
+            const int sectionId = 1920;
+            var result = diagnosticAssessmentService.GetDiagnosticAssessment(customisationId, candidateId, sectionId);
+
+            // Then
+            result.Should().BeNull();
+        }
+
+        [Test]
+        public void Get_diagnostic_assessment_should_return_null_if_all_diagStatus_are_false()
         {
             // Given
-            const int customisationId = 1530;
-            const int candidateId = 23573;
-            const int sectionId = 74;
+            const int customisationId = 4831;
+            const int candidateId = 11;
+            const int sectionId = 75;
 
             // When
             var result = diagnosticAssessmentService.GetDiagnosticAssessment(customisationId, candidateId, sectionId);
@@ -192,61 +180,166 @@
             result.Should().BeNull();
         }
 
-        [TestCase(70093, 3452, 110, 38227)]
-        [TestCase(84238, 6062, 105, 55672)]
-        [TestCase(83361, 5263, 161, 98818)]
-        [TestCase(173505, 11347, 178, 156377)]
-        [TestCase(121301, 5903, 169, 180362)]
-        [TestCase(181938, 5263, 153, 198839)]
-        [TestCase(269044, 10059, 212, 237170)]
-        public void Get_diagnostic_assessment_should_have_same_tutorials_as_stored_procedure(
-            int candidateId,
-            int customisationId,
-            int sectionId,
-            int progressId
-        )
+        [Test]
+        public void Get_diagnostic_assessment_should_not_have_tutorials_with_false_diagStatus()
         {
             // Given
-            var tutorialIdsReturnedFromOldStoredProcedure = diagnosticAssessmentTestHelper
-                .TutorialsFromOldStoredProcedure(progressId, sectionId)
-                .Select(tutorial => tutorial.TutorialId);
+            const int customisationId = 14579;
+            const int candidateId = 102375;
+            const int sectionId = 135;
 
             // When
-            var sectionIdsInCourseContent = diagnosticAssessmentService
-                .GetDiagnosticAssessment(customisationId, candidateId, sectionId)?
-                .Tutorials
-                .Select(section => section.Id);
+            var result = diagnosticAssessmentService.GetDiagnosticAssessment(customisationId, candidateId, sectionId);
 
             // Then
-            sectionIdsInCourseContent.Should().Equal(tutorialIdsReturnedFromOldStoredProcedure);
+            result.Should().NotBeNull();
+            var tutorialIds = result!.Tutorials.Select(tutorial => tutorial.Id).ToList();
+            tutorialIds.Should().NotContain(541); // Tutorial with DiagStatus 0
+            tutorialIds.Should().Equal(535, 536, 537, 538, 539, 540, 542);
         }
 
-        [TestCase(70093, 3452, 110, 38227)]
-        [TestCase(84238, 6062, 105, 55672)]
-        [TestCase(83361, 5263, 161, 98818)]
-        [TestCase(173505, 11347, 178, 156377)]
-        [TestCase(121301, 5903, 169, 180362)]
-        [TestCase(181938, 5263, 153, 198839)]
-        [TestCase(269044, 10059, 212, 237170)]
-        public void Get_diagnostic_assessment_should_have_same_scores_as_stored_procedure(
-            int candidateId,
-            int customisationId,
-            int sectionId,
-            int progressId
-        )
+        [Test]
+        public void Get_diagnostic_assessment_should_not_have_archived_tutorials()
         {
             // Given
-            var scoresReturnedFromOldStoredProcedure = diagnosticAssessmentTestHelper
-                .ScoresFromOldStoredProcedure(progressId, sectionId);
+            const int customisationId = 22416;
+            const int candidateId = 118178;
+            const int sectionId = 1955;
 
             // When
-            var sectionInCourseContent = diagnosticAssessmentService
-                .GetDiagnosticAssessment(customisationId, candidateId, sectionId);
+            var result = diagnosticAssessmentService.GetDiagnosticAssessment(customisationId, candidateId, sectionId);
 
             // Then
-            sectionInCourseContent.SectionScore.Should().Be(scoresReturnedFromOldStoredProcedure.SecScore);
-            sectionInCourseContent.MaxSectionScore.Should().Be(scoresReturnedFromOldStoredProcedure.SecOutOf);
-            sectionInCourseContent.DiagnosticAttempts.Should().Be(scoresReturnedFromOldStoredProcedure.DiagAttempts);
+            result.Should().NotBeNull();
+            var tutorialIds = result!.Tutorials.Select(tutorial => tutorial.Id).ToList();
+            tutorialIds.Should().NotContain(9366); // Archived tutorial
+            tutorialIds.Should().Equal(9332, 9333);
+        }
+
+        [Test]
+        public void Get_diagnostic_assessment_should_not_have_tutorials_with_diagAssessOutOf_less_than_1()
+        {
+            // Given
+            const int customisationId = 21058;
+            const int candidateId = 172968;
+            const int sectionId = 189;
+
+            // When
+            var result = diagnosticAssessmentService.GetDiagnosticAssessment(customisationId, candidateId, sectionId);
+
+            // Then
+            result.Should().NotBeNull();
+            var tutorialIds = result!.Tutorials.Select(tutorial => tutorial.Id).ToList();
+            tutorialIds.Should().NotContain(806); // Tutorial with DiagAssessOutOf = 0
+            tutorialIds.Should().Equal(804, 805, 807);
+        }
+
+        [Test]
+        public void Get_diagnostic_assessment_should_use_nonzero_originalTutorialIds()
+        {
+            // Given
+            const int customisationId = 26178;
+            const int candidateId = 285914;
+            const int sectionId = 2479;
+
+            // When
+            var result = diagnosticAssessmentService.GetDiagnosticAssessment(customisationId, candidateId, sectionId);
+
+            // Then
+            result.Should().NotBeNull();
+            var tutorialIds = result!.Tutorials.Select(tutorial => tutorial.Id).ToList();
+            // Tutorial ID 11376 was not replaced, but the other four have been
+            tutorialIds.Should().Equal(11376, 1576, 1577, 1578, 1579);
+        }
+
+        [Test]
+        public void Get_diagnostic_assessment_should_not_use_tutorials_with_diagStatus_0_in_scores()
+        {
+            // Given
+            const int customisationId = 14579;
+            const int candidateId = 102375;
+            const int sectionId = 135;
+
+            // When
+            var result = diagnosticAssessmentService.GetDiagnosticAssessment(customisationId, candidateId, sectionId);
+
+            // Then
+            result.Should().NotBeNull();
+            result!.SectionScore.Should().Be(0);
+            result!.MaxSectionScore.Should().Be(42); // Not 44 as uspReturnSectionsForCandCust_V2 returns because
+                                                     // it counts tutorial 541 with DiagStatus = 0
+            result!.DiagnosticAttempts.Should().Be(0);
+        }
+
+        [Test]
+        public void Get_diagnostic_assessment_should_not_use_archived_tutorials_in_scores()
+        {
+            // Given
+            const int customisationId = 22416;
+            const int candidateId = 118178;
+            const int sectionId = 1955;
+
+            // When
+            var result = diagnosticAssessmentService.GetDiagnosticAssessment(customisationId, candidateId, sectionId);
+
+            // Then
+            result.Should().NotBeNull();
+            result!.SectionScore.Should().Be(0);
+            result!.MaxSectionScore.Should().Be(2); // Not 3 as uspReturnSectionsForCandCust_V2 returns because
+                                                    // it counts archived tutorial 9366
+            result!.DiagnosticAttempts.Should().Be(1);
+        }
+
+        [Test]
+        public void Get_diagnostic_assessment_should_get_tutorialIds_ordered_by_orderBy()
+        {
+            // Given
+            const int candidateId = 210934;
+            const int customisationId = 17731;
+            const int sectionId = 801;
+
+            // Tutorial: 3330  OrderByNumber 1
+            // Tutorial: 3331  OrderByNumber 2
+            // Tutorial: 3332  OrderByNumber 3
+            // Tutorial: 3333  OrderByNumber 5
+            // Tutorial: 3334  OrderByNumber 4
+
+            // When
+            var result = diagnosticAssessmentService.GetDiagnosticAssessment(customisationId, candidateId, sectionId);
+
+            // Then
+            result.Should().NotBeNull();
+            var tutorialIds = result!.Tutorials.Select(tutorial => tutorial.Id).ToList();
+            tutorialIds.Should().Equal(3330, 3331, 3332, 3334, 3333);
+        }
+
+        [Test]
+        public void Get_diagnostic_assessment_should_get_tutorialIds_ordered_by_orderBy_then_tutorialId()
+        {
+            using (new TransactionScope())
+            {
+                // Given
+                const int candidateId = 1;
+                const int customisationId = 8194;
+                const int sectionId = 216;
+
+                diagnosticAssessmentTestHelper.UpdateDiagAssessOutOf(928, 1);
+
+                // ...
+                // Tutorial: 927  OrderByNumber 34
+                // Tutorial: 928  OrderByNumber 35
+                // Tutorial: 929  OrderByNumber 35
+                // ...
+
+                // When
+                var result = diagnosticAssessmentService.GetDiagnosticAssessment(customisationId, candidateId, sectionId);
+
+                // Then
+                result.Should().NotBeNull();
+                var tutorialIds = result!.Tutorials.Select(tutorial => tutorial.Id).ToList();
+                tutorialIds.Should()
+                    .Equal(923, 924, 925, 926, 927, 928, 929, 930, 931, 932, 933, 934, 935, 936, 937, 938, 939, 940);
+            }
         }
 
         [Test]
