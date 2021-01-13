deb8abf060c77f470cbd0ddedaa5d35f7a7ceced
Benji Marshall <Benji.Marshall@softwire.com>
2021-01-13 14:59:28 +0000

    Fix the tutorial page next button back end
    Get next section IDs when sections just have diagnostic assessments,
    or just have post learning assessments (previously it was only displayed
    if there was tutorial learning for that next section).
    
    Add tests to cover these cases.

--- DigitalLearningSolutions.Data.Tests/Services/TutorialContentServiceTests.cs
+++ DigitalLearningSolutions.Data.Tests/Services/TutorialContentServiceTests.cs
@@ -235,6 +235,66 @@
         }
 
         [Test]
+        public void Get_tutorial_information_nextSection_returns_section_with_only_diagnostic_assessment()
+        {
+            // Given
+            const int candidateId = 74411;
+            const int customisationId = 5852;
+            const int sectionId = 150;
+            const int tutorialId = 634;
+
+            const int nextSectionId = 151; // All tutorials are CustomisationTutorials.Status = 0, though some DiagStatus = 1
+
+            // When
+            var tutorial = tutorialContentService.GetTutorialInformation(candidateId, customisationId, sectionId, tutorialId);
+
+            // Then
+            tutorial.Should().NotBeNull();
+            tutorial!.NextSectionId.Should().Be(nextSectionId);
+        }
+
+        [Test]
+        public void Get_tutorial_information_nextSection_returns_section_with_only_post_learning_assessment()
+        {
+            // Given
+            const int customisationId = 10820;
+            const int candidateId = 1;
+            const int sectionId = 104;
+            const int tutorialId = 331;
+
+            const int expectedNextSectionId = 105;
+
+            // When
+            var tutorial = tutorialContentService.GetTutorialInformation(candidateId, customisationId, sectionId, tutorialId);
+
+            //Then
+            tutorial.NextSectionId.Should().Be(expectedNextSectionId);
+        }
+
+        [Test]
+        public void Get_tutorial_information_nextSection_skips_section_with_only_post_learning_assessment_but_no_path()
+        {
+            using (new TransactionScope())
+            {
+                // Given
+                const int customisationId = 10820;
+                const int candidateId = 1;
+                const int sectionId = 104;
+                const int tutorialId = 331;
+
+                const int originalNextSectionId = 105;
+                tutorialContentTestHelper.UpdatePLAssessPath(originalNextSectionId, null);
+                const int expectedNextSectionId = 106;
+
+                // When
+                var tutorial = tutorialContentService.GetTutorialInformation(candidateId, customisationId, sectionId, tutorialId);
+
+                //Then
+                tutorial.NextSectionId.Should().Be(expectedNextSectionId);
+            }
+        }
+
+        [Test]
         public void Get_tutorial_information_nextTutorial_returns_smaller_tutorialId_for_shared_orderByNumber()
         {
             // Given
