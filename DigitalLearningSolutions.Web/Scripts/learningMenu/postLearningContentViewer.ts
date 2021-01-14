39f0ade9127e5e5490d966fccec84d5b65a837e0
Frank Ma <frank.ma@softwire.com>
2021-01-14 11:45:50 +0000

    Add TypeScript for page close

---
+++ DigitalLearningSolutions.Web/Scripts/learningMenu/postLearningContentViewer.ts
@@ -0,0 +1,12 @@
+function postLearningCloseMpe(): void {
+  // Extract the current domain, customisationId and sectionId out of the URL
+  const matches = window.location.href.match(/^(.*)\/LearningMenu\/(\d+)\/(\d+)\/PostLearning\/Content$/);
+
+  if (!matches || matches.length < 4) {
+    return;
+  }
+
+  window.location.href = `${matches[1]}/LearningMenu/${matches[2]}/${matches[3]}/PostLearning`;
+}
+
+window.closeMpe = postLearningCloseMpe;
