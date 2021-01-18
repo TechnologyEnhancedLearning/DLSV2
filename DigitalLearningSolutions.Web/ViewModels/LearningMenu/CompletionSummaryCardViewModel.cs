c00d7e0132f4b41b6077a2955503cfe23dda88f2
Benji Marshall <benji.marshall@softwire.com>
2021-01-18 09:49:44 +0000

    HEEDLS-301 Extract completion summary card into partial (#175)
    Refactor the completion summary card out of the initial menu view and
    view model, into its own partial view.
    
    Rearrange some tests to reflect the refactoring.

---
+++ DigitalLearningSolutions.Web/ViewModels/LearningMenu/CompletionSummaryCardViewModel.cs
@@ -0,0 +1,36 @@
+﻿namespace DigitalLearningSolutions.Web.ViewModels.LearningMenu
+{
+    using System;
+    using DigitalLearningSolutions.Web.Helpers;
+
+    public class CompletionSummaryCardViewModel
+    {
+        public int CustomisationId { get; }
+        public string CompletionStatus { get; }
+        public string CompletionStyling { get; }
+        public string CompletionSummary { get; }
+
+        public CompletionSummaryCardViewModel(
+            int customisationId,
+            DateTime? completed,
+            int maxPostLearningAssessmentAttempts,
+            bool isAssessed,
+            int postLearningAssessmentPassThreshold,
+            int diagnosticAssessmentCompletionThreshold,
+            int tutorialsCompletionThreshold
+        )
+        {
+            CustomisationId = customisationId;
+            CompletionStatus = completed == null ? "Incomplete" : "Complete";
+            CompletionStyling = completed == null ? "incomplete" : "complete";
+            CompletionSummary = CompletionSummaryHelper.GetCompletionSummary(
+                completed,
+                maxPostLearningAssessmentAttempts,
+                isAssessed,
+                postLearningAssessmentPassThreshold,
+                diagnosticAssessmentCompletionThreshold,
+                tutorialsCompletionThreshold
+            );
+        }
+    }
+}
