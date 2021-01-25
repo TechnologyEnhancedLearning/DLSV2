8e22349f84ad64476e9535af0099a45fb9332227
Frank Ma <36454654+kwunkiuma@users.noreply.github.com>
2021-01-25 11:49:40 +0000

    HEEDLS-329 Diagnostic assessment fullscreen (#209)
    Add fullscreen functionality to diagnostic content page.
    
    The add responsive resizing; maintaining a 4:3 ratio.
    
    Modify typescript to work after entering fullscreen.
    
    Add tests for close page function after entering fullscreen.
    
    Issue with padding on sides is also fixed.
    
    Ran and passed all unit tests, screenshots taken in Google Chrome.
    
    There is an issue on Firefox where the height of content inside the iframe is too big.

--- DigitalLearningSolutions.Web/Scripts/learningMenu/diagnosticContentViewer.ts
+++ DigitalLearningSolutions.Web/Scripts/learningMenu/diagnosticContentViewer.ts
@@ -1,6 +1,8 @@
+import { setupFullscreen } from './fullscreen';
+
 function diagnosticCloseMpe(): void {
   // Extract the current domain, customisationId and sectionId out of the URL
-  const matches = window.location.href.match(/^(.*)\/LearningMenu\/(\d+)\/(\d+)\/Diagnostic\/Content(\?checkedTutorials=\d+(&checkedTutorials=\d+)*)?$/);
+  const matches = window.location.href.match(/^(.*)\/LearningMenu\/(\d+)\/(\d+)\/Diagnostic\/Content(\?checkedTutorials=\d+(&checkedTutorials=\d+)*)?#?$/);
 
   if (!matches || matches.length < 4) {
     return;
@@ -11,3 +13,4 @@ function diagnosticCloseMpe(): void {
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 (window as any).closeMpe = diagnosticCloseMpe;
+setupFullscreen();
