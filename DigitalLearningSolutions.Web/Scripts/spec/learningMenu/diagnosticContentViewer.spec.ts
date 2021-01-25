58b81e49f295d488df696fa1029ab73eba0c4b85
Frank Ma <frank.ma@softwire.com>
2021-01-25 11:08:00 +0000

    Add fullscreen to diagnostic content

--- DigitalLearningSolutions.Web/Scripts/spec/learningMenu/diagnosticContentViewer.spec.ts
+++ DigitalLearningSolutions.Web/Scripts/spec/learningMenu/diagnosticContentViewer.spec.ts
@@ -27,6 +27,18 @@ describe('closeMpe', () => {
       expect(window.location.href).toBe('https://localhost:44363/test/LearningMenu/123/456/Diagnostic');
     });
 
+  it('should redirect to diagnostic assessment with no checked tutorials after entering fullscreen',
+    () => {
+      // Given
+      window.location.href = 'https://localhost:44363/test/LearningMenu/123/456/Diagnostic/Content#';
+
+      // When
+      window.closeMpe();
+
+      // Then
+      expect(window.location.href).toBe('https://localhost:44363/test/LearningMenu/123/456/Diagnostic');
+    });
+
   it('should redirect to diagnostic assessment with one checked tutorial',
     () => {
       // Given
@@ -39,10 +51,34 @@ describe('closeMpe', () => {
       expect(window.location.href).toBe('https://localhost:44363/test/LearningMenu/123/456/Diagnostic');
     });
 
+  it('should redirect to diagnostic assessment with one checked tutorial after entering fullscreen',
+    () => {
+      // Given
+      window.location.href = 'https://localhost:44363/test/LearningMenu/123/456/Diagnostic/Content?checkedTutorials=1234#';
+
+      // When
+      window.closeMpe();
+
+      // Then
+      expect(window.location.href).toBe('https://localhost:44363/test/LearningMenu/123/456/Diagnostic');
+    });
+
   it('should redirect to diagnostic assessment with multiple checked tutorials',
     () => {
       // Given
       window.location.href = 'https://localhost:44363/test/LearningMenu/123/456/Diagnostic/Content?checkedTutorials=123&checkedTutorials=456';
+
+      // When
+      window.closeMpe();
+
+      // Then
+      expect(window.location.href).toBe('https://localhost:44363/test/LearningMenu/123/456/Diagnostic');
+    });
+
+  it('should redirect to diagnostic assessment with multiple checked tutorials after entering fullscreen',
+    () => {
+      // Given
+      window.location.href = 'https://localhost:44363/test/LearningMenu/123/456/Diagnostic/Content?checkedTutorials=123&checkedTutorials=456#';
 
       // When
       window.closeMpe();
