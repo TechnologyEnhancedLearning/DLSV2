6647c70aee9f538ed4e299968d3e0e80981f58bd
Daniel Bloxham <Daniel.Bloxham@softwire.com>
2021-03-17 09:16:15 +0000

    HEEDLS-356 - Created Migration that adds new ResetPassword table and adds new ResetPasswordID columns to AdminUsers table and Candidates table

---
+++ DigitalLearningSolutions.Data.Migrations/202103161535_AddResetPasswordTable.cs
@@ -0,0 +1,30 @@
+﻿namespace DigitalLearningSolutions.Data.Migrations
+{
+    using FluentMigrator;
+
+    [Migration(202103161535)]
+    public class AddResetPasswordTable : Migration
+    {
+        public override void Up()
+        {
+            Create.Table("ResetPassword")
+                .WithColumn("ID").AsInt32().NotNullable().PrimaryKey().Identity()
+                .WithColumn("ResetPasswordHash").AsCustom("text").NotNullable()
+                .WithColumn("PasswordResetDateTime").AsCustom("timestamp").NotNullable();
+
+            Alter.Table("AdminUsers")
+                .AddColumn("ResetPasswordID").AsInt32().Nullable();
+
+            Alter.Table("Candidates")
+                .AddColumn("ResetPasswordID").AsInt32().Nullable();
+
+        }
+
+        public override void Down()
+        {
+            Delete.Table("ResetPassword");
+            Delete.Column("ResetPasswordID").FromTable("AdminUsers");
+            Delete.Column("ResetPasswordID").FromTable("Candidates");
+        }
+    }
+}
