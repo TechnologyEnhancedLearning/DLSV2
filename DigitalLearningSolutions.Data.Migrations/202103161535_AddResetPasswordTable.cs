fd03aa030ac74291c5f5e98f546cf7fa4e9d5c38
Daniel Bloxham <Daniel.Bloxham@softwire.com>
2021-03-17 12:15:36 +0000

    HEEDLS-356 - Corrected the previously made migration to use the right data types and use foreign keys.

--- DigitalLearningSolutions.Data.Migrations/202103161535_AddResetPasswordTable.cs
+++ DigitalLearningSolutions.Data.Migrations/202103161535_AddResetPasswordTable.cs
@@ -9,22 +9,24 @@
         {
             Create.Table("ResetPassword")
                 .WithColumn("ID").AsInt32().NotNullable().PrimaryKey().Identity()
-                .WithColumn("ResetPasswordHash").AsCustom("text").NotNullable()
-                .WithColumn("PasswordResetDateTime").AsCustom("timestamp").NotNullable();
+                .WithColumn("ResetPasswordHash").AsString(64).NotNullable()
+                .WithColumn("PasswordResetDateTime").AsDateTime().NotNullable().WithDefault(SystemMethods.CurrentUTCDateTime); ;
 
             Alter.Table("AdminUsers")
-                .AddColumn("ResetPasswordID").AsInt32().Nullable();
+                .AddColumn("ResetPasswordID").AsInt32().Nullable().ForeignKey("FK_AdminUsers_ResetPasswordID_ResetPassword_ID", "ResetPassword", "ID");
 
             Alter.Table("Candidates")
-                .AddColumn("ResetPasswordID").AsInt32().Nullable();
+                .AddColumn("ResetPasswordID").AsInt32().Nullable().ForeignKey("FK_Candidates_ResetPasswordID_ResetPassword_ID", "ResetPassword", "ID"); ;
 
         }
 
         public override void Down()
         {
-            Delete.Table("ResetPassword");
+            Delete.ForeignKey("FK_AdminUsers_ResetPasswordID_ResetPassword_ID").OnTable("AdminUsers");
+            Delete.ForeignKey("FK_Candidates_ResetPasswordID_ResetPassword_ID").OnTable("Candidates");
             Delete.Column("ResetPasswordID").FromTable("AdminUsers");
             Delete.Column("ResetPasswordID").FromTable("Candidates");
+            Delete.Table("ResetPassword");
         }
     }
 }
