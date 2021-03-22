bb10dd4a028e47fae574675d1174af03fa2ac56c
Daniel Bloxham <Daniel.Bloxham@softwire.com>
2021-03-22 09:20:13 +0000

    HEEDLS-353 - Added a basic form page for resetting passwords, and a generic confirmation page.

---
+++ DigitalLearningSolutions.Web/Controllers/ForgotPasswordController.cs
@@ -0,0 +1,39 @@
+﻿namespace DigitalLearningSolutions.Web.Controllers
+{
+    using Microsoft.AspNetCore.Mvc;
+    using DigitalLearningSolutions.Web.ViewModels.ForgotPassword;
+    using DigitalLearningSolutions.Web.ViewModels.LearningSolutions;
+
+    public class ForgotPasswordController : Controller
+    {
+
+        [Route("/ForgotPassword")]
+        public IActionResult ForgotPassword()
+        {
+            var model = new ForgotPasswordViewModel();
+            return View(model);
+        }
+
+        [HttpPost]
+        public IActionResult SubmitEmail(string emailAddress)
+        {
+            //Do some validation
+            ConfirmationViewModel confirmationModel = GetForgotPasswordConfirmationViewModel();
+
+            return View("~/Views/LearningSolutions/Confirmation.cshtml", confirmationModel);
+        }
+
+        private ConfirmationViewModel GetForgotPasswordConfirmationViewModel()
+        {
+            string pageTitle = "Password Reset Email Sent";
+            string[] pageDescription =
+            {
+                "An email has been sent to you giving details of how to reset your password.",
+                "The link provided in that email will expire in two hours.",
+                "If you have not received an email, please contact your centre administrator."
+            };
+
+            return new ConfirmationViewModel(pageTitle, pageDescription);
+        }
+    }
+}
