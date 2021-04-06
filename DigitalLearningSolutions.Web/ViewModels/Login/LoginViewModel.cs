e1ed26bee39433c4a9c06bd23bab9160c4910d53
Daniel Bloxham <Daniel.Bloxham@softwire.com>
2021-04-06 11:03:19 +0100

    HEEDLS-363 - minor styling and removed dev comments.

--- DigitalLearningSolutions.Web/ViewModels/Login/LoginViewModel.cs
+++ DigitalLearningSolutions.Web/ViewModels/Login/LoginViewModel.cs
@@ -11,16 +11,13 @@
             RememberMe = false;
         }
 
-        [Display(Order = -1)]
         [Required(ErrorMessage = "Please enter a username to log in.")]
-        public string Username { get; set; }
+        public string? Username { get; set; }
 
-        [Display(Order = 2)]
         [Required (ErrorMessage = "Please enter a password to log in.")]
         [DataType(DataType.Password)]
-        public string Password { get; set; }
+        public string? Password { get; set; }
 
-        [Display(Order = 3)]
         public bool RememberMe { get; set; }
     }
 }
