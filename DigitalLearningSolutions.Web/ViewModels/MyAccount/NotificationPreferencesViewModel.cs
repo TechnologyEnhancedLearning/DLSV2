32a3ac5ba51ed1d46c67a08384ed438ad2ee1cef
Jonathan Bloxsom <jonathan.bloxsom@softwire.com>
2021-04-12 10:31:28 +0100

    HEEDLS-436: basic page structure

---
+++ DigitalLearningSolutions.Web/ViewModels/MyAccount/NotificationPreferencesViewModel.cs
@@ -0,0 +1,15 @@
+﻿namespace DigitalLearningSolutions.Web.ViewModels.MyAccount
+{
+    public class NotificationPreferencesViewModel
+    {
+        public NotificationPreferencesViewModel(int? adminId, int? delegateId)
+        {
+            AdminId = adminId;
+            DelegateId = delegateId;
+        }
+
+        public int? AdminId { get; set; }
+
+        public int? DelegateId { get; set; }
+    }
+}
