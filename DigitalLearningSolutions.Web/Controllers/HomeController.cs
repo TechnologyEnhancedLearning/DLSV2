be8ef252968950de12ff4af3d6583ce83ba927fe
Steve Jackson <stephen.jackson@softwire.com>
2021-04-14 14:04:35 +0100

    HEEDLS-338: Split landing page contents across minihub sections

--- DigitalLearningSolutions.Web/Controllers/HomeController.cs
+++ DigitalLearningSolutions.Web/Controllers/HomeController.cs
@@ -2,11 +2,57 @@
 
 namespace DigitalLearningSolutions.Web.Controllers
 {
+    using System.Collections.Generic;
+    using DigitalLearningSolutions.Web.ViewModels.Common.MiniHub;
+    using DigitalLearningSolutions.Web.ViewModels.Home;
+
     public class HomeController : Controller
     {
+        private readonly List<MiniHubSection> Sections = new List<MiniHubSection>(
+            new[]
+            {
+                new MiniHubSection { ControllerName = "Home", ActionName = "Welcome", SectionTitle = "Welcome" },
+                new MiniHubSection { ControllerName = "Home", ActionName = "Products", SectionTitle = "Products" },
+                new MiniHubSection
+                    { ControllerName = "Home", ActionName = "LearningContent", SectionTitle = "Learning Content" },
+            });
+
         public IActionResult Index()
         {
-            return View();
+            return RedirectToAction("Welcome");
+        }
+
+        [HttpGet]
+        [Route("Home/Section/Welcome")]
+        public IActionResult Welcome()
+        {
+            return View(GetLandingPageViewModel(0));
+        }
+
+        [HttpGet]
+        [Route("Home/Section/Products")]
+        public IActionResult Products()
+        {
+            return View(GetLandingPageViewModel(1));
+        }
+
+        [HttpGet]
+        [Route("Home/Section/LearningContent")]
+        public IActionResult LearningContent()
+        {
+            return View(GetLandingPageViewModel(2));
+        }
+
+        private LandingPageViewModel GetLandingPageViewModel(int sectionIndex)
+        {
+            return new LandingPageViewModel
+            {
+                MiniHubNavigationModel = new MiniHubNavigationModel
+                {
+                    Sections = Sections,
+                    CurrentSectionIndex = sectionIndex,
+                }
+            };
         }
     }
 }
---
+++ DigitalLearningSolutions.Web/ViewModels/Home/LandingPageViewModel.cs
@@ -0,0 +1,9 @@
+namespace DigitalLearningSolutions.Web.ViewModels.Home
+{
+    using DigitalLearningSolutions.Web.ViewModels.Common.MiniHub;
+
+    public class LandingPageViewModel
+    {
+        public MiniHubNavigationModel MiniHubNavigationModel { get; set; }
+    }
+}
