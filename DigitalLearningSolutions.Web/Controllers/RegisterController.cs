fde3303f266b0c80be828bf544029d43f583d0da
David May-Miller <David.May-Miller@softwire.com>
2021-04-15 16:41:20 +0100

    HEEDLS-347 Create first page of delegate registration form

---
+++ DigitalLearningSolutions.Web/Controllers/RegisterController.cs
@@ -0,0 +1,39 @@
+﻿namespace DigitalLearningSolutions.Web.Controllers
+{
+    using DigitalLearningSolutions.Web.ViewModels.Register;
+    using Microsoft.AspNetCore.Mvc;
+
+    public class Register : Controller
+    {
+        public IActionResult Index()
+        {
+            if (User.Identity.IsAuthenticated)
+            {
+                return RedirectToAction("Index", "Home");
+            }
+
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(RegisterViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View("Index", model);
+            }
+
+            return View("LearnerInformation", model);
+        }
+
+        public IActionResult LearnerInformation()
+        {
+            if (User.Identity.IsAuthenticated)
+            {
+                return RedirectToAction("Index", "Home");
+            }
+
+            return View();
+        }
+    }
+}
---
+++ DigitalLearningSolutions.Web/ViewModels/Register/RegisterViewModel.cs
@@ -0,0 +1,25 @@
+﻿namespace DigitalLearningSolutions.Web.ViewModels.Register
+{
+    using System.ComponentModel.DataAnnotations;
+
+    public class RegisterViewModel
+    {
+        public RegisterViewModel()
+        {
+            FirstName = string.Empty;
+            LastName = string.Empty;
+            Email = string.Empty;
+        }
+
+        [Required(ErrorMessage = "Please enter a first name to register.")]
+        public string? FirstName { get; set; }
+
+        [Required(ErrorMessage = "Please enter a last name to register.")]
+        public string? LastName { get; set; }
+
+        [Required(ErrorMessage = "Please enter an email address to register.")]
+        // QQ is this the correct error message
+        [EmailAddress(ErrorMessage = "Please enter a valid email address to register.")]
+        public string Email { get; set; }
+    }
+}
