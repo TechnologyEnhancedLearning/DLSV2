9b9225e514703506a5e3a6d68a5455661df63d8b
David May-Miller <David.May-Miller@softwire.com>
2021-04-22 14:25:01 +0100

    HEEDLS-347 Added tests

--- DigitalLearningSolutions.Data.Tests/Services/CentresServiceTests.cs
+++ DigitalLearningSolutions.Data.Tests/Services/CentresServiceTests.cs
@@ -1,5 +1,6 @@
 ﻿namespace DigitalLearningSolutions.Data.Tests.Services
 {
+    using System.Linq;
     using DigitalLearningSolutions.Data.Services;
     using DigitalLearningSolutions.Data.Tests.Helpers;
     using FakeItEasy;
@@ -67,6 +68,26 @@
 
             // Then
             result.Should().BeNull();
+        }
+
+        [Test]
+        public void Get_active_centres_should_contain_an_active_centre()
+        {
+            // When
+            var result = centresService.GetActiveCentres().ToList();
+
+            // Then
+            result.Contains((2, "North West Boroughs Healthcare NHS Foundation Trust")).Should().BeTrue();
+        }
+
+        [Test]
+        public void Get_active_centres_should_not_contain_an_inactive_centre()
+        {
+            // When
+            var result = centresService.GetActiveCentres().ToList();
+
+            // Then
+            result.Contains((6, "Closed_Basildon and Thurrock University Hospitals NHS Foundation Trust")).Should().BeFalse();
         }
     }
 }
---
+++ DigitalLearningSolutions.Data.Tests/Services/JobGroupsServiceTests.cs
@@ -0,0 +1,53 @@
+﻿namespace DigitalLearningSolutions.Data.Tests.Services
+{
+    using System.Linq;
+    using DigitalLearningSolutions.Data.Services;
+    using DigitalLearningSolutions.Data.Tests.Helpers;
+    using FakeItEasy;
+    using FluentAssertions;
+    using Microsoft.Extensions.Logging;
+    using NUnit.Framework;
+
+    public class JobGroupsServiceTests
+    {
+        private JobGroupsService jobGroupsService;
+
+        [SetUp]
+        public void Setup()
+        {
+            var connection = ServiceTestHelper.GetDatabaseConnection();
+            var logger = A.Fake<ILogger<JobGroupsService>>();
+            jobGroupsService = new JobGroupsService(connection, logger);
+        }
+
+        [Test]
+        public void Get_job_group_name_should_return_the_correct_value()
+        {
+            // When
+            var result = jobGroupsService.GetJobGroupName(2);
+
+            // Then
+            result.Should().Be("Doctor / surgeon / consultant / dentist");
+        }
+
+        [Test]
+        public void Get_job_group_name_should_return_null_when_the_job_group_does_not_exist()
+        {
+            // When
+            var result = jobGroupsService.GetJobGroupName(11);
+
+            // Then
+            result.Should().BeNull();
+        }
+
+        [Test]
+        public void Get_job_groups_should_contain_a_job_group()
+        {
+            // When
+            var result = jobGroupsService.GetJobGroups().ToList();
+
+            // Then
+            result.Contains((2, "Doctor / surgeon / consultant / dentist")).Should().BeTrue();
+        }
+    }
+}
