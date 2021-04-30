75c9edda95a095611244f7e5d1010fd9bfc537c5
David May-Miller <David.May-Miller@softwire.com>
2021-04-30 11:56:51 +0100

    HEEDLS-347 Added max lengths to registration form fields

--- DigitalLearningSolutions.Web/ViewModels/Register/RegisterViewModel.cs
+++ DigitalLearningSolutions.Web/ViewModels/Register/RegisterViewModel.cs
@@ -5,13 +5,16 @@
     public class RegisterViewModel
     {
         [Required(ErrorMessage = "Enter your first name")]
+        [MaxLength(100, ErrorMessage = "First name must be at most 100 characters")]
         public string? FirstName { get; set; }
 
         [Required(ErrorMessage = "Enter your last name")]
+        [MaxLength(100, ErrorMessage = "Last name must be at most 100 characters")]
         public string? LastName { get; set; }
 
         [Required(ErrorMessage = "Enter your email address")]
         [EmailAddress(ErrorMessage = "Enter an email address with an @ in it, such as example@domain.com")]
+        [MaxLength(100, ErrorMessage = "Email address must be at most 100 characters")]
         public string? Email { get; set; }
     }
 }
