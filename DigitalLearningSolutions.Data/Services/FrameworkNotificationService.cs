79548cf7bd97770aa453bfac442f86295ee64aba
kevwhitt-hee <kevin.whittaker@hee.nhs.uk>
2021-05-10 11:57:29 +0100

    DLSV2-178 Adds GetFrameworkUrl function to make URL generation more consistent

--- DigitalLearningSolutions.Data/Services/FrameworkNotificationService.cs
+++ DigitalLearningSolutions.Data/Services/FrameworkNotificationService.cs
@@ -38,9 +38,8 @@
                 var sender = recipients.First();
                 recipients.RemoveAt(0);
                 string emailSubject = "";
-                var commentUrl = getBaseURL();
-                commentUrl.Path += $"Framework/{frameworkId}/Comments/{(replyToCommentId == null ? commentId : replyToCommentId)}";
-
+                var commentUrl = GetFrameworkUrl(frameworkId, "Comments");
+                commentUrl += $"/{(replyToCommentId == null ? commentId : replyToCommentId)}";
                 foreach (var recipient in recipients)
                 {
 
@@ -71,14 +70,6 @@
                 }
             }
         }
-        public UriBuilder getBaseURL()
-        {
-            var trackingSystemBaseUrl = configService.GetConfigValue(ConfigService.AppBaseUrl) ??
-                                        throw new ConfigValueMissingException(configService.GetConfigValueMissingExceptionMessage("AppBaseUrl"));
-            ;
-            var baseUrl = new UriBuilder(trackingSystemBaseUrl);
-            return baseUrl;
-        }
         public void SendFrameworkCollaboratorInvite(int id, int invitedByAdminId)
         {
             var collaboratorNotification = frameworkService.GetCollaboratorNotification(id, invitedByAdminId);
@@ -86,14 +77,13 @@
             {
                 throw new NotificationDataException($"No record found when trying to fetch collaboratorNotification Data. id: {id}, invitedByAdminId: {invitedByAdminId})");
             }
-            var frameworkUrl = getBaseURL();
-            frameworkUrl.Path += $"Framework/{collaboratorNotification.FrameworkID}/Structure/";
+            var frameworkUrl = GetFrameworkUrl(collaboratorNotification.FrameworkID, "Structure");
             string emailSubjectLine = $"DLS Digital Framework {collaboratorNotification?.FrameworkRole} Invitation";
             var builder = new BodyBuilder
             {
                 TextBody = $@"Dear colleague,
                               You have been identified as a {collaboratorNotification?.FrameworkRole} for the framework, {collaboratorNotification?.FrameworkName}, by {collaboratorNotification?.InvitedByName} ({collaboratorNotification?.InvitedByEmail}).
-                              To access the framework, visit this url: {frameworkUrl.Uri}. You will need to be registered on the Digital Learning Solutions platform to view the framework.",
+                              To access the framework, visit this url: {frameworkUrl}. You will need to be registered on the Digital Learning Solutions platform to view the framework.",
                 HtmlBody = $@"<body style= 'font - family: Calibri; font - size: small;'><p>Dear colleague,</p><p>You have been identified as a {collaboratorNotification?.FrameworkRole} for the  framework, {collaboratorNotification?.FrameworkName}, by <a href='mailto:{collaboratorNotification?.InvitedByEmail}'>{collaboratorNotification?.InvitedByName}</a>.</p><p><a href='{frameworkUrl.Uri}'>Click here</a> to access the framework. You will need to be registered on the Digital Learning Solutions platform to view the framework.</p>"
             };
             emailService.SendEmail(new Email(emailSubjectLine, builder, collaboratorNotification.UserEmail, collaboratorNotification.InvitedByEmail));
@@ -106,17 +96,25 @@
             {
                 throw new NotificationDataException($"No record found when trying to fetch collaboratorNotification Data. id: {id}, invitedByAdminId: {invitedByAdminId})");
             }
-            var frameworkUrl = getBaseURL();
-            frameworkUrl.Path += $"Framework/{collaboratorNotification.FrameworkID}/Structure/";
+            var frameworkUrl = GetFrameworkUrl(collaboratorNotification.FrameworkID, "Structure");
             string emailSubjectLine = "DLS Digital Framework Review Request";
             var builder = new BodyBuilder
             {
                 TextBody = $@"Dear colleague,
                               You have been requested to review the framework, {collaboratorNotification?.FrameworkName}, by {collaboratorNotification?.InvitedByName} ({collaboratorNotification?.InvitedByEmail}).
-                              To review the framework, visit this url: {frameworkUrl.Uri}. Click the Review Framework button to submit your review and, if appropriate, sign-off the framework. You will need to be registered on the Digital Learning Solutions platform to review the framework.",
+                              To review the framework, visit this url: {frameworkUrl}. Click the Review Framework button to submit your review and, if appropriate, sign-off the framework. You will need to be registered on the Digital Learning Solutions platform to review the framework.",
                 HtmlBody = $@"<body style= 'font - family: Calibri; font - size: small;'><p>Dear colleague,</p><p>You have been requested to review the framework, {collaboratorNotification?.FrameworkName}, by <a href='mailto:{collaboratorNotification?.InvitedByEmail}'>{collaboratorNotification?.InvitedByName}</a>.</p><p><a href='{frameworkUrl.Uri}'>Click here</a> to review the framework. Click the Review Framework button to submit your review and, if appropriate, sign-off the framework. You will need to be registered on the Digital Learning Solutions platform to view the framework.</p>"
             };
             emailService.SendEmail(new Email(emailSubjectLine, builder, collaboratorNotification.UserEmail, collaboratorNotification.InvitedByEmail));
+        }
+        public string GetFrameworkUrl(int frameworkId, string tab)
+        {
+            var trackingSystemBaseUrl = configService.GetConfigValue(ConfigService.AppBaseUrl) ??
+                                       throw new ConfigValueMissingException(configService.GetConfigValueMissingExceptionMessage("AppBaseUrl"));
+            ;
+            var frameworkUrl = new UriBuilder(trackingSystemBaseUrl);
+            frameworkUrl.Path += $"Framework/{frameworkId}/{tab}/";
+            return frameworkUrl.Uri.ToString();
         }
     }
 }
