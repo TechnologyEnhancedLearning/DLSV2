6d0b20d2d8aa116cce355706722616efc7e38da2
Alex Jackson <Alexander.Jackson@dorsetsoftware.com>
2021-05-11 15:58:24 +0100

    HEEDLS-380 Add Manage registration prompts page

---
+++ DigitalLearningSolutions.Web/ViewModels/TrackingSystem/CustomPromptManagementViewModel.cs
@@ -0,0 +1,23 @@
+﻿namespace DigitalLearningSolutions.Web.ViewModels.TrackingSystem
+{
+    using System.Collections.Generic;
+
+    public class CustomPromptManagementViewModel
+    {
+        public CustomPromptManagementViewModel(int fieldId, string? prompt, bool mandatory, List<string> options)
+        {
+            CustomFieldId = fieldId;
+            CustomPrompt = prompt;
+            Mandatory = mandatory;
+            Options = options;
+        }
+
+        public int CustomFieldId { get; set; }
+
+        public string? CustomPrompt { get; set; }
+
+        public bool Mandatory { get; set; }
+
+        public List<string> Options { get; set; }
+    }
+}
---
+++ DigitalLearningSolutions.Web/ViewModels/TrackingSystem/DisplayRegistrationPromptsViewModel.cs
@@ -0,0 +1,30 @@
+﻿namespace DigitalLearningSolutions.Web.ViewModels.TrackingSystem
+{
+    using System.Collections.Generic;
+    using DigitalLearningSolutions.Data.Models.CustomPrompts;
+
+    public class DisplayRegistrationPromptsViewModel
+    {
+        public DisplayRegistrationPromptsViewModel(CentreCustomPrompts? customPrompts)
+        {
+            CustomFields = new List<CustomPromptManagementViewModel>();
+
+            TryAddCustomPromptManagementViewModelToList(1, customPrompts?.CustomField1);
+            TryAddCustomPromptManagementViewModelToList(2, customPrompts?.CustomField2);
+            TryAddCustomPromptManagementViewModelToList(3, customPrompts?.CustomField3);
+            TryAddCustomPromptManagementViewModelToList(4, customPrompts?.CustomField4);
+            TryAddCustomPromptManagementViewModelToList(5, customPrompts?.CustomField5);
+            TryAddCustomPromptManagementViewModelToList(6, customPrompts?.CustomField6);
+        }
+
+        private void TryAddCustomPromptManagementViewModelToList(int fieldNumber, CustomPrompt? customPrompt)
+        {
+            if (customPrompt != null)
+            {
+                CustomFields.Add(new CustomPromptManagementViewModel(fieldNumber, customPrompt.CustomPromptText, customPrompt.Mandatory, customPrompt.Options));
+            }
+        }
+
+        public List<CustomPromptManagementViewModel> CustomFields { get; set; }
+    }
+}
