fbee14d708cc48bde6fe599998bbed29767b7110
Jonathan Bloxsom <jonathan.bloxsom@softwire.com>
2021-05-13 14:35:39 +0100

    HEEDLS-422: bare delegates controller

---
+++ DigitalLearningSolutions.Web/Controllers/TrackingSystem/DelegatesController.cs
@@ -0,0 +1,16 @@
+﻿namespace DigitalLearningSolutions.Web.Controllers.TrackingSystem
+{
+    using DigitalLearningSolutions.Web.Helpers;
+    using Microsoft.AspNetCore.Authorization;
+    using Microsoft.AspNetCore.Mvc;
+
+    [Authorize(Policy = CustomPolicies.UserCentreAdminOnly)]
+    [Route("/TrackingSystem/Delegates")]
+    public class DelegatesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
