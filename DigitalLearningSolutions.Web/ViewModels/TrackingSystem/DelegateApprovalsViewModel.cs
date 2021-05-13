170775b5f4fde8141fd960840118fa3da38e0598
Jonathan Bloxsom <jonathan.bloxsom@softwire.com>
2021-05-13 16:03:42 +0100

    HEEDLS-422: view model

---
+++ DigitalLearningSolutions.Web/ViewModels/TrackingSystem/DelegateApprovalsViewModel.cs
@@ -0,0 +1,19 @@
+﻿namespace DigitalLearningSolutions.Web.ViewModels.TrackingSystem
+{
+    using System;
+    using System.Collections.Generic;
+
+    public class DelegateApprovalsViewModel
+    {
+        public List<ApprovableDelegate> delegates { get; set; }
+    }
+
+    public class ApprovableDelegate
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public DateTime DateRegistered { get; set; }
+    }
+}
