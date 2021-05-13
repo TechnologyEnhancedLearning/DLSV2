1f02432e6a4851df119e3a2b48d51d9d52bf4cd6
Steve Jackson <stephen.jackson@softwire.com>
2021-05-13 10:03:57 +0100

    HEEDLS-369: Stop passwords in text-input components being returned by server

--- DigitalLearningSolutions.Web/ViewComponents/TextInputViewComponent.cs
+++ DigitalLearningSolutions.Web/ViewComponents/TextInputViewComponent.cs
@@ -5,20 +5,31 @@
 
     public class TextInputViewComponent : ViewComponent
     {
+        /// <summary>
+        /// Render TextInput view component.
+        /// </summary>
+        /// <param name="aspFor"></param>
+        /// <param name="label"></param>
+        /// <param name="value">Leave blank to pass no value to client.</param>
+        /// <param name="type"></param>
+        /// <param name="spellCheck"></param>
+        /// <param name="hintText">Leave blank for no hint.</param>
+        /// <param name="autocomplete">Leave blank to set no autocomplete on the input element.</param>
+        /// <param name="cssClass"></param>
+        /// <returns></returns>
         public IViewComponentResult Invoke(
             string aspFor,
             string label,
+            string value,
             string type,
             bool spellCheck,
-            string? hintText,
-            string? autocomplete,
-            string? cssClass
-        )
+            string hintText,
+            string autocomplete,
+            string cssClass)
         {
             var model = ViewData.Model;
 
             var property = model.GetType().GetProperty(aspFor);
-            var propertyValue = property?.GetValue(model)?.ToString();
 
             var hasError = ViewData.ModelState[property?.Name]?.Errors?.Count > 0;
             var errorMessage = hasError ? ViewData.ModelState[property?.Name]?.Errors[0].ErrorMessage : null;
@@ -27,12 +38,12 @@
                 aspFor,
                 aspFor,
                 label,
-                propertyValue,
+                string.IsNullOrEmpty(value) ? null : value,
                 type,
                 spellCheck,
-                autocomplete,
-                cssClass,
-                hintText,
+                string.IsNullOrEmpty(autocomplete) ? null : autocomplete,
+                string.IsNullOrEmpty(cssClass) ? null : cssClass,
+                string.IsNullOrEmpty(hintText) ? null : hintText,
                 errorMessage,
                 hasError);
             return View(textBoxViewModel);
