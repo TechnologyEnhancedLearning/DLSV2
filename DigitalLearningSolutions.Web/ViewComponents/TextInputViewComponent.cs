c1dd8f1c3ac9890fc9b7a7e6ce62afee4c5277e9
Steve Jackson <stephen.jackson@softwire.com>
2021-05-13 09:23:43 +0100

    HEEDLS-369: Split constructer call over multiple lines

--- DigitalLearningSolutions.Web/ViewComponents/TextInputViewComponent.cs
+++ DigitalLearningSolutions.Web/ViewComponents/TextInputViewComponent.cs
@@ -13,7 +13,7 @@
             string? hintText,
             string? autocomplete,
             string? cssClass
-            )
+        )
         {
             var model = ViewData.Model;
 
@@ -23,7 +23,18 @@
             var hasError = ViewData.ModelState[property?.Name]?.Errors?.Count > 0;
             var errorMessage = hasError ? ViewData.ModelState[property?.Name]?.Errors[0].ErrorMessage : null;
 
-            var textBoxViewModel = new TextInputViewModel(aspFor, aspFor, label, propertyValue, type, spellCheck, autocomplete, cssClass, hintText, errorMessage, hasError);
+            var textBoxViewModel = new TextInputViewModel(
+                aspFor,
+                aspFor,
+                label,
+                propertyValue,
+                type,
+                spellCheck,
+                autocomplete,
+                cssClass,
+                hintText,
+                errorMessage,
+                hasError);
             return View(textBoxViewModel);
         }
     }
