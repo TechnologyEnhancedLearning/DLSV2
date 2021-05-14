4ed7a018a2919e90d215d2a678d1a8bfcedc6b8d
Stella <stella.veski@softwire.com>
2021-05-14 14:43:10 +0100

    HEEDLS-420: Fix accessibility tests in Jenkins.

--- DigitalLearningSolutions.Web.AutomatedUiTests/SeleniumServerFactory.cs
+++ DigitalLearningSolutions.Web.AutomatedUiTests/SeleniumServerFactory.cs
@@ -1,18 +1,15 @@
-﻿namespace DigitalLearningSolutions.Web.AutomatedUiTests
+namespace DigitalLearningSolutions.Web.AutomatedUiTests
 {
     using System;
-    using System.Data;
+    using System.IO;
     using System.Linq;
-    using Dapper.FluentMap;
-    using DigitalLearningSolutions.Web.Helpers;
     using Microsoft.AspNetCore;
     using Microsoft.AspNetCore.Hosting;
     using Microsoft.AspNetCore.Hosting.Server.Features;
     using Microsoft.AspNetCore.Mvc.Testing;
     using Microsoft.AspNetCore.TestHost;
-    using Microsoft.Data.SqlClient;
     using Microsoft.Extensions.Configuration;
-    using Microsoft.Extensions.DependencyInjection;
+    using Microsoft.Extensions.Configuration.Json;
     using Serilog;
 
     public class SeleniumServerFactory<TStartup> : WebApplicationFactory<TStartup> where TStartup : class
@@ -39,12 +36,22 @@
 
         protected sealed override IWebHostBuilder CreateWebHostBuilder()
         {
-            var builder = WebHost.CreateDefaultBuilder(Array.Empty<string>())
+            return WebHost.CreateDefaultBuilder(Array.Empty<string>())
                 .UseStartup<TStartup>()
                 .UseSerilog()
-                .UseEnvironment("Development");
-            ConfigureWebHost(builder);
-            return builder;
+                .ConfigureAppConfiguration(configBuilder =>
+                {
+                    var jsonConfigSources = configBuilder.Sources
+                        .Where(source => source.GetType() == typeof(JsonConfigurationSource))
+                        .ToList();
+
+                    foreach (var jsonConfigSource in jsonConfigSources)
+                    {
+                        configBuilder.Sources.Remove(jsonConfigSource);
+                    }
+
+                    configBuilder.AddConfiguration(GetConfigForUiTests());
+                });
         }
 
         protected override void Dispose(bool disposing)
@@ -56,19 +63,13 @@
             }
         }
 
-        protected override void ConfigureWebHost(IWebHostBuilder builder)
+        private static IConfigurationRoot GetConfigForUiTests()
         {
-            builder.ConfigureServices(services =>
-            {
-                var defaultSqlConnection = services.SingleOrDefault(
-                    service => service.ServiceType == typeof(IDbConnection));
-
-                services.Remove(defaultSqlConnection);
-
-                var config = ConfigHelper.GetAppConfig();
-                var connectionString = config.GetConnectionString(ConfigHelper.UnitTestConnectionStringName);
-                services.AddScoped<IDbConnection>(_ => new SqlConnection(connectionString));
-            });
+            return new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.SIT.json")
+                .AddEnvironmentVariables("DlsRefactor_")
+                .Build();
         }
     }
 }
