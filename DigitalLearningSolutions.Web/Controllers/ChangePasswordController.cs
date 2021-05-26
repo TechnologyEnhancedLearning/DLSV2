733548139448d314f2fe6f5cc38f2e7ca6445db2
Steve Jackson <stephen.jackson@softwire.com>
2021-05-26 16:53:26 +0100

    HEEDLS-350: Frontend for changing password

---
+++ DigitalLearningSolutions.Web/Controllers/ChangePasswordController.cs
@@ -0,0 +1,19 @@
+﻿namespace DigitalLearningSolutions.Web.Controllers
+{
+    using DigitalLearningSolutions.Web.ViewModels.MyAccount;
+    using Microsoft.AspNetCore.Mvc;
+
+    public class ChangePasswordController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View(new ChangePasswordViewModel());
+        }
+        [HttpPost]
+        public IActionResult Index(ChangePasswordViewModel model)
+        {
+            return View("Success");
+        }
+    }
+}
---
+++ DigitalLearningSolutions.Web/ViewModels/MyAccount/ChangePasswordViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace DigitalLearningSolutions.Web.ViewModels.MyAccount
+{
+    using DigitalLearningSolutions.Web.ViewModels.Common;
+
+    public class ChangePasswordViewModel : PasswordViewModel
+    {
+        public string? CurrentPassword { get; set; }
+    }
+}
