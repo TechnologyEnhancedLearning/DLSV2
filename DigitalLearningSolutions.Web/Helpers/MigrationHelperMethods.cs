4bf3818dfed56d05ae7f5e68e6343ad8992a2c31
Daniel Bloxham <Daniel.Bloxham@softwire.com>
2021-05-27 08:40:32 +0100

    HEEDLS-451 - Removed redundant assemblies from migration ScanIn statement and updated readme to reflect that.

--- DigitalLearningSolutions.Web/Helpers/MigrationHelperMethods.cs
+++ DigitalLearningSolutions.Web/Helpers/MigrationHelperMethods.cs
@@ -6,52 +6,21 @@
 
     public static class MigrationHelperMethods
     {
-        public static IServiceCollection RegisterMigrationRunner(this IServiceCollection serviceCollection, string connectionString)
+        public static IServiceCollection RegisterMigrationRunner
+            (this IServiceCollection serviceCollection, string connectionString)
         {
             return serviceCollection.AddFluentMigratorCore()
-                .ConfigureRunner(rb => rb
-                    .AddSqlServer2016()
-                    .WithGlobalConnectionString(connectionString)
-                    .ScanIn(
-                        typeof(AddSelfAssessmentTables).Assembly,
-                        typeof(AddSelfAssessmentResultTimestamp).Assembly,
-                        typeof(AddSelfAssessmentDates).Assembly,
-                        typeof(AddSelfAssessmentUseApiFlag).Assembly,
-                        typeof(AddFilteredMappingTables).Assembly,
-                        typeof(AddFilteredSPs).Assembly,
-                        typeof(AddCandidateAssessmentNewFields).Assembly,
-                        typeof(AddCentreSelfAsessments).Assembly,
-                        typeof(AddSelfAssessmentAdditionalFields).Assembly,
-                        typeof(AddNewGetAvailableSproc).Assembly,
-                        typeof(ApplyLPDefaultSPs).Assembly,
-                        typeof(ReflectiveAccounts).Assembly,
-                        typeof(CompetencyFrameworkDBChanges).Assembly,
-                        typeof(AddInsertCustomisationV3sp).Assembly,
-                        typeof(CandidateAssessmentSubmittedDate).Assembly,
-                        typeof(AddFrameworkIdToFrameworkCompetencies).Assembly,
-                        typeof(AddCompetencyNameColumn).Assembly,
-                        typeof(MakeCompetencyDecsriptionNullable).Assembly,
-                        typeof(ReorderFrameworkCompetenciesAndGroupsSPs).Assembly,
-                        typeof(ChangesForDigitalCapability).Assembly,
-                        typeof(AddFrameworkDefaultQuestionsTable).Assembly,
-                        typeof(AddAssessmentQuestionAdminIDColumn).Assembly,
-                        typeof(ChangesForDigitalCapability).Assembly,
-                        typeof(FixFilteredSPs).Assembly,
-                        typeof(FilteredFunctionTweak).Assembly,
-                        typeof(AssessmentQuestionMaxMinDescriptionNullable).Assembly,
-                        typeof(FixFilteredSPs).Assembly,
-                        typeof(AddAssessAttemptsIndex).Assembly,
-                        typeof(AddResetPasswordTable).Assembly,
-                        typeof(AddFrameworkCommentFrameworkID).Assembly,
-                        typeof(AddPasswordSubmittedField).Assembly,
-                        typeof(FixCustomisationUpdateSPs).Assembly,
-                        typeof(AddEmailToFrameworkCollaboratorTable).Assembly,
-                        typeof(AddFrameworkReviewsTable).Assembly,
-                        typeof(AddArchivedDateToFrameworkReviews).Assembly,
-                        typeof(AddMapDisplayColumnToCentres).Assembly
-                    ).For.Migrations()
-                ).AddLogging(lb => lb
-                    .AddFluentMigratorConsole()
+                .ConfigureRunner
+                (
+                    rb => rb
+                        .AddSqlServer2016()
+                        .WithGlobalConnectionString(connectionString)
+                        .ScanIn(typeof(AddSelfAssessmentTables).Assembly)
+                        .For.Migrations()
+                ).AddLogging
+                (
+                    lb => lb
+                        .AddFluentMigratorConsole()
                 );
         }
     }
