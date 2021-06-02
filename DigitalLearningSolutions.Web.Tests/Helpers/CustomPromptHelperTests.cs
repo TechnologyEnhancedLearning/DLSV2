a94ea3acf3de27d3f484c0aa8bc8d4a669f144c3
David May-Miller <david.may-miller@softwire.com>
2021-06-02 16:06:46 +0100

    HEEDLS-367 Added test for CentrePromptsHelper's validation method

--- DigitalLearningSolutions.Web.Tests/Helpers/CustomPromptHelperTests.cs
+++ DigitalLearningSolutions.Web.Tests/Helpers/CustomPromptHelperTests.cs
@@ -10,6 +10,7 @@
     using FakeItEasy;
     using FluentAssertions;
     using FluentAssertions.Execution;
+    using Microsoft.AspNetCore.Mvc.ModelBinding;
     using NUnit.Framework;
 
     public class CustomPromptHelperTests
@@ -53,6 +54,27 @@
                 result[1].CustomFieldId.Should().Be(2);
                 result[1].Options.Count().Should().Be(0);
             }
+        }
+
+        [Test]
+        public void ValidateCustomPrompts_adds_errors_to_invalid_answers_only()
+        {
+            // Given
+            var answer2 = "Answer2";
+            var customPrompt1 = CustomPromptsTestHelper.GetDefaultCustomPrompt(1, mandatory: true);
+            var customPrompt2 = CustomPromptsTestHelper.GetDefaultCustomPrompt(2, mandatory: true);
+            var centreCustomPrompts =
+                CustomPromptsTestHelper.GetDefaultCentreCustomPrompts(
+                    new List<CustomPrompt> { customPrompt1, customPrompt2 }, 1);
+            var modelState = new ModelStateDictionary();
+            A.CallTo(() => customPromptsService.GetCustomPromptsForCentreByCentreId(1)).Returns(centreCustomPrompts);
+
+            // When
+            customPromptHelper.ValidateCustomPrompts(1, null, answer2, null, null, null, null, modelState);
+
+            // Then
+            modelState["Answer1"].Errors.Count.Should().Be(1);
+            modelState["Answer2"].Should().BeNull();
         }
     }
 }
