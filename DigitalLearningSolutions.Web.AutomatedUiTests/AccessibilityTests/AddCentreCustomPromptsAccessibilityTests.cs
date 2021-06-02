495150428b727b9999ecd9989e7ff39a8f2b0842
Alex Jackson <Alexander.Jackson@dorsetsoftware.com>
2021-06-02 09:59:15 +0100

    HEEDLS-454 Check page headings in AddCentreCustomPromptsAccessibilityTests

--- DigitalLearningSolutions.Web.AutomatedUiTests/AccessibilityTests/AddCentreCustomPromptsAccessibilityTests.cs
+++ DigitalLearningSolutions.Web.AutomatedUiTests/AccessibilityTests/AddCentreCustomPromptsAccessibilityTests.cs
@@ -4,7 +4,7 @@
     using FluentAssertions;
     using Selenium.Axe;
     using Xunit;
-    
+
     public class AddCentreCustomPromptsAccessibilityTests : AccessibilityTestsBase
     {
         public AddCentreCustomPromptsAccessibilityTests(SeleniumServerFactory<Startup> factory) : base(factory) { }
@@ -18,16 +18,22 @@
 
             // when
             Driver.Navigate().GoToUrl(BaseUrl + startUrl);
+            AnalyzePageHeading("Add delegate registration prompt");
             var selectPromptResult = new AxeBuilder(Driver).Analyze();
             Driver.SelectDropdownItemValue("CustomPromptId", "1");
             Driver.SubmitForm();
 
+            AnalyzePageHeading("Configure answers");
             var configureAnswerInitialResult = new AxeBuilder(Driver).Analyze();
+
             AddAnswer("Answer 1");
             AddAnswer("Answer 2");
+            AnalyzePageHeading("Configure answers");
             var configureAnswerWithAnswersResult = new AxeBuilder(Driver).Analyze();
+
             Driver.ClickButtonByText("Next");
 
+            AnalyzePageHeading("Summary");
             var summaryResult = new AxeBuilder(Driver).Analyze();
             Driver.SubmitForm();
 
