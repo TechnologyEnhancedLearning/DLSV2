751cce97f3aeb1f5c4408ef4a56b338bb0eb7260
David May-Miller <david.may-miller@softwire.com>
2021-06-04 11:44:51 +0100

    HEEDLS-367 Added data test for RegistrationDataService

---
+++ DigitalLearningSolutions.Data.Tests/DataServices/RegistrationDataServiceTests.cs
@@ -0,0 +1,48 @@
+﻿namespace DigitalLearningSolutions.Data.Tests.DataServices
+{
+    using System.Threading.Tasks;
+    using System.Transactions;
+    using DigitalLearningSolutions.Data.DataServices;
+    using DigitalLearningSolutions.Data.Tests.TestHelpers;
+    using FluentAssertions;
+    using Microsoft.Data.SqlClient;
+    using NUnit.Framework;
+
+    public class RegistrationDataServiceTests
+    {
+        private SqlConnection connection = null!;
+        private RegistrationDataService service = null!;
+
+        [SetUp]
+        public void SetUp()
+        {
+            connection = ServiceTestHelper.GetDatabaseConnection();
+            service = new RegistrationDataService(connection);
+        }
+
+        [Test]
+        public async Task Sets_all_fields_correctly_on_registration()
+        {
+            using var transactionScope = new TransactionScope(TransactionScopeAsyncFlowOption.Enabled);
+
+            // Given
+            var delegateRegistrationModel = UserTestHelper.GetDefaultDelegateRegistrationModel();
+
+            // When
+            var candidateNumber = service.RegisterDelegate(delegateRegistrationModel);
+            var user = await connection.GetDelegateUserByCandidateNumberAsync(candidateNumber);
+
+            // Then
+            user.FirstName.Should().Be(delegateRegistrationModel.FirstName);
+            user.LastName.Should().Be(delegateRegistrationModel.LastName);
+            user.EmailAddress.Should().Be(delegateRegistrationModel.Email);
+            user.CentreId.Should().Be(delegateRegistrationModel.Centre);
+            user.Answer1.Should().Be(delegateRegistrationModel.Answer1);
+            user.Answer2.Should().Be(delegateRegistrationModel.Answer2);
+            user.Answer3.Should().Be(delegateRegistrationModel.Answer3);
+            user.Answer4.Should().Be(delegateRegistrationModel.Answer4);
+            user.Answer5.Should().Be(delegateRegistrationModel.Answer5);
+            user.Answer6.Should().Be(delegateRegistrationModel.Answer6);
+        }
+    }
+}
