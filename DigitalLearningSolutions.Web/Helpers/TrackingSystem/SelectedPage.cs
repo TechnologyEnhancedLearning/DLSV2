6a2ef9fc8e743a2d8f652b906faaf0ac897b0d17
Daniel Bloxham <Daniel.Bloxham@softwire.com>
2021-06-08 11:14:18 +0100

    HEEDLS-474 - refactored to use enum and NHS typography

---
+++ DigitalLearningSolutions.Web/Helpers/TrackingSystem/SelectedPage.cs
@@ -0,0 +1,10 @@
+﻿namespace DigitalLearningSolutions.Web.Helpers.TrackingSystem
+{
+    public enum SelectedPage
+    {
+        AllDelegates,
+        DelegateGroups,
+        CourseDelegates,
+        ApproveDelegateRegistrations
+    }
+}
