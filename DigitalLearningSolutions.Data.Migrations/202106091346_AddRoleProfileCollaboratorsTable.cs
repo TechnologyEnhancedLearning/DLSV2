dcc3975dc4269717b5409663de8d346bad424ef3
kevwhitt-hee <kevin.whittaker@hee.nhs.uk>
2021-06-09 13:57:09 +0100

    DLSV2-202 Migration to create role profile collaborators table

---
+++ DigitalLearningSolutions.Data.Migrations/202106091346_AddRoleProfileCollaboratorsTable.cs
@@ -0,0 +1,20 @@
+﻿namespace DigitalLearningSolutions.Data.Migrations
+{
+    using FluentMigrator;
+    [Migration(202106091346)]
+    public class AddRoleProfileCollaboratorsTable : Migration
+    {
+        public override void Up()
+        {
+            Create.Table("RoleProfileCollaborators").WithColumn("ID").AsInt32().NotNullable().PrimaryKey().Identity()
+                .WithColumn("RoleProfileID").AsInt32().NotNullable().PrimaryKey().ForeignKey("RoleProfile", "ID")
+                .WithColumn("UserEmail").AsString(255).NotNullable()
+                .WithColumn("AdminID").AsInt32().NotNullable().PrimaryKey().ForeignKey("AdminUsers", "AdminID")
+                .WithColumn("CanModify").AsBoolean().NotNullable().WithDefaultValue(false);
+        }
+        public override void Down()
+        {
+            Delete.Table("RoleProfileCollaborators");
+        }
+    }
+}
