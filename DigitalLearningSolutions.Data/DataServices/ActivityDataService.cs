6b0f7555b0e1a30862b76b0857a040179c0e71cb
Jonathan Bloxsom <jonathan.bloxsom@softwire.com>
2021-06-09 09:58:00 +0100

    HEEDLS-459 activity services WIP

---
+++ DigitalLearningSolutions.Data/DataServices/ActivityDataService.cs
@@ -0,0 +1,34 @@
+﻿namespace DigitalLearningSolutions.Data.DataServices
+{
+    using System.Collections.Generic;
+    using System.Data;
+    using System.Linq;
+    using Dapper;
+
+    public interface IActivityDataService
+    {
+        int GetActivityForMonthAndYear(int year, int month, string activityType);
+    }
+
+    public class ActivityDataService : IActivityDataService
+    {
+        private readonly IDbConnection connection;
+
+        public ActivityDataService(IDbConnection connection)
+        {
+            this.connection = connection;
+        }
+
+        public int GetActivityForMonthAndYear(int year, int month, string activityType)
+        {
+            return connection.Query<int>(
+                @"SELECT COUNT (LogID)
+                        FROM tActivityLog
+                        WHERE @activityType = 1
+                        AND LogYear = @year
+                        AND LogMonth = @month",
+            new {year, month, activityType}
+            ).First();
+        }
+    }
+}
