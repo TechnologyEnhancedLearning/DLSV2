6e2e487bd2c0fa6722e7ddf96c50bfd05d18e126
kevwhitt-hee <kevin.whittaker@hee.nhs.uk>
2021-06-10 12:48:30 +0100

    DLSV2-203 Models, services, view models and migrations for dashboard view

--- DigitalLearningSolutions.Data.Migrations/202106091346_AddRoleProfileCollaboratorsTable.cs
+++ DigitalLearningSolutions.Data.Migrations/202106091346_AddRoleProfileCollaboratorsTable.cs
@@ -6,14 +6,36 @@
     {
         public override void Up()
         {
-            Create.Table("RoleProfileCollaborators").WithColumn("ID").AsInt32().NotNullable().PrimaryKey().Identity()
-                .WithColumn("RoleProfileID").AsInt32().NotNullable().PrimaryKey().ForeignKey("RoleProfiles", "ID")
+            Create.Table("RoleProfileCollaborators")
+                .WithColumn("ID").AsInt32().NotNullable().PrimaryKey().Identity()
+                .WithColumn("RoleProfileID").AsInt32().NotNullable().ForeignKey("RoleProfiles", "ID")
                 .WithColumn("UserEmail").AsString(255).NotNullable()
-                .WithColumn("AdminID").AsInt32().NotNullable().PrimaryKey().ForeignKey("AdminUsers", "AdminID")
+                .WithColumn("AdminID").AsInt32().NotNullable().ForeignKey("AdminUsers", "AdminID")
                 .WithColumn("CanModify").AsBoolean().NotNullable().WithDefaultValue(false);
+            Create.Table("RoleProfileComments")
+                 .WithColumn("ID").AsInt32().NotNullable().PrimaryKey().Identity()
+                 .WithColumn("RoleProfileID").AsInt32().NotNullable().ForeignKey("RoleProfiles", "ID")
+                 .WithColumn("AdminID").AsInt32().NotNullable().ForeignKey("AdminUsers", "AdminID")
+                 .WithColumn("ReplyToRoleProfileCommentID").AsInt32().Nullable().ForeignKey("RoleProfileComments", "ID")
+                 .WithColumn("AddedDate").AsDateTime().NotNullable().WithDefaultValue(SystemMethods.CurrentDateTime)
+                 .WithColumn("Comments").AsString(int.MaxValue).NotNullable()
+                 .WithColumn("Archived").AsDateTime().Nullable()
+                 .WithColumn("LastEdit").AsDateTime().NotNullable().WithDefaultValue(SystemMethods.CurrentDateTime);
+            Create.Table("RoleProfileReviews")
+                .WithColumn("ID").AsInt32().NotNullable().PrimaryKey().Identity()
+                .WithColumn("RoleProfileID").AsInt32().NotNullable().ForeignKey("RoleProfiles", "ID")
+                .WithColumn("RoleProfileCollaboratorID").AsInt32().NotNullable().ForeignKey("RoleProfileCollaborators", "ID")
+                .WithColumn("ReviewRequested").AsDateTime().NotNullable().WithDefaultValue(SystemMethods.CurrentDateTime)
+                .WithColumn("ReviewComplete").AsDateTime().Nullable()
+                .WithColumn("SignedOff").AsBoolean().NotNullable().WithDefaultValue(0)
+                .WithColumn("RoleProfileCommentID").AsInt32().Nullable().ForeignKey("RoleProfileComments", "ID")
+                .WithColumn("SignOffRequired").AsBoolean().NotNullable().WithDefaultValue(false)
+            .WithColumn("Archived").AsDateTime().Nullable();
         }
         public override void Down()
         {
+            Delete.Table("RoleProfileReviews");
+            Delete.Table("RoleProfileComments");
             Delete.Table("RoleProfileCollaborators");
         }
     }
---
+++ DigitalLearningSolutions.Web/ViewModels/Frameworks/Dashboard/DashboardViewModel.cs
@@ -0,0 +1,15 @@
+﻿namespace DigitalLearningSolutions.Web.ViewModels.Frameworks.Dashboard
+{
+    using DigitalLearningSolutions.Data.Models.Frameworks;
+    using System.Collections.Generic;
+
+    public class DashboardViewModel
+    {
+        public bool IsFrameworkDeveloper { get; set; }
+        public bool IsFrameworkContributor { get; set; }
+        public bool IsWorkforceManager { get; set; }
+        public bool IsWorkforceContributor { get; set; }
+        public DashboardData dashboardData { get; set; }
+        public IEnumerable<DashboardToDoItem> dashboardToDoItems { get; set; }
+    }
+}
