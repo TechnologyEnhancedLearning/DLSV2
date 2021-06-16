da036d8f015625bdbda177ecf6bb469522684091
Jonathan Bloxsom <jonathan.bloxsom@softwire.com>
2021-06-16 17:31:23 +0100

    HEEDLS-459: view model and views

--- DigitalLearningSolutions.Web/ViewModels/TrackingSystem/Centre/Reports/ReportsViewModel.cs
+++ DigitalLearningSolutions.Web/ViewModels/TrackingSystem/Centre/Reports/ReportsViewModel.cs
@@ -1,6 +1,7 @@
 ﻿namespace DigitalLearningSolutions.Web.ViewModels.TrackingSystem.Centre.Reports
 {
     using System.Collections.Generic;
+    using DigitalLearningSolutions.Data.Models.TrackingSystem;
 
     public class ReportsViewModel
     {
@@ -14,6 +15,16 @@
 
     public class ActivityTableRow
     {
+        private static string[] MonthNames = { "Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Nov", "Dec"};
+
+        public ActivityTableRow(MonthOfActivity monthOfActivity)
+        {
+            Month = MonthNames[monthOfActivity.Month - 1];
+            Completions = monthOfActivity.Completions;
+            Evaluations = monthOfActivity.Evaluations;
+            Registrations = monthOfActivity.Registrations;
+        }
+
         public string? Month { get; set; }
         public int Completions { get; set; }
         public int Evaluations { get; set; }
