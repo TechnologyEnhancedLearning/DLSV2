30f3a3574e66d13c29b26c0f802be4c308a3ed05
Daniel Bloxham <Daniel.Bloxham@softwire.com>
2021-06-16 10:09:58 +0100

    HEEDLS-450 - new view component for side menu links with hrefs instead of controller/actions

--- DigitalLearningSolutions.Web/ViewComponents/SideMenuLinkWithHrefViewComponent.cs
+++ DigitalLearningSolutions.Web/ViewComponents/SideMenuLinkWithHrefViewComponent.cs
@@ -4,17 +4,16 @@
     using DigitalLearningSolutions.Web.ViewModels.Common;
     using Microsoft.AspNetCore.Mvc;
 
-    public class SideMenuLinkViewComponent : ViewComponent
+    public class SideMenuLinkWithHrefViewComponent : ViewComponent
     {
         public IViewComponentResult Invoke(
-            string aspController,
-            string aspAction,
+            string href,
             string linkText,
             bool isCurrentPage,
             Dictionary<string, string> aspAllRouteData
         )
         {
-            var model = new SideMenuLinkViewModel(aspController, aspAction, linkText, isCurrentPage, aspAllRouteData);
+            var model = new SideMenuLinkWithHrefViewModel(href, linkText, isCurrentPage, aspAllRouteData);
 
             return View(model);
         }
--- DigitalLearningSolutions.Web/ViewModels/Common/SideMenuLinkViewModel.cs
+++ DigitalLearningSolutions.Web/ViewModels/Common/SideMenuLinkViewModel.cs
@@ -2,35 +2,19 @@
 {
     using System.Collections.Generic;
 
-    public class SideMenuLinkViewModel
+    public class SideMenuLinkViewModel : LinkViewModel
     {
-        public readonly string? AspAction;
-
-        public readonly string? AspController;
-
-        public readonly string? HRef;
-
-        public readonly string LinkText;
-
         public readonly bool IsCurrentPage;
 
-        public readonly Dictionary<string, string>? AspAllRouteData;
-
-        public SideMenuLinkViewModel(string aspController, string aspAction, string linkText, bool isCurrentPage,  Dictionary<string, string>? aspAllRouteData)
+        public SideMenuLinkViewModel(
+            string aspController,
+            string aspAction,
+            string linkText,
+            bool isCurrentPage,
+            Dictionary<string, string>? aspAllRouteData
+        ) : base(aspController, aspAction, linkText, aspAllRouteData)
         {
-            AspAction = aspAction;
-            AspController = aspController;
-            LinkText = linkText;
             IsCurrentPage = isCurrentPage;
-            AspAllRouteData = aspAllRouteData;
-        }
-
-        public SideMenuLinkViewModel(string hRef, string linkText, bool isCurrentPage,  Dictionary<string, string>? aspAllRouteData)
-        {
-            HRef = hRef;
-            LinkText = linkText;
-            IsCurrentPage = isCurrentPage;
-            AspAllRouteData = aspAllRouteData;
         }
     }
 }
