6eb11bd7f90baa23b7dd4cac5df184be9da8af55
Ibrahim Munir-Zubair <ibrahimmunir14@gmail.com>
2021-06-21 18:32:40 +0100

    HEEDLS-357 Fix merge conflicts

--- DigitalLearningSolutions.Web/ViewModels/Register/PersonalInformationViewModel.cs
+++ DigitalLearningSolutions.Web/ViewModels/Register/PersonalInformationViewModel.cs
@@ -2,21 +2,23 @@
 {
     using System.Collections.Generic;
     using System.ComponentModel.DataAnnotations;
+    using DigitalLearningSolutions.Web.Attributes;
     using Microsoft.AspNetCore.Mvc.Rendering;
 
     public class PersonalInformationViewModel
     {
         [Required(ErrorMessage = "Enter your first name")]
-        [MaxLength(100, ErrorMessage = "First name must be at most 100 characters")]
+        [MaxLength(250, ErrorMessage = "First name must be 250 characters or fewer")]
         public string? FirstName { get; set; }
 
         [Required(ErrorMessage = "Enter your last name")]
-        [MaxLength(100, ErrorMessage = "Last name must be at most 100 characters")]
+        [MaxLength(250, ErrorMessage = "Last name must be 250 characters or fewer")]
         public string? LastName { get; set; }
 
         [Required(ErrorMessage = "Enter your email address")]
-        [EmailAddress(ErrorMessage = "Enter an email address with an @ in it, such as example@domain.com")]
-        [MaxLength(100, ErrorMessage = "Email address must be at most 100 characters")]
+        [MaxLength(250, ErrorMessage = "Email address must be 250 characters or fewer")]
+        [EmailAddress(ErrorMessage = "Enter an email address in the correct format, like name@example.com")]
+        [NoWhitespace("Email address must not contain any whitespace characters")]
         public string? Email { get; set; }
 
         [Required(ErrorMessage = "Select a centre")]
