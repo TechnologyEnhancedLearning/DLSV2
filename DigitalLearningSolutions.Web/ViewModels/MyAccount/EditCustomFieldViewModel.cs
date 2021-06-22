99b33dc8d5a9aa8722a8d1559bec7e02b779f309
Ibrahim Munir-Zubair <ibrahimmunir14@gmail.com>
2021-06-22 11:23:08 +0100

    HEEDLS-357 Change EditCustomFieldViewModel to extend CustomFieldViewModel

--- DigitalLearningSolutions.Web/ViewModels/MyAccount/EditCustomFieldViewModel.cs
+++ DigitalLearningSolutions.Web/ViewModels/MyAccount/EditCustomFieldViewModel.cs
@@ -2,26 +2,21 @@
 {
     using System.Collections.Generic;
     using DigitalLearningSolutions.Web.Helpers;
+    using DigitalLearningSolutions.Web.ViewModels.Common;
     using Microsoft.AspNetCore.Mvc.Rendering;
 
-    public class EditCustomFieldViewModel
+    public class EditCustomFieldViewModel : CustomFieldViewModel
     {
-        public EditCustomFieldViewModel(int fieldId, string prompt, bool mandatory, List<string> options, string? answer)
+        public EditCustomFieldViewModel(
+            int fieldId,
+            string prompt,
+            bool mandatory,
+            List<string> options,
+            string? answer
+        ) : base(fieldId, prompt, mandatory, answer)
         {
-            CustomFieldId = fieldId;
-            CustomPrompt = prompt;
-            Mandatory = mandatory;
-            Answer = answer;
             Options = SelectListHelper.MapOptionsToSelectListItems(options, answer);
         }
-
-        public int CustomFieldId { get; set; }
-
-        public string CustomPrompt { get; set; }
-
-        public bool Mandatory { get; set; }
-
-        public string? Answer { get; set; }
 
         public IEnumerable<SelectListItem> Options { get; set; }
     }
