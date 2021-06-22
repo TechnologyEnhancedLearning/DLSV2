cd5a4f3a773fe8adff2d20632e76d51f56009798
Steve Jackson <stephen.jackson@softwire.com>
2021-06-22 13:51:55 +0100

    HEEDLS-350: Use VS-style Equals() on UserReference

--- DigitalLearningSolutions.Data/Models/User/UserReference.cs
+++ DigitalLearningSolutions.Data/Models/User/UserReference.cs
@@ -16,27 +16,12 @@ namespace DigitalLearningSolutions.Data.Models.User
 
         public override bool Equals(object? obj)
         {
-            if (ReferenceEquals(null, obj))
-            {
-                return false;
-            }
-
-            if (ReferenceEquals(this, obj))
-            {
-                return true;
-            }
-
-            if (obj.GetType() != GetType())
-            {
-                return false;
-            }
-
-            return Equals((UserReference)obj);
+            return Equals(obj as UserReference);
         }
 
-        private bool Equals(UserReference other)
+        private bool Equals(UserReference? other)
         {
-            return Id == other.Id && UserType.Equals(other.UserType);
+            return other != null && Id == other.Id && UserType.Equals(other.UserType);
         }
 
         public override int GetHashCode()
