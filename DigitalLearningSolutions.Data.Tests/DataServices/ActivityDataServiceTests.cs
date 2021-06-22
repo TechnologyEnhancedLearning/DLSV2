b93b6ae12417d23889f0904577414af43cc9d6df
Jonathan Bloxsom <jonathan.bloxsom@softwire.com>
2021-06-22 15:49:31 +0100

    HEEDLS-459: tests

---
+++ DigitalLearningSolutions.Data.Tests/DataServices/ActivityDataServiceTests.cs
@@ -0,0 +1,44 @@
+﻿namespace DigitalLearningSolutions.Data.Tests.DataServices
+{
+    using System.Linq;
+    using DigitalLearningSolutions.Data.DataServices;
+    using DigitalLearningSolutions.Data.Tests.TestHelpers;
+    using FluentAssertions;
+    using NUnit.Framework;
+
+    class ActivityDataServiceTests
+    {
+        private IActivityDataService service = null!;
+
+        [SetUp]
+        public void Setup()
+        {
+            var connection = ServiceTestHelper.GetDatabaseConnection();
+            service = new ActivityDataService(connection);
+        }
+
+        [Test]
+        public void GetActivityForMonthsInYear_gets_activity_for_months_in_year()
+        {
+            // when
+            var result = service.GetActivityForMonthsInYear(101, 2014, new[] { 1, 2, 3, 4 }).ToList();
+
+            // then
+            result.Count().Should().Be(4);
+
+            var first = result.First();
+            first.Year.Should().Be(2014);
+            first.Month.Should().Be(4);
+            first.Completions.Should().Be(0);
+            first.Evaluations.Should().Be(1);
+            first.Registrations.Should().Be(7);
+
+            var last = result.Last();
+            last.Year.Should().Be(2014);
+            last.Month.Should().Be(1);
+            last.Completions.Should().Be(1);
+            last.Evaluations.Should().Be(0);
+            last.Registrations.Should().Be(12);
+        }
+    }
+}
---
+++ DigitalLearningSolutions.Data.Tests/Services/ActivityServiceTests.cs
@@ -0,0 +1,68 @@
+﻿namespace DigitalLearningSolutions.Data.Tests.Services
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using DigitalLearningSolutions.Data.DataServices;
+    using DigitalLearningSolutions.Data.Models.TrackingSystem;
+    using DigitalLearningSolutions.Data.Services;
+    using FakeItEasy;
+    using FluentAssertions;
+    using NUnit.Framework;
+
+    class ActivityServiceTests
+    {
+        private IActivityDataService activityDataService = null!;
+        private IClockService clockService = null!;
+        private IActivityService activityService = null!;
+
+        [SetUp]
+        public void SetUp()
+        {
+            activityDataService = A.Fake<IActivityDataService>();
+            clockService = A.Fake<IClockService>();
+            activityService = new ActivityService(activityDataService, clockService);
+        }
+
+        [Test]
+        public void GetRecentActivity_gets_recent_activity_from_one_year_in_december()
+        {
+            // given
+            var expectedActivityResult = new List<MonthOfActivity>{new MonthOfActivity()};
+            A.CallTo(() => activityDataService.GetActivityForMonthsInYear(A<int>._, A<int>._, A<IEnumerable<int>>._))
+                .Returns(expectedActivityResult);
+            GivenCurrentTimeIs(DateTime.Parse("2015-12-22 06:52:09.080"));
+
+            // when
+            var result = activityService.GetRecentActivity(101);
+
+            // then
+            A.CallTo(() => activityDataService.GetActivityForMonthsInYear(A<int>._, A<int>._, A<IEnumerable<int>>._))
+                .MustHaveHappened(1, Times.Exactly);
+            result.Should().BeEquivalentTo(expectedActivityResult);
+        }
+
+        [Test]
+        public void GetRecentActivity_gets_recent_activity_from_two_years_in_earlier_month()
+        {
+            // given
+            var expectedActivityResult = new List<MonthOfActivity> { new MonthOfActivity() };
+            A.CallTo(() => activityDataService.GetActivityForMonthsInYear(A<int>._, A<int>._, A<IEnumerable<int>>._))
+                .Returns(expectedActivityResult);
+            GivenCurrentTimeIs(DateTime.Parse("2015-10-22 06:52:09.080"));
+
+            // when
+            var result = activityService.GetRecentActivity(101);
+
+            // then
+            A.CallTo(() => activityDataService.GetActivityForMonthsInYear(A<int>._, A<int>._, A<IEnumerable<int>>._))
+                .MustHaveHappened(2, Times.Exactly);
+            result.Should().BeEquivalentTo(expectedActivityResult.Concat(expectedActivityResult));
+        }
+
+        private void GivenCurrentTimeIs(DateTime time)
+        {
+            A.CallTo(() => clockService.UtcNow).Returns(time);
+        }
+    }
+}
