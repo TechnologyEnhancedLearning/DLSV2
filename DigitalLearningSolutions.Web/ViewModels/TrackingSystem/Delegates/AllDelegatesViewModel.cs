e5155cc27929c90ef71ec64211a317217e155a04
Ibrahim Munir-Zubair <ibrahimmunir14@gmail.com>
2021-06-30 18:25:00 +0100

    HEEDLS-490 Create AllDelegates and SearchableDelegate view models

---
+++ DigitalLearningSolutions.Web/ViewModels/TrackingSystem/Delegates/AllDelegatesViewModel.cs
@@ -0,0 +1,16 @@
+﻿namespace DigitalLearningSolutions.Web.ViewModels.TrackingSystem.Delegates
+{
+    using System.Collections.Generic;
+    using System.Linq;
+    using DigitalLearningSolutions.Data.Models.User;
+
+    public class AllDelegatesViewModel
+    {
+        public AllDelegatesViewModel(IEnumerable<DelegateUser> delegateUsers)
+        {
+            Delegates = delegateUsers.Select(delegateUser => new SearchableDelegateViewModel(delegateUser));
+        }
+
+        public IEnumerable<SearchableDelegateViewModel> Delegates { get; }
+    }
+}
---
+++ DigitalLearningSolutions.Web/ViewModels/TrackingSystem/Delegates/SearchableDelegateViewModel.cs
@@ -0,0 +1,23 @@
+﻿namespace DigitalLearningSolutions.Web.ViewModels.TrackingSystem.Delegates
+{
+    using DigitalLearningSolutions.Data.Models.User;
+
+    /* TODO: Search and sort functionality is part of HEEDLS-491.
+       Filename includes 'Searchable' to avoid having to change name later */
+
+    public class SearchableDelegateViewModel
+    {
+        public SearchableDelegateViewModel(DelegateUser delegateUser)
+        {
+            Id = delegateUser.Id;
+            Name = delegateUser.SearchableName;
+            CandidateNumber = delegateUser.CandidateNumber;
+        }
+
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string CandidateNumber { get; set; }
+    }
+}
