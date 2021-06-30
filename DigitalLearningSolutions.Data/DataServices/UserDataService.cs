2f27f6c3cd17b4f5f0b8fdaa39096d3cfcae5cb7
Alex Jackson <Alexander.Jackson@dorsetsoftware.com>
2021-06-30 10:53:06 +0100

    HEEDLS-537 Common code for 532 and 416

--- DigitalLearningSolutions.Data/DataServices/UserDataService.cs
+++ DigitalLearningSolutions.Data/DataServices/UserDataService.cs
@@ -1,12 +1,10 @@
 ﻿namespace DigitalLearningSolutions.Data.DataServices
 {
-    using System;
     using System.Collections.Generic;
     using System.Data;
     using System.Linq;
     using System.Transactions;
     using Dapper;
-    using DigitalLearningSolutions.Data.Exceptions;
     using DigitalLearningSolutions.Data.Models.User;
 
     public interface IUserDataService
@@ -138,25 +136,28 @@
         {
             var users = connection.Query<AdminUser>(
                 @"SELECT
-                        AdminID AS Id,
-                        CentreID,
-                        Email AS EmailAddress,
-                        Forename AS FirstName,
-                        Surname AS LastName,
-                        Password,
-                        CentreAdmin AS IsCentreAdmin,
-                        IsCentreManager,
-                        ContentCreator AS IsContentCreator,
-                        ContentManager AS IsContentManager,
-                        PublishToAll,
-                        SummaryReports,
-                        UserAdmin AS IsUserAdmin,
-                        CategoryID,
-                        Supervisor AS IsSupervisor,
-                        Trainer AS IsTrainer,
-                        ImportOnly
-                    FROM AdminUsers
-                    WHERE Active = 1 AND Approved = 1 AND CentreId = @centreId",
+                        au.AdminID AS Id,
+                        au.CentreID,
+                        au.Email AS EmailAddress,
+                        au.Forename AS FirstName,
+                        au.Surname AS LastName,
+                        au.Password,
+                        au.CentreAdmin AS IsCentreAdmin,
+                        au.IsCentreManager,
+                        au.ContentCreator AS IsContentCreator,
+                        au.ContentManager AS IsContentManager,
+                        au.PublishToAll,
+                        au.SummaryReports,
+                        au.UserAdmin AS IsUserAdmin,
+                        au.CategoryID,
+                        cc.CategoryName,
+                        au.Supervisor AS IsSupervisor,
+                        au.Trainer AS IsTrainer,
+                        au.ImportOnly,
+                        au.FailedLoginCount
+                    FROM AdminUsers AS au
+                    LEFT JOIN CourseCategories AS cc ON cc.CourseCategoryID = au.CategoryID
+                    WHERE au.Active = 1 AND au.Approved = 1 AND au.CentreId = @centreId",
                 new { centreId }
             ).ToList();
 
@@ -399,7 +400,8 @@
                         FROM Candidates
                         WHERE CentreID = @centreId AND Answer{promptNumber} IS NOT NULL",
                 new { centreId }
-            ).Count(x => !string.IsNullOrWhiteSpace(x)); ;
+            ).Count(x => !string.IsNullOrWhiteSpace(x));
+            ;
         }
 
         public void DeleteAllAnswersForPrompt(int centreId, int promptNumber)
