9417bb4d1cdc7593822e7dddc0005947a12d969b
Ibrahim Munir-Zubair <ibrahimmunir14@gmail.com>
2021-06-30 13:46:52 +0100

    HEEDLS-490 Create AllDelegates controller with CentreAdmin policy; create empty page with delegates sidebar

---
+++ DigitalLearningSolutions.Web/Controllers/TrackingSystem/Delegates/AllDelegatesController.cs
@@ -0,0 +1,16 @@
+﻿namespace DigitalLearningSolutions.Web.Controllers.TrackingSystem.Delegates
+{
+    using DigitalLearningSolutions.Web.Helpers;
+    using Microsoft.AspNetCore.Authorization;
+    using Microsoft.AspNetCore.Mvc;
+
+    [Authorize(Policy = CustomPolicies.UserCentreAdmin)]
+    [Route("TrackingSystem/Delegates/All")]
+    public class AllDelegatesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
