f2bcd95176722c5c9b6b9879b7d743900f010905
Ibrahim Munir-Zubair <ibrahimmunir14@gmail.com>
2021-07-01 13:15:31 +0100

    HEEDLS-490 Add Name/Id/Email/RegDate/JobGroup to DelegateCard

--- DigitalLearningSolutions.Web/ViewModels/TrackingSystem/Delegates/SearchableDelegateViewModel.cs
+++ DigitalLearningSolutions.Web/ViewModels/TrackingSystem/Delegates/SearchableDelegateViewModel.cs
@@ -12,11 +12,19 @@
             Id = delegateUser.Id;
             Name = delegateUser.SearchableName;
             CandidateNumber = delegateUser.CandidateNumber;
+
             IsSelfReg = delegateUser.SelfReg;
             IsExternalReg = delegateUser.ExternalReg;
             IsActive = delegateUser.Active;
             IsAdmin = delegateUser.AdminId.HasValue;
             IsPasswordSet = delegateUser.Password != null;
+
+            Email = delegateUser.EmailAddress;
+            JobGroup = delegateUser.JobGroupName;
+            if (delegateUser.DateRegistered.HasValue)
+            {
+                RegistrationDate = delegateUser.DateRegistered.Value.ToShortDateString();
+            }
         }
 
         public int Id { get; set; }
@@ -28,5 +36,9 @@
         public bool IsActive { get; set; }
         public bool IsAdmin { get; set; }
         public bool IsPasswordSet { get; set; }
+
+        public string? Email { get; set; }
+        public string? JobGroup { get; set; }
+        public string? RegistrationDate { get; set; }
     }
 }
