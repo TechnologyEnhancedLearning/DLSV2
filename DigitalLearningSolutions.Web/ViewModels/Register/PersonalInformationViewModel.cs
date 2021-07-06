dd6f8ec799155533ed500e5d431c52704e63739b
Ibrahim Munir-Zubair <ibrahimmunir14@gmail.com>
2021-07-06 10:25:35 +0100

    HEEDLS-545 Modify error messages; use correct TrackingSystem nav menu

--- DigitalLearningSolutions.Web/ViewModels/Register/PersonalInformationViewModel.cs
+++ DigitalLearningSolutions.Web/ViewModels/Register/PersonalInformationViewModel.cs
@@ -27,15 +27,15 @@
             Alias = data.Alias;
         }
 
-        [Required(ErrorMessage = "Enter your first name")]
+        [Required(ErrorMessage = "Enter a first name")]
         [MaxLength(250, ErrorMessage = "First name must be 250 characters or fewer")]
         public string? FirstName { get; set; }
 
-        [Required(ErrorMessage = "Enter your last name")]
+        [Required(ErrorMessage = "Enter a last name")]
         [MaxLength(250, ErrorMessage = "Last name must be 250 characters or fewer")]
         public string? LastName { get; set; }
 
-        [Required(ErrorMessage = "Enter your email address")]
+        [Required(ErrorMessage = "Enter an email address")]
         [MaxLength(250, ErrorMessage = "Email address must be 250 characters or fewer")]
         [EmailAddress(ErrorMessage = "Enter an email address in the correct format, like name@example.com")]
         [NoWhitespace("Email address must not contain any whitespace characters")]
@@ -46,7 +46,6 @@
 
         [MaxLength(250, ErrorMessage = "Alias must be 250 characters or fewer")]
         public string? Alias { get; set; }
-        // TODO: change error messages, subclass? for delegate registration by centre
 
         public bool IsCentreSpecificRegistration { get; set; }
 
