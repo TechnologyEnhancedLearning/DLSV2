afba14c743f41ff73569ac52c22e6c5272590639
Ibrahim Munir-Zubair <ibrahimmunir14@gmail.com>
2021-07-07 13:41:57 +0100

    HEEDLS-490 Rename test for clarity

--- DigitalLearningSolutions.Data.Tests/DataServices/UserDataServiceTests.cs
+++ DigitalLearningSolutions.Data.Tests/DataServices/UserDataServiceTests.cs
@@ -390,7 +390,7 @@
         }
 
         [Test]
-        public void GetDelegateUserCardsByCentreId_populates_DelegateUserCard_fields_correctly()
+        public void GetDelegateUserCardsByCentreId_populates_DelegateUserCard_admin_fields_correctly()
         {
             // When
             var userCards = userDataService.GetDelegateUserCardsByCentreId(279);
