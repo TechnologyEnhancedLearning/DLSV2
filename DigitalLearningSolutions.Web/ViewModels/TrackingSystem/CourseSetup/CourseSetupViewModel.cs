97d699db878c191363d632b77a7f2aa2813491ab
Daniel Bloxham <Daniel.Bloxham@softwire.com>
2021-07-07 08:22:09 +0100

    HEEDLS-430 - Implemented basic centre course setup page

---
+++ DigitalLearningSolutions.Web/ViewModels/TrackingSystem/CourseSetup/CourseSetupViewModel.cs
@@ -0,0 +1,15 @@
+﻿namespace DigitalLearningSolutions.Web.ViewModels.TrackingSystem.CourseSetup
+{
+    using System.Collections.Generic;
+    using DigitalLearningSolutions.Data.Models.Courses;
+
+    public class CourseSetupViewModel
+    {
+        public CourseSetupViewModel(IEnumerable<CourseStatistics> courses)
+        {
+            Courses = courses;
+        }
+
+        public IEnumerable<CourseStatistics> Courses { get; set; }
+    }
+}
