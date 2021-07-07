4382d5d9be69d7aa9c2557a918591a5bf91b6851
Daniel Bloxham <Daniel.Bloxham@softwire.com>
2021-07-07 16:23:52 +0100

    HEEDLS-430 - neatened css and created new view model for course statistics

--- DigitalLearningSolutions.Web/ViewModels/TrackingSystem/CourseSetup/CourseSetupViewModel.cs
+++ DigitalLearningSolutions.Web/ViewModels/TrackingSystem/CourseSetup/CourseSetupViewModel.cs
@@ -1,15 +1,16 @@
 ﻿namespace DigitalLearningSolutions.Web.ViewModels.TrackingSystem.CourseSetup
 {
     using System.Collections.Generic;
+    using System.Linq;
     using DigitalLearningSolutions.Data.Models.Courses;
 
     public class CourseSetupViewModel
     {
         public CourseSetupViewModel(IEnumerable<CourseStatistics> courses)
         {
-            Courses = courses;
+            Courses = courses.Select(course => new SearchableCourseStatisticsViewModel(course));
         }
 
-        public IEnumerable<CourseStatistics> Courses { get; set; }
+        public IEnumerable<SearchableCourseStatisticsViewModel> Courses { get; set; }
     }
 }
