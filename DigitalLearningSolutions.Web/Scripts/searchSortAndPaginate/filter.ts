eb9e111aac42779d4d9002851582b9fdac0f4a11
Alex Jackson <Alexander.Jackson@dorsetsoftware.com>
2021-07-08 15:52:26 +0100

    HEEDLS-533 Refactor and tidy filter methods

--- DigitalLearningSolutions.Web/Scripts/searchSortAndPaginate/filter.ts
+++ DigitalLearningSolutions.Web/Scripts/searchSortAndPaginate/filter.ts
@@ -1,11 +1,11 @@
 ﻿import * as _ from 'lodash';
-import { SearchableElement } from './searchSortAndPaginate';
+import {SearchableElement} from './searchSortAndPaginate';
 
 export interface AppliedFilter {
   group: string;
   propertyName: string;
   propertyValue: string;
-  unseparated: string;
+  filterValue: string;
 }
 
 export interface AppliedFilterTag {
@@ -16,20 +16,8 @@ export interface AppliedFilterTag {
 const newlineSeparator: string = '\r\n';
 
 export function setupFilter(onFilterUpdated: VoidFunction): void {
-  const filterSubmits = Array.from(document.getElementsByClassName('filter-submit__dropdown'));
-  for (let filterSubmit of filterSubmits){
-    const element = <HTMLInputElement>filterSubmit;
-    element.addEventListener('click', (e) => {
-      e.preventDefault();
-      appendNewFilterToFilterBy(element);
-    });
-  }
-
-  document.getElementById('clear-filters')?.addEventListener('click', (e) => {
-    e.preventDefault();
-    clearFilters();
-  })
-
+  setUpFilterSubmitButtons();
+  setUpClearFiltersButton();
   document.getElementById('filter-by')?.addEventListener('change', onFilterUpdated);
 }
 
@@ -37,16 +25,7 @@ export function filterSearchableElements(
   searchableElements: SearchableElement[], possibleFilters: AppliedFilterTag[]
 ): SearchableElement[] {
   const filterBy = getFilterBy();
-  const listOfFilters = filterBy.split(newlineSeparator);
-  const appliedFilters = listOfFilters.map((filter) => {
-    const splitFilter = filter.split('|');
-    return <AppliedFilter>{
-      group : splitFilter[0],
-      propertyName : splitFilter[1],
-      propertyValue : splitFilter[2],
-      unseparated : filter
-    }
-  });
+  const appliedFilters = getAppliedFilters(filterBy);
 
   const filterGroups = _.groupBy(appliedFilters, (filter) => filter.group);
 
@@ -57,7 +36,7 @@ export function filterSearchableElements(
     searchableElements = _.uniq(flattenedElements);
   })
 
-  if (filterBy){
+  if (filterBy) {
     updateAppliedFilters(filterBy, possibleFilters);
     showAppliedFilters();
   }
@@ -65,108 +44,143 @@ export function filterSearchableElements(
   return searchableElements;
 }
 
-function filterElements(searchableElements: SearchableElement[], appliedFilter: AppliedFilter): SearchableElement[]{
+function setUpFilterSubmitButtons() {
+  const filterSubmits = Array.from(document.getElementsByClassName('filter-submit__dropdown'));
+  for (let filterSubmit of filterSubmits) {
+    const element = <HTMLInputElement>filterSubmit;
+    element.addEventListener('click', (e) => {
+      e.preventDefault();
+      appendNewFilterToFilterBy(element);
+    });
+  }
+}
+
+function setUpClearFiltersButton() {
+  document.getElementById('clear-filters')?.addEventListener('click', (e) => {
+    e.preventDefault();
+    clearFilters();
+  })
+}
+
+function getAppliedFilters(filterBy: string): AppliedFilter[] {
+  return filterBy.split(newlineSeparator).map((filter) => {
+    return newAppliedFilterFromFilter(filter);
+  });
+}
+
+function newAppliedFilterFromFilter(filter: string): AppliedFilter {
+  const splitFilter = filter.split('|');
+  return <AppliedFilter>{
+    group: splitFilter[0],
+    propertyName: splitFilter[1],
+    propertyValue: splitFilter[2],
+    filterValue: filter
+  }
+}
+
+function filterElements(searchableElements: SearchableElement[], appliedFilter: AppliedFilter): SearchableElement[] {
   return _.filter<SearchableElement>(
     searchableElements,
-    function(o) {
-      return doesElementHaveFilterValue(o, appliedFilter.unseparated);
-      // const elementFilterValue = getElementFilterValue(o, propertyName);
-      // return elementFilterValue === filterValue;
+    function (o) {
+      return doesElementMatchFilterValue(o, appliedFilter.filterValue);
     });
 }
 
 function appendNewFilterToFilterBy(filterSubmit: HTMLInputElement): void {
-  // grab new filter string from filter dropdown
+  const filterValue = getSelectedFilterFromDropdownAndResetDropdown(filterSubmit)
+  addNewFilterToFilterBy(filterValue);
+}
+
+function getSelectedFilterFromDropdownAndResetDropdown(filterSubmit: HTMLInputElement): string {
   const filterId = filterSubmit.id.replace('-submit', '');
   const filterElement = <HTMLSelectElement>document.getElementById(filterId);
   const filterValue = filterElement.value;
-  // update filter-by hidden with string
+  filterElement.selectedIndex = 0;
+  return filterValue;
+}
+
+function addNewFilterToFilterBy(filterValue: string): void {
   const filterBy = getFilterBy();
-  if (!filterBy.includes(filterValue)){
+  if (!filterBy.includes(filterValue)) {
     let newFilter;
-    if (filterBy){
+    if (filterBy) {
       newFilter = filterBy + newlineSeparator + filterValue;
     } else {
       newFilter = filterValue;
     }
     updateAllFilterByElementsByName(newFilter);
     updateFilterByById(newFilter);
-
   }
-  filterElement.selectedIndex = 0;
-
-  return;
 }
 
-function clearFilters(){
+function clearFilters(): void {
   updateAllFilterByElementsByName('');
   updateFilterByById('');
   clearAppliedFilters();
   hideAppliedFilters();
 }
 
-function doesElementHaveFilterValue(searchableElement: SearchableElement, filter: string): boolean{
+function doesElementMatchFilterValue(searchableElement: SearchableElement, filter: string): boolean {
   const filterElement = searchableElement.element.querySelector(`[data-filter-value="${filter}"]`);
   const filterValue = filterElement?.getAttribute('data-filter-value')?.trim() ?? '';
   return filterValue === filter;
 }
 
 export function getFilterBy(): string {
-  const element = <HTMLInputElement>document.getElementById('filter-by');
-  return element.value;
+  return getFilterByElement().value;
 }
 
 function updateFilterByById(newFilter: string): void {
-  const element = <HTMLInputElement>document.getElementById('filter-by');
+  const element = getFilterByElement();
   element.value = newFilter;
   element.dispatchEvent(new Event('change'));
 }
 
+function getFilterByElement(): HTMLInputElement {
+  return <HTMLInputElement>document.getElementById('filter-by');
+}
+
 function updateAllFilterByElementsByName(newFilter: string): void {
   const filterByElements = Array.from(document.getElementsByName('filterBy'));
-  for (let filterElement of filterByElements){
+  for (let filterElement of filterByElements) {
     const element = <HTMLInputElement>filterElement;
     element.value = newFilter;
   }
 }
 
-function showAppliedFilters(): void{
-  const element = document.getElementById('applied-filters');
-  if (element){
-    element.hidden = false;
-  }
+function showAppliedFilters(): void {
+  const element = getAppliedFiltersElement();
+  element.hidden = false;
 }
 
-function hideAppliedFilters(): void{
-  const element = document.getElementById('applied-filters');
-  if (element){
-    element.hidden = true;
-  }
+function hideAppliedFilters(): void {
+  const element = getAppliedFiltersElement();
+  element.hidden = true;
 }
 
-function clearAppliedFilters(){
-  const appliedFilterContainer = document.getElementById('applied-filter-container');
-  if (!appliedFilterContainer) {
-    return;
-  }
-
+function clearAppliedFilters() {
+  const appliedFilterContainer = getAppliedFilterContainer();
   appliedFilterContainer.textContent = '';
 }
 
 function updateAppliedFilters(filterBy: string, possibleFilters: AppliedFilterTag[]) {
-  const appliedFilterContainer = document.getElementById('applied-filter-container');
-  if (!appliedFilterContainer) {
-    return;
-  }
-  const listOfFilters = filterBy.split('\r\n');
-
+  const appliedFilterContainer = getAppliedFilterContainer();
+  const listOfFilters = filterBy.split(newlineSeparator);
   appliedFilterContainer.textContent = '';
   listOfFilters.forEach(
     (filter) => appliedFilterContainer.appendChild(getMatchingFilterTag(possibleFilters, filter)),
   );
 }
 
-function getMatchingFilterTag(possibleFilters: AppliedFilterTag[], filter: string): Element{
-  const appliedFilterTag = _.find(possibleFilters, (pf) => pf.filterValue === filter);
-  return appliedFilterTag!.element;
+function getMatchingFilterTag(possibleFilters: AppliedFilterTag[], filter: string): Element {
+  return _.find(possibleFilters, (pf) => pf.filterValue === filter)!.element;
 }
+
+function getAppliedFilterContainer(): Element {
+  return <HTMLElement>document.getElementById('applied-filter-container');
+}
+
+function getAppliedFiltersElement(): HTMLElement {
+  return <HTMLElement>document.getElementById('applied-filters');
+}
+
