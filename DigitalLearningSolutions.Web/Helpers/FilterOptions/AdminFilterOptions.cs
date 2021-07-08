8dfac6b89b761136547e2859b9e3c7ae86cf61bc
Alex Jackson <Alexander.Jackson@dorsetsoftware.com>
2021-07-08 13:56:25 +0100

    HEEDLS-532 Rename constant for consistency

--- DigitalLearningSolutions.Web/Helpers/FilterOptions/AdminFilterOptions.cs
+++ DigitalLearningSolutions.Web/Helpers/FilterOptions/AdminFilterOptions.cs
@@ -6,30 +6,30 @@
 
     public static class AdminRoleFilterOptions
     {
-        private const string Category = "Role";
+        private const string Group = "Role";
 
         public static readonly FilterOptionViewModel CentreAdministrator = new FilterOptionViewModel(
             "Centre administrator",
-            Category + FilteringHelper.Separator + nameof(AdminUser.IsCentreAdmin) + FilteringHelper.Separator + "true",
+            Group + FilteringHelper.Separator + nameof(AdminUser.IsCentreAdmin) + FilteringHelper.Separator + "true",
             FilterStatus.Default
         );
 
         public static readonly FilterOptionViewModel Supervisor = new FilterOptionViewModel(
             "Supervisor",
-            Category + FilteringHelper.Separator + nameof(AdminUser.IsSupervisor) + FilteringHelper.Separator + "true",
+            Group + FilteringHelper.Separator + nameof(AdminUser.IsSupervisor) + FilteringHelper.Separator + "true",
             FilterStatus.Default
         );
 
         public static readonly FilterOptionViewModel Trainer = new FilterOptionViewModel(
             "Trainer",
-            Category + FilteringHelper.Separator + nameof(AdminUser.IsTrainer) + FilteringHelper.Separator + "true",
+            Group + FilteringHelper.Separator + nameof(AdminUser.IsTrainer) + FilteringHelper.Separator + "true",
             FilterStatus.Default
         );
 
         public static readonly FilterOptionViewModel ContentCreatorLicense =
             new FilterOptionViewModel(
                 "Content Creator license",
-                Category + FilteringHelper.Separator + nameof(AdminUser.IsContentCreator) + FilteringHelper.Separator +
+                Group + FilteringHelper.Separator + nameof(AdminUser.IsContentCreator) + FilteringHelper.Separator +
                 "true",
                 FilterStatus.Default
             );
@@ -37,31 +37,31 @@
         public static readonly FilterOptionViewModel CmsAdministrator =
             new FilterOptionViewModel(
                 "CMS administrator",
-                Category + FilteringHelper.Separator + nameof(AdminUser.IsCmsAdministrator) +
+                Group + FilteringHelper.Separator + nameof(AdminUser.IsCmsAdministrator) +
                 FilteringHelper.Separator + "true",
                 FilterStatus.Default
             );
 
         public static readonly FilterOptionViewModel CmsManager = new FilterOptionViewModel(
             "CMS manager",
-            Category + FilteringHelper.Separator + nameof(AdminUser.IsCmsManager) + FilteringHelper.Separator + "true",
+            Group + FilteringHelper.Separator + nameof(AdminUser.IsCmsManager) + FilteringHelper.Separator + "true",
             FilterStatus.Default
         );
     }
 
     public static class AdminAccountStatusFilterOptions
     {
-        private const string Category = "AccountStatus";
+        private const string Group = "AccountStatus";
 
         public static readonly FilterOptionViewModel IsLocked = new FilterOptionViewModel(
             "Locked",
-            Category + FilteringHelper.Separator + nameof(AdminUser.IsLocked) + FilteringHelper.Separator + "true",
+            Group + FilteringHelper.Separator + nameof(AdminUser.IsLocked) + FilteringHelper.Separator + "true",
             FilterStatus.Warning
         );
 
         public static readonly FilterOptionViewModel IsNotLocked = new FilterOptionViewModel(
             "Not locked",
-            Category + FilteringHelper.Separator + nameof(AdminUser.IsLocked) + FilteringHelper.Separator + "false",
+            Group + FilteringHelper.Separator + nameof(AdminUser.IsLocked) + FilteringHelper.Separator + "false",
             FilterStatus.Default
         );
     }
