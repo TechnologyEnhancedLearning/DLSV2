20c36c0c3bd6aaa55dbd7ba56c836816c44fc097
Ibrahim Munir-Zubair <ibrahimmunir14@gmail.com>
2021-07-08 16:30:29 +0100

    HEEDLS-546 Create and use DateInput view component

---
+++ DigitalLearningSolutions.Web/ViewModels/Common/ViewComponents/DateInputViewModel.cs
@@ -0,0 +1,51 @@
+﻿namespace DigitalLearningSolutions.Web.ViewModels.Common.ViewComponents
+{
+    using DigitalLearningSolutions.Web.ControllerHelpers;
+
+    public class DateInputViewModel
+    {
+        public readonly bool HasDayError;
+        public readonly bool HasMonthError;
+        public readonly bool HasYearError;
+
+        public DateInputViewModel(
+            string name,
+            string label,
+            string? dayId,
+            string? monthId,
+            string? yearId,
+            string? dayValue,
+            string? monthValue,
+            string? yearValue,
+            DateValidator.ValidationResult? validationResult,
+            string? hintText = null
+        )
+        {
+            Name = name;
+            Label = label;
+            DayId = dayId ?? "Day";
+            MonthId = monthId ?? "Month";
+            YearId = yearId ?? "Year";
+            DayValue = dayValue;
+            MonthValue = monthValue;
+            YearValue = yearValue;
+            HintText = hintText;
+            ErrorMessage = validationResult?.ErrorMessage;
+            HasDayError = validationResult is { DayValid: false };
+            HasMonthError = validationResult is { MonthValid: false };
+            HasYearError = validationResult is { YearValid: false };
+        }
+
+        public string Name { get; set; }
+        public string Label { get; set; }
+        public string DayId { get; set; }
+        public string MonthId { get; set; }
+        public string YearId { get; set; }
+        public string? DayValue { get; set; }
+        public string? MonthValue { get; set; }
+        public string? YearValue { get; set; }
+        public string? HintText { get; set; }
+        public bool HasError => HasDayError || HasMonthError || HasYearError;
+        public string? ErrorMessage { get; set; }
+    }
+}
