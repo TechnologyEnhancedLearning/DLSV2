b3153a0df20bfb24ce8401a19092893a8e83eaa2
Ibrahim Munir-Zubair <ibrahimmunir14@gmail.com>
2021-07-13 19:36:39 +0100

    HEEDLS-554 Create ViewDelegate controller; configure manage button on delegate card
    Create ViewDelegate controller with delegateId parameter and required user policy; redirect to ViewDelegate index page from DelegateCard Manage Delegate button

---
+++ DigitalLearningSolutions.Web/Controllers/TrackingSystem/Delegates/ViewDelegateController.cs
@@ -0,0 +1,18 @@
+﻿namespace DigitalLearningSolutions.Web.Controllers.TrackingSystem.Delegates
+{
+    using DigitalLearningSolutions.Web.Helpers;
+    using Microsoft.AspNetCore.Authorization;
+    using Microsoft.AspNetCore.Mvc;
+    using Microsoft.FeatureManagement.Mvc;
+
+    [FeatureGate(FeatureFlags.RefactoredTrackingSystem)]
+    [Authorize(Policy = CustomPolicies.UserCentreAdmin)]
+    [Route("TrackingSystem/Delegates/View/{delegateId:int}")]
+    public class ViewDelegateController : Controller
+    {
+        public IActionResult Index(int delegateId)
+        {
+            return new ObjectResult(delegateId);
+        }
+    }
+}
