fd60274102a57238c6c457a54070d5e3cd6f0fae
Daniel Bloxham <Daniel.Bloxham@softwire.com>
2021-07-15 16:48:01 +0100

    HEEDLS-431 - Remove unused properties from view model

--- DigitalLearningSolutions.Web/ViewModels/TrackingSystem/CourseSetup/SearchableCourseStatisticsViewModel.cs
+++ DigitalLearningSolutions.Web/ViewModels/TrackingSystem/CourseSetup/SearchableCourseStatisticsViewModel.cs
@@ -9,28 +9,18 @@
         public SearchableCourseStatisticsViewModel(CourseStatistics courseStatistics)
         {
             CustomisationId = courseStatistics.CustomisationId;
-            CentreId = courseStatistics.CentreId;
-            Active = courseStatistics.Active;
             DelegateCount = courseStatistics.DelegateCount;
-            CompletedCount = courseStatistics.CompletedCount;
             InProgressCount = courseStatistics.InProgressCount;
-            PassRate = courseStatistics.PassRate;
             CourseName = courseStatistics.CourseName;
-            HideInLearnerPortal = courseStatistics.HideInLearnerPortal;
             CategoryName = courseStatistics.CategoryName;
             LearningMinutes = courseStatistics.LearningMinutes;
             Tags = FilterableTagHelper.GetCurrentTagsForCourseStatistics(courseStatistics);
         }
 
         public int CustomisationId { get; set; }
-        public int CentreId { get; set; }
-        public bool Active { get; set; }
         public int DelegateCount { get; set; }
-        public int CompletedCount { get; set; }
         public int InProgressCount { get; set; }
         public string CourseName { get; set; }
-        public double PassRate { get; set; }
-        public bool HideInLearnerPortal { get; set; }
         public string CategoryName { get; set; }
         public string LearningMinutes { get; set; }
         public string GenerateEmailHref => $"mailto:?subject={GenerateEmailSubject}&body={GenerateEmailContent}";
