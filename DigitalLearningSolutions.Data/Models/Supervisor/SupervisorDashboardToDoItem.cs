c17440fe32041afccfec5f43982f8742d86ffdc0
kevwhitt-hee <kevin.whittaker@hee.nhs.uk>
2021-07-15 16:30:31 +0100

    DLSV2-207 Fixes links on supervisor dashboard to do list

--- DigitalLearningSolutions.Data/Models/Supervisor/SupervisorDashboardToDoItem.cs
+++ DigitalLearningSolutions.Data/Models/Supervisor/SupervisorDashboardToDoItem.cs
@@ -3,7 +3,8 @@
     using System;
     public class SupervisorDashboardToDoItem
     {
-        public int? ID { get; set; }
+        public int ID { get; set; }
+        public int SupervisorDelegateId { get; set; }
         public string DelegateName { get; set; }
         public string ProfileName { get; set; }
         public DateTime Requested { get; set; }
