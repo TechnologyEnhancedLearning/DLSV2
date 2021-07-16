80a77caa7674a3e456c64a9296fed23dc9cb5aeb
kevwhitt-hee <kevin.whittaker@hee.nhs.uk>
2021-07-16 10:28:47 +0100

    Revert "Removes redundant add field if missing migration"
    This reverts commit 076f4739128cd1623477fa945f27297f6440545b.

---
+++ DigitalLearningSolutions.Data.Migrations/202107051209_AddSubmittedDateToCandidateAssessmentsIfMissing.cs
@@ -0,0 +1,23 @@
+﻿namespace DigitalLearningSolutions.Data.Migrations
+{
+    using FluentMigrator;
+    [Migration(202107051209)]
+    public class AddSubmittedDateToCandidateAssessmentsIfMissing : Migration
+    {
+        public override void Up()
+        {
+            if(!Schema.Table("CandidateAssessments").Column("SubmittedDate").Exists())
+            {
+                Alter.Table("CandidateAssessments").AddColumn("SubmittedDate").AsDateTime().Nullable();
+            }
+        }
+        public override void Down()
+        {
+            if (!Schema.Table("CandidateAssessments").Column("SubmittedDate").Exists())
+            {
+                Alter.Table("CandidateAssessments").AddColumn("SubmittedDate").AsDateTime().Nullable();
+            }
+        }
+
+    }
+}
