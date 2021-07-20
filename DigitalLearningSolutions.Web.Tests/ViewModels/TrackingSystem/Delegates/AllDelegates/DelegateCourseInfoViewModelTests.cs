89fcb17d5511a08c461620d73a0f6a86dc73cba9
Ibrahim Munir-Zubair <ibrahimmunir14@gmail.com>
2021-07-20 18:08:52 +0100

    HEEDLS-555 Create DelegateCourseInfoViewModel; write tests

---
+++ DigitalLearningSolutions.Web.Tests/ViewModels/TrackingSystem/Delegates/AllDelegates/DelegateCourseInfoViewModelTests.cs
@@ -0,0 +1,56 @@
+﻿namespace DigitalLearningSolutions.Web.Tests.ViewModels.TrackingSystem.Delegates.AllDelegates
+{
+    using System;
+    using DigitalLearningSolutions.Data.Models.Courses;
+    using DigitalLearningSolutions.Web.ViewModels.TrackingSystem.Delegates;
+    using FluentAssertions;
+    using NUnit.Framework;
+
+    public class DelegateCourseInfoViewModelTests
+    {
+        [Test]
+        public void DelegateCourseInfoViewModel_sets_date_strings_correctly()
+        {
+            // Given
+            var enrolledDate = new DateTime(2021, 05, 1);
+            var updatedDate = new DateTime(2021, 05, 2);
+            var completeByDate = new DateTime(2021, 05, 3);
+            var completedDate = new DateTime(2021, 05, 4);
+            var evaluatedDate = new DateTime(2021, 05, 5);
+            var info = new DelegateCourseInfo
+            {
+                Enrolled = enrolledDate, LastUpdated = updatedDate, CompleteBy = completeByDate,
+                Completed = completedDate, Evaluated = evaluatedDate
+            };
+
+            // When
+            var model = new DelegateCourseInfoViewModel(info);
+
+            // Then
+            model.Enrolled.Should().Be("01/05/2021");
+            model.LastUpdated.Should().Be("02/05/2021");
+            model.CompleteBy.Should().Be("03/05/2021");
+            model.Completed.Should().Be("04/05/2021");
+            model.Evaluated.Should().Be("05/05/2021");
+        }
+
+        [TestCase(1, "Self enrolled")]
+        [TestCase(2, "Administrator")]
+        [TestCase(3, "Group")]
+        [TestCase(4, "System")]
+        public void DelegateCourseInfoViewModel_sets_enrollment_method_correctly(
+            int enrollmentMethodId,
+            string enrollmentMethod
+        )
+        {
+            // Given
+            var info = new DelegateCourseInfo { EnrollmentMethodId = enrollmentMethodId };
+
+            // When
+            var model = new DelegateCourseInfoViewModel(info);
+
+            // Then
+            model.EnrollmentMethod.Should().Be(enrollmentMethod);
+        }
+    }
+}
---
+++ DigitalLearningSolutions.Web/ViewModels/TrackingSystem/Delegates/DelegateCourseInfoViewModel.cs
@@ -0,0 +1,55 @@
+﻿namespace DigitalLearningSolutions.Web.ViewModels.TrackingSystem.Delegates
+{
+    using System;
+    using DigitalLearningSolutions.Data.Models.Courses;
+
+    public class DelegateCourseInfoViewModel
+    {
+        public DelegateCourseInfoViewModel(DelegateCourseInfo info)
+        {
+            ApplicationName = info.ApplicationName;
+            CustomisationName = info.CustomisationName;
+            Supervisor = info.Supervisor;
+            Enrolled = info.Enrolled.ToString("dd/MM/yyyy");
+            LastUpdated = info.LastUpdated.ToString("dd/MM/yyyy");
+            if (info.CompleteBy.HasValue)
+            {
+                CompleteBy = info.CompleteBy.Value.ToString("dd/MM/yyyy");
+            }
+            if (info.Completed.HasValue)
+            {
+                Completed = info.Completed.Value.ToString("dd/MM/yyyy");
+            }
+            if (info.Evaluated.HasValue)
+            {
+                Evaluated = info.Evaluated.Value.ToString("dd/MM/yyyy");
+            }
+            EnrollmentMethod = info.EnrollmentMethodId switch
+            {
+                1 => "Self enrolled",
+                2 => "Administrator",
+                3 => "Group",
+                4 => "System",
+                _ => ""
+            };
+            LoginCount = info.LoginCount;
+            LearningTime = info.LearningTime + " mins";
+            DiagnosticScore = info.DiagnosticScore;
+            IsAssessed = info.IsAssessed;
+        }
+
+        public string ApplicationName { get; set; }
+        public string CustomisationName { get; set; }
+        public string? Supervisor { get; set; }
+        public string Enrolled { get; set; }
+        public string LastUpdated { get; set; }
+        public string? CompleteBy { get; set; }
+        public string? Completed { get; set; }
+        public string? Evaluated { get; set; }
+        public string EnrollmentMethod { get; set; }
+        public int LoginCount { get; set; }
+        public string LearningTime { get; set; }
+        public int? DiagnosticScore { get; set; }
+        public bool IsAssessed { get; set; }
+    }
+}
