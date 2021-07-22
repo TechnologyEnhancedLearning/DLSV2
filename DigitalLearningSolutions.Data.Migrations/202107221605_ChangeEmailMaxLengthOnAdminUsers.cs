311b2471f9f0c28fcae0a3cb17e11a08354aeed6
OliZor <olivia.zorn@softwire.com>
2021-07-22 16:35:54 +0100

    HEEDLS-557 Change email max length on AdminUsers table

---
+++ DigitalLearningSolutions.Data.Migrations/202107221605_ChangeEmailMaxLengthOnAdminUsers.cs
@@ -0,0 +1,16 @@
+﻿namespace DigitalLearningSolutions.Data.Migrations
+{
+    using FluentMigrator;
+    [Migration(202107221605)]
+    public class ChangeEmailMaxLengthOnAdminUsers : Migration
+    {
+        public override void Up()
+        {
+            Alter.Column("Email").OnTable("AdminUsers").AsString(255).NotNullable();
+        }
+        public override void Down()
+        {
+            Alter.Column("Email").OnTable("AdminUsers").AsString(250).NotNullable();
+        }
+    }
+}
