fa019636fd13613d179429ba9c3482004e4b1ef8
OliZor <olivia.zorn@softwire.com>
2021-07-23 14:42:55 +0100

    HEEDLS-557 Change migration to accomodate AdminUsers Email index

--- DigitalLearningSolutions.Data.Migrations/202107221605_ChangeEmailMaxLengthOnAdminUsers.cs
+++ DigitalLearningSolutions.Data.Migrations/202107221605_ChangeEmailMaxLengthOnAdminUsers.cs
@@ -1,16 +1,25 @@
 ﻿namespace DigitalLearningSolutions.Data.Migrations
 {
     using FluentMigrator;
+
     [Migration(202107221605)]
     public class ChangeEmailMaxLengthOnAdminUsers : Migration
     {
         public override void Up()
         {
-            Alter.Column("Email").OnTable("AdminUsers").AsString(255).NotNullable();
+            Delete.Index("IX_AdminUsers_Email");
+            Alter.Column("Email").OnTable("AdminUsers").AsString(255);
+            Create.Index("IX_AdminUsers_Email").OnTable("AdminUsers").OnColumn("Email").Ascending().WithOptions()
+                .Unique().WithOptions().NonClustered();
         }
+
         public override void Down()
         {
-            Alter.Column("Email").OnTable("AdminUsers").AsString(250).NotNullable();
+            Delete.Index("IX_AdminUsers_Email");
+            Alter.Column("Email").OnTable("AdminUsers").AsString(250);
+            Create.Index("IX_AdminUsers_Email").OnTable("AdminUsers").OnColumn("Email").Ascending().WithOptions()
+                .Unique().WithOptions().NonClustered();
+            ;
         }
     }
 }
