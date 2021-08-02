a0a49be454d27a3015664717f907526a49f5df81
Ibrahim Munir-Zubair <ibrahimmunir14@gmail.com>
2021-08-02 15:53:05 +0100

    HEEDLS-505 Add file upload to StartUpload form

--- DigitalLearningSolutions.Web/ViewModels/TrackingSystem/Delegates/UploadDelegatesViewModel.cs
+++ DigitalLearningSolutions.Web/ViewModels/TrackingSystem/Delegates/UploadDelegatesViewModel.cs
@@ -1,6 +1,14 @@
 ﻿namespace DigitalLearningSolutions.Web.ViewModels.TrackingSystem.Delegates
 {
+    using System.ComponentModel.DataAnnotations;
+    using DigitalLearningSolutions.Web.Attributes;
     using DigitalLearningSolutions.Web.ViewModels.Register.RegisterDelegateByCentre;
+    using Microsoft.AspNetCore.Http;
 
-    public class UploadDelegatesViewModel : WelcomeEmailViewModel { }
+    public class UploadDelegatesViewModel : WelcomeEmailViewModel
+    {
+        [Required(ErrorMessage = "Delegates update file is required.")]
+        [AllowedExtensions(new[] { ".xlsx" }, "Delegates update file must be in xlsx format.")]
+        public IFormFile? DelegatesDetailsFile { get; set; }
+    }
 }
