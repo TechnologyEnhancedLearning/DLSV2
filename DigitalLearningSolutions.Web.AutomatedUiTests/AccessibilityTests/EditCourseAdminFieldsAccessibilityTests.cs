3e7e4fd8a2c63c81bd50c06f1acfa0e877d8ad20
OliZor <olivia.zorn@softwire.com>
2021-08-05 11:00:15 +0100

    HEEDLS-437 Add accessibility tests for Edit Admin Course Fields

---
+++ DigitalLearningSolutions.Web.AutomatedUiTests/AccessibilityTests/EditCourseAdminFieldsAccessibilityTests.cs
@@ -0,0 +1,35 @@
+﻿namespace DigitalLearningSolutions.Web.AutomatedUiTests.AccessibilityTests
+{
+    using DigitalLearningSolutions.Web.AutomatedUiTests.TestHelpers;
+    using FluentAssertions;
+    using Selenium.Axe;
+    using Xunit;
+
+    public class EditCourseAdminFieldsAccessibilityTests : AccessibilityTestsBase
+    {
+        public EditCourseAdminFieldsAccessibilityTests(SeleniumServerFactory<Startup> factory) : base(factory) { }
+
+        [Fact]
+        public void EditRegistrationPrompt_journey_has_no_accessibility_errors()
+        {
+            // Given
+            Driver.LogUserInAsAdminAndDelegate(BaseUrl);
+            const string startUrl = "/TrackingSystem/CourseSetup/100/AdminFields/1/Edit";
+
+            // When
+            Driver.Navigate().GoToUrl(BaseUrl + startUrl);
+            ValidatePageHeading("Edit course admin field");
+            var editPageResult = new AxeBuilder(Driver).Analyze();
+
+            Driver.ClickButtonByText("Bulk edit");
+            ValidatePageHeading("Configure answers in bulk");
+            var bulkAdditionResult = new AxeBuilder(Driver).Analyze();
+
+            Driver.ClickButtonByText("Next");
+            ValidatePageHeading("Edit delegate registration prompt");
+
+            editPageResult.Violations.Should().BeEmpty();
+            bulkAdditionResult.Violations.Should().BeEmpty();
+        }
+    }
+}
