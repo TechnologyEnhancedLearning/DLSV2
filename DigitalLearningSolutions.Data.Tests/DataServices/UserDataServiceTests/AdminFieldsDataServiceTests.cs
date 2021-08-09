51317d2f0f25f526c21668d682b5b92c38264255
OliZor <olivia.zorn@softwire.com>
2021-08-09 12:49:40 +0100

    HEEDLS-439 Separate test into new file and change customisationId

---
+++ DigitalLearningSolutions.Data.Tests/DataServices/UserDataServiceTests/AdminFieldsDataServiceTests.cs
@@ -0,0 +1,18 @@
+﻿namespace DigitalLearningSolutions.Data.Tests.DataServices.UserDataServiceTests
+{
+    using FluentAssertions;
+    using NUnit.Framework;
+
+    public partial class UserDataServiceTests
+    {
+        [Test]
+        public void GetAdminCountWithAnswerForPrompt_returns_expected_count()
+        {
+            // When
+            var count = userDataService.GetAdminCountWithAnswerForPrompt(357, 1);
+
+            // Then
+            count.Should().Be(1);
+        }
+    }
+}
