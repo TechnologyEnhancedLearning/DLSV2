aaa6953133f2b68d7424a9c8ebd5437f565b0f0b
OliZor <olivia.zorn@softwire.com>
2021-08-10 08:43:36 +0100

    HEEDLS-439 Rename count method

--- DigitalLearningSolutions.Data.Tests/DataServices/UserDataServiceTests/AdminFieldsDataServiceTests.cs
+++ DigitalLearningSolutions.Data.Tests/DataServices/UserDataServiceTests/AdminFieldsDataServiceTests.cs
@@ -6,10 +6,10 @@
     public partial class UserDataServiceTests
     {
         [Test]
-        public void GetAdminCountWithAnswerForPrompt_returns_expected_count()
+        public void GetDelegateCountWithAnswerForCourseAdminField_returns_expected_count()
         {
             // When
-            var count = userDataService.GetAdminCountWithAnswerForPrompt(357, 1);
+            var count = userDataService.GetDelegateCountWithAnswerForCourseAdminField(357, 1);
 
             // Then
             count.Should().Be(1);
