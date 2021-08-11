1db382ba9a95159745ddc53b457329dd6ba2af71
Jonathan Bloxsom <jonathan.bloxsom@softwire.com>
2021-08-11 12:08:43 +0100

    HEEDLS-541 service tests

--- DigitalLearningSolutions.Data.Tests/Services/ActivityServiceTests.cs
+++ DigitalLearningSolutions.Data.Tests/Services/ActivityServiceTests.cs
@@ -4,6 +4,7 @@
     using System.Collections.Generic;
     using System.Linq;
     using DigitalLearningSolutions.Data.DataServices;
+    using DigitalLearningSolutions.Data.Enums;
     using DigitalLearningSolutions.Data.Models.TrackingSystem;
     using DigitalLearningSolutions.Data.Services;
     using FakeItEasy;
@@ -25,38 +26,55 @@
         }
 
         [Test]
-        public void GetRecentActivity_gets_recent_activity()
+        public void GetFilteredActivity_gets_correct_activity()
         {
             // given
-            var expectedActivityResult = new List<MonthOfActivity>{new MonthOfActivity()};
-            A.CallTo(() => activityDataService.GetActivityInRangeByMonth(A<int>._, A<DateTime>._, A<DateTime>._))
+            var expectedActivityResult = new List<ActivityLog> {new ActivityLog
+            {
+                Completed = 1,
+                Evaluated = 0,
+                Registered = 0,
+                LogDate = DateTime.Parse("2015-12-22"),
+                LogYear = 2015,
+                LogQuarter = 4,
+                LogMonth = 12
+            }};
+            A.CallTo(() => activityDataService.GetRawActivity(A<int>._, A<ActivityFilterData>._))
                 .Returns(expectedActivityResult);
             GivenCurrentTimeIs(DateTime.Parse("2015-12-22 06:52:09.080"));
 
             // when
-            var result = activityService.GetRecentActivity(101).ToList();
+            var filterData = new ActivityFilterData
+            {
+                StartDate = DateTime.Parse("2015-6-22"),
+                EndDate = DateTime.Parse("2016-6-22"),
+                ReportInterval = ReportInterval.Months
+            };
+            var result = activityService.GetFilteredActivity(101, filterData).ToList();
 
             // then
-            A.CallTo(() => activityDataService.GetActivityInRangeByMonth(A<int>._, A<DateTime>._, A<DateTime>._))
+            A.CallTo(() => activityDataService.GetRawActivity(A<int>._, A<ActivityFilterData>._))
                 .MustHaveHappened(1, Times.Exactly);
-            result.First().Should().BeEquivalentTo(new MonthOfActivity
-            {
-                Year = 2014,
-                Month = 12,
-                Completions = 0,
-                Registrations = 0,
-                Evaluations = 0
-            });
-            result.Last().Should().BeEquivalentTo(new MonthOfActivity
-            {
-                Year = 2015,
-                Month = 12,
-                Completions = 0,
-                Registrations = 0,
-                Evaluations = 0
-            });
-            result.Count().Should().Be(13);
-            result.All(m => m.Completions == 0 && m.Evaluations == 0 && m.Registrations == 0).Should().BeTrue();
+            result.First().Should().BeEquivalentTo(new PeriodOfActivity(
+                new DateInformation { Date = DateTime.Parse("2015-6-01"), Interval = ReportInterval.Months },
+                0,
+                0,
+                0
+            ));
+            result.Last().Should().BeEquivalentTo(new PeriodOfActivity(
+                new DateInformation { Date = DateTime.Parse("2016-6-01"), Interval = ReportInterval.Months },
+                0,
+                0,
+                0
+            ));
+            result.Single(p => p.Completions == 1).Should().BeEquivalentTo(new PeriodOfActivity(
+                new DateInformation { Date = DateTime.Parse("2015-12-01"), Interval = ReportInterval.Months },
+                0,
+                1,
+                0
+            ));
+            result.Count.Should().Be(13);
+            result.All(p => p.Evaluations == 0 && p.Registrations == 0).Should().BeTrue();
         }
 
         private void GivenCurrentTimeIs(DateTime time)
