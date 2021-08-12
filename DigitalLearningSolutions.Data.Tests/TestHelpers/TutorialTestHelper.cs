4f3f7e4ff649257ef0326339bbaa444affc650b4
Daniel Bloxham <Daniel.Bloxham@softwire.com>
2021-08-12 10:07:24 +0100

    HEEDLS-440 - Moved course content logic into view model and made unit tests for it

---
+++ DigitalLearningSolutions.Data.Tests/TestHelpers/TutorialTestHelper.cs
@@ -0,0 +1,23 @@
+﻿namespace DigitalLearningSolutions.Data.Tests.TestHelpers
+{
+    using DigitalLearningSolutions.Data.Models;
+
+    public static class TutorialTestHelper
+    {
+        public static Tutorial GetDefaultTutorial(
+            int tutorialId = 1,
+            string tutorialName = "tutorial",
+            bool status = false,
+            bool diagStatus = false
+        )
+        {
+            return new Tutorial
+            {
+                TutorialId = tutorialId,
+                TutorialName = tutorialName,
+                Status = status,
+                DiagStatus = diagStatus
+            };
+        }
+    }
+}
---
+++ DigitalLearningSolutions.Web.Tests/ViewModels/TrackingSystem/CourseSetup/CourseContentViewModelTests.cs
@@ -0,0 +1,85 @@
+﻿namespace DigitalLearningSolutions.Web.Tests.ViewModels.TrackingSystem.CourseSetup
+{
+    using System.Collections.Generic;
+    using DigitalLearningSolutions.Data.Models;
+    using DigitalLearningSolutions.Data.Tests.TestHelpers;
+    using DigitalLearningSolutions.Web.ViewModels.TrackingSystem.CourseSetup.CourseContent;
+    using FluentAssertions;
+    using NUnit.Framework;
+
+    internal class CourseContentViewModelTests
+    {
+        private static readonly Tutorial DisabledTutorial = TutorialTestHelper.GetDefaultTutorial();
+
+        private static readonly Section DisabledSection =
+            new Section(1, "disabled", new List<Tutorial> { DisabledTutorial, DisabledTutorial });
+
+        [Test]
+        public void CourseHasContent_is_true_when_at_least_one_section_has_learning_assessment_tutorial()
+        {
+            // Given
+            var enabledTutorial = TutorialTestHelper.GetDefaultTutorial(status: true);
+            var enabledSection = new Section(1, "test", new List<Tutorial> { enabledTutorial, DisabledTutorial });
+
+            // When
+            var viewModel = new CourseContentViewModel(
+                1,
+                "course",
+                false,
+                new List<Section> { enabledSection, DisabledSection }
+            );
+
+            // Then
+            viewModel.CourseHasContent.Should().BeTrue();
+        }
+
+        [Test]
+        public void CourseHasContent_is_true_when_at_least_one_section_has_diagnostic_assessment_tutorial()
+        {
+            // Given
+            var enabledTutorial = TutorialTestHelper.GetDefaultTutorial(diagStatus: true);
+            var enabledSection = new Section(1, "test", new List<Tutorial> { enabledTutorial, DisabledTutorial });
+
+            // When
+            var viewModel = new CourseContentViewModel(
+                1,
+                "course",
+                false,
+                new List<Section> { enabledSection, DisabledSection }
+            );
+
+            // Then
+            viewModel.CourseHasContent.Should().BeTrue();
+        }
+
+        [Test]
+        public void CourseHasContent_is_true_when_all_tutorials_disabled_but_has_post_learning_assessment()
+        {
+            // When
+            var viewModel = new CourseContentViewModel(1, "course", true, new List<Section> { DisabledSection });
+
+            // Then
+            viewModel.CourseHasContent.Should().BeTrue();
+        }
+
+        [Test]
+        public void CourseHasContent_is_false_when_all_tutorials_disabled_and_has_no_post_learning_assessment()
+        {
+            // When
+            var viewModel = new CourseContentViewModel(1, "course", false, new List<Section> { DisabledSection });
+
+            // Then
+            viewModel.CourseHasContent.Should().BeFalse();
+        }
+
+        [Test]
+        public void CourseHasContent_is_false_when_no_sections_are_present()
+        {
+            // When
+            var viewModel = new CourseContentViewModel(1, "course", true, new List<Section>());
+
+            // Then
+            viewModel.CourseHasContent.Should().BeFalse();
+        }
+    }
+}
