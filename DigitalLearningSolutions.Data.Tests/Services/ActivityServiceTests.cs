155ac76802c0f1aa93ff6547ca132839f6aaa40c
Jonathan Bloxsom <jonathan.bloxsom@softwire.com>
2021-08-12 13:50:15 +0100

    HEEDLS-541 assertionscope in service tests

--- DigitalLearningSolutions.Data.Tests/Services/ActivityServiceTests.cs
+++ DigitalLearningSolutions.Data.Tests/Services/ActivityServiceTests.cs
@@ -9,6 +9,7 @@
     using DigitalLearningSolutions.Data.Services;
     using FakeItEasy;
     using FluentAssertions;
+    using FluentAssertions.Execution;
     using NUnit.Framework;
 
     public class ActivityServiceTests
@@ -53,28 +54,31 @@
             var result = activityService.GetFilteredActivity(101, filterData).ToList();
 
             // then
-            A.CallTo(() => activityDataService.GetRawActivity(A<int>._, A<ActivityFilterData>._))
-                .MustHaveHappened(1, Times.Exactly);
-            result.First().Should().BeEquivalentTo(new PeriodOfActivity(
-                new DateInformation { Date = DateTime.Parse("2015-6-01"), Interval = ReportInterval.Months },
-                0,
-                0,
-                0
-            ));
-            result.Last().Should().BeEquivalentTo(new PeriodOfActivity(
-                new DateInformation { Date = DateTime.Parse("2016-6-01"), Interval = ReportInterval.Months },
-                0,
-                0,
-                0
-            ));
-            result.Single(p => p.Completions == 1).Should().BeEquivalentTo(new PeriodOfActivity(
-                new DateInformation { Date = DateTime.Parse("2015-12-01"), Interval = ReportInterval.Months },
-                0,
-                1,
-                0
-            ));
-            result.Count.Should().Be(13);
-            result.All(p => p.Evaluations == 0 && p.Registrations == 0).Should().BeTrue();
+            using (new AssertionScope())
+            {
+                A.CallTo(() => activityDataService.GetRawActivity(A<int>._, A<ActivityFilterData>._))
+                    .MustHaveHappened(1, Times.Exactly);
+                result.First().Should().BeEquivalentTo(new PeriodOfActivity(
+                    new DateInformation { Date = DateTime.Parse("2015-6-01"), Interval = ReportInterval.Months },
+                    0,
+                    0,
+                    0
+                ));
+                result.Last().Should().BeEquivalentTo(new PeriodOfActivity(
+                    new DateInformation { Date = DateTime.Parse("2016-6-01"), Interval = ReportInterval.Months },
+                    0,
+                    0,
+                    0
+                ));
+                result.Single(p => p.Completions == 1).Should().BeEquivalentTo(new PeriodOfActivity(
+                    new DateInformation { Date = DateTime.Parse("2015-12-01"), Interval = ReportInterval.Months },
+                    0,
+                    1,
+                    0
+                ));
+                result.Count.Should().Be(13);
+                result.All(p => p.Evaluations == 0 && p.Registrations == 0).Should().BeTrue();
+            }
         }
 
         private void GivenCurrentTimeIs(DateTime time)
