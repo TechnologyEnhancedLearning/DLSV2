fb74cdcc7eb72d36c41cf2af4b0944abbfebf413
Daniel Bloxham <Daniel.Bloxham@softwire.com>
2021-08-18 12:57:48 +0100

    HEEDLS-497 - Changed DelegateRegistrationByCentreData constructor

--- DigitalLearningSolutions.Web/Controllers/Register/RegisterDelegateByCentreController.cs
+++ DigitalLearningSolutions.Web/Controllers/Register/RegisterDelegateByCentreController.cs
@@ -248,10 +248,7 @@ namespace DigitalLearningSolutions.Web.Controllers.Register
 
         private void SetCentreDelegateRegistrationData(int centreId)
         {
-            var centreDelegateRegistrationData = new DelegateRegistrationByCentreData(centreId)
-            {
-                WelcomeEmailDate = DateTime.Today
-            };
+            var centreDelegateRegistrationData = new DelegateRegistrationByCentreData(centreId, DateTime.Today);
             var id = centreDelegateRegistrationData.Id;
 
             Response.Cookies.Append(
