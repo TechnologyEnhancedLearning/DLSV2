4c49d6f9f45a95c77a5b0f4db3b8b3abab631193
Ibrahim Munir-Zubair <ibrahimmunir14@gmail.com>
2021-08-23 16:50:06 +0100

    HEEDLS-579 Screen reader: comma-sep roles, centre name in choose button

---
+++ DigitalLearningSolutions.Data.Tests/Models/User/CentreUserDetailsTests.cs
@@ -0,0 +1,25 @@
+﻿namespace DigitalLearningSolutions.Data.Tests.Models.User
+{
+    using DigitalLearningSolutions.Data.Models.User;
+    using FluentAssertions;
+    using NUnit.Framework;
+
+    public class CentreUserDetailsTests
+    {
+        [TestCase(true, false, "Admin")]
+        [TestCase(false, true, "Delegate")]
+        [TestCase(true, true, "Admin, Delegate")]
+        public void CentreUserDetails_sets_tags_string_correctly(
+            bool isAdmin,
+            bool isDelegate,
+            string tagString
+        )
+        {
+            // When
+            var result = new CentreUserDetails(1, "centre", isAdmin, isDelegate);
+
+            // Then
+            result.TagString.Should().Be(tagString);
+        }
+    }
+}
