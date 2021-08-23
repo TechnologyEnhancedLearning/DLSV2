5abdb92ab98fe3acfa67b378e0caa9a1605023b6
Alex Jackson <Alexander.Jackson@dorsetsoftware.com>
2021-08-23 10:21:29 +0100

    HEEDLS-516 Change the course count on the group expanders

--- DigitalLearningSolutions.Data/DataServices/GroupsDataService.cs
+++ DigitalLearningSolutions.Data/DataServices/GroupsDataService.cs
@@ -28,13 +28,24 @@
 
         public IEnumerable<Group> GetGroupsForCentre(int centreId)
         {
+            const string courseCountSql = @"SELECT COUNT(*)
+                FROM GroupCustomisations AS gc
+                JOIN Customisations AS c ON c.CustomisationID = gc.CustomisationID
+                INNER JOIN dbo.CentreApplications AS ca ON ca.ApplicationID = c.ApplicationID
+                INNER JOIN dbo.Applications AS ap ON ap.ApplicationID = ca.ApplicationID
+                WHERE gc.GroupID = g.GroupID
+                AND ca.CentreId = @centreId
+                AND gc.InactivatedDate IS NULL
+                AND ap.ArchivedDate IS NULL
+                AND c.Active = 1";
+
             return connection.Query<Group>(
-                @"SELECT
+                @$"SELECT
 	                    GroupID,
 	                    GroupLabel,
 	                    GroupDescription,
 	                    (SELECT COUNT(*) FROM GroupDelegates AS gd WHERE gd.GroupID = g.GroupID) AS DelegateCount,
-	                    (SELECT COUNT(*) FROM GroupCustomisations AS gc WHERE gc.GroupID = g.GroupID AND InactivatedDate IS NULL) AS CoursesCount,
+	                    ({courseCountSql}) AS CoursesCount,
 	                    au.Forename AS AddedByFirstName,
 	                    au.Surname AS AddedByLastName,
 	                    LinkedToField,
