d9b19c926d5d36877b8331eb2ebf82f668f3e47a
Ibrahim Munir-Zubair <ibrahimmunir14@gmail.com>
2021-08-24 18:55:46 +0100

    HEEDLS-584 Create send emails page with relevant permissions, navbar, and list of delegates

---
+++ DigitalLearningSolutions.Web/Controllers/TrackingSystem/Delegates/EmailDelegatesController.cs
@@ -0,0 +1,31 @@
+﻿namespace DigitalLearningSolutions.Web.Controllers.TrackingSystem.Delegates
+{
+    using DigitalLearningSolutions.Data.Services;
+    using DigitalLearningSolutions.Web.Helpers;
+    using DigitalLearningSolutions.Web.ViewModels.TrackingSystem.Delegates.EmailDelegates;
+    using Microsoft.AspNetCore.Authorization;
+    using Microsoft.AspNetCore.Mvc;
+    using Microsoft.FeatureManagement.Mvc;
+
+    [FeatureGate(FeatureFlags.RefactoredTrackingSystem)]
+    [Authorize(Policy = CustomPolicies.UserCentreAdmin)]
+    [Route("TrackingSystem/Delegates/Email")]
+    public class EmailDelegatesController : Controller
+    {
+        private readonly IUserService userService;
+
+        public EmailDelegatesController(IUserService userService)
+        {
+            this.userService = userService;
+        }
+
+        public IActionResult Index()
+        {
+            var centreId = User.GetCentreId();
+            var delegateUsers = userService.GetDelegateUserCardsForWelcomeEmail(centreId);
+            var model = new EmailDelegatesViewModel(delegateUsers);
+
+            return View(model);
+        }
+    }
+}
---
+++ DigitalLearningSolutions.Web/ViewModels/TrackingSystem/Delegates/EmailDelegates/EmailDelegatesViewModel.cs
@@ -0,0 +1,16 @@
+﻿namespace DigitalLearningSolutions.Web.ViewModels.TrackingSystem.Delegates.EmailDelegates
+{
+    using System.Collections.Generic;
+    using System.Linq;
+    using DigitalLearningSolutions.Data.Models.User;
+
+    public class EmailDelegatesViewModel
+    {
+        public EmailDelegatesViewModel(IEnumerable<DelegateUserCard> delegateUserCards)
+        {
+            Delegates = delegateUserCards.Select(delegateUser => new EmailDelegatesItemViewModel(delegateUser));
+        }
+
+        public IEnumerable<EmailDelegatesItemViewModel> Delegates { get; set; }
+    }
+}
