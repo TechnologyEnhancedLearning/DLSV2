f2ec85291d02155003b2a7f4157aaae567378eeb
Ibrahim Munir-Zubair <ibrahimmunir14@gmail.com>
2021-09-08 13:00:40 +0100

    HEEDLS-505 Remove full stops from error messages

--- DigitalLearningSolutions.Web/ViewModels/TrackingSystem/Delegates/UploadDelegatesViewModel.cs
+++ DigitalLearningSolutions.Web/ViewModels/TrackingSystem/Delegates/UploadDelegatesViewModel.cs
@@ -17,9 +17,9 @@
             Year = welcomeEmailDate.Year;
         }
 
-        [Required(ErrorMessage = "Delegates update file is required.")]
-        [AllowedExtensions(new[] { ".xlsx" }, "Delegates update file must be in xlsx format.")]
-        [MaxFileSize(5*1000*1000, "Maximum allowed file size is 5000KB.")]
+        [Required(ErrorMessage = "Delegates update file is required")]
+        [AllowedExtensions(new[] { ".xlsx" }, "Delegates update file must be in xlsx format")]
+        [MaxFileSize(5*1000*1000, "Maximum allowed file size is 5000KB")]
         public IFormFile? DelegatesFile { get; set; }
 
         public DateTime? GetWelcomeEmailDate()
