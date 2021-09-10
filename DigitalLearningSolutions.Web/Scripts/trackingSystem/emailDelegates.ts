91b504c024fa0732c943363e5dd4779e360d0359
Ibrahim Munir-Zubair <ibrahimmunir14@gmail.com>
2021-09-10 20:18:40 +0100

    HEEDLS-585 Add Select/Deselect buttons with JS functionality
    No non-JS functionality yet

--- DigitalLearningSolutions.Web/Scripts/trackingSystem/emailDelegates.ts
+++ DigitalLearningSolutions.Web/Scripts/trackingSystem/emailDelegates.ts
@@ -7,3 +7,16 @@ const route = `TrackingSystem/Delegates/Email/AllEmailDelegateItems${queryString
 
 // eslint-disable-next-line no-new
 new SearchSortFilterAndPaginate(route, true, 'EmailDelegateFilter', false, false, false);
+
+const selectAll = () => {
+  const allCheckboxes = document.querySelectorAll('.delegate-checkbox') as NodeListOf<HTMLInputElement>;
+  allCheckboxes.forEach((checkbox) => { if (!checkbox.checked) checkbox.click(); });
+};
+const deselectAll = () => {
+  const allCheckboxes = document.querySelectorAll('.delegate-checkbox') as NodeListOf<HTMLInputElement>;
+  allCheckboxes.forEach((checkbox) => { if (checkbox.checked) checkbox.click(); });
+};
+const selectAllButton = document.getElementById('select-all-button') as HTMLButtonElement;
+const deselectAllButton = document.getElementById('deselect-all-button') as HTMLButtonElement;
+selectAllButton.addEventListener('click', selectAll);
+deselectAllButton.addEventListener('click', deselectAll);
