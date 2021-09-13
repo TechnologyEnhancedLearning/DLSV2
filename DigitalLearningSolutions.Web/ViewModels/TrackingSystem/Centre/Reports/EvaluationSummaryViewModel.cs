b39d03a8baa8037f7df804e781b65428c07dbecf
Ibrahim Munir-Zubair <ibrahimmunir14@gmail.com>
2021-09-13 17:56:39 +0100

    HEEDLS-462 Create EvaluationSummaryData model and helper to map to list of EvaluationSummaryVMs

--- DigitalLearningSolutions.Web/ViewModels/TrackingSystem/Centre/Reports/EvaluationSummaryViewModel.cs
+++ DigitalLearningSolutions.Web/ViewModels/TrackingSystem/Centre/Reports/EvaluationSummaryViewModel.cs
@@ -1,13 +1,20 @@
 ﻿namespace DigitalLearningSolutions.Web.ViewModels.TrackingSystem.Centre.Reports
 {
     using System.Collections.Generic;
+    using System.Linq;
 
     public class EvaluationSummaryViewModel
     {
-        public EvaluationSummaryViewModel(string question, Dictionary<string, float>? data)
+        public EvaluationSummaryViewModel(string question, Dictionary<string, int> counts)
         {
             Question = question;
-            Data = data;
+            var totalResponses = counts.Sum(x => x.Value);
+            if (totalResponses != 0)
+            {
+                Data = counts
+                    .Select(x => new KeyValuePair<string, float>(x.Key, 100f * x.Value / totalResponses))
+                    .ToDictionary(x => x.Key, x => x.Value);
+            }
         }
 
         public string Question { get; set; }
