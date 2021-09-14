9635b3afb7998f3a104644fe520e7ab1294bf9b1
Alex Jackson <Alexander.Jackson@dorsetsoftware.com>
2021-09-14 15:58:06 +0100

    HEEDLS-494 Add new Set Delegate Password pages

---
+++ DigitalLearningSolutions.Web/Controllers/TrackingSystem/Delegates/SetDelegatePasswordController.cs
@@ -0,0 +1,61 @@
+﻿namespace DigitalLearningSolutions.Web.Controllers.TrackingSystem.Delegates
+{
+    using DigitalLearningSolutions.Data.DataServices.UserDataService;
+    using DigitalLearningSolutions.Data.Exceptions;
+    using DigitalLearningSolutions.Data.Services;
+    using DigitalLearningSolutions.Web.Helpers;
+    using DigitalLearningSolutions.Web.ViewModels.TrackingSystem.Delegates.SetDelegatePassword;
+    using Microsoft.AspNetCore.Authorization;
+    using Microsoft.AspNetCore.Mvc;
+    using Microsoft.FeatureManagement.Mvc;
+    using System.Threading.Tasks;
+
+    [FeatureGate(FeatureFlags.RefactoredTrackingSystem)]
+    [Authorize(Policy = CustomPolicies.UserCentreAdmin)]
+    [Route("TrackingSystem/Delegates/SetDelegatePassword/{delegateId:int}")]
+    public class SetDelegatePasswordController : Controller
+    {
+        private readonly IPasswordService passwordService;
+        private readonly IUserDataService userDataService;
+
+        public SetDelegatePasswordController(IPasswordService passwordService, IUserDataService userDataService)
+        {
+            this.passwordService = passwordService;
+            this.userDataService = userDataService;
+        }
+
+        [HttpGet]
+        public IActionResult Index(int delegateId)
+        {
+            var delegateUser = userDataService.GetDelegateUserById(delegateId);
+
+            if (delegateUser == null || delegateUser.CentreId != User.GetCentreId())
+            {
+                return NotFound();
+            }
+
+            if (string.IsNullOrWhiteSpace(delegateUser.EmailAddress))
+            {
+                throw new NoDelegateEmailException();
+            }
+
+            var model = new SetDelegatePasswordViewModel(delegateUser.FullName);
+
+            return View(model);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> IndexAsync(SetDelegatePasswordViewModel model, int delegateId)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            var delegateUser = userDataService.GetDelegateUserById(delegateId);
+            await passwordService.ChangePasswordAsync(delegateUser!.EmailAddress!, model.Password!);
+
+            return View("Confirmation");
+        }
+    }
+}
---
+++ DigitalLearningSolutions.Web/ViewModels/TrackingSystem/Delegates/SetDelegatePassword/SetDelegatePasswordViewModel.cs
@@ -0,0 +1,16 @@
+﻿namespace DigitalLearningSolutions.Web.ViewModels.TrackingSystem.Delegates.SetDelegatePassword
+{
+    using DigitalLearningSolutions.Web.ViewModels.Common;
+
+    public class SetDelegatePasswordViewModel : PasswordViewModel
+    {
+        public SetDelegatePasswordViewModel() { }
+
+        public SetDelegatePasswordViewModel(string name)
+        {
+            Name = name;
+        }
+
+        public string Name { get; set; }
+    }
+}
