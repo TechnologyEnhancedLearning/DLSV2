2705e8a49027e94238d4e6dc555bf320df5bce3e
Alex Jackson <Alexander.Jackson@dorsetsoftware.com>
2021-09-15 12:21:21 +0100

    HEEDLS-563 Add unit tests to check standard date time formats

---
+++ DigitalLearningSolutions.Web.Tests/Helpers/DateHelperTests.cs
@@ -0,0 +1,36 @@
+﻿namespace DigitalLearningSolutions.Web.Tests.Helpers
+{
+    using System;
+    using DigitalLearningSolutions.Web.Helpers;
+    using FluentAssertions;
+    using NUnit.Framework;
+
+    public class DateHelperTests
+    {
+        [Test]
+        public void StandardDateFormat_has_expected_format()
+        {
+            // Given
+            var date = new DateTime(2021, 2, 1, 15, 30, 12);
+
+            // When
+            var result = date.ToString(DateHelper.StandardDateFormat);
+
+            // Then
+            result.Should().Be("01/02/2021");
+        }
+
+        [Test]
+        public void StandardDateAndTimeFormat_has_expected_format()
+        {
+            // Given
+            var date = new DateTime(2021, 2, 1, 15, 30, 12);
+
+            // When
+            var result = date.ToString(DateHelper.StandardDateAndTimeFormat);
+
+            // Then
+            result.Should().Be("01/02/2021 15:30");
+        }
+    }
+}
