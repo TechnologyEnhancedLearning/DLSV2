bf0e53185fdac4de97dbf26dd722d0d5cb148aec
Alex Jackson <Alexander.Jackson@dorsetsoftware.com>
2021-09-20 16:43:25 +0100

    HEEDLS-441 No javascript Select/Deselect buttons

--- DigitalLearningSolutions.Web/Controllers/TrackingSystem/CourseSetup/CourseContentController.cs
+++ DigitalLearningSolutions.Web/Controllers/TrackingSystem/CourseSetup/CourseContentController.cs
@@ -15,6 +15,12 @@
     [Route("/TrackingSystem/CourseSetup/{customisationId:int}/Content")]
     public class CourseContentController : Controller
     {
+        public const string SelectAllDiagnosticPrefix = "diagnostic-select-all-";
+        public const string DeselectAllDiagnosticPrefix = "diagnostic-deselect-all-";
+        public const string SelectAllLearningPrefix = "learning-select-all-";
+        public const string DeselectAllLearningPrefix = "learning-deselect-all-";
+        public const string SaveAction = "Save";
+
         private readonly ICourseDataService courseDataService;
         private readonly ISectionService sectionService;
 
@@ -86,7 +92,73 @@
 
         [HttpPost]
         [Route("Edit")]
-        public IActionResult Edit(EditCourseContentViewModel model, int customisationId)
+        public IActionResult Edit(EditCourseContentViewModel model, int customisationId, string action)
+        {
+            return action == SaveAction
+                ? EditSave(model, customisationId)
+                : ProcessBulkSelect(model, customisationId, action);
+        }
+
+        private IActionResult ProcessBulkSelect(EditCourseContentViewModel model, int customisationId, string action)
+        {
+            var lastDashIndex = action.LastIndexOf('-') + 1;
+            var actionPrefix = action.Substring(0, lastDashIndex);
+            var sectionId = int.Parse(action.Substring(lastDashIndex));
+
+            switch (actionPrefix)
+            {
+                case SelectAllDiagnosticPrefix:
+                    SelectAllDiagnosticsInSection(sectionId, model);
+                    break;
+                case DeselectAllDiagnosticPrefix:
+                    DeselectAllDiagnosticsInSection(sectionId, model);
+                    break;
+                case SelectAllLearningPrefix:
+                    SelectAllLearningInSection(sectionId, model);
+                    break;
+                case DeselectAllLearningPrefix:
+                    DeselectAllLearningInSection(sectionId, model);
+                    break;
+                default:
+                    return new StatusCodeResult(500);
+            }
+
+            return View(model);
+        }
+
+        public void SelectAllDiagnosticsInSection(int sectionId, EditCourseContentViewModel model)
+        {
+           foreach (var tutorial in model.Sections.Single(s => s.SectionId == sectionId).Tutorials)
+           {
+               tutorial.DiagnosticEnabled = true;
+           }
+        }
+
+        public void DeselectAllDiagnosticsInSection(int sectionId, EditCourseContentViewModel model)
+        {
+            foreach (var tutorial in model.Sections.Single(s => s.SectionId == sectionId).Tutorials)
+            {
+                tutorial.DiagnosticEnabled = false;
+            }
+        }
+
+        public void SelectAllLearningInSection(int sectionId, EditCourseContentViewModel model)
+        {
+            foreach (var tutorial in model.Sections.Single(s => s.SectionId == sectionId).Tutorials)
+            {
+                tutorial.LearningEnabled = true;
+            }
+        }
+
+        public void DeselectAllLearningInSection(int sectionId, EditCourseContentViewModel model)
+        {
+            foreach (var tutorial in model.Sections.Single(s => s.SectionId == sectionId).Tutorials)
+            {
+                tutorial.LearningEnabled = false;
+            }
+        }
+
+        private IActionResult EditSave(EditCourseContentViewModel model, int customisationId)
         {
             var sections = model.Sections.Select(
                 s => new Section(
