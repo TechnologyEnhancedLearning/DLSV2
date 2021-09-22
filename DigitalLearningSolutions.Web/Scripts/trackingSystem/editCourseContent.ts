2587c9a680b42baff20f763a4801bd6efbc31c35
Alex Jackson <Alexander.Jackson@dorsetsoftware.com>
2021-09-22 15:14:37 +0100

    HEEDLS-441 Add javascript select all

---
+++ DigitalLearningSolutions.Web/Scripts/trackingSystem/editCourseContent.ts
@@ -0,0 +1,44 @@
+﻿function selectAll(selectorClass: string): void {
+  const allCheckboxes = document.querySelectorAll(selectorClass)as NodeListOf<HTMLInputElement>;
+  allCheckboxes.forEach((checkbox) => {
+    if (!checkbox.checked) checkbox.click();
+  });
+}
+
+function deselectAll(selectorClass: string): void {
+  const allCheckboxes = document.querySelectorAll(selectorClass) as NodeListOf<HTMLInputElement>;
+  allCheckboxes.forEach((checkbox) => {
+    if (checkbox.checked) checkbox.click();
+  });
+}
+
+function setUpSelectAllButtons() {
+  const submits = Array.from(
+    document.querySelectorAll('.select-all-button'),
+  );
+  submits.forEach((submit) => {
+    const element = <HTMLInputElement>submit;
+    element.addEventListener('click', (e) => {
+      e.preventDefault();
+      const checkboxSelectorClass = `.${element.value.replace('select-all', 'checkbox')}`;
+      selectAll(checkboxSelectorClass);
+    });
+  });
+}
+
+function setUpDeselectAllButtons() {
+  const submits = Array.from(
+    document.querySelectorAll('.deselect-all-button'),
+  );
+  submits.forEach((submit) => {
+    const element = <HTMLInputElement>submit;
+    element.addEventListener('click', (e) => {
+      e.preventDefault();
+      const checkboxSelectorClass = `.${element.value.replace('deselect-all', 'checkbox')}`;
+      deselectAll(checkboxSelectorClass);
+    });
+  });
+}
+
+setUpSelectAllButtons();
+setUpDeselectAllButtons();
