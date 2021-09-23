08522a482c4cd56fecb14a6ce6df2d5f20de2bb6
David May-Miller <david.may-miller@softwire.com>
2021-09-23 14:47:04 +0100

    HEEDLS-509 Add test for GroupsService

---
+++ DigitalLearningSolutions.Data.Tests/Services/GroupsServiceTests.cs
@@ -0,0 +1,52 @@
+﻿namespace DigitalLearningSolutions.Data.Tests.Services
+{
+    using System;
+    using DigitalLearningSolutions.Data.DataServices;
+    using DigitalLearningSolutions.Data.Models.DelegateGroups;
+    using DigitalLearningSolutions.Data.Services;
+    using FakeItEasy;
+    using FluentAssertions;
+    using NUnit.Framework;
+
+    public class GroupsServiceTests
+    {
+        private IGroupsDataService groupsDataService = null!;
+        private IClockService clockService = null!;
+        private GroupsService groupsService = null!;
+
+        [SetUp]
+        public void Setup()
+        {
+            groupsDataService = A.Fake<IGroupsDataService>();
+            clockService = A.Fake<IClockService>();
+            groupsService = new GroupsService(groupsDataService, clockService);
+        }
+
+        [Test]
+        public void AddDelegateGroup_sets_GroupDetails_correctly()
+        {
+            // Given
+            const int centreId = 101;
+            const string groupLabel = "Group name";
+            const string groupDescription = "Group description";
+            const int adminUserId = 1;
+            var timeNow = DateTime.UtcNow;
+            GivenCurrentTimeIs(timeNow);
+
+            const int returnId = 1;
+            A.CallTo(() => groupsDataService.AddDelegateGroup(A<GroupDetails>._)).Returns(returnId);
+
+            // When
+            var result = groupsService.AddDelegateGroup(centreId, groupLabel, groupDescription, adminUserId);
+
+            // Then
+            result.Should().Be(returnId);
+            A.CallTo(() => groupsDataService.AddDelegateGroup(A<GroupDetails>._)).MustHaveHappenedOnceExactly();
+        }
+
+        private void GivenCurrentTimeIs(DateTime validationTime)
+        {
+            A.CallTo(() => clockService.UtcNow).Returns(validationTime);
+        }
+    }
+}
