658b6653b965eb43aad7b753decdefbebc345c8f
OliZor <olivia.zorn@softwire.com>
2021-09-29 23:20:11 +0100

    HEEDLS-438 Add HTML helpers for indicating which nav menu tab is selected

--- DigitalLearningSolutions.Web/Extensions/HtmlHelperExtensions.cs
+++ DigitalLearningSolutions.Web/Extensions/HtmlHelperExtensions.cs
@@ -1,10 +1,12 @@
 ﻿namespace DigitalLearningSolutions.Web.Extensions
 {
+    using System.Collections.Generic;
+    using System.Linq;
     using Microsoft.AspNetCore.Mvc.Rendering;
 
     public static class HtmlHelperExtensions
     {
-        public static string IsSelected(
+        public static string IsActionSelected(
             this IHtmlHelper htmlHelper,
             string action,
             string selectedCssClass = "selected",
@@ -15,6 +17,59 @@
             var currentAction = htmlHelper.ViewContext.RouteData.Values["action"] as string;
 
             return (controller == null || controller == currentController) && action == currentAction
+                ? selectedCssClass
+                : string.Empty;
+        }
+
+        public static string IsOneOfActionsSelected(
+            this IHtmlHelper htmlHelper,
+            IEnumerable<string> actions,
+            string selectedCssClass = "selected",
+            string? controller = null
+        )
+        {
+            var currentController = htmlHelper.ViewContext.RouteData.Values["controller"] as string;
+            var currentAction = htmlHelper.ViewContext.RouteData.Values["action"] as string;
+
+            return (controller == null || controller == currentController) && actions.Contains(currentAction)
+                ? selectedCssClass
+                : string.Empty;
+        }
+
+        public static string IsControllerSelected(
+            this IHtmlHelper htmlHelper,
+            string? controller,
+            string selectedCssClass = "selected"
+        )
+        {
+            var currentController = htmlHelper.ViewContext.RouteData.Values["controller"] as string;
+
+            return (controller == currentController)
+                ? selectedCssClass
+                : string.Empty;
+        }
+
+        public static string IsOneOfControllersSelected(
+            this IHtmlHelper htmlHelper,
+            IEnumerable<string> controllers,
+            string selectedCssClass = "selected"
+        )
+        {
+            var currentController = htmlHelper.ViewContext.RouteData.Values["controller"] as string;
+
+            return (controllers.Contains(currentController))
+                ? selectedCssClass
+                : string.Empty;
+        }
+
+        public static string IsMyAccountTabSelected(
+            this IHtmlHelper htmlHelper,
+            string selectedCssClass = "selected"
+        )
+        {
+            var currentController = htmlHelper.ViewContext.RouteData.Values["controller"] as string;
+
+            return (currentController == "MyAccount" || currentController == "ChangePassword" || currentController == "NotificationPreferences")
                 ? selectedCssClass
                 : string.Empty;
         }
