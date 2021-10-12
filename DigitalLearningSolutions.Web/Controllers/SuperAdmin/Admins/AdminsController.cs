854c059747e7dd67481d53c2e852996b3414a12e
Alex Jackson <Alexander.Jackson@dorsetsoftware.com>
2021-10-12 16:34:35 +0100

    HEEDLS-618 New controller, feature flag and authorisation policy

---
+++ DigitalLearningSolutions.Web/Controllers/SuperAdmin/Admins/AdminsController.cs
@@ -0,0 +1,18 @@
+﻿namespace DigitalLearningSolutions.Web.Controllers.SuperAdmin.Admins
+{
+    using DigitalLearningSolutions.Web.Helpers;
+    using Microsoft.AspNetCore.Authorization;
+    using Microsoft.AspNetCore.Mvc;
+    using Microsoft.FeatureManagement.Mvc;
+
+    [FeatureGate(FeatureFlags.RefactoredSuperAdminInterface)]
+    [Authorize(Policy = CustomPolicies.UserSuperAdmin)]
+    [Route("SuperAdmin/Admins")]
+    public class AdminsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
