a8f6b29be0b982386eb3b45d628cb4e36bb14cd5
kevwhitt-hee <kevin.whittaker@hee.nhs.uk>
2021-10-12 16:10:51 +0100

    DLSV2-369 Migration to update SelfAssessmentStructure ordering where necessary

---
+++ DigitalLearningSolutions.Data.Migrations/202110121601_UpdateSelfAssessmentStructureOrdering.cs
@@ -0,0 +1,22 @@
+﻿namespace DigitalLearningSolutions.Data.Migrations
+{
+    using FluentMigrator;
+    [Migration(202110121601)]
+    public class UpdateSelfAssessmentStructureOrdering : Migration
+    {
+        public override void Up()
+        {
+            Execute.Sql(@"WITH T AS (SELECT ID, SelfAssessmentID, CompetencyID, Ordering, ROW_NUMBER() OVER(Partition By SelfAssessmentID Order By ID) As NewOrdering
+                            FROM            SelfAssessmentStructure
+                            WHERE Ordering = 1)
+                            UPDATE SelfAssessmentStructure
+                            SET Ordering = T.NewOrdering
+                            FROM SelfAssessmentStructure AS sas INNER JOIN T ON T.ID = sas.ID
+                            WHERE T.NewOrdering > 1");
+        }
+        public override void Down()
+        {
+
+        }
+    }
+}
