a7aaf3d33b25abd03259b7ef848c99e3c4958803
kevwhitt-hee <kevin.whittaker@hee.nhs.uk>
2021-10-13 15:34:56 +0100

    DLSV2-370 and DLSV2-371 Migration to add sign off statement fields

---
+++ DigitalLearningSolutions.Data.Migrations/202110131529_AddSignOffStatementFieldsToSelfAssessments.cs
@@ -0,0 +1,13 @@
+﻿namespace DigitalLearningSolutions.Data.Migrations
+{
+    using FluentMigrator;
+    [Migration(202110131529)]
+    public class AddSignOffStatementFieldsToSelfAssessments : AutoReversingMigration
+    {
+        public override void Up()
+        {
+            Alter.Table("SelfAssessments").AddColumn("SignOffRequestorStatement").AsString(1000).Nullable()
+                .AddColumn("SignOffSupervisorStatement").AsString(1000).Nullable();
+        }
+    }
+}
