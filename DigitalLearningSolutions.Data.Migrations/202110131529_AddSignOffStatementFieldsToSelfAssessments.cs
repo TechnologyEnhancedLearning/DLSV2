5d9fda6f291a5f611a25ea5d7cc8f320583f8131
kevwhitt-hee <kevin.whittaker@hee.nhs.uk>
2021-10-13 15:42:14 +0100

    DLSV2-370 and DLSV2-371 Tweaks migration to add manual down migration
    Needed to tweak length of fields for consistency

--- DigitalLearningSolutions.Data.Migrations/202110131529_AddSignOffStatementFieldsToSelfAssessments.cs
+++ DigitalLearningSolutions.Data.Migrations/202110131529_AddSignOffStatementFieldsToSelfAssessments.cs
@@ -2,12 +2,19 @@
 {
     using FluentMigrator;
     [Migration(202110131529)]
-    public class AddSignOffStatementFieldsToSelfAssessments : AutoReversingMigration
+    public class AddSignOffStatementFieldsToSelfAssessments : Migration
     {
-        public override void Up()
+       public override void Up()
         {
             Alter.Table("SelfAssessments").AddColumn("SignOffRequestorStatement").AsString(1000).Nullable()
-                .AddColumn("SignOffSupervisorStatement").AsString(1000).Nullable();
+                .AddColumn("SignOffSupervisorStatement").AsString(1000).Nullable()
+                .AlterColumn("ManageOptionalCompetenciesPrompt").AsString(1000).Nullable();
+        }
+        public override void Down()
+        {
+            Alter.Table("SelfAssessments").AlterColumn("ManageOptionalCompetenciesPrompt").AsString(500).Nullable();
+            Delete.Column("SignOffRequestorStatement").FromTable("SelfAssessments");
+            Delete.Column("SignOffSupervisorStatement").FromTable("SelfAssessments");
         }
     }
 }
