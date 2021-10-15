2ab1d124bab9176c4347f4b01b9ef75cbf7ad773
OliZor <olivia.zorn@softwire.com>
2021-10-15 10:14:53 +0100

    HEEDLS-615 Remove RouteParamKeys

--- DigitalLearningSolutions.Web/Attributes/ValidateAllowedDlsSubApplicationAttribute.cs
+++ DigitalLearningSolutions.Web/Attributes/ValidateAllowedDlsSubApplicationAttribute.cs
@@ -13,7 +13,7 @@
     {
         private readonly string applicationArgumentName;
 
-        public ValidateAllowedDlsSubApplicationAttribute(string applicationArgumentName = RouteParamKeys.DlsSubApplication)
+        public ValidateAllowedDlsSubApplicationAttribute(string applicationArgumentName = "dlsSubApplication")
         {
             this.applicationArgumentName = applicationArgumentName;
         }
--- DigitalLearningSolutions.Web/Helpers/RouteParamKeys.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace DigitalLearningSolutions.Web.Helpers
-{
-    public static class RouteParamKeys
-    {
-        public const string DlsSubApplication = "dlsSubApplication";
-    }
-}
