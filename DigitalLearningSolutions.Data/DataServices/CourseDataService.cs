7b4f1b48aaa64bd9dde5e52159579194ee75147d
Jonathan Bloxsom <jonathan.bloxsom@softwire.com>
2021-10-15 17:01:18 +0100

    HEEDLS-541 remove redundant join

--- DigitalLearningSolutions.Data/DataServices/CourseDataService.cs
+++ DigitalLearningSolutions.Data/DataServices/CourseDataService.cs
@@ -394,7 +394,6 @@ namespace DigitalLearningSolutions.Data.DataServices
                     INNER JOIN Progress AS p ON p.CandidateID = cn.CandidateID
                     INNER JOIN Customisations AS c ON c.CustomisationID = p.CustomisationId
                     INNER JOIN dbo.Applications AS ap ON ap.ApplicationID = c.ApplicationID
-                    LEFT JOIN dbo.CentreApplications AS ca ON ca.ApplicationID = ap.ApplicationID
                     WHERE cn.CentreID = @centreID
 	                AND (ap.CourseCategoryID = @categoryId OR @categoryId IS NULL)
                     AND ap.ArchivedDate IS NULL",
