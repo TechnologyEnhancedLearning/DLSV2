259e85a715e59383a0aa02a604f67f9d92b8f91f
kevwhitt-hee <kevin.whittaker@hee.nhs.uk>
2021-10-20 15:14:46 +0100

    DLSV2-384 Switches to using url navigation in LP accessibility tests

--- DigitalLearningSolutions.Web.AutomatedUiTests/AccessibilityTests/LearningPortalAccessibilityTests.cs
+++ DigitalLearningSolutions.Web.AutomatedUiTests/AccessibilityTests/LearningPortalAccessibilityTests.cs
@@ -14,25 +14,35 @@
         public void LearningPortal_interface_has_no_accessibility_errors()
         {
             {
+                // Given
                 Driver.LogUserInAsAdminAndDelegate(BaseUrl);
                 const string startUrl = "/LearningPortal/Current";
+                const string completedUrl = "/LearningPortal/Completed";
+                const string availableUrl = "/LearningPortal/Available";
+                const string completeByUrl = "/LearningPortal/Current/CompleteBy/19262";
 
                 // When
                 Driver.Navigate().GoToUrl(BaseUrl + startUrl);
                 ValidatePageHeading("My Current Activities");
                 var currentActivitiesResult = new AxeBuilder(Driver).Analyze();
 
-                Driver.ClickLinkContainingText("Completed activities");
+                Driver.Navigate().GoToUrl(BaseUrl + completedUrl);
                 ValidatePageHeading("My Completed Activities");
                 var completedActivitiesResult = new AxeBuilder(Driver).Analyze();
 
-                Driver.ClickLinkContainingText("Available activities");
+                Driver.Navigate().GoToUrl(BaseUrl + availableUrl);
                 ValidatePageHeading("Available Activities");
                 var availableActivitiesResult = new AxeBuilder(Driver).Analyze();
 
+                Driver.Navigate().GoToUrl(BaseUrl + completeByUrl);
+                ValidatePageHeading("Enter a completion date for Excel 2013 for the Workplace - Testing");
+                var completeByResult = new AxeBuilder(Driver).Analyze();
+
+                //Then
                 currentActivitiesResult.Violations.Should().BeEmpty();
                 completedActivitiesResult.Violations.Should().BeEmpty();
                 availableActivitiesResult.Violations.Should().BeEmpty();
+                completeByResult.Violations.Should().BeEmpty();
             }
         }
     }
