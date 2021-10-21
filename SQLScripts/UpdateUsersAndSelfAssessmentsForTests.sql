fd82ea763f04e1208e7c2f3ad995a81a1b9f2d2c
Steve Jackson <stephen.jackson@softwire.com>
2021-10-21 08:09:16 +0100

    Make new test data SQL script idempotent

--- SQLScripts/UpdateUsersAndSelfAssessmentsForTests.sql
+++ SQLScripts/UpdateUsersAndSelfAssessmentsForTests.sql
@@ -4,7 +4,10 @@ UPDATE Candidates SET EmailAddress = 'kevin.whittaker1@nhs.net', FirstName = 'xx
 GO
 UPDATE Candidates SET Password = 'ADyLcAmuAkPwMkZW+ivvk/GCq/0yn0m08eP2hIPPvjKJwmvj6pBfmDrTv16tMz8xww==' WHERE CandidateID = 254480
 GO
-INSERT INTO CandidateAssessments (CandidateID, SelfAssessmentID, StartedDate) VALUES (11,1,'2020-09-01 14:10:37.447')
+IF EXISTS (SELECT * FROM CandidateAssessments WHERE CandidateID = 11)
+    UPDATE CandidateAssessments SET SelfAssessmentID = 1, StartedDate = '2020-09-01 14:10:37.447' WHERE CandidateID = 11
+ELSE
+    INSERT INTO CandidateAssessments (CandidateID, SelfAssessmentID, StartedDate) VALUES (11,1,'2020-09-01 14:10:37.447')
 GO
 UPDATE SelfAssessments SET Vocabulary = 'Capability', LinearNavigation = 1 WHERE ID = 1
 GO
@@ -12,14 +15,15 @@ UPDATE [AssessmentQuestions] SET AssessmentQuestionInputTypeID = 2 WHERE ID IN(1
 GO
 UPDATE Applications SET IncludeCertification = 1 WHERE ApplicationID = 308
 GO
-INSERT INTO [dbo].[CentreSelfAssessments]
-           ([CentreID]
-           ,[SelfAssessmentID]
-           ,[AllowEnrolment])
-     VALUES
-           (101
-           ,1
-           ,1)
+IF EXISTS (SELECT * FROM CentreSelfAssessments WHERE CentreID = 101 and SelfAssessmentID = 1)
+    UPDATE CentreSelfAssessments SET SelfAssessmentID = 1 WHERE CentreID = 101 AND SelfAssessmentID = 1
+ELSE
+    INSERT INTO [dbo].[CentreSelfAssessments]
+               ([CentreID]
+               ,[SelfAssessmentID]
+               ,[AllowEnrolment])
+         VALUES
+               (101
+               ,1
+               ,1)
 GO
-
-
