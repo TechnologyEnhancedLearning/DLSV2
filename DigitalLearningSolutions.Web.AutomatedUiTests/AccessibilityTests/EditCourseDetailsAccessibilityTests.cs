fac4ae631fc96f16e4621ff3831b91ef0ffa8043
OliZor <olivia.zorn@softwire.com>
2021-10-26 11:04:47 +0100

    HEEDLS-444 Adjust styling and remove unnecessary code

--- DigitalLearningSolutions.Web.AutomatedUiTests/AccessibilityTests/EditCourseDetailsAccessibilityTests.cs
+++ DigitalLearningSolutions.Web.AutomatedUiTests/AccessibilityTests/EditCourseDetailsAccessibilityTests.cs
@@ -23,36 +23,19 @@
             ValidatePageHeading("Edit course details");
             var editResult = new AxeBuilder(Driver).Analyze();
 
-            editResult.Violations.Should().HaveCount(3);
-
             // Expect an axe violation caused by having an aria-expanded attribute on an input
             // The target inputs are nhs-tested components so ignore these violation
-            var ariaViolation = editResult.Violations[0];
+            editResult.Violations.Should().HaveCount(1);
+            var violation = editResult.Violations[0];
 
-            ariaViolation.Id.Should().Be("aria-allowed-attr");
-            ariaViolation.Nodes.Should().HaveCount(3);
-            ariaViolation.Nodes[0].Target.Should().HaveCount(1);
-            ariaViolation.Nodes[0].Target[0].Selector.Should().Be("#PasswordProtected");
-            ariaViolation.Nodes[1].Target.Should().HaveCount(1);
-            ariaViolation.Nodes[1].Target[0].Selector.Should().Be("#ReceiveNotificationEmails");
-            ariaViolation.Nodes[2].Target.Should().HaveCount(1);
-            ariaViolation.Nodes[2].Target[0].Selector.Should().Be("#OtherCompletionCriteria");
-
-            // Expect two axe violations caused by having no visible label on the CustomisationNameSuffix input
-            // The target input is meant to have only a hidden label
-            var visibleLabelViolation = editResult.Violations[1];
-
-            visibleLabelViolation.Id.Should().Be("label-title-only");
-            visibleLabelViolation.Nodes.Should().HaveCount(1);
-            visibleLabelViolation.Nodes[0].Target.Should().HaveCount(1);
-            visibleLabelViolation.Nodes[0].Target[0].Selector.Should().Be("#CustomisationNameSuffix");
-
-            var labelViolation = editResult.Violations[2];
-
-            labelViolation.Id.Should().Be("label");
-            labelViolation.Nodes.Should().HaveCount(1);
-            labelViolation.Nodes[0].Target.Should().HaveCount(1);
-            labelViolation.Nodes[0].Target[0].Selector.Should().Be("#CustomisationNameSuffix");
+            violation.Id.Should().Be("aria-allowed-attr");
+            violation.Nodes.Should().HaveCount(3);
+            violation.Nodes[0].Target.Should().HaveCount(1);
+            violation.Nodes[0].Target[0].Selector.Should().Be("#PasswordProtected");
+            violation.Nodes[1].Target.Should().HaveCount(1);
+            violation.Nodes[1].Target[0].Selector.Should().Be("#ReceiveNotificationEmails");
+            violation.Nodes[2].Target.Should().HaveCount(1);
+            violation.Nodes[2].Target[0].Selector.Should().Be("#OtherCompletionCriteria");
         }
     }
 }
