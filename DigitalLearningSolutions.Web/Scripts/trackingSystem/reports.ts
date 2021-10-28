3c847bd7152c48b70091540c2cc6c07cb32e99b7
showkath10 <90783892+showkath10@users.noreply.github.com>
2021-10-28 20:04:56 +0100

    HEEDLS-522 ReportsMakeUsageStatsTable

--- DigitalLearningSolutions.Web/Scripts/trackingSystem/reports.ts
+++ DigitalLearningSolutions.Web/Scripts/trackingSystem/reports.ts
@@ -11,128 +11,128 @@ const activityToggleableRowDisplayTableRow = 'table-row';
 
 interface IActivityDataRowModel {
   period: string;
-	completions: number;
-	evaluations: number;
-	registrations: number;
+  completions: number;
+  evaluations: number;
+  registrations: number;
 }
 
 function constructChartistData(data: Array<IActivityDataRowModel>): Chartist.IChartistData {
-	const labels = data.map((d) => d.period);
-	const series = [
-		data.map((d) => d.completions),
-		data.map((d) => d.evaluations),
-		data.map((d) => d.registrations),
-	];
-	return { labels, series };
+  const labels = data.map((d) => d.period);
+  const series = [
+    data.map((d) => d.completions),
+    data.map((d) => d.evaluations),
+    data.map((d) => d.registrations),
+  ];
+  return { labels, series };
 }
 
 function generateChart(request: XMLHttpRequest) {
-	let { response } = request;
-	// IE does not support automatic parsing to JSON with XMLHttpRequest.responseType
-	// so we need to manually parse the JSON string if not already parsed
-	if (typeof request.response === 'string') {
-		response = JSON.parse(response);
-	}
-	const data = constructChartistData(response);
+  let { response } = request;
+  // IE does not support automatic parsing to JSON with XMLHttpRequest.responseType
+  // so we need to manually parse the JSON string if not already parsed
+  if (typeof request.response === 'string') {
+    response = JSON.parse(response);
+  }
+  const data = constructChartistData(response);
 
-	const options = {
-		axisY: {
-			scaleMinSpace: 10,
-			onlyInteger: true,
-		},
-		chartPadding: {
-			bottom: 32,
-		},
-	};
+  const options = {
+    axisY: {
+      scaleMinSpace: 10,
+      onlyInteger: true,
+    },
+    chartPadding: {
+      bottom: 32,
+    },
+  };
 
-	const chart = new Chartist.Line('.ct-chart', data, options);
+  const chart = new Chartist.Line('.ct-chart', data, options);
 
-	chart.on('draw',
-		// The type here is Chartist.ChartDrawData, but the type specification is missing getNode()
-		// eslint-disable-next-line @typescript-eslint/no-explicit-any
-		(drawnElement: any) => {
-			const { element } = drawnElement;
-			// IE renders text in SVGs with 'text' tags that do not work with most CSS properties
-			// so we set the relevant attributes manually
-			if (element.getNode().tagName === 'text'
-				&& element.classes().indexOf('ct-horizontal') >= 0
-				&& element.classes().indexOf('ct-label') >= 0) {
-				const xOrigin = Number(element.getNode().getAttribute('x'));
-				const yOrigin = element.getNode().getAttribute('y');
-				const width = Number(element.getNode().getAttribute('width'));
-				// this should match the NHS tablet breakpoint
-				const mediaQuery = window.matchMedia('(min-width: 641px)');
-				const rotation = mediaQuery.matches ? -45 : -60;
-				element.attr({
-					transform: `translate(-${width}) rotate(${rotation} ${xOrigin + width} ${yOrigin})`,
-				});
-			}
-		});
+  chart.on('draw',
+    // The type here is Chartist.ChartDrawData, but the type specification is missing getNode()
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    (drawnElement: any) => {
+      const { element } = drawnElement;
+      // IE renders text in SVGs with 'text' tags that do not work with most CSS properties
+      // so we set the relevant attributes manually
+      if (element.getNode().tagName === 'text'
+        && element.classes().indexOf('ct-horizontal') >= 0
+        && element.classes().indexOf('ct-label') >= 0) {
+        const xOrigin = Number(element.getNode().getAttribute('x'));
+        const yOrigin = element.getNode().getAttribute('y');
+        const width = Number(element.getNode().getAttribute('width'));
+        // this should match the NHS tablet breakpoint
+        const mediaQuery = window.matchMedia('(min-width: 641px)');
+        const rotation = mediaQuery.matches ? -45 : -60;
+        element.attr({
+          transform: `translate(-${width}) rotate(${rotation} ${xOrigin + width} ${yOrigin})`,
+        });
+      }
+    });
 }
 
 function toggleVisibleActivityRows() {
-	const viewMoreLink = GetViewMoreLink();
-	const activityRow = <HTMLElement>document.getElementsByClassName(toggleableActivityRowClass)
-		.item(0);
+  const viewMoreLink = GetViewMoreLink();
+  const activityRow = <HTMLElement>document.getElementsByClassName(toggleableActivityRowClass)
+    .item(0);
 
-	if (activityRow?.style.display === activityToggleableRowDisplayNone) {
-		viewMoreRows();
-		viewMoreLink.innerText = 'View Less';
-	} else {
-		viewLessRows();
-		viewMoreLink.innerText = 'View More';
-	}
+  if (activityRow?.style.display === activityToggleableRowDisplayNone) {
+    viewMoreRows();
+    viewMoreLink.innerText = 'View Less';
+  } else {
+    viewLessRows();
+    viewMoreLink.innerText = 'View More';
+  }
 }
 
 function viewMoreRows(): void {
-	const activityTableRows = <HTMLElement[]>Array.from(
-		document.getElementsByClassName(toggleableActivityRowClass),
-	);
+  const activityTableRows = <HTMLElement[]>Array.from(
+    document.getElementsByClassName(toggleableActivityRowClass),
+  );
 
-	activityTableRows.forEach((row) => {
-		const rowElement = row;
-		rowElement.style.display = activityToggleableRowDisplayTableRow;
-	});
+  activityTableRows.forEach((row) => {
+    const rowElement = row;
+    rowElement.style.display = activityToggleableRowDisplayTableRow;
+  });
 }
 
 function viewLessRows(): void {
-	const activityTableRows = <HTMLElement[]>Array.from(
-		document.getElementsByClassName(toggleableActivityRowClass),
-	);
+  const activityTableRows = <HTMLElement[]>Array.from(
+    document.getElementsByClassName(toggleableActivityRowClass),
+  );
 
-	activityTableRows.forEach((row) => {
-		const rowElement = row;
-		rowElement.style.display = activityToggleableRowDisplayNone;
-	});
+  activityTableRows.forEach((row) => {
+    const rowElement = row;
+    rowElement.style.display = activityToggleableRowDisplayNone;
+  });
 }
 
 function makeChartistRequest() {
-	const request = new XMLHttpRequest();
+  const request = new XMLHttpRequest();
 
-	request.onload = () => {
-		generateChart(request);
-	};
+  request.onload = () => {
+    generateChart(request);
+  };
 
-	request.open('GET', path, true);
-	request.responseType = 'json';
-	request.send();
+  request.open('GET', path, true);
+  request.responseType = 'json';
+  request.send();
 }
 
 function GetViewMoreLink() {
-	return <HTMLElement>document.getElementsByClassName(toggleableActivityButtonClass)
-		.item(0);
+  return <HTMLElement>document.getElementsByClassName(toggleableActivityButtonClass)
+    .item(0);
 }
 
 function pageLoad() {
-	const viewMoreLink = GetViewMoreLink();
+  const viewMoreLink = GetViewMoreLink();
 
-	viewMoreLink.addEventListener('click', (event) => {
-		event.preventDefault();
-		toggleVisibleActivityRows();
-	});
+  viewMoreLink.addEventListener('click', (event) => {
+    event.preventDefault();
+    toggleVisibleActivityRows();
+  });
 
-	viewMoreLink.style.display = 'block';
-	viewLessRows();
+  viewMoreLink.style.display = 'block';
+  viewLessRows();
 
   makeChartistRequest();
 }
