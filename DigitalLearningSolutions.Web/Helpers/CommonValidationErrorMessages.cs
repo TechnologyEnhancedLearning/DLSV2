a4ef47b70c60d5104435c1a56d336f243772e2d8
showkath10 <90783892+showkath10@users.noreply.github.com>
2021-11-09 16:02:41 +0000

    HEEDLS-511 Delegate Group Edit - constants update

--- DigitalLearningSolutions.Web/Helpers/CommonValidationErrorMessages.cs
+++ DigitalLearningSolutions.Web/Helpers/CommonValidationErrorMessages.cs
@@ -3,19 +3,18 @@
     public static class CommonValidationErrorMessages
     {
         public const string IncorrectPassword = "The password you have entered is incorrect";
-        public const string TooLongFirstName = "First name must be {1} characters or fewer";
-        public const string TooLongLastName = "Last name must be {1} characters or fewer";
-        public const string TooLongAlias = "{0} must be {1} characters or fewer";
-        public const string TooLongEmail = "{0} must be {1} characters or fewer";
+        public const string TooLongFirstName = "First name must be 250 characters or fewer";
+        public const string TooLongLastName = "Last name must be 250 characters or fewer";
+        public const string TooLongAlias = "Alias must be 250 characters or fewer";
+        public const string TooLongEmail = "Email address must be 255 characters or fewer";
         public const string InvalidEmail = "Enter an email address in the correct format, like name@example.com";
         public const string WhitespaceInEmail = "Email address must not contain any whitespace characters";
 
         public const string PasswordRegex = @"(?=.*?[^\w\s])(?=.*?[0-9])(?=.*?[A-Za-z]).*";
         public const string PasswordInvalidCharacters = "Password must contain at least 1 letter, 1 number and 1 symbol";
         public const string PasswordRequired = "Enter a password";
-        public const string PasswordMinLength = "{0} must be {1} characters or more";
-        public const string PasswordMaxLength = "{0} must be {1} characters or fewer";
+        public const string PasswordMinLength = "Password must be 8 characters or more";
+        public const string PasswordMaxLength = "Password must be 100 characters or fewer";
         public const string StringMaxLengthValidation = "{0} must be {1} characters or fewer";
-
     }
 }
