1c27f7ef21adc66176cd923a15d97087058c0228
Alex Jackson <Alexander.Jackson@dorsetsoftware.com>
2021-11-18 13:47:36 +0000

    HEEDLS-668 Add migration to add CandidateAssessmentLearningLogItems table

---
+++ DigitalLearningSolutions.Data.Migrations/202111181337_AddCandidateAssessmentLearningLogItemsTable.cs
@@ -0,0 +1,21 @@
+﻿namespace DigitalLearningSolutions.Data.Migrations
+{
+    using FluentMigrator;
+
+    [Migration(202111181337)]
+    public class AddCandidateAssessmentLearningLogItemsTable : Migration
+    {
+        public override void Up()
+        {
+            Create.Table("CandidateAssessmentLearningLogItems")
+                .WithColumn("ID").AsInt32().NotNullable().PrimaryKey().Identity()
+                .WithColumn("CandidateAssessmentID").AsInt32().NotNullable().ForeignKey("CandidateAssessments", "ID")
+                .WithColumn("LearningLogItemID").AsInt32().NotNullable().ForeignKey("LearningLogItems", "LearningLogItemID");
+        }
+
+        public override void Down()
+        {
+            Delete.Table("CandidateAssessmentLearningLogItems");
+        }
+    }
+}
