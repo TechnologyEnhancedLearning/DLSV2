a154bc4eb94770cd3c9f47fcef68f8e05869a145
kevwhitt-hee <kevin.whittaker@hee.nhs.uk>
2021-11-19 11:58:25 +0000

    DLSV2-332 Adds import competencies button, controller action and basic view

---
+++ DigitalLearningSolutions.Web/Controllers/FrameworksController/ImportCompetencies.cs
@@ -0,0 +1,26 @@
+﻿using DigitalLearningSolutions.Data.Models.Frameworks;
+using DigitalLearningSolutions.Web.Helpers;
+using DigitalLearningSolutions.Web.ViewModels.Frameworks;
+using DigitalLearningSolutions.Web.ViewModels.Frameworks.Dashboard;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.Extensions.Logging;
+using System.Linq;
+using DigitalLearningSolutions.Web.Extensions;
+using DigitalLearningSolutions.Data.Models.SessionData.Frameworks;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+
+namespace DigitalLearningSolutions.Web.Controllers.FrameworksController
+{
+    public partial class FrameworksController
+    {
+        [Route("/Framework/{frameworkId}/Structure/Import")]
+        public IActionResult ImportCompetencies(int frameworkId)
+        {
+            
+            return View("Developer/ImportCompetencies");
+        }
+    }
+}
