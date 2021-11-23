b3cdaea0a10d0fa7a77baeef561fdd924be9be01
OliZor <olivia.zorn@softwire.com>
2021-11-23 09:12:02 +0000

    HEEDLS-672 Set up api client methods

---
+++ DigitalLearningSolutions.Data/Helpers/ExternalApis/LearningHubApiClient.cs
@@ -0,0 +1,67 @@
+﻿namespace DigitalLearningSolutions.Data.Helpers.ExternalApis
+{
+    using System.Collections.Generic;
+    using System.Net.Http;
+    using System.Threading.Tasks;
+    using DigitalLearningSolutions.Data.Models.LearningHubApiClient;
+    using Microsoft.Extensions.Configuration;
+    using Newtonsoft.Json;
+
+    public interface ILearningHubApiClient
+    {
+        Task<ResourceSearchResult> SearchResource(
+            string text,
+            int? offset = null,
+            int? limit = null,
+            IEnumerable<string>? resourceTypes = null
+        );
+
+        Task<ResourceReferenceWithReferenceDetails> GetResourceByReferenceId(int resourceReferenceId);
+
+        Task<ResourceReferenceWithReferenceDetails> GetResourcesBulk(IEnumerable<int> resourceReferenceIds);
+    }
+
+    public class LearningHubApiClient : ILearningHubApiClient
+    {
+        private readonly HttpClient client;
+        private readonly string LearningHubApiBaseUrl;
+
+        public LearningHubApiClient(HttpClient httpClient, IConfiguration config)
+        {
+            var learningHubApiKey = config.GetLearningHubApiKey();
+            LHApiBaseUrl =
+                "https://uks-learninghubnhsuk-openapi-dev.azurewebsites.net";
+
+            client = httpClient;
+        }
+
+        public async Task<ResourceSearchResult> SearchResource(
+            string text,
+            int? offset = null,
+            int? limit = null,
+            IEnumerable<string>? resourceTypes = null
+        )
+        {
+            client.DefaultRequestHeaders.Accept.Clear();
+            var response = await client.GetStringAsync(mapsApiBaseUrl + postcode);
+            var result = JsonConvert.DeserializeObject<ResourceSearchResult>(response);
+            return result;
+        }
+
+        public async Task<ResourceReferenceWithReferenceDetails> GetResourceByReferenceId(int resourceReferenceId)
+        {
+            client.DefaultRequestHeaders.Accept.Clear();
+            var response = await client.GetStringAsync(mapsApiBaseUrl + postcode);
+            var result = JsonConvert.DeserializeObject<ResourceReferenceWithReferenceDetails>(response);
+            return result;
+        }
+
+        public async Task<ResourceReferenceWithReferenceDetails> GetResourcesBulk(IEnumerable<int> resourceReferenceIds)
+        {
+            client.DefaultRequestHeaders.Accept.Clear();
+            var response = await client.GetStringAsync(mapsApiBaseUrl + postcode);
+            var result = JsonConvert.DeserializeObject<ResourceReferenceWithReferenceDetails>(response);
+            return result;
+        }
+    }
+}
