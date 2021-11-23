022591135b736845db9f1cad624b30400336bab3
Hemanth Kumar <hemanthn@hfea.gov.uk>
2021-11-23 17:03:46 +0000

    Added 1st update

---
+++ DigitalLearningSolutions.Data.Migrations/202111231505_AddFieldsToSelfassessment.cs
@@ -0,0 +1,21 @@
+﻿namespace DigitalLearningSolutions.Data.Migrations
+{
+    using FluentMigrator;
+    [Migration(202111231505)]
+    public class AddFieldsToSelfassessment : Migration
+    {
+        public override void Up()
+        {            
+            Alter.Table("SelfAssessments")
+                .AddColumn("QuestionLabel").AsString(50).Nullable()
+                .AddColumn("DescriptionLabel").AsString(50).Nullable();
+
+        }
+
+        public override void Down()
+        {            
+            Delete.Column("QuestionLabel").FromTable("SelfAssessments");
+            Delete.Column("DescriptionLabel").FromTable("SelfAssessments");
+        }
+    }
+}
