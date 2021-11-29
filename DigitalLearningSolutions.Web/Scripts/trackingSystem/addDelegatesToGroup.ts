78afaf0c6958057e829778e4c69273393a7d362a
showkath10 <90783892+showkath10@users.noreply.github.com>
2021-11-29 16:16:49 +0000

    Updated ts file

--- DigitalLearningSolutions.Web/Scripts/trackingSystem/addDelegatesToGroup.ts
+++ DigitalLearningSolutions.Web/Scripts/trackingSystem/addDelegatesToGroup.ts
@@ -1,4 +1,2 @@
 ﻿import { SearchSortFilterAndPaginate } from '../searchSortFilterAndPaginate/searchSortFilterAndPaginate';
-
-// eslint-disable-next-line no-new
 new SearchSortFilterAndPaginate('TrackingSystem/Delegates/Groups/AddDelegateToGroup/AddDelegateToGroupItems', true, true, true, 'AddDelegateToGroupFilter');
