b913179b462ee55caf75a6a00d5e2d4bf65315e2
Hemanth Kumar <hemanthn@hfea.gov.uk>
2021-11-29 14:53:18 +0000

    Added the lint error updates

--- DigitalLearningSolutions.Web/Scripts/supervisor/assessmentVerify.ts
+++ DigitalLearningSolutions.Web/Scripts/supervisor/assessmentVerify.ts
@@ -1,24 +1,14 @@
-﻿const selectRadioButtons = document.getElementsByName('SignedOff');
-
+const selectRadioButtons = document.getElementsByName('SignedOff');
 selectRadioButtons.forEach((button) => {
-  button.addEventListener('click',
-    () => {
-      const radioValue = button.getAttribute('value') as string;
-      const txtArea = document.getElementById('SupervisorComments');
-            
-      if (radioValue === 'false')
-      {
-        if (txtArea)
-        {
+  button.addEventListener('click', () => {
+    const radioValue = button.getAttribute('value') as string;
+    const txtArea = document.getElementById('SupervisorComments');
+    if (radioValue === 'false') {
+      if (txtArea) {
         txtArea.setAttribute('required', 'required');
-        }        
       }
-      else
-      {
-        if (txtArea)
-        {
-          txtArea.removeAttribute('required');
-        }
-      }
-    });
+    } else if (txtArea) {
+      txtArea.removeAttribute('required');
+    }
+  });
 });
