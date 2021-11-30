c56cce7f59f2770004a32900c7384b2817909b49
showkath10 <90783892+showkath10@users.noreply.github.com>
2021-11-30 08:59:11 +0000

    Typescript for delegates group page

--- DigitalLearningSolutions.Web/Scripts/trackingSystem/addDelegatesToGroup.ts
+++ DigitalLearningSolutions.Web/Scripts/trackingSystem/addDelegatesToGroup.ts
@@ -1,3 +1,4 @@
 import { SearchSortFilterAndPaginate } from '../searchSortFilterAndPaginate/searchSortFilterAndPaginate';
 
+// eslint-disable-next-line no-new
 new SearchSortFilterAndPaginate('TrackingSystem/Delegates/Groups/AddDelegateToGroup/AddDelegateToGroupItems', true, true, true, 'AddDelegateToGroupFilter');
