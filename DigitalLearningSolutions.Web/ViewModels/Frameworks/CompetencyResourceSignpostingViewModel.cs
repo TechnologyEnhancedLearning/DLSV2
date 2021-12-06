cd7fe64ad9064385526ee8557d50616880f4e239
Daniel Manta <Daniel.Manta@hee.nhs.uk>
2021-12-06 20:37:46 +0000

    DLSV2-439 Search Learning Hub resources basic view

--- DigitalLearningSolutions.Web/ViewModels/Frameworks/CompetencyResourceSignpostingViewModel.cs
+++ DigitalLearningSolutions.Web/ViewModels/Frameworks/CompetencyResourceSignpostingViewModel.cs
@@ -1,26 +1,36 @@
-﻿using System;
+﻿using DigitalLearningSolutions.Web.Helpers;
+using DigitalLearningSolutions.Web.ViewModels.Common.SearchablePage;
+using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace DigitalLearningSolutions.Web.ViewModels.Frameworks
 {
-    public class CompetencyResourceSignpostingViewModel
+    public class CompetencyResourceSignpostingViewModel : BaseSearchablePageViewModel
     {
         public string NameOfCompetency { get; set; }
         public int FrameworkId { get; set; }
         public int? FrameworkCompetencyId { get; set; }
         public int? FrameworkCompetencyGroupId { get; set; }
         public List<SignpostingCardViewModel> CompetencyResourceLinks { get; set; }
+        public bool NoSearchOrFilter => SearchString == null && FilterBy == null;
+        public bool SortEnabled { get; set; } 
+        public IEnumerable<SearchableCompetencyViewModel> Delegates { get; set; }
 
         public CompetencyResourceSignpostingViewModel(int frameworkId, int? frameworkCompetencyId, int? frameworkCompetencyGroupId)
+            : base(null, 1, filterEnabled: false, "Search resource", null, null,10, "Search")
         {
             FrameworkId = frameworkId;
             FrameworkCompetencyId = frameworkCompetencyId;
             FrameworkCompetencyGroupId = frameworkCompetencyGroupId;
         }
 
-        public CompetencyResourceSignpostingViewModel()
+        public override IEnumerable<(string, string)> SortOptions { get; } = new[]
         {
+            DelegateSortByOptions.Name,
+            DelegateSortByOptions.RegistrationDate
+        };
 
-        }
+        public override bool NoDataFound => !Delegates.Any() && NoSearchOrFilter;
     }
 }
---
+++ DigitalLearningSolutions.Web/ViewModels/Frameworks/SearchableCompetencyViewModel.cs
@@ -0,0 +1,16 @@
+﻿using DigitalLearningSolutions.Web.ViewModels.Common.SearchablePage;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DigitalLearningSolutions.Web.ViewModels.Frameworks
+{
+    public class SearchableCompetencyViewModel : BaseFilterableViewModel
+    {
+        public SearchableCompetencyViewModel()
+        {
+
+        }
+    }
+}
