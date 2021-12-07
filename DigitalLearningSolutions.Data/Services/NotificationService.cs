9057f2aa3a165c68dc39fde016d29448514acc7e
showkath10 <90783892+showkath10@users.noreply.github.com>
2021-12-07 08:37:44 +0000

    Added tracking system enabled check

--- DigitalLearningSolutions.Data/Services/NotificationService.cs
+++ DigitalLearningSolutions.Data/Services/NotificationService.cs
@@ -3,6 +3,7 @@
     using System;
     using DigitalLearningSolutions.Data.Models;
     using DigitalLearningSolutions.Data.Models.Email;
+    using Microsoft.FeatureManagement;
     using MimeKit;
 
     public interface INotificationService
@@ -14,17 +15,20 @@
     {
         private readonly IConfigService configService;
         private readonly IEmailService emailService;
+        private readonly IFeatureManager featureManager;
         private readonly INotificationDataService notificationDataService;
 
         public NotificationService(
             INotificationDataService notificationDataService,
             IConfigService configService,
-            IEmailService emailService
+            IEmailService emailService,
+            IFeatureManager featureManager
         )
         {
             this.notificationDataService = notificationDataService;
             this.configService = configService;
             this.emailService = emailService;
+            this.featureManager = featureManager;
         }
 
         public void SendUnlockRequest(int progressId)
@@ -32,13 +36,27 @@
             var unlockData = notificationDataService.GetUnlockData(progressId);
             if (unlockData == null)
             {
-                throw new NotificationDataException($"No record found when trying to fetch Unlock Data. Progress ID: {progressId}");
+                throw new NotificationDataException(
+                    $"No record found when trying to fetch Unlock Data. Progress ID: {progressId}"
+                );
             }
 
             unlockData.ContactForename = unlockData.ContactForename == "" ? "Colleague" : unlockData.ContactForename;
             var trackingSystemBaseUrl = configService.GetConfigValue(ConfigService.TrackingSystemBaseUrl) ??
-                                        throw new ConfigValueMissingException(configService.GetConfigValueMissingExceptionMessage("TrackingSystemBaseUrl"));
-            var unlockUrl = new UriBuilder(trackingSystemBaseUrl);
+                                        throw new ConfigValueMissingException(
+                                            configService.GetConfigValueMissingExceptionMessage("TrackingSystemBaseUrl")
+                                        );
+
+            var v2AppBaseUrl = configService.GetConfigValue(ConfigService.AppBaseUrl) ??
+                               throw new ConfigValueMissingException(
+                                   configService.GetConfigValueMissingExceptionMessage("AppBaseUrl")
+                               );
+
+            var trackingSystemSupportEnabled = featureManager.IsEnabledAsync("RefactoredTrackingSystem").Result;
+
+            var unlockUrl = trackingSystemSupportEnabled
+                ? new UriBuilder(trackingSystemBaseUrl): new UriBuilder(v2AppBaseUrl);
+
             unlockUrl.Path += "coursedelegates";
             unlockUrl.Query = $"CustomisationID={unlockData.CustomisationId}";
             string emailSubjectLine = "Digital Learning Solutions Progress Unlock Request";
@@ -52,10 +70,12 @@ To review and unlock their progress, visit the this url: ${unlockUrl.Uri}.",
                                     <p>Dear {unlockData?.ContactForename}</p>
                                     <p>Digital Learning Solutions Delegate, {unlockData?.DelegateName}, has requested that you unlock their progress for the course {unlockData?.CourseName}</p>
                                     <p>They have reached the maximum number of assessment attempt allowed without passing.</p><p>To review and unlock their progress, <a href='{unlockUrl.Uri}'>click here</a>.</p>
-                                </body>"
+                                </body>",
             };
 
-            emailService.SendEmail(new Email(emailSubjectLine, builder, unlockData.ContactEmail, unlockData.DelegateEmail));
+            emailService.SendEmail(
+                new Email(emailSubjectLine, builder, unlockData.ContactEmail, unlockData.DelegateEmail)
+            );
         }
     }
 }
