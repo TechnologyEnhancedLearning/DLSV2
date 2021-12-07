7c32c2c2e5ce333c5e1d88a6842a572c1fa1b7d3
Daniel Manta <Daniel.Manta@hee.nhs.uk>
2021-12-07 12:22:33 +0000

    DLSV2-439 Search Learning Hub resources basic view

--- DigitalLearningSolutions.Web/ViewModels/Frameworks/CompetencyResourceSignpostingViewModel.cs
+++ DigitalLearningSolutions.Web/ViewModels/Frameworks/CompetencyResourceSignpostingViewModel.cs
@@ -6,31 +6,21 @@ using System.Linq;
 
 namespace DigitalLearningSolutions.Web.ViewModels.Frameworks
 {
-    public class CompetencyResourceSignpostingViewModel : BaseSearchablePageViewModel
+    public class CompetencyResourceSignpostingViewModel
     {
         public string NameOfCompetency { get; set; }
         public int FrameworkId { get; set; }
         public int? FrameworkCompetencyId { get; set; }
         public int? FrameworkCompetencyGroupId { get; set; }
         public List<SignpostingCardViewModel> CompetencyResourceLinks { get; set; }
-        public bool NoSearchOrFilter => SearchString == null && FilterBy == null;
         public bool SortEnabled { get; set; } 
         public IEnumerable<SearchableCompetencyViewModel> Delegates { get; set; }
 
         public CompetencyResourceSignpostingViewModel(int frameworkId, int? frameworkCompetencyId, int? frameworkCompetencyGroupId)
-            : base(null, 1, filterEnabled: false, "Search resource", null, null,10, "Search")
         {
             FrameworkId = frameworkId;
             FrameworkCompetencyId = frameworkCompetencyId;
             FrameworkCompetencyGroupId = frameworkCompetencyGroupId;
         }
-
-        public override IEnumerable<(string, string)> SortOptions { get; } = new[]
-        {
-            DelegateSortByOptions.Name,
-            DelegateSortByOptions.RegistrationDate
-        };
-
-        public override bool NoDataFound => !Delegates.Any() && NoSearchOrFilter;
     }
 }
