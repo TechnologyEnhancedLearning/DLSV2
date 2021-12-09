eb9ca7e8c3c51a0ac88be97d1cf48c39269b95db
Daniel Manta <Daniel.Manta@hee.nhs.uk>
2021-12-09 12:33:38 +0000

    DLSV2-440 Search Learning Hub resources results and pagination (mockup)

--- DigitalLearningSolutions.Web/Controllers/FrameworksController/Signposting.cs
+++ DigitalLearningSolutions.Web/Controllers/FrameworksController/Signposting.cs
@@ -1,6 +1,8 @@
 ﻿using DigitalLearningSolutions.Web.ViewModels.Frameworks;
 using Microsoft.AspNetCore.Mvc;
 using System.Collections.Generic;
+using DigitalLearningSolutions.Data.Models.LearningHubApiClient;
+using System;
 
 namespace DigitalLearningSolutions.Web.Controllers.FrameworksController
 {
@@ -20,10 +22,34 @@ namespace DigitalLearningSolutions.Web.Controllers.FrameworksController
             return View("Developer/AddCompetencyLearningResources", model);
         }
 
+        [HttpPost]
+        public IActionResult SearchLearningResources(CompetencyResourceSignpostingViewModel model)
+        {
+            return GoToPage(1, model);
+        }
+
+        [HttpPost]
+        public IActionResult GoToNextPage(CompetencyResourceSignpostingViewModel model)
+        {
+            return GoToPage(model.Page + 1, model);
+        }
+
+        [HttpPost]
+        public IActionResult GoToPreviousPage(CompetencyResourceSignpostingViewModel model)
+        {
+            return GoToPage(model.Page - 1, model);
+        }
+
+        private IActionResult GoToPage(int page, CompetencyResourceSignpostingViewModel model)
+        {
+            model.Page = page;
+            GetResourcesFromApi(page, model);
+            return View("Developer/AddCompetencyLearningResources", model);
+        }
+
         private CompetencyResourceSignpostingViewModel PopulatedModel(int frameworkId, int? frameworkCompetencyGroupId = null, int? frameworkCompetencyId = null)
         {
             var model = new CompetencyResourceSignpostingViewModel(frameworkId, frameworkCompetencyId, frameworkCompetencyId);
-            model.SortEnabled = false;
             model.NameOfCompetency = "I can organise my information and content using files and folders (either on my device, across multiple devices, or on the Cloud)";
             model.CompetencyResourceLinks = new List<SignpostingCardViewModel>()
             {
@@ -47,6 +73,65 @@ namespace DigitalLearningSolutions.Web.Controllers.FrameworksController
                 },
             };
             return model;
+        }
+
+        private void GetResourcesFromApi(int page, CompetencyResourceSignpostingViewModel model)
+        {
+            var resourceTypes = new string[] { "Cloud storage", "A guide to online storage", "Towards a paperless HR Department" };
+            var descriptions = new string[]
+            {
+                "There are a lot of online storage services but here are a few of the top free online options.<br />",
+                "<p>Businesses, firms, and individuals are increasingly adopting cloud data storage because they require more flexibility, versatility<p>",
+                "<b>It's estimated that we use over 10000 sheets of paper, per employee, per year</b>. Learn how to cut back in your business with paperless HR"
+            };
+            var catalogues = new string[]
+            {
+                "Digital Unite", "Community contributions"
+            };
+            var response = new ResourceSearchResult()
+            {
+                Results = new List<ResourceMetadata>(),
+                TotalNumResources = 24, /* alleged search results */
+                Offset = (page - 1) * CompetencyResourceSignpostingViewModel.ItemsPerPage
+            };
+            int lastItemOfPage = Math.Min(response.Offset + CompetencyResourceSignpostingViewModel.ItemsPerPage, response.TotalNumResources - 1);
+
+            if (String.IsNullOrEmpty(model.SearchText?.Trim())) return;
+
+            for (int i = response.Offset  ; i <= lastItemOfPage; i++)
+            {
+                response.Results.Add(new ResourceMetadata()
+                {
+                    ResourceId = i,
+                    Title = "title" + i.ToString(),
+                    Description = descriptions[i % descriptions.Length],
+                    ResourceType = resourceTypes[i % resourceTypes.Length],
+                    References = new List<ResourceReference>()
+                    {
+                        new ResourceReference()
+                        {
+                            RefId = i * 100,
+                            Catalogue = new Catalogue()
+                            {
+                                Id = i * 1000,
+                                Name = catalogues[i % catalogues.Length]
+                            },
+                            Link = $"https://learninghub.nhs.uk/Resource/${ i * 100}"
+                        },
+                        new ResourceReference()
+                        {
+                            RefId = i * 100 + 1,
+                            Catalogue = new Catalogue()
+                            {
+                                Id = i * 1000 + 1,
+                                Name = catalogues[(i + 1) % catalogues.Length]
+                            },
+                            Link = $"https://learninghub.nhs.uk/Resource/${ (i + 1) * 100 }"
+                        }
+                    }
+                });
+            };
+            model.SearchResult = response;
         }
     }
 }
--- DigitalLearningSolutions.Web/ViewModels/Frameworks/CompetencyResourceSignpostingViewModel.cs
+++ DigitalLearningSolutions.Web/ViewModels/Frameworks/CompetencyResourceSignpostingViewModel.cs
@@ -1,4 +1,5 @@
-﻿using DigitalLearningSolutions.Web.Helpers;
+﻿using DigitalLearningSolutions.Data.Models.LearningHubApiClient;
+using DigitalLearningSolutions.Web.Helpers;
 using DigitalLearningSolutions.Web.ViewModels.Common.SearchablePage;
 using System;
 using System.Collections.Generic;
@@ -8,19 +9,42 @@ namespace DigitalLearningSolutions.Web.ViewModels.Frameworks
 {
     public class CompetencyResourceSignpostingViewModel
     {
+        public const int ItemsPerPage = 10;
         public string NameOfCompetency { get; set; }
         public int FrameworkId { get; set; }
         public int? FrameworkCompetencyId { get; set; }
         public int? FrameworkCompetencyGroupId { get; set; }
         public List<SignpostingCardViewModel> CompetencyResourceLinks { get; set; }
-        public bool SortEnabled { get; set; } 
         public IEnumerable<SearchableCompetencyViewModel> Delegates { get; set; }
+        public string SearchText { get; set; }
+        public int Page { get; set; }
+        public int TotalPages
+        {
+            get
+            {
+                return (int)Math.Ceiling((SearchResult?.TotalNumResources ?? 0) / (double)ItemsPerPage);
+            }
+        }
+        public int TotalNumResources
+        {
+            get
+            {
+                return SearchResult?.TotalNumResources ?? 0;
+            }
+        }
+        public ResourceSearchResult SearchResult { get; set; }
 
         public CompetencyResourceSignpostingViewModel(int frameworkId, int? frameworkCompetencyId, int? frameworkCompetencyGroupId)
         {
             FrameworkId = frameworkId;
             FrameworkCompetencyId = frameworkCompetencyId;
             FrameworkCompetencyGroupId = frameworkCompetencyGroupId;
+            Page = 1;
+        }
+
+        public CompetencyResourceSignpostingViewModel()
+        {
+
         }
     }
 }
