d191e59da834024cd6fefc024967921dfa1e9ad1
showkath10 <90783892+showkath10@users.noreply.github.com>
2021-12-14 07:13:07 +0000

    Code changes done as suggested in peer review

--- DigitalLearningSolutions.Data.Tests/DataServices/UserDataServiceTests/DelegateUserDataServiceTests.cs
+++ DigitalLearningSolutions.Data.Tests/DataServices/UserDataServiceTests/DelegateUserDataServiceTests.cs
@@ -3,7 +3,6 @@
     using System;
     using System.Linq;
     using System.Transactions;
-    using DigitalLearningSolutions.Data.Enums;
     using DigitalLearningSolutions.Data.Tests.TestHelpers;
     using FluentAssertions;
     using FluentAssertions.Execution;
---
+++ DigitalLearningSolutions.Web/Helpers/ValidatableObjectHelper.cs
@@ -0,0 +1,29 @@
+﻿namespace DigitalLearningSolutions.Web.Helpers
+{
+    using Microsoft.AspNetCore.Mvc.ModelBinding;
+    using System.ComponentModel.DataAnnotations;
+
+    public static class ValidatableObjectHelper
+    {
+        public static void ValidateManually(this IValidatableObject model, ModelStateDictionary modelState)
+        {
+            var validationResults = model.Validate(new ValidationContext(model, null, null));
+            foreach (var result in validationResults)
+            {
+                var key = string.Join("", result.MemberNames);
+                if (modelState.TryGetValue(key, out var value))
+                {
+                    if (value.ValidationState == ModelValidationState.Invalid)
+                    {
+                        continue;
+                    }
+                }
+
+                modelState.TryAddModelError(
+                    key,
+                    result.ErrorMessage
+                );
+            }
+        }
+    }
+}
