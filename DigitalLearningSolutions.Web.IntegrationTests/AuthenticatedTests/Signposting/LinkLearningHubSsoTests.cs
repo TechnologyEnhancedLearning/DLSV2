ea2f1076e5cc74bbcf289184f211cc5e5f224ccd
Martin Wilkins <Martin.Wilkins@softwire.com>
2021-12-15 13:33:33 +0000

    HEEDLS-681 Add Interation Tests

---
+++ DigitalLearningSolutions.Web.IntegrationTests/AuthenticatedTests/Signposting/LinkLearningHubSsoTests.cs
@@ -0,0 +1,57 @@
+﻿namespace DigitalLearningSolutions.Web.IntegrationTests.AuthenticatedTests.Signposting
+{
+    using System.Net;
+    using System.Threading.Tasks;
+    using DigitalLearningSolutions.Data.Models.Signposting.LinkLearningHubSso;
+    using DigitalLearningSolutions.Data.Services;
+    using DigitalLearningSolutions.Web.IntegrationTests.TestHelpers;
+    using FluentAssertions;
+    using FluentAssertions.Execution;
+    using Microsoft.Extensions.DependencyInjection;
+    using Xunit;
+
+    public class LinkLearningHubSsoTests : IClassFixture<AuthenticationWebApplicationFactory<Startup>>
+    {
+        private readonly AuthenticationWebApplicationFactory<Startup> _factory;
+
+        public LinkLearningHubSsoTests(AuthenticationWebApplicationFactory<Startup> factory)
+        {
+            _factory = factory;
+        }
+
+        [Theory]
+        [InlineData(1, 1, "Your account has already been linked with the Learning Hub in a previous link request.")]
+        [InlineData(2, 2, "Your account has been linked with the Learning Hub.")]
+        public async Task LinkAccountValidRequestShowsConfirmationPage(
+            int delegateId,
+            int learningHubAuthId,
+            string expectedContent
+        )
+        {
+            // Given
+            using var scope = _factory.Services.CreateScope();
+            var learningHubSecurityService = scope.ServiceProvider.GetRequiredService<ILearningHubSsoSecurityService>();
+            var userService = scope.ServiceProvider.GetRequiredService<IUserService>();
+            var testUserData = TestUserDataService.GetDelegate(delegateId);
+            var userIdHash = learningHubSecurityService.GenerateHash(learningHubAuthId.ToString());
+            var state = $"{testUserData.SessionData[LinkLearningHubRequest.SessionIdentifierKey]}_refId:12345";
+
+            var client = await HttpClientHelper.SetDelegateSessionAndGetClient(_factory, delegateId);
+
+            // When
+            var response = await client.GetAsync(
+                $"/Signposting/LinkLearningHubSso?State={state}&Hash={WebUtility.UrlEncode(userIdHash)}&UserId={learningHubAuthId}"
+            );
+
+            // Then
+            using (new AssertionScope())
+            {
+                response.EnsureSuccessStatusCode();
+                var responseContent = await response.Content.ReadAsStringAsync();
+
+                responseContent.Should().Contain(expectedContent);
+                userService.DelegateUserLearningHubAccountIsLinked(delegateId).Should().BeTrue();
+            }
+        }
+    }
+}
---
+++ DigitalLearningSolutions.Web.IntegrationTests/AuthenticationWebApplicationFactory.cs
@@ -0,0 +1,41 @@
+﻿namespace DigitalLearningSolutions.Web.IntegrationTests
+{
+    using System.Linq;
+    using DigitalLearningSolutions.Data.DataServices.UserDataService;
+    using DigitalLearningSolutions.Web.IntegrationTests.TestHelpers;
+    using Microsoft.AspNetCore.Hosting;
+    using Microsoft.AspNetCore.Mvc;
+    using Microsoft.AspNetCore.Mvc.Testing;
+    using Microsoft.Extensions.DependencyInjection;
+
+    public class AuthenticationWebApplicationFactory<TStartup>
+        : WebApplicationFactory<TStartup> where TStartup : class
+    {
+        protected sealed override IWebHostBuilder CreateWebHostBuilder()
+        {
+            return base.CreateWebHostBuilder();
+        }
+
+        protected override void ConfigureWebHost(IWebHostBuilder builder)
+        {
+            builder
+                .UseEnvironment("SIT")
+                .ConfigureServices(
+                    services =>
+                    {
+                        services.Configure<MvcOptions>(
+                            o => { o.Filters.Remove(new AutoValidateAntiforgeryTokenAttribute()); }
+                        );
+
+                        services.AddTransient<IStartupFilter, TestUserAppStartupFilter>();
+
+                        var userDataServiceDescriptor = services.SingleOrDefault(
+                            d => d.ServiceType == typeof(IUserDataService)
+                        );
+                        services.Remove(userDataServiceDescriptor);
+                        services.AddScoped(x => TestUserDataService.FakeUserDataService());
+                    }
+                );
+        }
+    }
+}
