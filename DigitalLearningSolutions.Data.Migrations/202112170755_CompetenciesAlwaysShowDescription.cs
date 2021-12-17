68afccb17db34d92a318e556f51bb1b596939810
kevwhitt-hee <kevin.whittaker@hee.nhs.uk>
2021-12-17 08:38:08 +0000

    Update 202112170755_CompetenciesAlwaysShowDescription.cs

--- DigitalLearningSolutions.Data.Migrations/202112170755_CompetenciesAlwaysShowDescription.cs
+++ DigitalLearningSolutions.Data.Migrations/202112170755_CompetenciesAlwaysShowDescription.cs
@@ -4,12 +4,12 @@
     [Migration(202112170755)]
     public class CompetenciesAlwaysShowDescription : Migration
     {
-        public override void Down()
+        public override void Up()
         {
             Alter.Table("Competencies").AddColumn("AlwaysShowDescription").AsBoolean().WithDefaultValue(false);
         }
 
-        public override void Up()
+        public override void Down()
         {
             Delete.Column("AlwaysShowDescription").FromTable("Competencies");
         }
