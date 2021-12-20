e4c725eba5dc91bf39ceb4b0102ec85f37e46618
kevwhitt-hee <kevin.whittaker@hee.nhs.uk>
2021-12-20 09:55:31 +0000

    DLSV2-461 Migration to add EnforceRoleRequirementsForSignOff flag

---
+++ DigitalLearningSolutions.Data.Migrations/202112200950_AddEnforceRoleRequirementsForSignOffFlag.cs
@@ -0,0 +1,17 @@
+﻿namespace DigitalLearningSolutions.Data.Migrations
+{
+    using FluentMigrator;
+    [Migration(202112200950)]
+    public class AddEnforceRoleRequirementsForSignOffFlag : Migration
+    {
+        public override void Up()
+        {
+            Alter.Table("SelfAssessments").AddColumn("EnforceRoleRequirementsForSignOff").AsBoolean()
+                .WithDefaultValue(false);
+        }
+        public override void Down()
+        {
+            Delete.Column("EnforceRoleRequirementsForSignOff").FromTable("SelfAssessments");
+        }
+    }
+}
