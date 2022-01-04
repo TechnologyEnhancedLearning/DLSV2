7e7f5746102bcfe29756d45f232c575732ef1289
OliZor <olivia.zorn@softwire.com>
2022-01-04 18:58:24 +0000

    HEEDLS-551 Fix issue with checkboxes increasing on set course options page

--- DigitalLearningSolutions.Web/Models/AddNewCentreCourseData.cs
+++ DigitalLearningSolutions.Web/Models/AddNewCentreCourseData.cs
@@ -3,6 +3,7 @@
     using System;
     using DigitalLearningSolutions.Data.Models.Courses;
     using DigitalLearningSolutions.Web.ViewModels.TrackingSystem.CourseSetup.AddNewCentreCourse;
+    using DigitalLearningSolutions.Web.ViewModels.TrackingSystem.CourseSetup.CourseDetails;
 
     public class AddNewCentreCourseData
     {
@@ -14,7 +15,7 @@
         public Guid Id { get; set; }
         public ApplicationDetails? Application { get; set; }
         public SetCourseDetailsViewModel? SetCourseDetailsModel { get; set; }
-        public SetCourseOptionsViewModel? SetCourseOptionsModel { get; set; }
+        public EditCourseOptionsFormData? SetCourseOptionsModel { get; set; }
         public SetCourseContentViewModel? SetCourseContentModel { get; set; }
         public SetSectionContentViewModel? SetSectionContentModel { get; set; }
 
--- DigitalLearningSolutions.Web/ViewModels/TrackingSystem/CourseSetup/AddNewCentreCourse/SetCourseOptionsViewModel.cs
+++ DigitalLearningSolutions.Web/ViewModels/TrackingSystem/CourseSetup/AddNewCentreCourse/SetCourseOptionsViewModel.cs
@@ -5,20 +5,7 @@
 
     public class SetCourseOptionsViewModel : EditCourseOptionsFormData
     {
-        public SetCourseOptionsViewModel() { }
 
-        public SetCourseOptionsViewModel(bool diagAssess)
-        {
-            if (diagAssess)
-            {
-                Checkboxes.Add(
-                    new CheckboxListItemViewModel(
-                        nameof(DiagnosticObjectiveSelection),
-                        "Allow diagnostic objective selection",
-                        "Allow the learner to choose which objectives to be assessed against when starting a diagnostic assessment."
-                    )
-                );
-            }
-        }
+        public SetCourseOptionsViewModel(bool diagAssess) { }
     }
 }
