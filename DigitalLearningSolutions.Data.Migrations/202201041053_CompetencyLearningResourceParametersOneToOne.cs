8b8f9b6ae9db53faa34aee1e2acc0dabba563996
kevwhitt-hee <kevin.whittaker@hee.nhs.uk>
2022-01-05 07:51:26 +0000

    DLSV2-467 Migration to add unique constraint

---
+++ DigitalLearningSolutions.Data.Migrations/202201041053_CompetencyLearningResourceParametersOneToOne.cs
@@ -0,0 +1,23 @@
+﻿namespace DigitalLearningSolutions.Data.Migrations
+{
+    using FluentMigrator;
+    [Migration(202201041053)]
+    public class CompetencyLearningResourceParametersOneToOne : Migration
+    {
+        public override void Up()
+        {
+            Execute.Sql(@"DELETE FROM CompetencyResourceAssessmentQuestionParameters
+                             WHERE ID <>
+                                (SELECT MIN(ID) As ID
+                                    FROM CompetencyResourceAssessmentQuestionParameters as craqp
+                                    WHERE craqp.CompetencyLearningResourceID = CompetencyResourceAssessmentQuestionParameters.CompetencyLearningResourceID)");
+            Create.UniqueConstraint("IX_CompetencyResourceAssessmentQuestionParameters_CompetencyLearningResourceID")
+                .OnTable("CompetencyResourceAssessmentQuestionParameters").Column("CompetencyLearningResourceID");
+        }
+        public override void Down()
+        {
+            Delete.Index("IX_CompetencyResourceAssessmentQuestionParameters_CompetencyLearningResourceID")
+                .OnTable("CompetencyResourceAssessmentQuestionParameters");
+        }
+    }
+}
