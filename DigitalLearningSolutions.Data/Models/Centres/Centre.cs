fea61c300aac5c755a77280c44bd027030b23516
Martin Wilkins <Martin.Wilkins@softwire.com>
2022-01-07 15:48:47 +0000

    HEEDLS-640 Markup cleanup

--- DigitalLearningSolutions.Data/Models/Centres/Centre.cs
+++ DigitalLearningSolutions.Data/Models/Centres/Centre.cs
@@ -31,11 +31,8 @@
         public int TrainerSpots { get; set; }
         public string? IpPrefix { get; set; }
         public string? ContractType { get; set; }
-        public int CentreTypeId { get; set; }
-        public string CentreType { get; set; }
         public int CustomCourses { get; set; }
         public long ServerSpaceUsed { get; set; }
         public long ServerSpaceBytes { get; set; }
-        public bool Active { get; set; }
     }
 }
