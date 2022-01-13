8862362e514a45706abe72128bbe9ec1ee40d5e9
Daniel Manta <Daniel.Manta@hee.nhs.uk>
2022-01-13 11:31:30 +0000

    DLSV2-466 Added migration for enabling soft deletion on table CompetencyLearningResources

---
+++ DigitalLearningSolutions.Data.Migrations/202202131111_AddRemovedDateToCompetencyLearningResources.cs
@@ -0,0 +1,17 @@
+﻿namespace DigitalLearningSolutions.Data.Migrations
+{
+    using FluentMigrator;
+
+    [Migration(202202131111)]
+    public class AddRemovedDateToCompetencyLearningResources : Migration
+    {
+        public override void Up()
+        {
+            Alter.Table("CompetencyLearningResources").AddColumn("RemovedDate").AsDateTime().Nullable();
+        }
+        public override void Down()
+        {
+            Delete.Column("RemovedDate").FromTable("CompetencyLearningResources");
+        }
+    }
+}
