87286c37fe6f32a34804a410bc575d3be75af219
Jonathan Bloxsom <jonathan.bloxsom@softwire.com>
2022-01-19 13:18:40 +0000

    HEEDLS-608 move application verification logic out of support tickets controller

--- DigitalLearningSolutions.Web/Controllers/Support/SupportTicketsController.cs
+++ DigitalLearningSolutions.Web/Controllers/Support/SupportTicketsController.cs
@@ -6,6 +6,7 @@
     using DigitalLearningSolutions.Web.Attributes;
     using DigitalLearningSolutions.Web.Helpers;
     using DigitalLearningSolutions.Web.Models.Enums;
+    using DigitalLearningSolutions.Web.ServiceFilter;
     using DigitalLearningSolutions.Web.ViewModels.Support;
     using Microsoft.AspNetCore.Authorization;
     using Microsoft.AspNetCore.Mvc;
@@ -16,6 +17,7 @@
     [Authorize(Policy = CustomPolicies.UserCentreAdminOrFrameworksAdmin)]
     [SetDlsSubApplication]
     [SetSelectedTab(nameof(NavMenuTab.Support))]
+    [TypeFilter(typeof(ValidateAllowedDlsSubApplication), Arguments = new object[] { new[] { nameof(DlsSubApplication.TrackingSystem), nameof(DlsSubApplication.Frameworks) } })]
     public class SupportTicketsController : Controller
     {
         private readonly IConfiguration configuration;
@@ -27,34 +29,14 @@
             this.configuration = configuration;
         }
 
-        public async Task<IActionResult> Index(DlsSubApplication dlsSubApplication)
+        public IActionResult Index(DlsSubApplication dlsSubApplication)
         {
-            if (!DlsSubApplication.TrackingSystem.Equals(dlsSubApplication) &&
-                !DlsSubApplication.Frameworks.Equals(dlsSubApplication))
-            {
-                return NotFound();
-            }
-
-            // TODO HEEDLS-608 If the user is centre admin but tracking system is off we need to show a 404
-            // TODO HEEDLS-608 name these something appropriate
-            var trackingSystemSupportEnabled =
-                DlsSubApplication.TrackingSystem.Equals(dlsSubApplication) &&
-                User.HasCentreAdminPermissions() &&
-                await featureManager.IsEnabledAsync(FeatureFlags.RefactoredTrackingSystem);
-            var frameworksSupportEnabled = DlsSubApplication.Frameworks.Equals(dlsSubApplication) &&
-                                           User.HasFrameworksAdminPermissions();
-
-            if (trackingSystemSupportEnabled || frameworksSupportEnabled)
-            {
-                var model = new SupportTicketsViewModel(
-                    dlsSubApplication,
-                    SupportPage.SupportTickets,
-                    configuration.GetCurrentSystemBaseUrl()
-                );
-                return View("Index", model);
-            }
-
-            return RedirectToAction("Index", "Home");
+            var model = new SupportTicketsViewModel(
+                dlsSubApplication,
+                SupportPage.SupportTickets,
+                configuration.GetCurrentSystemBaseUrl()
+            );
+            return View("Index", model);
         }
     }
 }
