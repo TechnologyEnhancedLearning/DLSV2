1b1572a01675d08b1228c2d6335ee0bb830cfd1d
Alex Jackson <Alexander.Jackson@dorsetsoftware.com>
2022-01-28 14:34:46 +0000

    HEEDLS-565 Add unit test for new data service method

--- DigitalLearningSolutions.Data.Tests/DataServices/CourseDelegatesDataServiceTests.cs
+++ DigitalLearningSolutions.Data.Tests/DataServices/CourseDelegatesDataServiceTests.cs
@@ -41,11 +41,58 @@
                 Completed = null,
                 CustomisationId = 1,
                 AllAttempts = 0,
-                AttemptsPassed = 0
+                AttemptsPassed = 0,
             };
 
             // When
             var result = courseDelegatesDataService.GetDelegatesOnCourse(1, 2).ToList();
+
+            // Then
+            using (new AssertionScope())
+            {
+                result.Should().HaveCount(3);
+                result.First().Should().BeEquivalentTo(expectedFirstRecord);
+            }
+        }
+
+        [Test]
+        public void GetDelegatesOnCourseForExport_returns_expected_values()
+        {
+            // Given
+            var expectedFirstRecord = new CourseDelegateForExport
+            {
+                Active = true,
+                CandidateNumber = "PC97",
+                CompleteByDate = null,
+                DelegateId = 32926,
+                EmailAddress = "erpock.hs@5bntu",
+                Enrolled = new DateTime(2012, 07, 02, 13, 30, 37, 807),
+                FirstName = "xxxxx",
+                LastName = "xxxx",
+                LastUpdated = new DateTime(2012, 07, 31, 10, 18, 39, 993),
+                Locked = false,
+                ProgressId = 18395,
+                RemovedDate = null,
+                Completed = null,
+                CustomisationId = 1,
+                Answer1 = null,
+                Answer2 = null,
+                Answer3 = null,
+                Answer4 = null,
+                Answer5 = null,
+                Answer6 = null,
+                CourseAnswer1 = "",
+                CourseAnswer2 = "",
+                CourseAnswer3 = "",
+                LoginCount = 1,
+                Duration = 0,
+                DiagnosticScore = 0,
+                AllAttempts = 0,
+                AttemptsPassed = 0,
+            };
+
+            // When
+            var result = courseDelegatesDataService.GetDelegatesOnCourseForExport(1, 2).ToList();
 
             // Then
             using (new AssertionScope())
