cae29d44d207e56d8935a5cf594a7d56c01fd69b
OliZor <olivia.zorn@softwire.com>
2022-02-09 14:43:50 +0000

    HEEDLS-510 Add migration to increase max length of GroupLabel

---
+++ DigitalLearningSolutions.Data.Migrations/202202091437_IncreaseGroupLabelMaxCharacterLength.cs
@@ -0,0 +1,24 @@
+﻿namespace DigitalLearningSolutions.Data.Migrations
+{
+    using FluentMigrator;
+
+    [Migration(202202091437)]
+    public class IncreaseGroupLabelMaxCharacterLength : Migration
+    {
+        public override void Up()
+        {
+            Alter.Column("GroupLabel")
+                .OnTable("Groups")
+                .AsString(255)
+                .NotNullable();
+        }
+
+        public override void Down()
+        {
+            Alter.Column("GroupLabel")
+                .OnTable("Groups")
+                .AsString(100)
+                .NotNullable();
+        }
+    }
+}
