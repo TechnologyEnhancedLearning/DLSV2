3aa5a2f784a4022f252185f4dcb7ffdd5464c03b
Alex Jackson <Alexander.Jackson@dorsetsoftware.com>
2022-02-18 13:41:33 +0000

    HEEDLS-606 Tidy up AdminUserDataService

--- DigitalLearningSolutions.Data/DataServices/UserDataService/AdminUserDataService.cs
+++ DigitalLearningSolutions.Data/DataServices/UserDataService/AdminUserDataService.cs
@@ -7,45 +7,49 @@
 
     public partial class UserDataService
     {
+        private const string BaseSelectAdminQuery =
+            @"SELECT
+                au.AdminID AS Id,
+                au.CentreID,
+                ct.CentreName,
+                ct.Active AS CentreActive,
+                au.Active,
+                au.Approved,
+                au.Email AS EmailAddress,
+                au.Forename AS FirstName,
+                au.Surname AS LastName,
+                au.Password,
+                au.CentreAdmin AS IsCentreAdmin,
+                au.IsCentreManager,
+                au.ContentCreator AS IsContentCreator,
+                au.ContentManager AS IsContentManager,
+                au.PublishToAll,
+                au.SummaryReports,
+                au.UserAdmin AS IsUserAdmin,
+                au.CategoryID,
+                CASE
+                    WHEN au.CategoryID = 0 THEN 'All'
+                    ELSE cc.CategoryName
+                END AS CategoryName,
+                au.Supervisor AS IsSupervisor,
+                au.Trainer AS IsTrainer,
+                au.IsFrameworkDeveloper,
+                au.ProfileImage,
+                au.IsFrameworkContributor,
+                au.IsWorkforceManager,
+                au.IsWorkforceContributor,
+                au.IsLocalWorkforceManager,
+                au.ImportOnly,
+                au.FailedLoginCount,
+                au.ResetPasswordId
+            FROM AdminUsers AS au
+            INNER JOIN Centres AS ct ON ct.CentreID = au.CentreID
+            LEFT JOIN CourseCategories AS cc ON cc.CourseCategoryID = au.CategoryID";
+
         public AdminUser? GetAdminUserById(int id)
         {
             var user = connection.Query<AdminUser>(
-                @"SELECT
-                        au.AdminID AS Id,
-                        au.CentreID,
-                        ct.CentreName,
-                        ct.Active AS CentreActive,
-                        au.Active,
-                        au.Approved,
-                        au.Email AS EmailAddress,
-                        au.Forename AS FirstName,
-                        au.Surname AS LastName,
-                        au.Password,
-                        au.CentreAdmin AS IsCentreAdmin,
-                        au.IsCentreManager,
-                        au.ContentCreator AS IsContentCreator,
-                        au.ContentManager AS IsContentManager,
-                        au.PublishToAll,
-                        au.SummaryReports,
-                        au.UserAdmin AS IsUserAdmin,
-                        au.CategoryID,
-                        CASE
-                            WHEN au.CategoryID = 0 THEN 'All'
-                            ELSE cc.CategoryName
-                        END AS CategoryName,
-                        au.Supervisor AS IsSupervisor,
-                        au.Trainer AS IsTrainer,
-                        au.IsFrameworkDeveloper,
-                        au.ProfileImage,
-                        au.IsFrameworkContributor,
-                        au.IsWorkforceManager,
-                        au.IsWorkforceContributor,
-                        au.IsLocalWorkforceManager,
-                        au.ImportOnly,
-                        au.FailedLoginCount
-                    FROM AdminUsers AS au
-                    INNER JOIN Centres AS ct ON ct.CentreID = au.CentreID
-                    LEFT JOIN CourseCategories AS cc ON cc.CourseCategoryID = au.CategoryID
+                @$"{BaseSelectAdminQuery}
                     WHERE au.AdminID = @id",
                 new { id }
             ).SingleOrDefault();
@@ -56,42 +60,7 @@
         public AdminUser? GetAdminUserByUsername(string username)
         {
             return connection.Query<AdminUser>(
-                @"SELECT
-                        au.AdminID AS Id,
-                        au.CentreID,
-                        ct.CentreName,
-                        ct.Active AS CentreActive,
-                        au.Active,
-                        au.Approved,
-                        au.Email AS EmailAddress,
-                        au.Forename AS FirstName,
-                        au.Surname AS LastName,
-                        au.Password,
-                        au.CentreAdmin AS IsCentreAdmin,
-                        au.IsCentreManager,
-                        au.ContentCreator AS IsContentCreator,
-                        au.ContentManager AS IsContentManager,
-                        au.PublishToAll,
-                        au.SummaryReports,
-                        au.UserAdmin AS IsUserAdmin,
-                        au.CategoryID,
-                        CASE
-                            WHEN au.CategoryID = 0 THEN 'All'
-                            ELSE cc.CategoryName
-                        END AS CategoryName,
-                        au.Supervisor AS IsSupervisor,
-                        au.Trainer AS IsTrainer,
-                        au.IsFrameworkDeveloper,
-                        au.ProfileImage,
-                        au.IsFrameworkContributor,
-                        au.IsWorkforceManager,
-                        au.IsWorkforceContributor,
-                        au.IsLocalWorkforceManager,
-                        au.ImportOnly,
-                        au.FailedLoginCount
-                    FROM AdminUsers AS au
-                    INNER JOIN Centres AS ct ON ct.CentreID = au.CentreID
-                    LEFT JOIN CourseCategories AS cc ON cc.CourseCategoryID = au.CategoryID
+                @$"{BaseSelectAdminQuery}
                     WHERE au.Active = 1 AND au.Approved = 1 AND (au.Login = @username OR au.Email = @username)",
                 new { username }
             ).SingleOrDefault();
@@ -100,43 +69,7 @@
         public AdminUser? GetAdminUserByEmailAddress(string emailAddress)
         {
             return connection.Query<AdminUser>(
-                @"SELECT
-                        au.AdminID AS Id,
-                        au.CentreID,
-                        ct.CentreName,
-                        ct.Active AS CentreActive,
-                        au.Active,
-                        au.Approved,
-                        au.Email AS EmailAddress,
-                        au.Forename AS FirstName,
-                        au.Surname AS LastName,
-                        au.Password,
-                        au.CentreAdmin AS IsCentreAdmin,
-                        au.IsCentreManager,
-                        au.ContentCreator AS IsContentCreator,
-                        au.ContentManager AS IsContentManager,
-                        au.PublishToAll,
-                        au.SummaryReports,
-                        au.UserAdmin AS IsUserAdmin,
-                        au.CategoryID,
-                        CASE
-                            WHEN au.CategoryID = 0 THEN 'All'
-                            ELSE cc.CategoryName
-                        END AS CategoryName,
-                        au.Supervisor AS IsSupervisor,
-                        au.Trainer AS IsTrainer,
-                        au.IsFrameworkDeveloper,
-                        au.ProfileImage,
-                        au.IsFrameworkContributor,
-                        au.IsWorkforceManager,
-                        au.IsWorkforceContributor,
-                        au.IsLocalWorkforceManager,
-                        au.ImportOnly,
-                        au.FailedLoginCount,
-                        au.ResetPasswordId
-                    FROM AdminUsers AS au
-                    INNER JOIN Centres AS ct ON ct.CentreID = au.CentreID
-                    LEFT JOIN CourseCategories AS cc ON cc.CourseCategoryID = au.CategoryID
+                @$"{BaseSelectAdminQuery}
                     WHERE (au.Email = @emailAddress)",
                 new { emailAddress }
             ).SingleOrDefault();
@@ -145,42 +78,7 @@
         public List<AdminUser> GetAdminUsersByCentreId(int centreId)
         {
             var users = connection.Query<AdminUser>(
-                @"SELECT
-                        au.AdminID AS Id,
-                        au.CentreID,
-                        ct.CentreName,
-                        ct.Active AS CentreActive,
-                        au.Active,
-                        au.Approved,
-                        au.Email AS EmailAddress,
-                        au.Forename AS FirstName,
-                        au.Surname AS LastName,
-                        au.Password,
-                        au.CentreAdmin AS IsCentreAdmin,
-                        au.IsCentreManager,
-                        au.ContentCreator AS IsContentCreator,
-                        au.ContentManager AS IsContentManager,
-                        au.PublishToAll,
-                        au.SummaryReports,
-                        au.UserAdmin AS IsUserAdmin,
-                        au.CategoryID,
-                        CASE
-                            WHEN au.CategoryID = 0 THEN 'All'
-                            ELSE cc.CategoryName
-                        END AS CategoryName,
-                        au.Supervisor AS IsSupervisor,
-                        au.Trainer AS IsTrainer,
-                        au.IsFrameworkDeveloper,
-                        au.ProfileImage,
-                        au.IsFrameworkContributor,
-                        au.IsWorkforceManager,
-                        au.IsWorkforceContributor,
-                        au.IsLocalWorkforceManager,
-                        au.ImportOnly,
-                        au.FailedLoginCount
-                    FROM AdminUsers AS au
-                    INNER JOIN Centres AS ct ON ct.CentreID = au.CentreID
-                    LEFT JOIN CourseCategories AS cc ON cc.CourseCategoryID = au.CategoryID
+                @$"{BaseSelectAdminQuery}
                     WHERE au.Active = 1 AND au.Approved = 1 AND au.CentreId = @centreId",
                 new { centreId }
             ).ToList();
