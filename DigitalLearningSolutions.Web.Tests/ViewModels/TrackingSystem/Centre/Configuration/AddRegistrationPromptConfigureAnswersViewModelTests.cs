d4b62d1cf162a7998288d7a9021025aff459e0dd
Jonathan Bloxsom <Jonathan.Bloxsom@softwire.com>
2022-03-18 15:23:01 +0000

    HEEDLS-596 test for new nontrivial view model behaviour

---
+++ DigitalLearningSolutions.Web.Tests/ViewModels/TrackingSystem/Centre/Configuration/AddRegistrationPromptConfigureAnswersViewModelTests.cs
@@ -0,0 +1,31 @@
+﻿namespace DigitalLearningSolutions.Web.Tests.ViewModels.TrackingSystem.Centre.Configuration
+{
+    using DigitalLearningSolutions.Web.ViewModels.TrackingSystem.Centre.Configuration.RegistrationPrompts;
+    using FluentAssertions;
+    using NUnit.Framework;
+
+    public class AddRegistrationPromptConfigureAnswersViewModelTests
+    {
+        [Test]
+        [TestCase("test\r\ntest", true)]
+        [TestCase("test\r\nTest", true)]
+        [TestCase("test  \r\ntest", true)]
+        [TestCase("test  \r\nTest", true)]
+        [TestCase("test \r\nToast \r\nTest", true)]
+        [TestCase("test \r\nToast", false)]
+        public void OptionsStringContainsDuplicates_detects_duplicates_in_options_string(
+            string optionsString,
+            bool expectedResult
+        )
+        {
+            // given
+            var model = new RegistrationPromptAnswersViewModel(optionsString);
+
+            // when
+            var result = model.OptionsStringContainsDuplicates();
+
+            // then
+            result.Should().Be(expectedResult);
+        }
+    }
+}
