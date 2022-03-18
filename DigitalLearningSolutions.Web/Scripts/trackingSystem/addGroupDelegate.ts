f299b09e37c7dfb0c8df34dc558ef1c282e5894f
Jonathan Bloxsom <Jonathan.Bloxsom@softwire.com>
2022-03-18 10:41:53 +0000

    HEEDLS-800 emails in search javascript

--- DigitalLearningSolutions.Web/Scripts/trackingSystem/addGroupDelegate.ts
+++ DigitalLearningSolutions.Web/Scripts/trackingSystem/addGroupDelegate.ts
@@ -1,6 +1,11 @@
-import { SearchSortFilterAndPaginate } from '../searchSortFilterAndPaginate/searchSortFilterAndPaginate';
+import {SearchSortFilterAndPaginate} from '../searchSortFilterAndPaginate/searchSortFilterAndPaginate';
 
 const groupIdElement = <HTMLSelectElement>document.getElementById('selected-group-Id');
 const groupId = groupIdElement?.value.trim();
 // eslint-disable-next-line no-new
-new SearchSortFilterAndPaginate(`TrackingSystem/Delegates/Groups/${groupId}/Delegates/Add/SelectDelegate/AllItems`, true, true, true, 'AddGroupDelegateFilter');
+new SearchSortFilterAndPaginate(`TrackingSystem/Delegates/Groups/${groupId}/Delegates/Add/SelectDelegate/AllItems`,
+  true,
+  true,
+  true,
+  'AddGroupDelegateFilter',
+  ['title', 'email', 'candidate-number']);
