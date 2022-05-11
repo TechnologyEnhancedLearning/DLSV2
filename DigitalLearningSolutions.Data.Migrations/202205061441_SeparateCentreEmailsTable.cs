2921e8e1ab963a84cc7ba65b117d326c58ab99bf
George Blackwell <George.Blackwell@softwire.com>
2022-05-11 16:22:49 +0100

    add typing & constraints

--- DigitalLearningSolutions.Data.Migrations/202205061441_SeparateCentreEmailsTable.cs
+++ DigitalLearningSolutions.Data.Migrations/202205061441_SeparateCentreEmailsTable.cs
@@ -17,6 +17,15 @@
             Rename.Column("Email").OnTable("AdminAccounts").To("Email_deprecated");
             Rename.Column("Email").OnTable("DelegateAccounts").To("Email_deprecated");
 
+            Execute.Sql(
+                @"CREATE UNIQUE NONCLUSTERED INDEX IX_UserCenterDetails_CentreId_Email
+                ON UserCenterDetails (CentreId, Email)
+                WHERE Email IS NOT NULL"
+            );
+
+            Create.UniqueConstraint("IX_UserCenterDetails_UserId_CentreId").OnTable("UserCenterDetails")
+                .Columns("UserId", "CentreId");
+
             Delete.Column("EmailVerified").FromTable("AdminAccounts");
             Delete.Column("EmailVerified").FromTable("DelegateAccounts");
         }
@@ -28,8 +37,8 @@
             Rename.Column("Email_deprecated").OnTable("AdminAccounts").To("Email");
             Rename.Column("Email_deprecated").OnTable("DelegateAccounts").To("Email");
 
-            Alter.Table("AdminAccounts").AddColumn("EmailVerified");
-            Alter.Table("DelegateAccounts").AddColumn("EmailVerified");
+            Alter.Table("AdminAccounts").AddColumn("EmailVerified").AsDateTime().Nullable();
+            Alter.Table("DelegateAccounts").AddColumn("EmailVerified").AsDateTime().Nullable();
         }
     }
 }
