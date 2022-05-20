824f8f418c45c47bea347af2e7e815f1cd34d4ae
Alex Jackson <Alexander.Jackson@dorsetsoftware.com>
2022-05-20 16:57:42 +0100

    HEEDLS-898 Bring back tests lost in auto conflict resolving

--- DigitalLearningSolutions.Data.Tests/DataServices/UserDataServiceTests/UserDataServiceTests.cs
+++ DigitalLearningSolutions.Data.Tests/DataServices/UserDataServiceTests/UserDataServiceTests.cs
@@ -3,6 +3,7 @@
     using DigitalLearningSolutions.Data.DataServices.UserDataService;
     using DigitalLearningSolutions.Data.Mappers;
     using DigitalLearningSolutions.Data.Tests.TestHelpers;
+    using FluentAssertions;
     using Microsoft.Data.SqlClient;
     using NUnit.Framework;
 
@@ -22,6 +23,28 @@
         {
             connection = ServiceTestHelper.GetDatabaseConnection();
             userDataService = new UserDataService(connection);
+        }
+
+        [Test]
+        [TestCase("email@test.com", 61188)]
+        [TestCase("ES2", 1)]
+        public void GetUserIdFromUsername_returns_expected_id_for_username(string username, int expectedUserId)
+        {
+            // When
+            var result = userDataService.GetUserIdFromUsername(username);
+
+            // Then
+            result.Should().Be(expectedUserId);
+        }
+
+        [Test]
+        public void GetUserAccountById_returns_expected_user_account()
+        {
+            // When
+            var result = userDataService.GetUserAccountById(2);
+
+            // Then
+            result.Should().BeEquivalentTo(UserTestHelper.GetDefaultUserAccount());
         }
     }
 }
