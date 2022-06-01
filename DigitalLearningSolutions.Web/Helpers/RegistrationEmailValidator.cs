a35185062e5fd1539862de46a14b24bb4abad13b
OliZor <olivia.zorn@softwire.com>
2022-06-01 09:06:00 +0100

    UAR-900 Refine validation and spacing in views

--- DigitalLearningSolutions.Web/Helpers/RegistrationEmailValidator.cs
+++ DigitalLearningSolutions.Web/Helpers/RegistrationEmailValidator.cs
@@ -1,6 +1,7 @@
 ﻿namespace DigitalLearningSolutions.Web.Helpers
 {
     using DigitalLearningSolutions.Data.Services;
+    using DigitalLearningSolutions.Web.Extensions;
     using DigitalLearningSolutions.Web.ViewModels.Register;
     using Microsoft.AspNetCore.Mvc.ModelBinding;
 
@@ -18,7 +19,11 @@
                 return;
             }
 
-            if (userService.EmailIsInUse(model.PrimaryEmail))
+            var isRegisterAdminJourney = centresService != null;
+            var primaryEmailIsValid = !modelState.HasError(nameof(PersonalInformationViewModel.PrimaryEmail));
+            var secondaryEmailIsValid = !modelState.HasError(nameof(PersonalInformationViewModel.SecondaryEmail));
+
+            if (primaryEmailIsValid && userService.EmailIsInUse(model.PrimaryEmail))
             {
                 modelState.AddModelError(
                     nameof(PersonalInformationViewModel.PrimaryEmail),
@@ -26,15 +31,21 @@
                     "if this is you, please log in at this centre via the My Account page"
                 );
             }
-            else if (centresService != null && model.Centre != null && model.SecondaryEmail == null && !centresService.DoesEmailMatchCentre(
-                model.PrimaryEmail,
-                model.Centre.Value
-            ))
+            else if (primaryEmailIsValid && isRegisterAdminJourney &&
+                     model.SecondaryEmail == null && !centresService.DoesEmailMatchCentre(
+                         model.PrimaryEmail,
+                         model.Centre.Value
+                     ))
             {
                 modelState.AddModelError(
                     nameof(PersonalInformationViewModel.PrimaryEmail),
                     "This email address does not match the one held by the centre"
                 );
+            }
+
+            if (!secondaryEmailIsValid)
+            {
+                return;
             }
 
             if (model.SecondaryEmail != null && userService.EmailIsInUse(model.SecondaryEmail))
@@ -45,7 +56,7 @@
                     "if this is you, please log in at this centre via the My Account page"
                 );
             }
-            else if (centresService != null && model.Centre != null && model.SecondaryEmail != null && !centresService.DoesEmailMatchCentre(
+            else if (isRegisterAdminJourney && model.SecondaryEmail != null && !centresService.DoesEmailMatchCentre(
                 model.SecondaryEmail,
                 model.Centre.Value
             ))
