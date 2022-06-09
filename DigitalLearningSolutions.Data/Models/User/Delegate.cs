c4c815eb7e74659b6942d62cf1d3ad97d599b464
OliZor <olivia.zorn@softwire.com>
2022-06-09 14:45:07 +0100

    UAR-951 Create new models

---
+++ DigitalLearningSolutions.Data/Models/User/Delegate.cs
@@ -0,0 +1,9 @@
+﻿namespace DigitalLearningSolutions.Data.Models.User
+{
+    public class Delegate
+    {
+        public DelegateAccount DelegateAccount { get; set; }
+        public UserAccount UserAccount { get; set; }
+        public UserCentreDetails UserCentreDetails { get; set; }
+    }
+}
