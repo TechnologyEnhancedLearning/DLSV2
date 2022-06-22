a8fa5c35f9383507209ac519b10ad025b2f43e28
OliZor <olivia.zorn@softwire.com>
2022-06-22 12:10:46 +0100

    HEEDLS-959 Add recurring SQL jobs

---
+++ DigitalLearningSolutions.Data.Migrations/RecurringSqlJobs/DeleteMultipageFormDataOlderThan30Days.sql
@@ -0,0 +1,4 @@
+﻿-- Deletes MultiPageFormData records older than 30 days. Should be run every night at 2AM.
+DELETE
+FROM MultiPageFormData
+WHERE CAST(CreatedDate AS DATE) <= GETDATE() - 30
---
+++ DigitalLearningSolutions.Data.Migrations/RecurringSqlJobs/DeleteResetPasswordRecordsOlderThan4Days.sql
@@ -0,0 +1,12 @@
+﻿-- Deletes ResetPassword records older than 4 days. Should be run every night at 3AM.
+BEGIN
+UPDATE c
+SET c.ResetPasswordID = NULL FROM Candidates AS c
+        INNER JOIN ResetPassword AS r
+ON r.ID = c.ResetPasswordID
+WHERE CAST (r.PasswordResetDateTime AS DATE) <= GETDATE() -4
+
+DELETE
+FROM ResetPassword
+WHERE CAST(PasswordResetDateTime AS DATE) <= GETDATE() - 4
+END
