d3af2adee5275c0b9332f1ef1bb4c3fd1d698a0a
OliZor <olivia.zorn@softwire.com>
2022-06-27 11:43:39 +0100

    HEEDLS-959 Clear old ResetPasswordIDs on admins

--- DigitalLearningSolutions.Data.Migrations/RecurringSqlJobs/DeleteResetPasswordRecordsOlderThan4Days.sql
+++ DigitalLearningSolutions.Data.Migrations/RecurringSqlJobs/DeleteResetPasswordRecordsOlderThan4Days.sql
@@ -1,5 +1,11 @@
 ﻿-- Deletes ResetPassword records older than 4 days. Should be run every night at 3AM.
 BEGIN
+UPDATE au
+SET au.ResetPasswordID = NULL FROM AdminUsers AS au
+INNER JOIN ResetPassword AS r
+ON r.ID = au.ResetPasswordID
+WHERE r.PasswordResetDateTime < DATEADD(day, -4, GETDATE())
+
 UPDATE c
 SET c.ResetPasswordID = NULL FROM Candidates AS c
 INNER JOIN ResetPassword AS r
