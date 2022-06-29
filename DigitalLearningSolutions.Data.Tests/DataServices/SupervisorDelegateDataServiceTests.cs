3d0c1dccd5a532ff8695c09c643f1943d5a92528
OliZor <olivia.zorn@softwire.com>
2022-06-29 08:11:34 +0100

    HEEDLS-899 Add tests for SupervisorDelegateDataService methods

---
+++ DigitalLearningSolutions.Data.Tests/DataServices/SupervisorDelegateDataServiceTests.cs
@@ -0,0 +1,123 @@
+﻿namespace DigitalLearningSolutions.Data.Tests.DataServices
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Transactions;
+    using Dapper;
+    using DigitalLearningSolutions.Data.DataServices;
+    using DigitalLearningSolutions.Data.Models.Email;
+    using DigitalLearningSolutions.Data.Models.Supervisor;
+    using DigitalLearningSolutions.Data.Tests.TestHelpers;
+    using FakeItEasy;
+    using FluentAssertions;
+    using Microsoft.Data.SqlClient;
+    using Microsoft.Extensions.Logging;
+    using NUnit.Framework;
+
+    internal class SupervisorDelegateDataServiceTests
+    {
+        private readonly Guid inviteHashForFirstSupervisorDelegateRecord =
+            Guid.Parse("72e44c4d-77bd-4bed-a254-7cc27ab32927");
+
+        private SqlConnection connection = null!;
+        private SupervisorDelegateDataService supervisorDelegateDataService = null!;
+
+        [SetUp]
+        public void Setup()
+        {
+            connection = ServiceTestHelper.GetDatabaseConnection();
+            var logger = A.Fake<ILogger<SupervisorDelegateDataService>>();
+            supervisorDelegateDataService = new SupervisorDelegateDataService(connection, logger);
+        }
+
+        [Test]
+        public void GetSupervisorDelegateRecordByInviteHash_returns_correct_record()
+        {
+            // Given
+            var expectedRecord = new SupervisorDelegate
+            {
+                ID = 8,
+                SupervisorAdminID = 1,
+                DelegateEmail = "kevin.whittaker@hee.nhs.uk",
+                CandidateID = 254480,
+                Added = DateTime.Parse("2021-06-28 16:40:35.507"),
+                NotificationSent = DateTime.Parse("2021-06-28 16:40:35.507"),
+                Removed = null,
+                SupervisorEmail = "kevin.whittaker@hee.nhs.uk",
+                AddedByDelegate = false,
+                CentreId = 101,
+            };
+
+            // When
+            var result =
+                supervisorDelegateDataService.GetSupervisorDelegateRecordByInviteHash(
+                    inviteHashForFirstSupervisorDelegateRecord
+                );
+
+            // Then
+            result.Should().BeEquivalentTo(expectedRecord);
+        }
+
+        [Test]
+        public void GetPendingSupervisorDelegateRecordsByEmailsAndCentre_returns_correct_records()
+        {
+            using var transaction = new TransactionScope();
+
+            // Given
+            const int centreId = 101;
+            const string delegateEmailForValidRecord = "primary@email.com";
+            const string delegateEmailForRecordWithNonNullCandidateId = "kevin.whittaker@hee.nhs.uk";
+            const string delegateEmailForRemovedRecord = "louis.theroux@gmail.com";
+            const string? nullDelegateEmail = null;
+            var currentTime = DateTime.UtcNow;
+
+            connection.Execute(
+                @"INSERT INTO SupervisorDelegates (SupervisorAdminID, DelegateEmail, CandidateID, Added,
+                                 NotificationSent, Removed, SupervisorEmail, AddedByDelegate, InviteHash)
+                    OUTPUT INSERTED.ID
+                    VALUES (1, @delegateEmailForValidRecord, null, @currentTime, @currentTime, null,
+                            'supervisor@email.com', 0, '72e44c4d-77bd-4bed-a254-7cc27ab32928')",
+                new { delegateEmailForValidRecord, currentTime }
+            );
+
+            // When
+            var result = supervisorDelegateDataService.GetPendingSupervisorDelegateRecordsByEmailsAndCentre(
+                centreId,
+                new List<string?>
+                {
+                    delegateEmailForValidRecord,
+                    delegateEmailForRecordWithNonNullCandidateId,
+                    delegateEmailForRemovedRecord,
+                    nullDelegateEmail,
+                }
+            ).ToList();
+
+            // Then
+            result.Count.Should().Be(1);
+            result.First().DelegateEmail.Should().Be(delegateEmailForValidRecord);
+        }
+
+        [Test]
+        public void UpdateSupervisorDelegateRecordsCandidateId_updates_record_correctly()
+        {
+            using var transaction = new TransactionScope();
+
+            // Given
+            const int candidateId = 1;
+
+            // When
+            supervisorDelegateDataService.UpdateSupervisorDelegateRecordsCandidateId(
+                new List<int> { 6, 7, 8 },
+                1
+            );
+
+            // Then
+            var updatedRecord =
+                supervisorDelegateDataService.GetSupervisorDelegateRecordByInviteHash(
+                    inviteHashForFirstSupervisorDelegateRecord
+                );
+            updatedRecord!.CandidateID.Should().Be(candidateId);
+        }
+    }
+}
