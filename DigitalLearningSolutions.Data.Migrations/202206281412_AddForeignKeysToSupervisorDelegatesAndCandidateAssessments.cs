0970cd025101751a77a176837f6c7d06d7e510da
Jonathan Bloxsom <Jonathan.Bloxsom@softwire.com>
2022-07-01 17:32:08 +0100

    HEEDLS-932 further cleaning

--- DigitalLearningSolutions.Data.Migrations/202206281412_AddForeignKeysToSupervisorDelegatesAndCandidateAssessments.cs
+++ DigitalLearningSolutions.Data.Migrations/202206281412_AddForeignKeysToSupervisorDelegatesAndCandidateAssessments.cs
@@ -7,7 +7,6 @@
     {
         public override void Up()
         {
-            // TODO HEEDLS-932 does the column name want to be capitalised?
             Alter.Table("SupervisorDelegates")
                 .AddColumn("DelegateUserID").AsInt32().Nullable()
                 .ForeignKey("Users", "ID");
@@ -18,15 +17,15 @@
                 .AddColumn("CentreID").AsInt32().Nullable()
                 .ForeignKey("Centres", "CentreID");
 
-            // script to populate columns
+
             Execute.Script("DigitalLearningSolutions.Data.Migrations/Scripts/HEEDLS-932-PopulateNewKeysOnSupervisorDelegatesAndCandidateAssessments.sql");
 
-            // remove null option on columns
+
             Alter.Table("CandidateAssessments")
                 .AlterColumn("DelegateUserID").AsInt32().NotNullable()
                 .AlterColumn("CentreID").AsInt32().NotNullable();
 
-            // remove constraints
+
             Delete.ForeignKey().FromTable("SupervisorDelegates").ForeignColumn("CandidateId")
                 .ToTable("Candidates").PrimaryColumn("CandidateID");
             Alter.Table("SupervisorDelegates").AlterColumn("CandidateId").AsInt32().Nullable();
@@ -35,7 +34,7 @@
                 .ToTable("Candidates").PrimaryColumn("CandidateID");
             Alter.Table("CandidateAssessments").AlterColumn("CandidateID").AsInt32().Nullable();
 
-            // rename column
+
             Rename.Column("CandidateID").OnTable("SupervisorDelegates").To("CandidateID_deprecated");
             Rename.Column("CandidateID").OnTable("CandidateAssessments").To("CandidateID_deprecated");
         }
@@ -45,11 +44,12 @@
             Rename.Column("CandidateID_deprecated").OnTable("SupervisorDelegates").To("CandidateID");
             Rename.Column("CandidateID_deprecated").OnTable("CandidateAssessments").To("CandidateID");
 
+
             Alter.Table("SupervisorDelegates").AlterColumn("CandidateID").AsInt32().Nullable()
                 .ForeignKey("DelegateAccounts", "ID");
-
             Alter.Table("CandidateAssessments").AlterColumn("CandidateID").AsInt32().Nullable()
                 .ForeignKey("DelegateAccounts", "ID");
+
 
             Delete.ForeignKey().FromTable("SupervisorDelegates").ForeignColumn("DelegateUserID")
                 .ToTable("Users").PrimaryColumn("ID");
