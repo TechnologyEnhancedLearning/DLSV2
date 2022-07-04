983fb6491f73ac0b5f3df1cfa254b370fc61e42c
kevwhitt-hee <kevin.whittaker@hee.nhs.uk>
2022-07-04 10:11:10 +0100

    DLSV2-563 Fixes confirmed filter so rejected results aren't matched

--- DigitalLearningSolutions.Web/Helpers/CompetencyFilterHelper.cs
+++ DigitalLearningSolutions.Web/Helpers/CompetencyFilterHelper.cs
@@ -47,7 +47,7 @@ namespace DigitalLearningSolutions.Web.Helpers
                     || (filters.Contains((int)SelfAssessmentCompetencyFilter.SelfAssessed) && c.AssessmentQuestions.Any(q => q.Verified == null && q.ResultId != null))
                     || (filters.Contains((int)SelfAssessmentCompetencyFilter.ConfirmationRequested) && c.AssessmentQuestions.Any(q => q.Verified == null && q.Requested != null))
                     || (filters.Contains((int)SelfAssessmentCompetencyFilter.ConfirmationRejected) && c.AssessmentQuestions.Any(q => q.Verified.HasValue && q.SignedOff != true))
-                    || (filters.Contains((int)SelfAssessmentCompetencyFilter.Verified) && c.AssessmentQuestions.Any(q => q.Verified.HasValue))
+                    || (filters.Contains((int)SelfAssessmentCompetencyFilter.Verified) && c.AssessmentQuestions.Any(q => q.Verified.HasValue && q.SignedOff == true))
                     where (wordsInSearchText.Count() == 0 || searchTextMatchesGroup || searchTextMatchesCompetencyDescription || searchTextMatchesCompetencyName)
                         && (!appliedResponseStatusFilters.Any() || responseStatusFilterMatchesAnyQuestion)
                     select c;
