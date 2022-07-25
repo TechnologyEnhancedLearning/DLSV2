75b810bce09975244fe47cea6726c50a6e71e393
Simon Brent <simon.brent@softwire.com>
2022-07-25 16:08:34 +0100

    HEEDLS-975 Added partial view for fields required when changing a user's password

---
+++ DigitalLearningSolutions.Web/ViewModels/Common/ViewComponents/SetPasswordViewModel.cs
@@ -0,0 +1,14 @@
+﻿namespace DigitalLearningSolutions.Web.ViewModels.Common.ViewComponents
+{
+    public class SetPasswordViewModel : ConfirmPasswordViewModel
+    {
+        public SetPasswordViewModel(
+            string label = "Password"
+        )
+        {
+            Label = label;
+        }
+
+        public string Label { get; set; }
+    }
+}
