5d02c0636cf2de32e122125a0ffd20675e3ab277
kevwhitt-hee <kevin.whittaker@hee.nhs.uk>
2022-08-08 08:49:14 +0100

    DLSV2-597 Self Assessment report view with DCSA report link

---
+++ DigitalLearningSolutions.Data/Models/SelfAssessments/Export/DCSADelegateCompletionStatus.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace DigitalLearningSolutions.Data.Models.SelfAssessments.Export
+{
+    public class DCSADelegateCompletionStatus
+    {
+        public int? EnrolledMonth { get; set; }
+        public int? EnrolledYear { get; set; }
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public string? Email { get; set; }
+        public string? CentreField1 { get; set; }
+        public string? CentreField2 { get; set; }
+        public string? CentreField3 { get; set; }
+        public string? Status { get; set; }
+    }
+}
---
+++ DigitalLearningSolutions.Web/Controllers/TrackingSystem/Centre/SelfAssessmentReports/SelfAssessmentReportsController.cs
@@ -0,0 +1,36 @@
+﻿namespace DigitalLearningSolutions.Web.Controllers.TrackingSystem.Centre.SelfAssessmentReports
+{
+    using DigitalLearningSolutions.Data.Services;
+    using DigitalLearningSolutions.Web.Helpers;
+    using Microsoft.AspNetCore.Mvc;
+    using System;
+
+    public class SelfAssessmentReportsController : Controller
+    {
+        private readonly ISelfAssessmentReportService selfAssessmentReportService;
+
+        public SelfAssessmentReportsController(
+            ISelfAssessmentReportService selfAssessmentReportService
+        )
+        {
+            this.selfAssessmentReportService = selfAssessmentReportService;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        [Route("Download")]
+        public IActionResult DownloadDigitalCapabilityToExcel()
+        {
+            var centreId = User.GetCentreId();
+            var dataFile = selfAssessmentReportService.GetDigitalCapabilityExcelExportForCentre(centreId);
+            var fileName = $"DLS DCSA Report - Centre {centreId} - downloaded {DateTime.Today:yyyy-MM-dd}.xlsx";
+            return File(
+                dataFile,
+                FileHelper.GetContentTypeFromFileName(fileName),
+                fileName
+            );
+        }
+    }
+}
