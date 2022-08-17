fa643944ef24cf39bf6e08cb64e670a75c2d9069
Jonathan Bloxsom <Jonathan.Bloxsom@softwire.com>
2022-08-17 17:01:18 +0100

    HEEDLS-1044 exclude inactive delegate account emails

--- DigitalLearningSolutions.Data/DataServices/UserDataService/UserCentreDetailsDataService.cs
+++ DigitalLearningSolutions.Data/DataServices/UserDataService/UserCentreDetailsDataService.cs
@@ -162,7 +162,7 @@
                     FROM DelegateAccounts AS da
                     INNER JOIN Centres AS c ON c.CentreID = da.CentreID
                     LEFT JOIN UserCentreDetails AS ucd ON ucd.UserID = da.UserID AND ucd.CentreID = c.CentreID
-                    WHERE da.UserID = @userId
+                    WHERE da.UserID = @userId AND da.Active = 1
 
                     UNION
 
