1975c28f022b083ba49ffb929c0daf897aa8ab69
kevwhitt-hee <kevin.whittaker@hee.nhs.uk>
2022-08-26 14:35:07 +0100

    Update htmleditor.ts

--- DigitalLearningSolutions.Web/Scripts/frameworks/htmleditor.ts
+++ DigitalLearningSolutions.Web/Scripts/frameworks/htmleditor.ts
@@ -3,7 +3,7 @@ import DOMPurify from 'dompurify';
 
 let jodited = false;
 if (jodited === false) {
-  const editor = Jodit.make(".html-editor", {
+  const editor = Jodit.make('.html-editor', {
     buttons: [
       'source', '|',
       'bold',
@@ -58,7 +58,7 @@ if (jodited === false) {
       'ul',
       'ol', '|',
       'undo', 'redo',
-    ], 
+    ],
     style: {
       backgroundColor: '#FFF',
     },
@@ -66,7 +66,7 @@ if (jodited === false) {
 
   if (editor != null) {
     jodited = true;
-    editor.e.on("blur", () => {
+    editor.e.on('blur', () => {
       const clean = DOMPurify.sanitize(editor.editor.innerHTML);
       editor.editor.innerHTML = clean;
     });
