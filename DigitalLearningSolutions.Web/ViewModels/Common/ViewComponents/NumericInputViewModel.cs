f64b01780b0c8e34a5c0d0fe3de47b59204a72df
kevwhitt-hee <kevin.whittaker@hee.nhs.uk>
2022-09-12 15:14:08 +0100

    DLSV2-548 Adds required to numeric-input vc model
    Dynamically adds aria-required attribute and (optional) label append when appropriate

--- DigitalLearningSolutions.Web/ViewModels/Common/ViewComponents/NumericInputViewModel.cs
+++ DigitalLearningSolutions.Web/ViewModels/Common/ViewComponents/NumericInputViewModel.cs
@@ -15,7 +15,8 @@
             string type,
             IEnumerable<string> errorMessages,
             string? cssClass = null,
-            string? hintText = null
+            string? hintText = null,
+            bool required = false
         )
         {
             var errorMessageList = errorMessages.ToList();
@@ -23,12 +24,13 @@
             Id = id;
             Class = cssClass;
             Name = name;
-            Label = label;
+            Label = (!required && !label.EndsWith("(optional)") ? label + " (optional)" : label);
             Value = value;
             Type = type;
             HintText = hintText;
             ErrorMessages = errorMessageList;
             HasError = errorMessageList.Any();
+            Required = required;
         }
 
         public string Id { get; set; }
@@ -39,5 +41,6 @@
         public string Type { get; set; }
         public string? HintText { get; set; }
         public IEnumerable<string> ErrorMessages { get; set; }
+        public bool Required { get; set; }
     }
 }
