c9505126f904412444510338d98f94b34c793b7e
Martin Foster <martin.foster2@hee.nhs.uk>
2022-09-21 11:21:49 +0100

    Improved unit tests for viewcomponent binding helper class.

--- DigitalLearningSolutions.Web.Tests/Helpers/ViewComponentValueToSetHelperTests.cs
+++ DigitalLearningSolutions.Web.Tests/Helpers/ViewComponentValueToSetHelperTests.cs
@@ -1,6 +1,4 @@
-﻿using System.Collections.Generic;
-
-namespace DigitalLearningSolutions.Web.Tests.Helpers
+﻿namespace DigitalLearningSolutions.Web.Tests.Helpers
 {
     using DigitalLearningSolutions.Web.ViewComponents;
     using FluentAssertions;
@@ -11,64 +9,107 @@ namespace DigitalLearningSolutions.Web.Tests.Helpers
 
     public class ViewComponentValueToSetHelperTests
     {
+        private ModelStateDictionary? _modelState;
+        private EmptyModelMetadataProvider? _modelMetadataProvider;
+        private ViewDataDictionary? _viewData;
+        private bool _populateWithCurrentValue;
+
         [SetUp]
         public void Setup()
         {
+            _modelState = new ModelStateDictionary();
+            _modelMetadataProvider = new EmptyModelMetadataProvider();
+            _viewData = new ViewDataDictionary(_modelMetadataProvider, _modelState);
+            _populateWithCurrentValue = true;
         }
 
         [Test]
         public void ValueToSetForSimpleType_returns_model_value()
         {
             // Given
-            var aspFor = "TestElement";
-            var populateWithCurrentValue = true;
-            var modelState = new ModelStateDictionary();
-            var modelMetadataProvider = new EmptyModelMetadataProvider();
-            var viewData = new ViewDataDictionary(modelMetadataProvider, modelState);
-            IEnumerable<string> errorMessages;
-
-            var model = new { TestElement = "SimpleValue" };
+            const string aspFor = "Description";
+            const string modelItemValue = "Framework description text.";
+            var model = new { Description = modelItemValue };
 
             // When
             var result =
                 ViewComponentValueToSetHelper.ValueToSetForSimpleType(
-                    model, aspFor, populateWithCurrentValue, viewData, out errorMessages
+                    model, aspFor, _populateWithCurrentValue, _viewData!, out var errorMessages
                 );
 
             // Then
             using (new AssertionScope())
             {
-                result.Should().BeEquivalentTo("SimpleValue");
+                result.Should().BeEquivalentTo(modelItemValue);
+                errorMessages.Should().HaveCount(0);
             }
         }
 
-        //[Test]
-        //public void ValueToSetForComplexType_returns_model_value()
-        //{
-        //    // Given
-        //    var aspFor = "SubElement.SubValue";
-        //    var populateWithCurrentValue = true;
-        //    var modelState = new ModelStateDictionary();
-        //    var modelMetadataProvider = new EmptyModelMetadataProvider();
-        //    var viewData = new ViewDataDictionary(modelMetadataProvider, modelState);
-        //    IEnumerable<string> errorMessages;
-        //    var types = aspFor.Split('.');
+        [Test]
+        public void ValueToSetForComplexType_returns_model_value()
+        {
+            // Given
+            const string subModelItemValue = "Description of competency group base.";
+            var subModelItem = new { Description = subModelItemValue };
+            var complexModel = new { CompetencyGroupBase = subModelItem };
 
-        //    var modelLeaf = new { Name = "NameValue" };
-        //    var simpleModel = new { TestElement = modelLeaf };
-        //    var complexModel = new { SubModel = simpleModel };
+            const string aspFor = "CompetencyGroupBase.Description";
+            var types = aspFor.Split('.');
 
-        //    // When
-        //    var result =
-        //        ViewComponentValueToSetHelper.ValueToSetForComplexType(
-        //            complexModel, aspFor, populateWithCurrentValue, types, viewData, out errorMessages
-        //        );
+            // When
+            var result =
+                ViewComponentValueToSetHelper.ValueToSetForComplexType(
+                    complexModel, aspFor, _populateWithCurrentValue, types, _viewData!, out var errorMessages
+                );
 
-        //    // Then
-        //    using (new AssertionScope())
-        //    {
-        //        result.Should().BeEquivalentTo("SubValue");
-        //    }
-        //}
+            // Then
+            using (new AssertionScope())
+            {
+                result.Should().BeEquivalentTo(subModelItemValue);
+                errorMessages.Should().HaveCount(0);
+            }
+        }
+
+        [Test]
+        public void DeriveValueToSet_returns_derived_complex_model_value()
+        {
+            // Given
+            const string subModelItemValue = "Description of competency.";
+            var subModelItem = new { Description = subModelItemValue };
+            var complexModel = new { Competency = subModelItem };
+
+            var aspFor = "Competency.Description";
+
+            // When
+            var result = ViewComponentValueToSetHelper.DeriveValueToSet(ref aspFor, _populateWithCurrentValue, complexModel, _viewData!, out var errorMessages);
+
+            // Then
+            using (new AssertionScope())
+            {
+                result.Should().BeEquivalentTo(subModelItemValue);
+                errorMessages.Should().HaveCount(0);
+            }
+        }
+
+        [Test]
+        public void DeriveValueToSet_returns_derived_simple_model_value()
+        {
+            // Given
+            const string modelItemValue = "Description of framework.";
+            var simpleModel = new { Description = modelItemValue };
+
+            var aspFor = "Description";
+
+            // When
+            var result = ViewComponentValueToSetHelper.DeriveValueToSet(ref aspFor, _populateWithCurrentValue, simpleModel, _viewData!, out var errorMessages);
+
+            // Then
+            using (new AssertionScope())
+            {
+                result.Should().BeEquivalentTo(modelItemValue);
+                errorMessages.Should().HaveCount(0);
+            }
+        }
+
     }
 }
