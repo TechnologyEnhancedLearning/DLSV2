71a92831f57fa3224bb2a617fe10691b9e8b68f6
LAPTOP-CTLLJBUM\man_l <man_luck@yahoo.com>
2022-10-03 12:32:12 +0100

    DLSV2: make migration for the stored procedure

---
+++ DigitalLearningSolutions.Data.Migrations/Scripts/DropActiveAvailableV6.sql
@@ -0,0 +1,4 @@
+/****** Object:  StoredProcedure [dbo].[GetActiveAvailableCustomisationsForCentreFiltered_V6]    Script Date: 03/10/2022 06:00:00 ******/
+DROP PROCEDURE [dbo].[GetActiveAvailableCustomisationsForCentreFiltered_V6]
+GO
+
