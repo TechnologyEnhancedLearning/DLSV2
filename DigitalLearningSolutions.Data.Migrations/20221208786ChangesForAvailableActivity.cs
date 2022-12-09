595adf169be5a75296d8a58098a8dd085ceddb8a
Manish <man_luck@yahoo.com>
2022-12-09 08:42:32 +0000

    TD-786 Show activities that do not allow self-enrolment on the Available Activities (without enrol button)

---
+++ DigitalLearningSolutions.Data.Migrations/20221208786ChangesForAvailableActivity.cs
@@ -0,0 +1,18 @@
+﻿namespace DigitalLearningSolutions.Data.Migrations
+{
+    using FluentMigrator;
+    using FluentMigrator.SqlServer;
+
+    [Migration(20221208786)]
+    public class ChangesForAvailableActivity : Migration
+    {
+        public override void Up()
+        {
+            Execute.Sql(Properties.Resources.TD_786_GetSelfRegisteredFlag_UP);
+        }
+        public override void Down()
+        {
+            Execute.Sql(Properties.Resources.TD_786_GetSelfRegisteredFlag_DOWN);
+        }
+    }
+}
