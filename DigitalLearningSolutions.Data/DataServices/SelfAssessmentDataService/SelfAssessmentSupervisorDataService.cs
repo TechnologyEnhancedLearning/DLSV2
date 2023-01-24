285c412dcf17be90cc216aa544bb50a2df7fc2cc
kevwhitt-hee <kevin.whittaker@hee.nhs.uk>
2023-01-24 08:37:02 +0000

    Adds missing bracket and excludes GUID emails in query

--- DigitalLearningSolutions.Data/DataServices/SelfAssessmentDataService/SelfAssessmentSupervisorDataService.cs
+++ DigitalLearningSolutions.Data/DataServices/SelfAssessmentDataService/SelfAssessmentSupervisorDataService.cs
@@ -239,7 +239,7 @@
                         INNER JOIN CentreSelfAssessments CSA on csa.CentreID = DA.CentreID
                         where DA.UserID = @delegateUserId And DA.Active = 1
                         AND CSA.SelfAssessmentID=@selfAssessmentId)
-                        AND (COALESCE(CategoryID, 0) = 0) OR (CategoryID IN (select CategoryID from SelfAssessments where ID=@selfAssessmentId)))
+                        AND ((COALESCE(CategoryID, 0) = 0) OR (CategoryID IN (select CategoryID from SelfAssessments where ID=@selfAssessmentId)))
                         AND AdminID NOT IN (
                         SELECT sd.SupervisorAdminID
                         FROM CandidateAssessmentSupervisors AS cas
@@ -253,7 +253,7 @@
                         AND (cas.Removed IS NULL)
                         AND (sd.Removed IS NULL)
                         )
-                        AND (Supervisor = 1 OR NominatedSupervisor = 1) AND (Active = 1) ",
+                        AND (Supervisor = 1 OR NominatedSupervisor = 1) AND (Active = 1) AND (Email LIKE '%@%') ",
                 new { centreId, selfAssessmentId, delegateUserId }
             );
         }
