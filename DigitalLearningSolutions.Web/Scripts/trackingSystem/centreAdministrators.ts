efe69fbf4158570d7dccc8d7a8a95b59de4ff788
Manish <man_luck@yahoo.com>
2023-02-17 10:46:12 +0000

    Fixed centre administrators not showing any search results while searching from email

--- DigitalLearningSolutions.Web/Scripts/trackingSystem/centreAdministrators.ts
+++ DigitalLearningSolutions.Web/Scripts/trackingSystem/centreAdministrators.ts
@@ -1,4 +1,4 @@
 import { SearchSortFilterAndPaginate } from '../searchSortFilterAndPaginate/searchSortFilterAndPaginate';
 
 // eslint-disable-next-line no-new
-new SearchSortFilterAndPaginate('TrackingSystem/Centre/Administrators/AllAdmins', true, true, true, 'AdminFilter');
+new SearchSortFilterAndPaginate('TrackingSystem/Centre/Administrators/AllAdmins', true, true, true, 'AdminFilter', ['title', 'email']);
