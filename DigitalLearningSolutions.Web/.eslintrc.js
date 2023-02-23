7c36db2788a5caf0ad2681da2be1c55b5c7eb703
martinfoster-hee <martin.foster2@hee.nhs.uk>
2023-02-23 15:23:35 +0000

    Completed reformat of other files types in Web project.

--- DigitalLearningSolutions.Web/.eslintrc.js
+++ DigitalLearningSolutions.Web/.eslintrc.js
@@ -1,58 +1,58 @@
 const rules = {
-  'import/extensions': [
-    'error',
-    'ignorePackages',
-    {
-      js: 'never',
-      jsx: 'never',
-      ts: 'never',
-      tsx: 'never',
-    },
-  ],
-  'linebreak-style': 'off',
-  'import/no-extraneous-dependencies': ['error', { devDependencies: ['Scripts/spec/**/*.ts', './*.js'] }],
-  'no-use-before-define': ['error', { functions: false }],
-  'max-len': 'off',
+    'import/extensions': [
+        'error',
+        'ignorePackages',
+        {
+            js: 'never',
+            jsx: 'never',
+            ts: 'never',
+            tsx: 'never',
+        },
+    ],
+    'linebreak-style': 'off',
+    'import/no-extraneous-dependencies': ['error', { devDependencies: ['Scripts/spec/**/*.ts', './*.js'] }],
+    'no-use-before-define': ['error', { functions: false }],
+    'max-len': 'off',
 };
 
 module.exports = {
-  env: {
-    browser: true,
-    es2020: true,
-    jasmine: true,
-  },
-  settings: {
-    'import/resolver': {
-      node: {},
-      webpack: {},
+    env: {
+        browser: true,
+        es2020: true,
+        jasmine: true,
     },
-  },
-  extends: [
-    'airbnb-base',
-    'eslint:recommended',
-  ],
-  plugins: [
-    'jasmine',
-  ],
-  rules,
-  overrides: [
-    {
-      files: ['*.ts'],
-      parser: '@typescript-eslint/parser',
-      parserOptions: {
-        ecmaVersion: 11,
-        sourceType: 'module',
-      },
-      extends: [
+    settings: {
+        'import/resolver': {
+            node: {},
+            webpack: {},
+        },
+    },
+    extends: [
         'airbnb-base',
-        'plugin:@typescript-eslint/eslint-recommended',
-        'plugin:@typescript-eslint/recommended',
-      ],
-      plugins: [
-        '@typescript-eslint',
+        'eslint:recommended',
+    ],
+    plugins: [
         'jasmine',
-      ],
-      rules,
-    },
-  ],
+    ],
+    rules,
+    overrides: [
+        {
+            files: ['*.ts'],
+            parser: '@typescript-eslint/parser',
+            parserOptions: {
+                ecmaVersion: 11,
+                sourceType: 'module',
+            },
+            extends: [
+                'airbnb-base',
+                'plugin:@typescript-eslint/eslint-recommended',
+                'plugin:@typescript-eslint/recommended',
+            ],
+            plugins: [
+                '@typescript-eslint',
+                'jasmine',
+            ],
+            rules,
+        },
+    ],
 };
--- DigitalLearningSolutions.Web/webpack.config.js
+++ DigitalLearningSolutions.Web/webpack.config.js
@@ -5,70 +5,70 @@ const coreJsVersion = require('core-js/package.json').version;
 
 const entry = {};
 glob.sync(
-  './Scripts/**/*.ts',
-  {
-    ignore: [
-      './Scripts/**/*.d.ts',
-      './Scripts/helpers/**/*.ts',
-      './Scripts/spec/**/*.*',
-    ],
-  },
+    './Scripts/**/*.ts',
+    {
+        ignore: [
+            './Scripts/**/*.d.ts',
+            './Scripts/helpers/**/*.ts',
+            './Scripts/spec/**/*.*',
+        ],
+    },
 ).forEach((file) => {
-  const name = file.replace('./Scripts/', '').replace('.ts', '');
-  entry[name] = file;
+    const name = file.replace('./Scripts/', '').replace('.ts', '');
+    entry[name] = file;
 });
 
 const config = {
-  entry,
-  mode: 'production',
-  target: ['web', 'es5'],
-  output: {
-    path: path.join(__dirname, './wwwroot/js/'),
-    filename: '[name].js',
-    devtoolModuleFilenameTemplate: (info) => path.join('../../', info.resourcePath),
-  },
-  module: {
-    rules: [
-      {
-        test: /\.[j,t]s$/,
-        exclude: [
-          /\bcore-js\b/,
-        ],
-        loader: 'babel-loader',
-        options: {
-          presets: [
-            ['@babel/preset-env',
-              {
-                targets: {
-                  ie: '11',
+    entry,
+    mode: 'production',
+    target: ['web', 'es5'],
+    output: {
+        path: path.join(__dirname, './wwwroot/js/'),
+        filename: '[name].js',
+        devtoolModuleFilenameTemplate: (info) => path.join('../../', info.resourcePath),
+    },
+    module: {
+        rules: [
+            {
+                test: /\.[j,t]s$/,
+                exclude: [
+                    /\bcore-js\b/,
+                ],
+                loader: 'babel-loader',
+                options: {
+                    presets: [
+                        ['@babel/preset-env',
+                            {
+                                targets: {
+                                    ie: '11',
+                                },
+                                corejs: coreJsVersion,
+                                useBuiltIns: 'entry',
+                            },
+                        ],
+                        ['@babel/preset-typescript',
+                            {
+                                onlyRemoveTypeImports: true,
+                            },
+                        ],
+                    ],
                 },
-                corejs: coreJsVersion,
-                useBuiltIns: 'entry',
-              },
-            ],
-            ['@babel/preset-typescript',
-              {
-                onlyRemoveTypeImports: true,
-              },
-            ],
-          ],
-        },
-      },
+            },
+        ],
+    },
+    plugins: [
+        new ForkTsCheckerWebpackPlugin({
+            typescript: {
+                diagnosticOptions: {
+                    semantic: true,
+                    syntactic: true,
+                },
+            },
+        }),
     ],
-  },
-  plugins: [
-    new ForkTsCheckerWebpackPlugin({
-      typescript: {
-        diagnosticOptions: {
-          semantic: true,
-          syntactic: true,
-        },
-      },
-    }),
-  ],
-  resolve: {
-    extensions: ['.ts', '.js'],
-  },
+    resolve: {
+        extensions: ['.ts', '.js'],
+    },
 };
 
 module.exports = config;
