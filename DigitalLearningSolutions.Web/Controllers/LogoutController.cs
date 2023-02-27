1d2369a32d3b8936d40353cefa9f37611ed0e71e
Colin Beeby <colin.beeby@hee.nhs.uk>
2023-02-27 13:42:15 +0000

    Added code to prevent concurrent logins from the same user

--- DigitalLearningSolutions.Web/Controllers/LogoutController.cs
+++ DigitalLearningSolutions.Web/Controllers/LogoutController.cs
@@ -1,15 +1,46 @@
 ﻿namespace DigitalLearningSolutions.Web.Controllers
 {
+    using System;
     using System.Threading.Tasks;
     using DigitalLearningSolutions.Web.Extensions;
+    using DigitalLearningSolutions.Web.Helpers;
+    using DigitalLearningSolutions.Web.Services;
     using Microsoft.AspNetCore.Mvc;
 
     public class LogoutController : Controller
     {
+        private readonly ISessionService sessionService;
+
+        public LogoutController(
+            ISessionService sessionService
+        )
+        {
+            this.sessionService = sessionService;
+        }
+
+        [HttpGet]
         [HttpPost]
         public async Task<IActionResult> Index()
         {
             await HttpContext.Logout();
+            int? adminId = User.GetCustomClaimAsRequiredInt(CustomClaimTypes.UserAdminId);
+            if (adminId != null)
+            {
+                try
+                {
+                    if(TempData.Peek("AdminSession") != null)
+                    {
+                        int adminSessionId = int.Parse(TempData.Peek("AdminSessionID").ToString());
+
+                        sessionService.StopAdminSession(adminId.Value, adminSessionId);
+                    }
+                }
+                catch(InvalidCastException ex)
+                {
+                    Serilog.Log.Error(ex.Message);
+                }
+               
+            }
             return RedirectToAction("Index", "Home");
         }
     }
