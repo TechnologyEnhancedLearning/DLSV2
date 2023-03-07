74337c8ee8245df9d14cc0ee27ca529ce2cf3b61
OluwatobiAwe <oluwatobi.awe@hee.nhs.uk>
2023-03-07 11:30:15 +0000

    TD-899 Service Test update

---
+++ DigitalLearningSolutions.Data.Tests/Services/SupervisorServiceTest.cs
@@ -0,0 +1,33 @@
+﻿using DigitalLearningSolutions.Data.DataServices;
+using DigitalLearningSolutions.Data.Tests.TestHelpers;
+using FakeItEasy;
+using FluentAssertions;
+using Microsoft.Extensions.Logging;
+using NUnit.Framework;
+
+namespace DigitalLearningSolutions.Data.Tests.Services
+{
+    public class SupervisorServiceTest
+    {
+        private ISupervisorService supervisorService = null!;
+
+        [SetUp]
+        public void SetUp()
+        {
+            var connection = ServiceTestHelper.GetDatabaseConnection();
+            var logger = A.Fake<ILogger<SupervisorService>>();
+            supervisorService = new SupervisorService(connection, logger);
+        }
+
+
+        [Test]
+        public void GetNonNationalSupervisorDelegateAssessmentWithoutAssignedSupervisor_returns_empty_record()
+        {
+            // When
+            var result = supervisorService.GetSelfAssessmentsForSupervisorDelegateId(8, 1);
+
+            // Then
+            result.Should().BeEmpty();
+        }
+    }
+}
