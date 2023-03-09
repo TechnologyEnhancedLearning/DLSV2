544015c1bef278469ac9bfe82e91b57b23960713
Sarathlal-Sarangadharan <Sarathlal.Sarangadharan@hee.nhs.uk>
2023-03-09 11:05:13 +0000

    TD-997:Super Admin - User Accounts - Set password view

---
+++ DigitalLearningSolutions.Web/Controllers/SuperAdmin/Users/AdminUserSetPasswordController.cs
@@ -0,0 +1,60 @@
+﻿namespace DigitalLearningSolutions.Web.Controllers.SuperAdmin.Users
+{
+    using DigitalLearningSolutions.Data.Enums;
+    using DigitalLearningSolutions.Web.Attributes;
+    using DigitalLearningSolutions.Web.Helpers;
+    using DigitalLearningSolutions.Web.Models.Enums;
+    using DigitalLearningSolutions.Web.Services;
+    using DigitalLearningSolutions.Web.ViewModels.SuperAdmin.Users;
+    using Microsoft.AspNetCore.Authorization;
+    using Microsoft.AspNetCore.Mvc;
+    using Microsoft.FeatureManagement.Mvc;
+    using System.Threading.Tasks;
+
+    [FeatureGate(FeatureFlags.RefactoredSuperAdminInterface)]
+    [Authorize(Policy = CustomPolicies.UserSuperAdmin)]
+    [SetDlsSubApplication(nameof(DlsSubApplication.SuperAdmin))]
+    [SetSelectedTab(nameof(NavMenuTab.Admins))]
+    [Route("SuperAdmin/Users/AdminUserSetPassword/{userId:int}")]
+    public class AdminUserSetPasswordController : Controller
+    {
+        private readonly IPasswordService passwordService;
+        private readonly IUserService userService;
+        public AdminUserSetPasswordController(IPasswordService passwordService, IUserService userService)
+        {
+            this.passwordService = passwordService;
+            this.userService = userService;
+        }
+        [HttpGet]
+        public IActionResult Index(int userId, DlsSubApplication dlsSubApplication)
+        {
+            var userEntity = userService.GetUserById(userId);
+            TempData["UserID"] = userId;
+            TempData["UserName"]= userEntity.UserAccount.FirstName+" "+ userEntity.UserAccount.LastName+" ("+userEntity.UserAccount.PrimaryEmail+")";
+            var model = new SetAdminUserPasswordViewModel(dlsSubApplication);
+            return View("AdminUserSetPassword", model);
+        }
+        
+        [HttpPost]
+        public async Task<IActionResult> Index(SetPasswordFormData formData, DlsSubApplication dlsSubApplication)
+        {
+            TempData.Peek("UserID");
+            var userId = TempData["UserID"];
+            
+
+            if (!ModelState.IsValid)
+            {
+                var model = new SetAdminUserPasswordViewModel(formData, dlsSubApplication);
+                return View("AdminUserSetPassword", model);
+            }
+
+            var newPassword = formData.Password!;
+
+            await passwordService.ChangePasswordAsync((int)userId, newPassword);
+
+            //Reload user account page here.Waiting for TD-992 to completed
+            var model1 = new UserAccountsViewModel();
+            return View("Index",model1);
+        }
+    }
+}
---
+++ DigitalLearningSolutions.Web/ViewModels/SuperAdmin/Users/SetPasswordViewModel.cs
@@ -0,0 +1,19 @@
+﻿namespace DigitalLearningSolutions.Web.ViewModels.SuperAdmin.Users
+{
+    using DigitalLearningSolutions.Web.Models.Enums;
+    public class SetPasswordViewModel : SetPasswordFormData
+    {
+        public SetPasswordViewModel(DlsSubApplication dlsSubApplication) : this(
+            new SetPasswordFormData(),
+            dlsSubApplication
+        )
+        { }
+
+        public SetPasswordViewModel(SetPasswordFormData formData, DlsSubApplication dlsSubApplication) : base(formData)
+        {
+            DlsSubApplication = dlsSubApplication;
+        }
+
+        public DlsSubApplication DlsSubApplication { get; set; }
+    }
+}
