5f7a98607ae3381b68296e21168dc385cf5e2b06
Sarathlal-Sarangadharan <Sarathlal.Sarangadharan@hee.nhs.uk>
2023-03-09 14:33:50 +0000

    TD-997:Super Admin - User Accounts - Set password view

---
+++ DigitalLearningSolutions.Web/ViewModels/SuperAdmin/Users/SetPasswordFormData.cs
@@ -0,0 +1,14 @@
+﻿namespace DigitalLearningSolutions.Web.ViewModels.SuperAdmin.Users
+{
+    using DigitalLearningSolutions.Web.ViewModels.Common;
+    public class SetPasswordFormData : ConfirmPasswordViewModel
+    {
+        public SetPasswordFormData() { }
+
+        protected SetPasswordFormData(SetPasswordFormData formData)
+        {
+            Password = formData.Password;
+            ConfirmPassword = formData.ConfirmPassword;
+        }
+    }
+}
