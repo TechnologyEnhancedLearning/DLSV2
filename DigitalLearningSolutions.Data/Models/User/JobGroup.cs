d80f5eff4d378017362b7b56b6a299b237025ec2
Manish <man_luck@yahoo.com>
2023-03-12 16:19:32 +0000

    TD-992

---
+++ DigitalLearningSolutions.Data/Models/User/JobGroup.cs
@@ -0,0 +1,9 @@
+﻿namespace DigitalLearningSolutions.Data.Models.User
+{
+    public class JobGroup
+    {
+        public int JobGroupID { get; set; }
+
+        public string JobGroupName { get; set; }
+    }
+}
