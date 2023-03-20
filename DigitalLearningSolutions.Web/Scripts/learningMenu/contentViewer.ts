b5088a0104a2d52a574c45c68f0786a1db1806fb
Colin Beeby <colin.beeby@hee.nhs.uk>
2023-03-20 16:35:32 +0000

    Added session keep alive heartbeat code

--- DigitalLearningSolutions.Web/Scripts/learningMenu/contentViewer.ts
+++ DigitalLearningSolutions.Web/Scripts/learningMenu/contentViewer.ts
@@ -1,4 +1,5 @@
 import { setupFullscreen } from './fullscreen';
+import { keepSessionAlive, heartbeatInterval } from './keepSessionAlive';
 
 function closeMpe(): void {
   // Extract the current domain, customisationId, sectionId and tutorialId out of the URL
@@ -14,3 +15,6 @@ function closeMpe(): void {
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 (window as any).closeMpe = closeMpe;
 setupFullscreen();
+
+// send out a heartbeat, to keep this session alive, once a minute
+setInterval(keepSessionAlive, heartbeatInterval);
