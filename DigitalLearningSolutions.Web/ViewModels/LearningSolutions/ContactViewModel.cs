c859308222f7c0b561a16372fc6789200b35a0cb
OluwatobiAwe <oluwatobi.awe@hee.nhs.uk>
2023-04-12 12:11:22 +0100

    TD-1283 Add-centre-contact details into the contact-us screen.

--- DigitalLearningSolutions.Web/ViewModels/LearningSolutions/ContactViewModel.cs
+++ DigitalLearningSolutions.Web/ViewModels/LearningSolutions/ContactViewModel.cs
@@ -1,14 +1,23 @@
 ﻿namespace DigitalLearningSolutions.Web.ViewModels.LearningSolutions
 {
+    using DigitalLearningSolutions.Data.Models.Centres;
     using Microsoft.AspNetCore.Html;
 
     public class ContactViewModel
     {
         public HtmlString ContactText { get; }
 
+        public CentreSummaryForFindYourCentre CentreSummary { get; }
+
         public ContactViewModel(string contactText)
         {
             ContactText = new HtmlString(contactText);
+
+        }
+        public ContactViewModel(string contactText, CentreSummaryForFindYourCentre centreSummary)
+        {
+            ContactText = new HtmlString(contactText);
+            CentreSummary = centreSummary;
         }
     }
 }
