9c670d5b8a9d05b5727e68eed19e2a16fe5224f0
Sarathlal-Sarangadharan <Sarathlal.Sarangadharan@hee.nhs.uk>
2023-04-12 18:53:55 +0100

    TD-1220:Review comments implemented

--- DigitalLearningSolutions.Data.Migrations/202304111130_AddSystemVersioning_SelfAssessmentResultSupervisorVerifications.cs
+++ DigitalLearningSolutions.Data.Migrations/202304111130_AddSystemVersioning_SelfAssessmentResultSupervisorVerifications.cs
@@ -2,42 +2,16 @@
 {
     using FluentMigrator;
 
-    [Migration(202304111330)]
+    [Migration(202304121130)]
     public class AddSystemVersioning_SelfAssessmentResultSupervisorVerifications : Migration
     {
         public override void Up()
         {
-            Execute.Sql(
-                   @$"DELETE FROM SelfAssessmentResultSupervisorVerifications
-                        WHERE SelfAssessmentResultId NOT IN (SELECT MAX(ID)
-                        FROM SelfAssessmentResults
-                        GROUP BY CompetencyID, AssessmentQuestionID, DelegateUserID)"
-               );
-            Execute.Sql(
-                  @$"DELETE FROM SelfAssessmentResults
-                        WHERE ID NOT IN (
-                        SELECT MAX(ID)
-                        FROM SelfAssessmentResults
-                        GROUP BY CompetencyID, AssessmentQuestionID, DelegateUserID)"
-              );
-            Create.UniqueConstraint(
-                    "IX_SelfAssessmentResults_DelegateUserID_CompetencyID_AssessmentQuestionID"
-                )
-                .OnTable("SelfAssessmentResults").Columns(
-                    "DelegateUserID",
-                    "CompetencyID",
-                    "AssessmentQuestionID"
-                );
             Execute.Sql(Properties.Resources.TD_1220_AddSystemVersioning_SelfAssessmentResultSupervisorVerifications);
-
         }
 
         public override void Down()
         {
-            Delete.UniqueConstraint(
-                    "IX_SelfAssessmentResults_DelegateUserID_CompetencyID_AssessmentQuestionID"
-                )
-                .FromTable("SelfAssessmentResults");
             Execute.Sql(Properties.Resources.TD_1220_RemoveSystemVersioning_SelfAssessmentResultSupervisorVerifications);
         }
     }
---
+++ DigitalLearningSolutions.Data.Migrations/202304121130_Delete_SelfAssessmentResult_SupervisorVerifications.cs
@@ -0,0 +1,44 @@
+﻿namespace DigitalLearningSolutions.Data.Migrations
+{
+    using FluentMigrator;
+
+    [Migration(202304121630)]
+    public class Delete_SelfAssessmentResult_SupervisorVerifications : Migration
+    {
+        public override void Up()
+        {
+
+            Execute.Sql(
+                   @$"DELETE FROM SelfAssessmentResultSupervisorVerifications
+                        WHERE SelfAssessmentResultId NOT IN (SELECT MAX(ID)
+                        FROM SelfAssessmentResults
+                        GROUP BY CompetencyID, AssessmentQuestionID, DelegateUserID)"
+               );
+            Execute.Sql(
+                  @$"DELETE FROM SelfAssessmentResults
+                        WHERE ID NOT IN (
+                        SELECT MAX(ID)
+                        FROM SelfAssessmentResults
+                        GROUP BY CompetencyID, AssessmentQuestionID, DelegateUserID)"
+              );
+            Create.UniqueConstraint(
+                    "IX_SelfAssessmentResults_DelegateUserID_CompetencyID_AssessmentQuestionID"
+                )
+                .OnTable("SelfAssessmentResults").Columns(
+                    "DelegateUserID",
+                    "CompetencyID",
+                    "AssessmentQuestionID"
+                );
+
+
+        }
+
+        public override void Down()
+        {
+            Delete.UniqueConstraint(
+                    "IX_SelfAssessmentResults_DelegateUserID_CompetencyID_AssessmentQuestionID"
+                )
+                .FromTable("SelfAssessmentResults");
+        }
+    }
+}
