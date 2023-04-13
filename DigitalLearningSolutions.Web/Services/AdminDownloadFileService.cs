9d610e589e571873151b359d1192b59812642cfd
Auldrin-Possa <auldrin.possa@hee.nhs.uk>
2023-04-13 12:08:59 +0100

    TD-951- columns added to excel report.

--- DigitalLearningSolutions.Web/Services/AdminDownloadFileService.cs
+++ DigitalLearningSolutions.Web/Services/AdminDownloadFileService.cs
@@ -204,6 +204,12 @@
                     new DataColumn(IsSuperAdmin),
                     new DataColumn(IsReportsViewer),
 
+                    new DataColumn(IsFrameworkDeveloper),
+                    new DataColumn(IsFrameworkContributor),
+                    new DataColumn(IsWorkforceManager),
+                    new DataColumn(IsWorkforceContributor),
+                    new DataColumn(IsLocalWorkforceManager),
+
                     new DataColumn(UserID),
                     new DataColumn(CentreID),
                     new DataColumn(CategoryID)
@@ -242,6 +248,12 @@
             row[IsSuperAdmin] = adminRecord.AdminAccount?.IsSuperAdmin;
             row[IsReportsViewer] = adminRecord.AdminAccount?.IsReportsViewer;
 
+            row[IsFrameworkDeveloper] = adminRecord.AdminAccount?.IsFrameworkDeveloper;
+            row[IsFrameworkContributor] = adminRecord.AdminAccount?.IsFrameworkContributor;
+            row[IsWorkforceManager] = adminRecord.AdminAccount?.IsWorkforceManager;
+            row[IsWorkforceContributor] = adminRecord.AdminAccount?.IsWorkforceContributor;
+            row[IsLocalWorkforceManager] = adminRecord.AdminAccount?.IsLocalWorkforceManager;
+
             row[UserID] = adminRecord.AdminAccount?.UserId;
             row[CentreID] = adminRecord.AdminAccount?.CentreId;
             row[CategoryID] = adminRecord.AdminAccount?.CategoryId;
@@ -258,7 +270,8 @@
             }
             var boolColumns = new[] { IsCentreAdmin, IsReportsViewer, IsSuperAdmin,
                                         IsCentreManager, IsContentCreator, IsSupervisor,
-                                        IsCMSManager,IsCMSAdministrator,
+                                        IsCMSManager,IsCMSAdministrator, IsFrameworkDeveloper, IsFrameworkContributor,
+                                        IsWorkforceManager, IsWorkforceContributor, IsLocalWorkforceManager,
                                         IsTrainer, IsNominatedSupervisor,Locked,IsCMSManager,IsCMSAdministrator
                                     };
             foreach (var columnName in boolColumns)
