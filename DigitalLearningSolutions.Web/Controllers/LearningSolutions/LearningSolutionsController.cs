9317cc5af85914f83760f02e4ffef8cfe0c49d3a
OluwatobiAwe <oluwatobi.awe@hee.nhs.uk>
2023-04-17 13:34:09 +0100

    TD-1283: null value exception handling

--- DigitalLearningSolutions.Web/Controllers/LearningSolutions/LearningSolutionsController.cs
+++ DigitalLearningSolutions.Web/Controllers/LearningSolutions/LearningSolutionsController.cs
@@ -74,7 +74,7 @@ namespace DigitalLearningSolutions.Web.Controllers.LearningSolutions
             var centreId = User.GetCentreId();
             if (centreId.GetValueOrDefault() > 0)
             {
-                var centreSummary = centresService.GetAllCentreSummariesForFindCentre().First(x=>x.CentreId == centreId);
+                var centreSummary = centresService.GetAllCentreSummariesForFindCentre().FirstOrDefault(x=>x.CentreId == centreId);
                 return View(new ContactViewModel(contactText, centreSummary));
             }
      
