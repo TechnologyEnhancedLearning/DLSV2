860ae6341925b56980db6737981b39c2955e3353
Swapnamol Abraham <Swapnamol.Abraham@hee.nhs.uk>
2023-04-20 16:28:43 +0100

    Test push

--- DigitalLearningSolutions.Web/Controllers/LearningPortalController/SelfAssessment.cs
+++ DigitalLearningSolutions.Web/Controllers/LearningPortalController/SelfAssessment.cs
@@ -822,6 +822,7 @@
             {
                 return RedirectToAction("StatusCode", "LearningSolutions", new { code = (int)HttpStatusCode.Gone });
             }
+
             int? selfAssessmentSupervisorRoleId = null;
             string selfAssessmentName;
             var supervisorAdminId = 0;
