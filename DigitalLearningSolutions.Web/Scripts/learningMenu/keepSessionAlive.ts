6b8cfcc8432b30a32b5ef7e072e30080bab06058
Colin Beeby <colin.beeby@hee.nhs.uk>
2023-05-03 10:58:06 +0100

    Revert "Merge pull request #1759 from TechnologyEnhancedLearning/Develop/Feature/TD-269-CorrectUseOfSubdomains"
    This reverts commit fa9339bed9cee2299b4ccac251fcf2e1a0b7dd13, reversing
    changes made to 1f2b3136f2524ec8e00bed3bb4357f6595fc6a0c.

--- DigitalLearningSolutions.Web/Scripts/learningMenu/keepSessionAlive.ts
+++ DigitalLearningSolutions.Web/Scripts/learningMenu/keepSessionAlive.ts
@@ -2,8 +2,6 @@ export const heartbeatInterval = 60000;
 
 export function keepSessionAlive(): void {
   const request = new XMLHttpRequest();
-  const keepAlivePingPath = <HTMLInputElement>document.getElementById('keepAlivePingPath');
-  const path = keepAlivePingPath?.value;
-  request.open('GET', path, true);
+  request.open('GET', '/keepSessionAlive/ping', true);
   request.send();
 }
