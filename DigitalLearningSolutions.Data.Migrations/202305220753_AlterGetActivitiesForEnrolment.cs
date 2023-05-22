9b8a1ae4eb2bf91d3079d1b1dde82e1c31f2cba1
kevwhitt-hee <kevin.whittaker@hee.nhs.uk>
2023-05-22 07:59:36 +0100

    TD-1610 Removes erroneous clause from sproc query

---
+++ DigitalLearningSolutions.Data.Migrations/202305220753_AlterGetActivitiesForEnrolment.cs
@@ -0,0 +1,18 @@
+﻿using FluentMigrator;
+
+namespace DigitalLearningSolutions.Data.Migrations
+{
+    [Migration(202305220753)]
+    public class AlterGetActivitiesForEnrolment : Migration
+    {
+        public override void Up()
+        {
+            Execute.Sql(Properties.Resources.td_1610_update_getactivitiesfordelegateenrolment_proc_up);
+        }
+        public override void Down()
+        {
+            Execute.Sql(Properties.Resources.td_1610_update_getactivitiesfordelegateenrolment_proc_down);
+        }
+    }
+}
+
