79e5ba9344c9677b0509ce322c7a4b5df07591d4
kevwhitt-hee <kevin.whittaker@hee.nhs.uk>
2023-06-26 08:17:36 +0100

    TD-1438 Migration to add VersionInfo primary key

---
+++ DigitalLearningSolutions.Data.Migrations/202306260804_AddVersionInfoPrimaryKey.cs
@@ -0,0 +1,20 @@
+﻿namespace DigitalLearningSolutions.Data.Migrations
+{
+    using FluentMigrator;
+
+    [Migration(202306260804)]
+    public class AddVersionInfoPrimaryKey : Migration
+    {
+        public override void Up()
+        {
+            if (!Schema.Table("VersionInfo").Constraint("PK_VersionInfo").Exists())
+            {
+                Create.PrimaryKey("PK_VersionInfo").OnTable("VersionInfo").Column("Version");
+            }
+        }
+        public override void Down()
+        {
+            Delete.PrimaryKey("PK_VersionInfo").FromTable("VersionInfo");
+        }
+    }
+}
