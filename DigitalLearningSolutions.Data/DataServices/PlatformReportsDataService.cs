65878f2d6d87baec097779aae399c54c33bde890
kevwhitt-hee <kevin.whittaker@hee.nhs.uk>
2023-06-30 17:22:20 +0100

    TD-935 Implements reports navigation and platform stats page

---
+++ DigitalLearningSolutions.Data/DataServices/PlatformReportsDataService.cs
@@ -0,0 +1,56 @@
+﻿namespace DigitalLearningSolutions.Data.DataServices
+{
+    using Dapper;
+    using DigitalLearningSolutions.Data.Models.Centres;
+    using DigitalLearningSolutions.Data.Models.PlatformReports;
+    using Microsoft.Extensions.Logging;
+    using System.Data;
+    public interface IPlatformReportsDataService
+    {
+        PlatformUsageSummary GetPlatformUsageSummary();
+    }
+    public class PlatformReportsDataService : IPlatformReportsDataService
+    {
+        private readonly IDbConnection connection;
+        private readonly ILogger<PlatformReportsDataService> logger;
+        public PlatformReportsDataService(IDbConnection connection)
+        {
+            this.connection = connection;
+            this.logger = logger;
+        }
+        public PlatformUsageSummary GetPlatformUsageSummary()
+        {
+            return connection.QueryFirstOrDefault<PlatformUsageSummary>(
+       @"SELECT (SELECT COUNT(CentreName) AS ActiveCentres
+              FROM    Centres AS Centres_1 WITH (NOLOCK)
+              WHERE  (Active = 1) AND (AutoRegistered = 1)) AS ActiveCentres,
+                 (SELECT COUNT(SessionID) AS LearnerLogins
+                 FROM    Sessions AS e WITH (NOLOCK)) AS LearnerLogins,
+                 (SELECT COUNT(ID) AS Learners
+                 FROM    DelegateAccounts WITH (NOLOCK)
+                 WHERE (Active = 1)) AS Learners,
+                 (SELECT SUM(Duration) AS CourseLearningTime
+                 FROM    Sessions AS e WITH (NOLOCK)) / 60 AS CourseLearningTime,
+				 (SELECT COUNT(ProgressID) AS CourseEnrolments
+                 FROM    Progress AS P WITH (NOLOCK)) AS CourseEnrolments,
+                 (SELECT COUNT(ProgressID) AS CourseCompletions
+                 FROM    Progress AS P WITH (NOLOCK)
+                 WHERE (Completed IS NOT NULL)) AS CourseCompletions,
+                 (SELECT COUNT(*) AS Expr1
+                 FROM    CandidateAssessments WITH (NOLOCK)
+                 WHERE (SelfAssessmentID = 1)) AS DCSAEnrolments,
+                 (SELECT COUNT(*) AS Expr1
+                 FROM    CandidateAssessments AS CandidateAssessments_2 WITH (NOLOCK)
+                 WHERE (SelfAssessmentID = 1) AND (SubmittedDate IS NOT NULL)) AS DCSACompletions,
+                 (SELECT COUNT(*) AS Expr1
+                 FROM    CandidateAssessments AS CandidateAssessments_1 WITH (NOLOCK)
+                 WHERE (SelfAssessmentID > 1)) AS NursingPassportEnrolments,
+                 (SELECT COUNT(*) AS Expr1
+                 FROM    CandidateAssessments AS ca WITH (NOLOCK) INNER JOIN
+                              CandidateAssessmentSupervisors AS cas WITH (NOLOCK) ON ca.ID = cas.CandidateAssessmentID INNER JOIN
+                              CandidateAssessmentSupervisorVerifications AS casv WITH (NOLOCK) ON cas.ID = casv.CandidateAssessmentSupervisorID AND casv.SignedOff = 1
+                 WHERE (ca.SelfAssessmentID > 1)) AS NursingPassportCompletions"
+            );
+        }
+    }
+}
---
+++ DigitalLearningSolutions.Web/Services/PlatformReportService.cs
@@ -0,0 +1,25 @@
+﻿namespace DigitalLearningSolutions.Web.Services
+{
+    using System.Threading.Tasks;
+    using DigitalLearningSolutions.Data.DataServices;
+    using DigitalLearningSolutions.Data.Models.PlatformReports;
+
+    public interface IPlatformReportsService
+    {
+        PlatformUsageSummary GetPlatformUsageSummary();
+    }
+
+    public class PlatformReportsService : IPlatformReportsService
+    {
+        private readonly IPlatformReportsDataService platformReportsDataService;
+        public PlatformReportsService(IPlatformReportsDataService platformReportsDataService)
+        {
+            this.platformReportsDataService = platformReportsDataService;
+        }
+
+        public PlatformUsageSummary GetPlatformUsageSummary()
+        {
+           return platformReportsDataService.GetPlatformUsageSummary();
+        }
+    }
+}
