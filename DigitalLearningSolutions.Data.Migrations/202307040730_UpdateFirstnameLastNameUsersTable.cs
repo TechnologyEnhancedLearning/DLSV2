a870ca2063a01c124aa7a39dc9be04e62f4beaa8
rshrirohit <Rohit.Shrivastava@hee.nhs.uk>
2023-07-04 07:21:56 +0100

    Updates User table to trim whitespaces from first name and last name

---
+++ DigitalLearningSolutions.Data.Migrations/202307040730_UpdateFirstnameLastNameUsersTable.cs
@@ -0,0 +1,16 @@
+﻿namespace DigitalLearningSolutions.Data.Migrations
+{
+    using FluentMigrator;
+
+    [Migration(202307040730)]
+    public class UpdateFirstnameLastNameUsersTable : Migration
+    {
+        public override void Up()
+        {
+            Execute.Sql(@"Update Users set FirstName = TRIM(FirstName), LastName = TRIM(LastName)");
+        }
+        public override void Down()
+        {
+        }
+    }
+}
