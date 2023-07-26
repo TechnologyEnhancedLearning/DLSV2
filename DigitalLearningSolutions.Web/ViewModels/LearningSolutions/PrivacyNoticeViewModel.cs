fdd06b17561adf6210759c32912748bb71f76c55
Sarathlal Sarangadharan <Sarathlal.Sarangadharan@hee.nhs.uk>
2023-07-26 10:55:32 +0100

    TD-1786:New web page for privacy notice has been created and scripts added in migration.

---
+++ DigitalLearningSolutions.Web/ViewModels/LearningSolutions/PrivacyNoticeViewModel.cs
@@ -0,0 +1,17 @@
+﻿namespace DigitalLearningSolutions.Web.ViewModels.LearningSolutions
+{
+    using DigitalLearningSolutions.Web.Models;
+    using Microsoft.AspNetCore.Html;
+    using System;
+    public class PrivacyNoticeViewModel : PageReviewModel
+    {
+        public HtmlString TermsText { get; }
+
+        public PrivacyNoticeViewModel(string termsText, DateTime lastReviewedDate, DateTime nextReviewDate)
+        {
+            TermsText = new HtmlString(termsText);
+            LastReviewedDate = lastReviewedDate;
+            NextReviewDate = nextReviewDate;
+        }
+    }
+}
