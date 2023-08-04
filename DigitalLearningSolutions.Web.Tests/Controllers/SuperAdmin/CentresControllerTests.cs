61dc75768f548e97a628773c36cd2d903521d027
Martin Foster <martin.foster2@hee.nhs.uk>
2023-08-04 13:41:19 +0100

    Added unit tests.

--- DigitalLearningSolutions.Web.Tests/Controllers/SuperAdmin/CentresControllerTests.cs
+++ DigitalLearningSolutions.Web.Tests/Controllers/SuperAdmin/CentresControllerTests.cs
@@ -1,7 +1,9 @@
 ﻿using DigitalLearningSolutions.Data.DataServices;
+using DigitalLearningSolutions.Data.Models.Centres;
 using DigitalLearningSolutions.Web.Controllers.SuperAdmin.Centres;
 using DigitalLearningSolutions.Web.Services;
 using DigitalLearningSolutions.Web.Tests.ControllerHelpers;
+using DigitalLearningSolutions.Web.ViewModels.Register.ClaimAccount;
 using DigitalLearningSolutions.Web.ViewModels.SuperAdmin.Centres;
 using DocumentFormat.OpenXml.EMMA;
 using FakeItEasy;
@@ -54,7 +56,7 @@ namespace DigitalLearningSolutions.Web.Tests.Controllers.SuperAdmin
         }
 
         [Test]
-        public void EditCentreDetailst_updates_centre_and_redirects_with_successful_save()
+        public void EditCentreDetails_updates_centre_and_redirects_with_successful_save()
         {
             // Given
             var model = new EditCentreDetailsSuperAdminViewModel
@@ -84,6 +86,124 @@ namespace DigitalLearningSolutions.Web.Tests.Controllers.SuperAdmin
                                                 model.IpPrefix,
                                                 model.ShowOnMap))
                                                 .MustHaveHappenedOnceExactly();
+        }
+
+
+
+        [Test]
+        public void CentreRoleLimits_route_loads_existing_role_limits_with_derived_flags_set()
+        {
+            // Given
+            var roleLimits = new CentreSummaryForRoleLimits
+            {
+                CentreId = 374,
+                RoleLimitCmsAdministrators = -1,    // not set
+                RoleLimitCmsManagers = -1,          // not set
+                RoleLimitCcLicenses = 10,           // set
+                RoleLimitCustomCourses = 20,        // set
+                RoleLimitTrainers = 30,             // set
+            };
+
+            A.CallTo(() => centresDataService.GetRoleLimitsForCentre(
+                A<int>._
+            )).Returns(roleLimits);
+
+            var expectedVM = new CentreRoleLimitsViewModel
+            {
+                CentreId = 374,
+                RoleLimitCmsAdministrators = -1,
+                IsRoleLimitSetCmsAdministrators = false,    // automatically set off
+                RoleLimitCmsManagers = -1,
+                IsRoleLimitSetCmsManagers = false,    // automatically set off
+                IsRoleLimitSetContentCreatorLicenses = true,
+                RoleLimitContentCreatorLicenses = 10,
+                IsRoleLimitSetCustomCourses = true,
+                RoleLimitCustomCourses = 20,
+                IsRoleLimitSetTrainers = true,
+                RoleLimitTrainers = 30,
+            };
+
+            // When
+            var result = controller.CentreRoleLimits(374);
+
+            // Then
+
+            A.CallTo(() => centresDataService.GetRoleLimitsForCentre(374)).MustHaveHappenedOnceExactly();
+            result.Should().BeViewResult("CentreRoleLimits").ModelAs<CentreRoleLimitsViewModel>().Should().BeEquivalentTo(expectedVM);
+        }
+
+        [Test]
+        public void EditCentreRoleLimits_updates_centre_role_limits_and_redirects_with_successful_save()
+        {
+            // Given
+            var model = new CentreRoleLimitsViewModel
+            {
+                CentreId = 374,
+                IsRoleLimitSetCmsAdministrators = true,
+                IsRoleLimitSetCmsManagers = false,
+                IsRoleLimitSetContentCreatorLicenses = true,
+                IsRoleLimitSetCustomCourses = false,
+                IsRoleLimitSetTrainers = true,
+                RoleLimitCmsAdministrators = 1,
+                RoleLimitCmsManagers = -1,
+                RoleLimitContentCreatorLicenses = 2,
+                RoleLimitCustomCourses = -1,
+                RoleLimitTrainers = 0
+            };
+
+            // When
+            var result = controller.EditCentreRoleLimits(model);
+
+            // Then
+            A.CallTo(
+                    () => centresDataService.UpdateCentreRoleLimits(
+                        model.CentreId,
+                        model.RoleLimitCmsAdministrators,
+                        model.RoleLimitCmsManagers,
+                        model.RoleLimitContentCreatorLicenses,
+                        model.RoleLimitCustomCourses,
+                        model.RoleLimitTrainers
+                    )
+                )
+                .MustHaveHappenedOnceExactly();
+            result.Should().BeRedirectToActionResult().WithActionName("ManageCentre").WithRouteValue("centreId", model.CentreId);
+        }
+
+        [Test]
+        public void EditCentreRoleLimits_default_role_limit_value_to_negative_if_not_set()
+        {
+            // Given
+            var model = new CentreRoleLimitsViewModel
+            {
+                CentreId = 374,
+                IsRoleLimitSetCmsAdministrators = true,
+                IsRoleLimitSetCmsManagers = false,
+                IsRoleLimitSetContentCreatorLicenses = false,
+                IsRoleLimitSetCustomCourses = false,
+                IsRoleLimitSetTrainers = true,
+                RoleLimitCmsAdministrators = 1,
+                RoleLimitCmsManagers = 10,
+                RoleLimitContentCreatorLicenses = 20,
+                RoleLimitCustomCourses = 30,
+                RoleLimitTrainers = -1,
+            };
+
+            // When
+            var result = controller.EditCentreRoleLimits(model);
+
+            // Then
+            A.CallTo(
+                    () => centresDataService.UpdateCentreRoleLimits(
+                        model.CentreId,
+                        model.RoleLimitCmsAdministrators,
+                        -1,
+                        -1,
+                        -1,
+                        model.RoleLimitTrainers
+                    )
+                )
+                .MustHaveHappenedOnceExactly();
+            result.Should().BeRedirectToActionResult().WithActionName("ManageCentre").WithRouteValue("centreId", model.CentreId);
         }
     }
 }
