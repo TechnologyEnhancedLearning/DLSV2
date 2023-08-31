6d898b1b9acfedf59963cc31d8aed5cceec6ffe1
Sherif Olaboye <Sherif.Olaboye@hee.nhs.uk>
2023-08-31 12:33:36 +0100

    TD-19436 Column headings missed on 'Cookie Policy

---
+++ DigitalLearningSolutions.Data.Migrations/2023083111401943_Columnheadingsmissedon_CookiePolicy.cs
@@ -0,0 +1,33 @@
+﻿using FluentMigrator;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DigitalLearningSolutions.Data.Migrations
+{
+    [Migration(202309011452)]
+    public  class Update_Columnheadingsmissedon_CookiePolicy : Migration
+    {
+        public override void Up()
+        {
+            var CookiePolicyContentHtml = Properties.Resources.TD_1943_CookiePolicyContentHtml;
+
+            Execute.Sql(
+                @$"UPDATE Config SET  ConfigText ='{CookiePolicyContentHtml}'
+                    WHERE   ConfigName='CookiePolicyContentHtml';"
+            );
+           
+        }
+        public override void Down()
+        {
+            var CookiePolicyContentHtmlOldrecord = Properties.Resources.CookiePolicy;
+
+            Execute.Sql(
+                 @$"UPDATE Config SET  ConfigText ='{CookiePolicyContentHtmlOldrecord}'
+                    WHERE   ConfigName='CookiePolicyContentHtml';"
+             );
+        }
+    }
+}
