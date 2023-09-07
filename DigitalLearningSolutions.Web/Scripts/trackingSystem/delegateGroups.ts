be6de7e15aeaa7bf8e996b8e7c648b52731abadf
Martin Foster <martin.foster2@hee.nhs.uk>
2023-09-07 12:03:38 +0100

    wip

--- DigitalLearningSolutions.Web/Scripts/trackingSystem/delegateGroups.ts
+++
@@ -1,4 +0,0 @@
-import { SearchSortFilterAndPaginate } from '../searchSortFilterAndPaginate/searchSortFilterAndPaginate';
-
-// eslint-disable-next-line no-new
-new SearchSortFilterAndPaginate('TrackingSystem/Delegates/Groups/AllDelegateGroups', true, true, true, 'DelegateGroupsFilter');
