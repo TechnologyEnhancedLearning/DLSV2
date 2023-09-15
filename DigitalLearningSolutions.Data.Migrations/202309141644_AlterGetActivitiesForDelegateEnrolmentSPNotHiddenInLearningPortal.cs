2974da708a6c2a2904241f020facc551d1a78e12
Auldrin Possa <Auldrin.Possa@hee.nhs.uk>
2023-09-15 08:20:29 +0100

    TD-2508 - stored proc modified to get records which are not hidden in learning portal.

---
+++ DigitalLearningSolutions.Data.Migrations/202309141644_AlterGetActivitiesForDelegateEnrolmentSPNotHiddenInLearningPortal.cs
@@ -0,0 +1,17 @@
+﻿namespace DigitalLearningSolutions.Data.Migrations
+{
+    using FluentMigrator;
+
+    [Migration(202309141644)]
+    public class AlterGetActivitiesForDelegateEnrolmentSPNotHiddenInLearningPortal : Migration
+    {
+        public override void Up()
+        {
+            Execute.Sql(Properties.Resources.TD_2508_GetActivitiesForDelegateEnrolmentNotHiddenInLearningPortal);
+        }
+        public override void Down()
+        {
+            Execute.Sql(Properties.Resources.TD_2508_GetActivitiesForDelegateEnrolmentNotHiddenInLearningPortal_down);
+        }
+    }
+}
