5aa26e038f7fce295220631baf17c8a7c3e6e47f
Sarathlal Sarangadharan <Sarathlal.Sarangadharan@hee.nhs.uk>
2023-09-18 10:35:44 +0100

    TD-2492: Restrict the number of rows that can be uploaded for bulk update in Excel

--- DigitalLearningSolutions.Web/ViewModels/TrackingSystem/Delegates/UploadDelegatesViewModel.cs
+++ DigitalLearningSolutions.Web/ViewModels/TrackingSystem/Delegates/UploadDelegatesViewModel.cs
@@ -21,7 +21,7 @@
         [AllowedExtensions(new[] { ".xlsx" }, "Delegates update file must be in xlsx format")]
         [MaxFileSize(5 * 1024 * 1024, "Maximum allowed file size is 5MB")]
         public IFormFile? DelegatesFile { get; set; }
-
+        public int MaxBulkUploadRows { get; set; }
         public DateTime GetWelcomeEmailDate()
         {
             return new DateTime(Year!.Value, Month!.Value, Day!.Value);
