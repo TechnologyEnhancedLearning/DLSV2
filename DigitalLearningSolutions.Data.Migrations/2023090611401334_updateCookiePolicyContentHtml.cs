e577761721d3182a7b0005bf76061d56f49e9985
Sherif Olaboye <Sherif.Olaboye@hee.nhs.uk>
2023-09-21 09:06:03 +0100

    TD-1943 Ensuring the Resource file is part of the push

--- DigitalLearningSolutions.Data.Migrations/2023090611401334_updateCookiePolicyContentHtml.cs
+++ DigitalLearningSolutions.Data.Migrations/2023090611401334_updateCookiePolicyContentHtml.cs
@@ -7,13 +7,13 @@ using System.Threading.Tasks;
 
 namespace DigitalLearningSolutions.Data.Migrations
 {
-    [Migration(202309211452)]
+    [Migration(202309221452)]
 
     public class UpdateCookiePolicyContentHtml : Migration
     {
         public override void Up()
         {
-            var CookiePolicyContentHtml = Properties.Resources.TD_1943_CookiesPolicyContentHtml;
+            var CookiePolicyContentHtml = Properties.Resources.TD_1943_CookiePolicyContentHtml;
 
             Execute.Sql(
                 @"UPDATE Config SET  ConfigText =N'" + CookiePolicyContentHtml + "' " +
