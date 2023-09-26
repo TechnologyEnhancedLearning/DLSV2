ec186e8c1cb696dfe1abc4336b9a53a6a8dd16be
Sherif Olaboye <Sherif.Olaboye@hee.nhs.uk>
2023-09-26 09:49:19 +0100

    TD-1943 removing a character that is not part of the cookies policy

--- DigitalLearningSolutions.Data.Migrations/2023090611401334_updateCookiePolicyContentHtml.cs
+++ DigitalLearningSolutions.Data.Migrations/2023090611401334_updateCookiePolicyContentHtml.cs
@@ -7,13 +7,13 @@ using System.Threading.Tasks;
 
 namespace DigitalLearningSolutions.Data.Migrations
 {
-    [Migration(202309221452)]
+    [Migration(202309261452)]
 
     public class UpdateCookiePolicyContentHtml : Migration
     {
         public override void Up()
         {
-            var CookiePolicyContentHtml = Properties.Resources.TD_1943_CookiePolicyContentHtml;
+            var CookiePolicyContentHtml = Properties.Resources.TD_1943_CookiesPolicyContentHtml;
 
             Execute.Sql(
                 @"UPDATE Config SET  ConfigText =N'" + CookiePolicyContentHtml + "' " +
