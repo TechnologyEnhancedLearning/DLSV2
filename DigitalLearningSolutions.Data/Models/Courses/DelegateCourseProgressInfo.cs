37281a26d16524d1615e46515c2b5aee376ef2c1
Auldrin Possa <Auldrin.Possa@hee.nhs.uk>
2023-10-06 13:20:39 +0100

    TD-2481 - removed 'IsAssessed' property. Modified logic for Post Learning Assessment in the view.

--- DigitalLearningSolutions.Data/Models/Courses/DelegateCourseProgressInfo.cs
+++ DigitalLearningSolutions.Data/Models/Courses/DelegateCourseProgressInfo.cs
@@ -5,16 +5,15 @@ namespace DigitalLearningSolutions.Data.Models.Courses
 {
     public class DelegateCourseProgressInfo : DelegateCourseInfo
     {
-        public string CentreName { get; set; }
-        public string CandidateName { get; set; }
-        public string Course { get; set; }
+        public string? CentreName { get; set; }
+        public string? CandidateName { get; set; }
+        public string? Course { get; set; }
         public int DiagnosticAttempts { get; set; }
         public int TotalTime { get; set; }
         public int LearningDone { get; set; }
         public int PLAttempts { get; set; }
         public int PLPasses { get; set; }
         public int Sections { get; set; }
-        public bool IsAssessed { get; set; }
         public int TutCompletionThreshold { get; set; }
         public int DiagCompletionThreshold { get; set; }
         public int AssessAttempts { get; set; }
