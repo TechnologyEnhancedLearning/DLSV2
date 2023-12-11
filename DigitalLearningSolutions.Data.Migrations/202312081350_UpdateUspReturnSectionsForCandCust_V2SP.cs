f3531ba3e3e63b6900a006dd62f2faf315a3e3d2
Auldrin Possa <Auldrin.Possa@hee.nhs.uk>
2023-12-11 11:41:38 +0000

    TD-2481- stroed procedure updated to show correct dignostic score. SQL and views modifed to show tutorial for inactive courses.

---
+++ DigitalLearningSolutions.Data.Migrations/202312081350_UpdateUspReturnSectionsForCandCust_V2SP.cs
@@ -0,0 +1,17 @@
+﻿namespace DigitalLearningSolutions.Data.Migrations
+{
+    using FluentMigrator;
+
+    [Migration(202312081350)]
+    public class UpdateUspReturnSectionsForCandCust_V2SP : Migration
+    {
+        public override void Up()
+        {
+            Execute.Sql(Properties.Resources.TD_2481_Update_uspReturnSectionsForCandCust_V2_up);
+        }
+        public override void Down()
+        {
+            Execute.Sql(Properties.Resources.TD_2481_Update_uspReturnSectionsForCandCust_V2_down);
+        }
+    }
+}
