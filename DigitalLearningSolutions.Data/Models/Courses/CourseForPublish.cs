d8138c02988c97e8dbe0dd931596c4b73754a62c
kevwhitt-hee <kevin.whittaker@hee.nhs.uk>
2023-12-14 10:25:55 +0000

    TD-3217 Adds model and 3 data service methods for retrieving course lists

---
+++ DigitalLearningSolutions.Data/Models/Courses/CourseForPublish.cs
@@ -0,0 +1,9 @@
+﻿namespace DigitalLearningSolutions.Data.Models.Courses
+{
+    public class CourseForPublish
+    {
+        public int Id { get; set; }
+        public string? Course { get; set; }
+        public string? Provider { get; set; }
+    }
+}
