9a643cb2a0a36a4094fcf5ef98c5f7870c502a76
Rohit Shrivastava <Rohit.Shrivastava@hee.nhs.uk>
2024-01-10 14:31:17 +0000

    TD-3222 Correcting unit tests

--- DigitalLearningSolutions.Web.Tests/Services/DashboardInformationServiceTests.cs
+++ DigitalLearningSolutions.Web.Tests/Services/DashboardInformationServiceTests.cs
@@ -9,6 +9,7 @@
     using FakeItEasy;
     using FluentAssertions;
     using NUnit.Framework;
+    using System.Collections.Generic;
 
     public class DashboardInformationServiceTests
     {
@@ -126,7 +127,12 @@
             A.CallTo(() => centresDataService.GetCentreDetailsById(CentreId))
                 .Returns(CentreTestHelper.GetDefaultCentre(CentreId));
             A.CallTo(() => userDataService.GetAdminUserById(AdminId))
-                .Returns(adminUser);
+            .Returns(adminUser);
+
+            A.CallTo(() => userDataService.GetDelegateUserCards("", 0, 10, "SearchableName", "Ascending", CentreId,
+            "Any", "Any", "Any", "Any", "Any", "Any", 0, "Any", "Any", "Any", "Any", "Any", "Any"))
+                .Returns((new List<DelegateUserCard>(), delegateCount)
+                );
 
             A.CallTo(
                 () =>
