c79553956a7d7a97bca701c82febe1752d2cfa7e
Auldrin Possa <Auldrin.Possa@hee.nhs.uk>
2024-01-12 12:06:53 +0000

    TD-3207- typescript code added to copy course url to clipboard.

---
+++ DigitalLearningSolutions.Web/Scripts/trackingSystem/courseSetup.ts
@@ -0,0 +1,32 @@
+﻿const copyLinkEls = document.getElementsByName('copy-course-link');
+
+copyLinkEls.forEach((button) => {
+  button.addEventListener('click', () => {
+    var customisationId = button.id.substring(12);
+    copyToClipboard(customisationId);
+    removeExistingLinkCopiedText();
+    button.textContent = 'Copy course link - Link copied!';
+  });
+});
+function copyToClipboard(customisationId: string) {
+  var rootPath = (<HTMLInputElement>document.getElementById("appRootPath")).value;
+  var courseUrl = rootPath + "/LearningMenu/" + customisationId;
+  const el = document.createElement("textarea");
+  el.value = courseUrl;
+  el.setAttribute("readonly", "");
+  el.style.position = "absolute";
+  el.style.left = "-9999px";
+  document.body.appendChild(el);
+  el.select();
+  document.execCommand("copy");
+  document.body.removeChild(el);
+}
+
+function removeExistingLinkCopiedText() {
+  Array.prototype.forEach.call(copyLinkEls, function (el) {
+    if (el.textContent === "Copy course link - Link copied!") {
+      el.textContent = "Copy course link";
+    }
+  });
+}
+
