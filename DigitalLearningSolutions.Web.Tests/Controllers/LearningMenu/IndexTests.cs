20377f24f716d4b5557c6189cce267cb31e8ee97
kevwhitt-hee <kevin.whittaker@hee.nhs.uk>
2024-01-19 16:50:54 +0000

    TD-3616 Reworks code and queries to avoid transaction deadlocks

--- DigitalLearningSolutions.Web.Tests/Controllers/LearningMenu/IndexTests.cs
+++ DigitalLearningSolutions.Web.Tests/Controllers/LearningMenu/IndexTests.cs
@@ -147,7 +147,7 @@
             controller.Index(CustomisationId);
 
             // Then
-            A.CallTo(() => courseContentService.UpdateProgress(progressId)).MustHaveHappened();
+            A.CallTo(() => sessionService.StartOrUpdateDelegateSession(CandidateId, CustomisationId, A<ISession>._)).MustHaveHappened();
         }
 
         [Test]
