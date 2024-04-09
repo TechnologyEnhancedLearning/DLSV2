1fd24f9b1247be29af7ac494cc5a5a069179adf3
Sherif Olaboye <Sherif.Olaboye@hee.nhs.uk>
2024-04-09 16:06:55 +0100

    TD-3978 Delegates are not being enrolled on courses related to registration fields when they register

---
+++ DigitalLearningSolutions.Data.Migrations/202404091703_UpdateGroups_SetLinkedToFieldToTrue.cs
@@ -0,0 +1,25 @@
+﻿using FluentMigrator;
+
+namespace DigitalLearningSolutions.Data.Migrations
+{
+    [Migration(202404091703)]
+    public class UpdateGroups_SetLinkedToFieldToTrue : Migration
+    {
+        public override void Up()
+        {
+            Execute.Sql(
+                @$"UPDATE Groups SET
+                    LinkedToField =1,
+                    SyncFieldChanges=1,
+                    AddNewRegistrants=1,
+                    PopulateExisting=1
+                    WHERE  GroupLabel IN ('BH Pharmacist','BH Anaesthetist');"
+            );
+        }
+
+        public override void Down()
+        {
+        }
+    }
+}
+
