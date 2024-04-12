81c845e0403af888518efbc0e8a0bcfef8b971e5
kevwhitt-hee <kevin.whittaker@hee.nhs.uk>
2024-04-12 09:49:27 +0100

    TD-3835 Migration to inactivate delegate accounts with no email

---
+++ DigitalLearningSolutions.Data.Migrations/202404120935_InactivateDelegateAccountsWithNoEmail.cs
@@ -0,0 +1,19 @@
+﻿namespace DigitalLearningSolutions.Data.Migrations
+{
+    using FluentMigrator;
+
+    [Migration(202404120935)]
+    public class InactivateDelegateAccountsWithNoEmail : ForwardOnlyMigration
+    {
+        public override void Up()
+        {
+            Execute.Sql(
+                @$"UPDATE DelegateAccounts
+                    SET       Active = 1
+                    FROM   Users AS u INNER JOIN
+                                 DelegateAccounts ON u.ID = DelegateAccounts.UserID
+                    WHERE (NOT (u.PrimaryEmail LIKE N'%@%')) AND (DelegateAccounts.Active = 1)"
+                );
+        }
+    }
+}
