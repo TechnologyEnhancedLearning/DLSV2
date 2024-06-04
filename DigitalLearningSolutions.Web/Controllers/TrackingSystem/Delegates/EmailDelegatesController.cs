8fbc741188fd42772d22d16e045d1a85c1ec8f05
rshrirohit <126668828+rshrirohit@users.noreply.github.com>
2024-06-04 11:51:53 +0100

    Removed Commented code

--- DigitalLearningSolutions.Web/Controllers/TrackingSystem/Delegates/EmailDelegatesController.cs
+++ DigitalLearningSolutions.Web/Controllers/TrackingSystem/Delegates/EmailDelegatesController.cs
@@ -3,7 +3,6 @@ namespace DigitalLearningSolutions.Web.Controllers.TrackingSystem.Delegates
     using System;
     using System.Collections.Generic;
     using System.Linq;
-    //using DigitalLearningSolutions.Data.DataServices;
     using DigitalLearningSolutions.Data.Enums;
     using DigitalLearningSolutions.Data.Extensions;
     using DigitalLearningSolutions.Data.Helpers;
