882a81170a1c697f644e23c0c6ebec499f4f6d82
Sherif Olaboye <Sherif.Olaboye@hee.nhs.uk>
2024-07-22 16:59:51 +0100

    TD-4243 Few 'add on' courses showing on 'Learning Portal' not showing on 'Course set up' & 'Delegate activities' screens

---
+++ DigitalLearningSolutions.Data.Migrations/202407221645_AlterGetCurrentCoursesForCandidate_V2.cs
@@ -0,0 +1,17 @@
+﻿namespace DigitalLearningSolutions.Data.Migrations
+{
+    using FluentMigrator;
+
+    [Migration(202407221645)]
+    public class AlterGetCurrentCoursesForCandidate_V3 : Migration
+    {
+        public override void Up()
+        {
+            Execute.Sql(Properties.Resources.TD_4243_Alter_GetCurrentCoursesForCandidate_V2_proc_up);
+        }
+        public override void Down()
+        {
+            Execute.Sql(Properties.Resources.TD_4243_Alter_GetCurrentCoursesForCandidate_V2_proc_down);
+        }
+    }
+}
