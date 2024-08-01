ec60ef01ddcf32e1178de2e944c890d21e509801
Sherif Olaboye <Sherif.Olaboye@hee.nhs.uk>
2024-08-01 12:01:26 +0100

    TD-4426 Add a minimum optional competencies int field to the self assessments table

---
+++ DigitalLearningSolutions.Data.Migrations/AddMinimumOptionalCompetenciesToSelfAssessmentsTable.cs
@@ -0,0 +1,22 @@
+﻿
+using FluentMigrator;
+using System.Diagnostics.Metrics;
+
+namespace DigitalLearningSolutions.Data.Migrations
+{
+    using FluentMigrator;
+
+    [Migration(202401081132)]
+    public  class AddMinimumOptionalCompetenciesToSelfAssessmentsTable : Migration
+    {
+        public override void Up()
+        {
+            Alter.Table("SelfAssessments").AddColumn("MinimumOptionalCompetencies").AsInt32().NotNullable().WithDefaultValue(0);
+        }
+
+        public override void Down()
+        {
+            Delete.Column("MinimumOptionalCompetencies").FromTable("SelfAssessments");
+        }
+    }
+}
