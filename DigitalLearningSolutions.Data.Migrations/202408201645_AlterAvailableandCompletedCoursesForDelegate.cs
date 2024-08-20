66a793014cbab4c86efed4742de7fa0120fa1498
Sherif Olaboye <Sherif.Olaboye@hee.nhs.uk>
2024-08-20 13:27:15 +0100

    TD-4243 Removing the unpublish courses from completed, Available activities, enrol activity and ‘Tracking system’-Activities

---
+++ DigitalLearningSolutions.Data.Migrations/202408201645_AlterAvailableandCompletedCoursesForDelegate.cs
@@ -0,0 +1,19 @@
+﻿namespace DigitalLearningSolutions.Data.Migrations
+{
+    using FluentMigrator;
+
+    [Migration(202408201645)]
+    public class AlterAvailableandCompletedCoursesForDelegate : Migration
+    {
+        public override void Up()
+        {
+            Execute.Sql(Properties.Resources.TD_4243_Alter_GetActivitiesForDelegateEnrolment_proc_up);
+            Execute.Sql(Properties.Resources.TD_4243_Alter_GetCompletedCoursesForCandidate_proc_up);
+        }
+        public override void Down()
+        {
+            Execute.Sql(Properties.Resources.TD_4243_Alter_GetActivitiesForDelegateEnrolment_proc_down);
+            Execute.Sql(Properties.Resources.TD_4243_Alter_GetCompletedCoursesForCandidate_proc_down);
+        }
+    }
+}
--- DigitalLearningSolutions.Data.Migrations/Properties/Resources.Designer.cs
+++ DigitalLearningSolutions.Data.Migrations/Properties/Resources.Designer.cs
@@ -1926,6 +1926,94 @@ namespace DigitalLearningSolutions.Data.Migrations.Properties {
         
         /// <summary>
         ///   Looks up a localized string similar to 
+        ////****** Object:  StoredProcedure [dbo].[GetActivitiesForDelegateEnrolment]    Script Date: 20/08/2024 11:57:38 ******/
+        ///SET ANSI_NULLS ON
+        ///GO
+        ///
+        ///SET QUOTED_IDENTIFIER ON
+        ///GO
+        ///
+        ///-- =============================================
+        ///-- Author:		Kevin Whittaker
+        ///-- Create date: 24/01/2023
+        ///-- Description:	Returns active available for delegate enrolment based on original GetActiveAvailableCustomisationsForCentreFiltered_V6 sproc but adjusted for user account refactor and filters properly for category.
+        ///-- ======= [rest of string was truncated]&quot;;.
+        /// </summary>
+        internal static string TD_4243_Alter_GetActivitiesForDelegateEnrolment_proc_down {
+            get {
+                return ResourceManager.GetString("TD_4243_Alter_GetActivitiesForDelegateEnrolment_proc_down", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to 
+        ////****** Object:  StoredProcedure [dbo].[GetActivitiesForDelegateEnrolment]    Script Date: 20/08/2024 11:57:38 ******/
+        ///SET ANSI_NULLS ON
+        ///GO
+        ///
+        ///SET QUOTED_IDENTIFIER ON
+        ///GO
+        ///
+        ///-- =============================================
+        ///-- Author:		Kevin Whittaker
+        ///-- Create date: 24/01/2023
+        ///-- Description:	Returns active available for delegate enrolment based on original GetActiveAvailableCustomisationsForCentreFiltered_V6 sproc but adjusted for user account refactor and filters properly for category.
+        ///-- ======= [rest of string was truncated]&quot;;.
+        /// </summary>
+        internal static string TD_4243_Alter_GetActivitiesForDelegateEnrolment_proc_up {
+            get {
+                return ResourceManager.GetString("TD_4243_Alter_GetActivitiesForDelegateEnrolment_proc_up", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to 
+        ////****** Object:  StoredProcedure [dbo].[GetCompletedCoursesForCandidate]    Script Date: 20/08/2024 11:58:45 ******/
+        ///SET ANSI_NULLS ON
+        ///GO
+        ///
+        ///SET QUOTED_IDENTIFIER ON
+        ///GO
+        ///
+        ///-- =============================================
+        ///-- Author:		Kevin Whittaker
+        ///-- Create date: 16/12/2016
+        ///-- Description:	Returns a list of completed courses for the candidate.
+        ///-- 21/06/2021: Adds Applications.ArchivedDate field to output.
+        ///-- =============================================
+        ///ALTER PROCEDURE [dbo].[GetCompletedCourses [rest of string was truncated]&quot;;.
+        /// </summary>
+        internal static string TD_4243_Alter_GetCompletedCoursesForCandidate_proc_down {
+            get {
+                return ResourceManager.GetString("TD_4243_Alter_GetCompletedCoursesForCandidate_proc_down", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to 
+        ////****** Object:  StoredProcedure [dbo].[GetCompletedCoursesForCandidate]    Script Date: 20/08/2024 11:58:45 ******/
+        ///SET ANSI_NULLS ON
+        ///GO
+        ///
+        ///SET QUOTED_IDENTIFIER ON
+        ///GO
+        ///
+        ///-- =============================================
+        ///-- Author:		Kevin Whittaker
+        ///-- Create date: 16/12/2016
+        ///-- Description:	Returns a list of completed courses for the candidate.
+        ///-- 21/06/2021: Adds Applications.ArchivedDate field to output.
+        ///-- =============================================
+        ///ALTER PROCEDURE [dbo].[GetCompletedCourses [rest of string was truncated]&quot;;.
+        /// </summary>
+        internal static string TD_4243_Alter_GetCompletedCoursesForCandidate_proc_up {
+            get {
+                return ResourceManager.GetString("TD_4243_Alter_GetCompletedCoursesForCandidate_proc_up", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to 
         ////****** Object:  StoredProcedure [dbo].[GetCurrentCoursesForCandidate_V2]    Script Date: 22/07/2024 10:11:35 ******/
         ///SET ANSI_NULLS ON
         ///GO
@@ -1939,7 +2027,7 @@ namespace DigitalLearningSolutions.Data.Migrations.Properties {
         ///-- Description:	Returns a list of active progress records for the candidate.
         ///-- Change 18/09/2018: Adds logic to exclude Removed courses from returned results.
         ///-- =============================================
-        ///ALTER PROCEDURE [dbo].[GetC [rest of string was truncated]&quot;;.
+        ///ALTER PROCEDU [rest of string was truncated]&quot;;.
         /// </summary>
         internal static string TD_4243_Alter_GetCurrentCoursesForCandidate_V2_proc_down {
             get {
@@ -1963,7 +2051,7 @@ namespace DigitalLearningSolutions.Data.Migrations.Properties {
         ///-- Description:	Returns a list of active progress records for the candidate.
         ///-- Change 18/09/2018: Adds logic to exclude Removed courses from returned results.
         ///-- =============================================
-        ///ALTER PROCEDURE [dbo].[Ge [rest of string was truncated]&quot;;.
+        ///ALTER PROC [rest of string was truncated]&quot;;.
         /// </summary>
         internal static string TD_4243_Alter_GetCurrentCoursesForCandidate_V2_proc_up {
             get {
