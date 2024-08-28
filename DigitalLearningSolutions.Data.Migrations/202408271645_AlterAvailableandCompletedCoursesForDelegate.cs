690c64bff6a5167000eeec7ceb6754bc653fed76
Sherif Olaboye <Sherif.Olaboye@hee.nhs.uk>
2024-08-28 10:11:50 +0100

    TD-4243 Few 'add on' courses showing on 'Learning Portal' not showing on 'Course set up' & 'Delegate activities' screens

---
+++ DigitalLearningSolutions.Data.Migrations/202408271645_AlterAvailableandCompletedCoursesForDelegate.cs
@@ -0,0 +1,21 @@
+﻿
+
+namespace DigitalLearningSolutions.Data.Migrations
+{
+    using FluentMigrator;
+
+    [Migration(202408271645)]
+    public class AlterAvailableandCompletedCoursesForDelegate : Migration
+    {
+        public override void Up()
+        {
+            Execute.Sql(Properties.Resources.TD_4243_Alter_GetActivitiesForDelegateEnrolment_proc_up);
+            Execute.Sql(Properties.Resources.TD_4243_Alter_GetCompletedCoursesForCandidate_proc_up);
+        }
+        public override void Down()
+        {
+           Execute.Sql(Properties.Resources.TD_4243_Alter_GetActivitiesForDelegateEnrolment_proc_down);
+          Execute.Sql(Properties.Resources.TD_4243_Alter_GetCompletedCoursesForCandidate_proc_down);
+        }
+    }
+}
---
+++ DigitalLearningSolutions.Data.Migrations/Resources/TD-4243_Alter_GetCompletedCoursesForCandidate_proc_down.sql
@@ -0,0 +1,47 @@
+
+/****** Object:  StoredProcedure [dbo].[GetCompletedCoursesForCandidate]    Script Date: 20/08/2024 11:58:45 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+-- =============================================
+-- Author:		Kevin Whittaker
+-- Create date: 16/12/2016
+-- Description:	Returns a list of completed courses for the candidate.
+-- 21/06/2021: Adds Applications.ArchivedDate field to output.
+-- =============================================
+ALTER PROCEDURE [dbo].[GetCompletedCoursesForCandidate]
+	-- Add the parameters for the stored procedure here
+	@CandidateID int
+AS
+BEGIN
+	-- SET NOCOUNT ON added to prevent extra result sets from
+	-- interfering with SELECT statements.
+	SET NOCOUNT ON;
+
+    -- Insert statements for procedure here
+    SELECT p.ProgressID, (CASE WHEN cu.CustomisationName <> '' THEN a.ApplicationName + ' - ' + cu.CustomisationName ELSE a.ApplicationName END) AS CourseName, p.CustomisationID, p.SubmittedTime AS LastAccessed, p.Completed, 
+               p.FirstSubmittedTime AS StartedDate, p.RemovedDate, p.DiagnosticScore, p.PLLocked, cu.IsAssessed, dbo.CheckCustomisationSectionHasDiagnostic(p.CustomisationID, 0) 
+               AS HasDiagnostic, dbo.CheckCustomisationSectionHasLearning(p.CustomisationID, 0) AS HasLearning,
+                     COALESCE
+                             ((SELECT        COUNT(Passes) AS Passes
+                                 FROM            (SELECT        COUNT(AssessAttemptID) AS Passes
+                                                           FROM            AssessAttempts AS aa
+                                                           WHERE        (CandidateID = p.CandidateID) AND (CustomisationID = p.CustomisationID) AND (Status = 1)
+                                                           GROUP BY SectionNumber) AS derivedtbl_2), 0) AS Passes,
+                   (SELECT COUNT(SectionID) AS Sections
+                    FROM   Sections AS s
+                    WHERE (ApplicationID = cu.ApplicationID)) AS Sections, p.Evaluated, p.FollupUpEvaluated, a.ArchivedDate
+FROM  Progress AS p INNER JOIN
+               Customisations AS cu ON p.CustomisationID = cu.CustomisationID INNER JOIN
+               Applications AS a ON cu.ApplicationID = a.ApplicationID
+WHERE (NOT (p.Completed IS NULL)) AND (p.CandidateID = @CandidateID)
+ORDER BY p.Completed DESC
+	
+END
+GO
+
+
+
