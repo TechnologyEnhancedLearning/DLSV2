7da62dd3bf855aa74123be43e7cfd6f13a5853c6
Auldrin Possa <Auldrin.Possa@hee.nhs.uk>
2024-10-07 14:22:56 +0100

    TD-4787-Changed role from Educator/Manager to Assessor for nominated supervisors

---
+++ DigitalLearningSolutions.Data.Migrations/202410071400_UpdateCandidateAssessmentSupervisorsTabl.cs
@@ -0,0 +1,26 @@
+﻿
+namespace DigitalLearningSolutions.Data.Migrations
+{
+    using FluentMigrator;
+
+    [Migration(202410071400)]
+    public class UpdateCandidateAssessmentSupervisorsTabl : Migration
+    {
+        public override void Up()
+        {
+            Execute.Sql("UPDATE cas " +
+                "SET SelfAssessmentSupervisorRoleID = (SELECT ID FROM SelfAssessmentSupervisorRoles " +
+                    "WHERE SelfAssessmentID = ssr.SelfAssessmentID and AllowDelegateNomination = 1) " +
+                "FROM CandidateAssessmentSupervisors cas INNER JOIN " +
+                    "SelfAssessmentSupervisorRoles ssr ON cas.SelfAssessmentSupervisorRoleID = ssr.ID " +
+                    "AND cas.Removed IS NULL AND ssr.AllowDelegateNomination = 0 INNER JOIN " +
+                    "SupervisorDelegates sd ON cas.SupervisorDelegateId = sd.ID INNER JOIN " +
+                    "AdminAccounts aa ON sd.SupervisorAdminID = aa.ID WHERE aa.IsSupervisor = 0 AND aa.IsNominatedSupervisor = 1");
+        }
+
+        public override void Down()
+        {
+
+        }
+    }
+}
