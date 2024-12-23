3880318c5335c1763263f06944936fbe23c6574f
kevwhitt-hee <kevin.whittaker@hee.nhs.uk>
2024-12-23 12:21:13 +0000

    TD-5154 Implements basic pre-process logic and view and adds validation to exported sheet

--- DigitalLearningSolutions.Data/Models/Frameworks/Import/ImportCompetenciesResult.cs
+++ DigitalLearningSolutions.Data/Models/Frameworks/Import/ImportCompetenciesResult.cs
@@ -8,10 +8,11 @@
     {
         public enum ErrorReason
         {
+            InvalidId,
             MissingCompetencyName,
-            TooLongCompetencyGroupName,
             TooLongCompetencyName,
-            AlreadyExists
+            TooLongCompetencyGroupName,
+            InvalidAlwaysShowDescription
         }
         public ImportCompetenciesResult() { }
 
@@ -20,16 +21,18 @@
         )
         {
             ProcessedCount = competencyTableRows.Count;
-            CompetenciesInsertedCount = competencyTableRows.Count(dr => dr.RowStatus == RowStatus.CompetencyInserted | dr.RowStatus == RowStatus.CompetencyGroupAndCompetencyInserted);
-            CompetencyGroupsInsertedCount = competencyTableRows.Count(dr => dr.RowStatus == RowStatus.CompetencyGroupInserted | dr.RowStatus == RowStatus.CompetencyGroupAndCompetencyInserted);
+            CompetencyAddedCount = competencyTableRows.Count(dr => dr.RowStatus == RowStatus.CompetencyInserted | dr.RowStatus == RowStatus.CompetencyGroupAndCompetencyInserted);
+            GroupAddedCount = competencyTableRows.Count(dr => dr.RowStatus == RowStatus.CompetencyGroupInserted | dr.RowStatus == RowStatus.CompetencyGroupAndCompetencyInserted);
             SkippedCount = competencyTableRows.Count(dr => dr.RowStatus == RowStatus.Skipped);
             Errors = competencyTableRows.Where(dr => dr.Error.HasValue).Select(dr => (dr.RowNumber, dr.Error!.Value));
         }
 
         public IEnumerable<(int RowNumber, ErrorReason Reason)>? Errors { get; set; }
         public int ProcessedCount { get; set; }
-        public int CompetenciesInsertedCount { get; set; }
-        public int CompetencyGroupsInsertedCount { get; set; }
+        public int CompetencyAddedCount { get; set; }
+        public int CompetencyUpdatedCount { get; set; }
+        public int GroupAddedCount { get; set; }
+        public int GroupUpdatedCount { get; set; }
         public int SkippedCount { get; set; }
     }
 }
