7b7e2b9709670eed27719263185cef1a487ad220
kevwhitt-hee <kevin.whittaker@hee.nhs.uk>
2024-12-30 10:43:24 +0000

    TD-5155 Begins to implement add questions view model

---
+++ DigitalLearningSolutions.Web/ViewModels/Frameworks/Import/AddAssessmentQuestionsViewModel.cs
@@ -0,0 +1,13 @@
+﻿using DigitalLearningSolutions.Data.Models.Frameworks;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Collections.Generic;
+
+namespace DigitalLearningSolutions.Web.ViewModels.Frameworks.Import
+{
+    public class AddAssessmentQuestionsViewModel
+    {
+        public DetailFramework Framework { get; set; }
+        public IEnumerable<AssessmentQuestion>? FrameworkDefaultQuestions { get; set; }
+        public SelectList? QuestionSelectList { get; set; }
+    }
+}
