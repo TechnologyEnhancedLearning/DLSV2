f0da7760c360a8bc1d7f9d4ac0ab45238c1f5dac
kevwhitt-hee <kevin.whittaker@hee.nhs.uk>
2024-12-30 10:41:12 +0000

    TD-5216 Moves to import namespace and separates form data in viewmodel

---
+++ DigitalLearningSolutions.Web/ViewModels/Frameworks/Import/ImportCompetenciesViewModel.cs
@@ -0,0 +1,13 @@
+﻿namespace DigitalLearningSolutions.Web.ViewModels.Frameworks.Import
+{
+    using DigitalLearningSolutions.Data.Models.Frameworks;
+    using DigitalLearningSolutions.Web.Attributes;
+    using Microsoft.AspNetCore.Http;
+    using System.ComponentModel.DataAnnotations;
+
+    public class ImportCompetenciesViewModel : ImportCompetenciesFormData
+    {
+        public DetailFramework Framework { get; set; }
+        public bool IsNotBlank { get; set; }
+    }
+}
