748000c16e29cbcb6859d6640cbb812d4df2d70d
kevwhitt-hee <kevin.whittaker@hee.nhs.uk>
2024-12-30 15:30:56 +0000

    TD-5216 Adds competency vocabulary to views

--- DigitalLearningSolutions.Web/ViewModels/Frameworks/Import/ImportCompetenciesViewModel.cs
+++ DigitalLearningSolutions.Web/ViewModels/Frameworks/Import/ImportCompetenciesViewModel.cs
@@ -1,13 +1,25 @@
 ﻿namespace DigitalLearningSolutions.Web.ViewModels.Frameworks.Import
 {
     using DigitalLearningSolutions.Data.Models.Frameworks;
-    using DigitalLearningSolutions.Web.Attributes;
-    using Microsoft.AspNetCore.Http;
-    using System.ComponentModel.DataAnnotations;
+    using DigitalLearningSolutions.Web.Helpers;
 
     public class ImportCompetenciesViewModel : ImportCompetenciesFormData
     {
-        public DetailFramework Framework { get; set; }
+        public ImportCompetenciesViewModel(DetailFramework framework, bool isNotBlank)
+        {
+            FrameworkID = framework.ID;
+            FrameworkName = framework.FrameworkName;
+            FrameworkVocabularySingular = FrameworkVocabularyHelper.VocabularySingular(framework.FrameworkConfig);
+            FrameworkVocabularyPlural = FrameworkVocabularyHelper.VocabularyPlural(framework.FrameworkConfig);
+            PublishStatusID = framework.PublishStatusID;
+            IsNotBlank = isNotBlank;
+        }
+        public int FrameworkID { get; set; }
+        public string FrameworkName { get; set; }
+        public string FrameworkVocabularySingular { get; set; }
+        public string FrameworkVocabularyPlural { get; set; }
+        public int PublishStatusID { get; set; }
         public bool IsNotBlank { get; set; }
+
     }
 }
