ee10005c4e990508bf3e5b49d1124af817c6506f
kevwhitt-hee <kevin.whittaker@hee.nhs.uk>
2024-12-31 09:37:40 +0000

    TD-5155 Adds view model and constructor

--- DigitalLearningSolutions.Web/ViewModels/Frameworks/Import/AddAssessmentQuestionsViewModel.cs
+++ DigitalLearningSolutions.Web/ViewModels/Frameworks/Import/AddAssessmentQuestionsViewModel.cs
@@ -1,13 +1,26 @@
 ﻿using DigitalLearningSolutions.Data.Models.Frameworks;
+using DigitalLearningSolutions.Web.Helpers;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using System.Collections.Generic;
 
 namespace DigitalLearningSolutions.Web.ViewModels.Frameworks.Import
 {
-    public class AddAssessmentQuestionsViewModel
+    public class AddAssessmentQuestionsViewModel(
+        DetailFramework framework,
+        int newCompetencies,
+        int existingCompetencies,
+        IEnumerable<AssessmentQuestion> defaultQuestions,
+        SelectList questionSelectList
+        ) : AddAssessmentQuestionsFormData
     {
-        public DetailFramework Framework { get; set; }
-        public IEnumerable<AssessmentQuestion>? FrameworkDefaultQuestions { get; set; }
-        public SelectList? QuestionSelectList { get; set; }
+        public int FrameworkID { get; set; } = framework.ID;
+        public string FrameworkName { get; set; } = framework.FrameworkName;
+        public string FrameworkVocabularySingular { get; set; } = FrameworkVocabularyHelper.VocabularySingular(framework.FrameworkConfig);
+        public string FrameworkVocabularyPlural { get; set; } = FrameworkVocabularyHelper.VocabularyPlural(framework.FrameworkConfig);
+        public int PublishStatusID { get; set; } = framework.PublishStatusID;
+        public int NewCompetencies { get; set; } = newCompetencies;
+        public int ExistingCompetencies { get; set; } = existingCompetencies;
+        public IEnumerable<AssessmentQuestion>? DefaultQuestions { get; set; } = defaultQuestions;
+        public SelectList? QuestionSelectList { get; set; } = questionSelectList;
     }
 }
