bf9c2b6b5adfeb10adbd5047ed81af8ee2fe0bb2
kevwhitt-hee <kevin.whittaker@hee.nhs.uk>
2024-12-31 09:37:40 +0000

    TD-5155 Adds view model and constructor

---
+++ DigitalLearningSolutions.Web/ViewModels/Frameworks/Import/AddAssessmentQuestionsFormData.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace DigitalLearningSolutions.Web.ViewModels.Frameworks.Import
+{
+    public class AddAssessmentQuestionsFormData
+    {
+        public List<int> DefaultAssessmentQuestionIDs { get; set; }
+        public List<int> OtherAssessmentQuestionIDs { get; set; }
+    }
+}
