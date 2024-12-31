0c2c9952a15ada59b6be61ec2bd81db944b080e3
kevwhitt-hee <kevin.whittaker@hee.nhs.uk>
2024-12-31 16:36:33 +0000

    TD-5155 Implements conditional preselected checkboxes for default questions

--- DigitalLearningSolutions.Web/ViewModels/Frameworks/Import/AddAssessmentQuestionsFormData.cs
+++ DigitalLearningSolutions.Web/ViewModels/Frameworks/Import/AddAssessmentQuestionsFormData.cs
@@ -4,6 +4,8 @@ namespace DigitalLearningSolutions.Web.ViewModels.Frameworks.Import
 {
     public class AddAssessmentQuestionsFormData
     {
+        public bool AddDefaultAssessmentQuestions { get; set; }
+        public bool AddCustomAssessmentQuestion { get; set; }
         public List<int> DefaultAssessmentQuestionIDs { get; set; }
         public List<int> OtherAssessmentQuestionIDs { get; set; }
     }
