ec55a053aacad80d60672a878ac92f9411e95fc8
Sherif Olaboye <Sherif.Olaboye@hee.nhs.uk>
2025-01-21 10:01:44 +0000

    TD-4435 Mismatch of 'Enrolment method' showing on 'Tracking system' & 'Delegate activities' screen for 'Digital skills assessment tool' assessment

--- DigitalLearningSolutions.Data/DataServices/CourseDataService.cs
+++ DigitalLearningSolutions.Data/DataServices/CourseDataService.cs
@@ -537,16 +537,16 @@ namespace DigitalLearningSolutions.Data.DataServices
             if (candidateAssessmentId > 1 && supervisorDelegateId == 0)
             {
                 connection.Execute(
-                        @"UPDATE CandidateAssessments SET RemovedDate = NULL, EnrolmentMethodId = @enrolmentMethodId, CompleteByDate = @completeByDateDynamic
+                        @"UPDATE CandidateAssessments SET RemovedDate = NULL, EnrolmentMethodId = @enrolmentMethodId, CompleteByDate = @completeByDateDynamic, EnrolledByAdminId = @enrolledByAdminId
                   WHERE ID = @candidateAssessmentId",
-                        new { candidateAssessmentId, enrolmentMethodId, completeByDateDynamic }
+                        new { candidateAssessmentId, enrolmentMethodId, completeByDateDynamic, enrolledByAdminId }
                     );
             }
             if (candidateAssessmentId > 1 && supervisorDelegateId != 0)
             {
                 string sqlQuery = $@"
                 BEGIN TRANSACTION
-                UPDATE CandidateAssessments SET RemovedDate = NULL, EnrolmentMethodId = @enrolmentMethodId, CompleteByDate = @completeByDateDynamic
+                UPDATE CandidateAssessments SET RemovedDate = NULL, EnrolmentMethodId = @enrolmentMethodId, CompleteByDate = @completeByDateDynamic, EnrolledByAdminId = @enrolledByAdminId
                   WHERE ID = @candidateAssessmentId
 
                 UPDATE CandidateAssessmentSupervisors SET Removed = NULL
@@ -556,7 +556,7 @@ namespace DigitalLearningSolutions.Data.DataServices
                 COMMIT TRANSACTION";
 
                 connection.Execute(sqlQuery
-                , new { candidateAssessmentId, selfAssessmentSupervisorRoleId, enrolmentMethodId, completeByDateDynamic, supervisorDelegateId });
+                , new { candidateAssessmentId, selfAssessmentSupervisorRoleId, enrolmentMethodId, completeByDateDynamic, supervisorDelegateId, enrolledByAdminId });
             }
 
             if (supervisorId > 0)
