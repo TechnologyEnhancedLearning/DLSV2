58b567bed904ab8e54079bf6a827f8ba2589347b
Rohit Shrivastava <Rohit.Shrivastava@hee.nhs.uk>
2025-01-28 22:07:56 +0000

    TD-4169 Framework Contributor Permission Not Working

--- DigitalLearningSolutions.Data/DataServices/FrameworkDataService.cs
+++ DigitalLearningSolutions.Data/DataServices/FrameworkDataService.cs
@@ -1824,7 +1824,7 @@ WHERE (FrameworkID = @frameworkId)",
         public int GetAdminUserRoleForFrameworkId(int adminId, int frameworkId)
         {
             return (int)connection.ExecuteScalar(
-                @"SELECT CASE WHEN FW.OwnerAdminID = @adminId THEN 3 WHEN fwc.CanModify = 1 THEN 2 WHEN fwc.CanModify = 0 THEN 1 ELSE 0 END AS UserRole
+                @"SELECT MAX (CASE WHEN FW.OwnerAdminID = @adminId THEN 3 WHEN fwc.CanModify = 1 THEN 2 WHEN fwc.CanModify = 0 THEN 1 ELSE 0 END) AS UserRole
                 FROM Frameworks AS FW LEFT OUTER JOIN
                 FrameworkCollaborators AS fwc ON fwc.FrameworkID = FW.ID AND fwc.AdminID = @adminId
                 WHERE FW.ID = @frameworkId",
