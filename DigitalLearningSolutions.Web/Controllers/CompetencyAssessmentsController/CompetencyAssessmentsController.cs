8f1a1f2ad2ea3c6fea082924e64893bdd22e6e77
kevwhitt-hee <kevin.whittaker@hee.nhs.uk>
2025-01-28 09:07:22 +0000

    TD-5286 Renames role profiles to competency assessments throughout the application

---
+++ DigitalLearningSolutions.Web/Controllers/CompetencyAssessmentsController/CompetencyAssessmentsController.cs
@@ -0,0 +1,54 @@
+﻿namespace DigitalLearningSolutions.Web.Controllers.CompetencyAssessmentsController
+{
+    using DigitalLearningSolutions.Web.Helpers;
+    using DigitalLearningSolutions.Web.Services;
+    using Microsoft.AspNetCore.Authorization;
+    using Microsoft.AspNetCore.Mvc;
+    using Microsoft.Extensions.Configuration;
+    using Microsoft.Extensions.Logging;
+
+    [Authorize(Policy = CustomPolicies.UserFrameworksAdminOnly)]
+    public partial class CompetencyAssessmentsController : Controller
+    {
+        private readonly ICompetencyAssessmentService competencyAssessmentService;
+        private readonly ICommonService commonService;
+        private readonly IFrameworkNotificationService frameworkNotificationService;
+        private readonly ILogger<CompetencyAssessmentsController> logger;
+        private readonly IConfiguration config;
+        public CompetencyAssessmentsController(
+           ICompetencyAssessmentService competencyAssessmentService,
+           ICommonService commonService,
+           IFrameworkNotificationService frameworkNotificationService,
+           ILogger<CompetencyAssessmentsController> logger,
+           IConfiguration config)
+        {
+            this.competencyAssessmentService = competencyAssessmentService;
+            this.commonService = commonService;
+            this.frameworkNotificationService = frameworkNotificationService;
+            this.logger = logger;
+            this.config = config;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+        private int? GetCentreId()
+        {
+            return User.GetCustomClaimAsInt(CustomClaimTypes.UserCentreId);
+        }
+        private int GetAdminID()
+        {
+            return User.GetCustomClaimAsRequiredInt(CustomClaimTypes.UserAdminId);
+        }
+        private bool GetIsWorkforceManager()
+        {
+            var isWorkforceManager = User.GetCustomClaimAsBool(CustomClaimTypes.IsWorkforceManager);
+            return isWorkforceManager != null && (bool)isWorkforceManager;
+        }
+        private bool GetIsWorkforceContributor()
+        {
+            var isWorkforceContributor = User.GetCustomClaimAsBool(CustomClaimTypes.IsWorkforceContributor);
+            return isWorkforceContributor != null && (bool)isWorkforceContributor;
+        }
+    }
+}
---
+++ DigitalLearningSolutions.Web/Services/CompetencyAssessmentService.cs
@@ -0,0 +1,68 @@
+﻿using DigitalLearningSolutions.Data.DataServices;
+using DigitalLearningSolutions.Data.Models.CompetencyAssessments;
+using System.Collections.Generic;
+
+namespace DigitalLearningSolutions.Web.Services
+{
+    public interface ICompetencyAssessmentService
+    {
+        //GET DATA
+        IEnumerable<CompetencyAssessment> GetAllCompetencyAssessments(int adminId);
+
+        IEnumerable<CompetencyAssessment> GetCompetencyAssessmentsForAdminId(int adminId);
+
+        CompetencyAssessmentBase? GetCompetencyAssessmentBaseById(int competencyAssessmentId, int adminId);
+
+        CompetencyAssessmentBase? GetCompetencyAssessmentByName(string competencyAssessmentName, int adminId);
+
+        IEnumerable<NRPProfessionalGroups> GetNRPProfessionalGroups();
+
+        //UPDATE DATA
+        bool UpdateCompetencyAssessmentName(int competencyAssessmentId, int adminId, string competencyAssessmentName);
+
+        bool UpdateCompetencyAssessmentProfessionalGroup(int competencyAssessmentId, int adminId, int? nrpProfessionalGroupID);
+
+    }
+    public class CompetencyAssessmentService : ICompetencyAssessmentService
+    {
+        private readonly ICompetencyAssessmentDataService competencyAssessmentDataService;
+        public CompetencyAssessmentService(ICompetencyAssessmentDataService competencyAssessmentDataService)
+        {
+            this.competencyAssessmentDataService = competencyAssessmentDataService;
+        }
+        public IEnumerable<CompetencyAssessment> GetAllCompetencyAssessments(int adminId)
+        {
+            return competencyAssessmentDataService.GetAllCompetencyAssessments(adminId);
+        }
+
+        public IEnumerable<NRPProfessionalGroups> GetNRPProfessionalGroups()
+        {
+            return competencyAssessmentDataService.GetNRPProfessionalGroups();
+        }
+
+        public CompetencyAssessmentBase? GetCompetencyAssessmentBaseById(int competencyAssessmentId, int adminId)
+        {
+            return competencyAssessmentDataService.GetCompetencyAssessmentBaseById(competencyAssessmentId, adminId);
+        }
+
+        public CompetencyAssessmentBase? GetCompetencyAssessmentByName(string competencyAssessmentName, int adminId)
+        {
+            return competencyAssessmentDataService.GetCompetencyAssessmentByName(competencyAssessmentName, adminId);
+        }
+
+        public IEnumerable<CompetencyAssessment> GetCompetencyAssessmentsForAdminId(int adminId)
+        {
+            return competencyAssessmentDataService.GetCompetencyAssessmentsForAdminId(adminId);
+        }
+
+        public bool UpdateCompetencyAssessmentName(int competencyAssessmentId, int adminId, string competencyAssessmentName)
+        {
+            return competencyAssessmentDataService.UpdateCompetencyAssessmentName(competencyAssessmentId, adminId, competencyAssessmentName);
+        }
+
+        public bool UpdateCompetencyAssessmentProfessionalGroup(int competencyAssessmentId, int adminId, int? nrpProfessionalGroupID)
+        {
+            return competencyAssessmentDataService.UpdateCompetencyAssessmentProfessionalGroup(competencyAssessmentId, adminId, nrpProfessionalGroupID);
+        }
+    }
+}
