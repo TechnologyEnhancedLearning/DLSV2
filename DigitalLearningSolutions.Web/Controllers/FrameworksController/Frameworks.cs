3c7168b87621a54be283526f6dc88ea3bdeb7768
Rohit Shrivastava <Rohit.Shrivastava@hee.nhs.uk>
2025-01-30 22:32:13 +0000

    TD 4169 Run the formatter

--- DigitalLearningSolutions.Web/Controllers/FrameworksController/Frameworks.cs
+++ DigitalLearningSolutions.Web/Controllers/FrameworksController/Frameworks.cs
@@ -166,7 +166,7 @@ namespace DigitalLearningSolutions.Web.Controllers.FrameworksController
                 var sessionNewFramework = multiPageFormService.GetMultiPageFormData<SessionNewFramework>(
                     MultiPageFormDataFeature.AddNewFramework,
                     TempData
-                ).GetAwaiter().GetResult(); 
+                ).GetAwaiter().GetResult();
                 multiPageFormService.SetMultiPageFormData(sessionNewFramework, MultiPageFormDataFeature.AddNewFramework, TempData);
                 detailFramework = sessionNewFramework.DetailFramework;
             }
@@ -252,7 +252,7 @@ namespace DigitalLearningSolutions.Web.Controllers.FrameworksController
             {
                 return StatusCode(500);
             }
-            var sessionNewFramework = multiPageFormService.GetMultiPageFormData<SessionNewFramework>(MultiPageFormDataFeature.AddNewFramework, TempData).GetAwaiter().GetResult();  
+            var sessionNewFramework = multiPageFormService.GetMultiPageFormData<SessionNewFramework>(MultiPageFormDataFeature.AddNewFramework, TempData).GetAwaiter().GetResult();
             multiPageFormService.SetMultiPageFormData(
                 sessionNewFramework,
                 MultiPageFormDataFeature.AddNewFramework,
