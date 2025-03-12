319c2f969337bc6ffeb62daa2dba5bc85c64fded
Auldrin Possa <Auldrin.Possa@hee.nhs.uk>
2025-03-12 15:03:17 +0000

    TD-5412-Added null value check for variables in the message body

---
+++ DigitalLearningSolutions.Data.Migrations/202503121438_Alter_SendExpiredTBCReminders.cs
@@ -0,0 +1,19 @@
+﻿
+
+namespace DigitalLearningSolutions.Data.Migrations
+{
+    using FluentMigrator;
+
+    [Migration(202503121438)]
+    public class Alter_SendExpiredTBCReminders : Migration
+    {
+        public override void Up()
+        {
+            Execute.Sql(Properties.Resources.TD_5412_Alter_SendExpiredTBCReminders_Up);
+        }
+        public override void Down()
+        {
+            Execute.Sql(Properties.Resources.TD_5412_Alter_SendExpiredTBCReminders_Down);
+        }
+    }
+}
