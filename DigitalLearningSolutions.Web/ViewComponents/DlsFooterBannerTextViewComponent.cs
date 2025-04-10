d1a41de01854a53a395f62d364c60669510464b8
ABSINHAA <abhimanu.sinha@nhs.net>
2025-04-10 20:50:23 +0100

    TD_5434 code changes for the new way for showing the banner text in the footer using view component, removing the banner text from the supervisor module and adding the centre information to the footer there.

---
+++ DigitalLearningSolutions.Web/ViewComponents/DlsFooterBannerTextViewComponent.cs
@@ -0,0 +1,14 @@
+﻿using DigitalLearningSolutions.Web.ViewModels.Common.ViewComponents;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DigitalLearningSolutions.Web.ViewComponents
+{
+    public class DlsFooterBannerTextViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke(string centreName)
+        {
+            var model = new DlsFooterBannerTextViewModel(centreName);
+            return View(model);
+        }
+    }
+}
---
+++ DigitalLearningSolutions.Web/ViewModels/Common/ViewComponents/DlsFooterBannerTextViewModel.cs
@@ -0,0 +1,13 @@
+﻿namespace DigitalLearningSolutions.Web.ViewModels.Common.ViewComponents
+{
+    public class DlsFooterBannerTextViewModel
+    {
+        public  readonly string? CentreName;
+
+        public DlsFooterBannerTextViewModel(string centreName)
+        {
+            CentreName = centreName;
+        }
+
+    }
+}
