d70947030a1481004787b15776099345a16e6866
Sherif Olaboye <Sherif.Olaboye@hee.nhs.uk>
2025-04-23 11:00:56 +0100

    TD-5487 Issue showing multiple comments when clicked the back link after submitted the review on Frameworks section

--- DigitalLearningSolutions.Web/Controllers/FrameworksController/Review.cs
+++ DigitalLearningSolutions.Web/Controllers/FrameworksController/Review.cs
@@ -81,6 +81,8 @@
         public IActionResult SubmitFrameworkReview(int frameworkId, int reviewId, string? comment, bool signedOff)
         {
             var adminId = GetAdminId();
+            var framework = frameworkService.GetBaseFrameworkByFrameworkId(frameworkId, adminId);
+            if (framework.FrameworkReviewID == 0 || framework.FrameworkReviewID == null) return RedirectToAction("StatusCode", "LearningSolutions", new { code = 410 });
             int? commentId = null;
             if (!string.IsNullOrWhiteSpace(comment)) commentId = frameworkService.InsertComment(frameworkId, adminId, comment, null);
             frameworkService.SubmitFrameworkReview(frameworkId, reviewId, signedOff, commentId);
