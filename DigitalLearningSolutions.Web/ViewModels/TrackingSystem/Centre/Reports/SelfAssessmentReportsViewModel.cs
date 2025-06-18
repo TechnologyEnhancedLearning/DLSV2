5beabf1f4c65e0acf6bf324a0b828602e7a70324
sherif-olaboye <123654949+sherif-olaboye@users.noreply.github.com>
2025-06-18 12:07:09 +0100

    TD-5683 Issue showing self assessment reports on Tracking system which are not published at the centre

--- DigitalLearningSolutions.Web/ViewModels/TrackingSystem/Centre/Reports/SelfAssessmentReportsViewModel.cs
+++ DigitalLearningSolutions.Web/ViewModels/TrackingSystem/Centre/Reports/SelfAssessmentReportsViewModel.cs
@@ -6,15 +6,17 @@
     public class SelfAssessmentReportsViewModel
     {
         public SelfAssessmentReportsViewModel(
-            IEnumerable<SelfAssessmentSelect> selfAssessmentSelects, int? adminCategoryId, int categoryId
+            IEnumerable<SelfAssessmentSelect> selfAssessmentSelects, int? adminCategoryId, int categoryId, bool dSATreportIsPublish
             )
         {
             SelfAssessmentSelects = selfAssessmentSelects;
             AdminCategoryId = adminCategoryId;
             CategoryId = categoryId;
+            DSATreportIsPublish = dSATreportIsPublish;
         }
         public IEnumerable<SelfAssessmentSelect> SelfAssessmentSelects { get; set; }
         public int? AdminCategoryId { get; set; }
         public int CategoryId { get; set; }
+        public bool DSATreportIsPublish { get; set; }
     }
 }
