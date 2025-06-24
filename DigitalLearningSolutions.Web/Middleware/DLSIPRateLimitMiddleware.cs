04054dbd5cc1720fa3ab80c0dd36026562365b5d
ABSinhaa <abhimanu.sinha@nhs.net>
2025-06-24 19:55:30 +0100

    TD_5410_Adding config to the rate limiter so that we can conpare the environment, and add production logic to it.

--- DigitalLearningSolutions.Web/Middleware/DLSIPRateLimitMiddleware.cs
+++ DigitalLearningSolutions.Web/Middleware/DLSIPRateLimitMiddleware.cs
@@ -1,17 +1,22 @@
 namespace DigitalLearningSolutions.Web.Middleware
 {
+    using System;
     using System.Threading.Tasks;
     using AspNetCoreRateLimit;
     using Microsoft.AspNetCore.Http;
+    using Microsoft.Extensions.Configuration;
     using Microsoft.Extensions.Logging;
     using Microsoft.Extensions.Options;
+    using static Org.BouncyCastle.Math.EC.ECCurve;
 
     public class DLSIPRateLimitMiddleware : IpRateLimitMiddleware
     {
+        private readonly IConfiguration _configuration;
         public DLSIPRateLimitMiddleware(
             RequestDelegate next,
             IProcessingStrategy processingStrategy,
             IOptions<IpRateLimitOptions> options,
+            IConfiguration configuration,
             IIpPolicyStore policyStore,
             IRateLimitConfiguration config,
             ILogger<IpRateLimitMiddleware> logger)
@@ -21,16 +26,23 @@ namespace DigitalLearningSolutions.Web.Middleware
                   policyStore,
                   config,
                   logger)
-        { }
+        {
+            _configuration = configuration;
+        }
 
         public override Task ReturnQuotaExceededResponse(
             HttpContext httpContext,
             RateLimitRule rule,
             string retryAfter)
         {
-            httpContext.Response.Headers["Location"] = "/toomanyrequests";
-            httpContext.Response.StatusCode = 302;
+            if (_configuration["ASPNETCORE_ENVIRONMENT"] == "PRODUCTION")
+            {
+                httpContext.Response.Headers["Location"] = "/toomanyrequests";
+                httpContext.Response.StatusCode = 302;
+                
+            }
             return httpContext.Response.WriteAsync("");
+
         }
     }
 }
