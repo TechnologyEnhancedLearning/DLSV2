4efaeda01312d33a659e024022399fe622bcbbe4
kevwhitt-hee <kevin.whittaker@hee.nhs.uk>
2025-07-03 09:29:58 +0100

    TD-5759 Refactors self assessment report sql to use SP, CTE and TVF

--- DigitalLearningSolutions.Data/DataServices/SelfAssessmentDataService/SelfAsssessmentReportDataService.cs
+++ DigitalLearningSolutions.Data/DataServices/SelfAssessmentDataService/SelfAsssessmentReportDataService.cs
@@ -45,94 +45,9 @@
 
         public IEnumerable<SelfAssessmentReportData> GetSelfAssessmentReportDataForCentre(int centreId, int selfAssessmentId)
         {
-            return connection.Query<SelfAssessmentReportData>(
-                @"WITH LatestAssessmentResults AS
-                                (
-                    SELECT	s.DelegateUserID
-                    		, CASE WHEN COALESCE (rr.LevelRAG, 0) = 3 THEN s.ID ELSE NULL END AS SelfAssessed
-                    		, CASE WHEN sv.Verified IS NOT NULL AND sv.SignedOff = 1 AND COALESCE (rr.LevelRAG, 0) = 3 THEN s.ID ELSE NULL END AS Confirmed
-                    		, CASE WHEN sas.Optional = 1  THEN s.CompetencyID ELSE NULL END AS Optional
-                    FROM   SelfAssessmentResults AS s LEFT OUTER JOIN
-                                 SelfAssessmentStructure AS sas ON sas.SelfAssessmentID = @selfAssessmentId AND s.CompetencyID = sas.CompetencyID LEFT OUTER JOIN
-                                 SelfAssessmentResultSupervisorVerifications AS sv ON s.ID = sv.SelfAssessmentResultId AND sv.Superceded = 0 LEFT OUTER JOIN
-                                 CompetencyAssessmentQuestionRoleRequirements AS rr ON s.CompetencyID = rr.CompetencyID AND s.AssessmentQuestionID = rr.AssessmentQuestionID AND sas.SelfAssessmentID = rr.SelfAssessmentID AND s.Result = rr.LevelValue
-                    WHERE (sas.SelfAssessmentID = @selfAssessmentId)
-                                )
-                    SELECT 
-                        sa.Name AS SelfAssessment
-                    	, u.LastName + ', ' + u.FirstName AS Learner
-						, da.Active AS LearnerActive
-                    	, u.ProfessionalRegistrationNumber AS PRN
-                        , jg.JobGroupName AS JobGroup
-                    	, da.Answer1 AS RegistrationAnswer1
-                    	, da.Answer2 AS RegistrationAnswer2
-                    	, da.Answer3 AS RegistrationAnswer3
-                    	, da.Answer4 AS RegistrationAnswer4
-                    	, da.Answer5 AS RegistrationAnswer5
-                    	, da.Answer6 AS RegistrationAnswer6
-                        , dbo.GetOtherCentresForSelfAssessment(da.UserID, @SelfAssessmentID, c.CentreID) AS OtherCentres
-                        , CASE
-                            WHEN aa.ID IS NULL THEN 'Learner'
-                            WHEN aa.IsCentreManager = 1 THEN 'Centre Manager'
-                            WHEN aa.IsCentreAdmin = 1 AND aa.IsCentreManager = 0 THEN 'Centre Admin'
-                            WHEN aa.IsSupervisor = 1 THEN 'Supervisor'
-                            WHEN aa.IsNominatedSupervisor = 1 THEN 'Nominated supervisor'
-                        END AS DLSRole
-                    	, da.DateRegistered AS Registered
-                        , ca.StartedDate AS Started
-                        , ca.LastAccessed
-                    	, COALESCE(COUNT(DISTINCT LAR.Optional), NULL) AS [OptionalProficienciesAssessed]
-                    	, COALESCE(COUNT(DISTINCT LAR.SelfAssessed), NULL) AS [SelfAssessedAchieved]
-                    	, COALESCE(COUNT(DISTINCT LAR.Confirmed), NULL) AS [ConfirmedResults]
-                        , max(casv.Requested) AS SignOffRequested
-                        , max(1*casv.SignedOff) AS SignOffAchieved
-                        , min(casv.Verified) AS ReviewedDate
-                    FROM   
-                        CandidateAssessments AS ca INNER JOIN
-						DelegateAccounts AS da ON ca.DelegateUserID = da.UserID and da.CentreID = @centreId INNER JOIN
-						Users as u ON u.ID = da.UserID INNER JOIN
-                        SelfAssessments AS sa INNER JOIN
-                        CentreSelfAssessments AS csa ON sa.ID = csa.SelfAssessmentID INNER JOIN
-                        Centres AS c ON csa.CentreID = c.CentreID ON da.CentreID = c.CentreID AND ca.SelfAssessmentID = sa.ID INNER JOIN
-                        JobGroups AS jg ON u.JobGroupID = jg.JobGroupID LEFT OUTER JOIN
-                        AdminAccounts AS aa ON da.UserID = aa.UserID AND aa.CentreID = da.CentreID AND aa.Active = 1 LEFT OUTER JOIN
-                        CandidateAssessmentSupervisors AS cas ON ca.ID = cas.CandidateAssessmentID left JOIN
-                        CandidateAssessmentSupervisorVerifications AS casv ON casv.CandidateAssessmentSupervisorID = cas.ID LEFT JOIN
-	                    SupervisorDelegates AS sd ON cas.SupervisorDelegateId = sd.ID 
-	                    LEFT OUTER JOIN LatestAssessmentResults AS LAR ON LAR.DelegateUserID = ca.DelegateUserID
-                    WHERE
-                        (sa.ID = @SelfAssessmentID) AND (sa.ArchivedDate IS NULL) AND (c.Active = 1) AND (ca.RemovedDate IS NULL AND ca.NonReportable = 0)
-                    Group by sa.Name
-	                    , u.LastName + ', ' + u.FirstName
-						, da.Active
-	                    , u.ProfessionalRegistrationNumber
-	                    , c.CustomField1PromptID
-	                    , c.CustomField2PromptID
-	                    , c.CustomField3PromptID
-	                    , c.CustomField4PromptID
-	                    , c.CustomField5PromptID
-	                    , c.CustomField6PromptID
-                        , c.CentreID
-                        , jg.JobGroupName
-                        , da.ID
-	                    , da.Answer1
-	                    , da.Answer2
-	                    , da.Answer3
-	                    , da.Answer4
-	                    , da.Answer5
-	                    , da.Answer6
-	                    , da.DateRegistered
-                        , da.UserID
-                        , aa.ID
-                        , aa.IsCentreManager
-                        , aa.IsCentreAdmin
-                        , aa.IsSupervisor
-                        , aa.IsNominatedSupervisor
-                        , ca.StartedDate
-                        , ca.LastAccessed
-                    ORDER BY
-                        SelfAssessment, u.LastName + ', ' + u.FirstName",
-                new { centreId, selfAssessmentId }
+            return connection.Query<SelfAssessmentReportData>("usp_GetSelfAssessmentReport",
+                new { selfAssessmentId, centreId },
+                commandType: CommandType.StoredProcedure
             );
         }
     } 
