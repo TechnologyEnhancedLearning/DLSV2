0e63d485540dbe4ba12556be25b2eb648796dad7
kevwhitt-hee <kevin.whittaker@hee.nhs.uk>
2025-08-29 17:09:36 +0100

    Viewmodels for manage optional competencies

---
+++ DigitalLearningSolutions.Web/ViewModels/CompetencyAssessments/SelectOptionalCompetenciesFormData.cs
@@ -0,0 +1,9 @@
+﻿namespace DigitalLearningSolutions.Web.ViewModels.CompetencyAssessments
+{
+    public class SelectOptionalCompetenciesFormData
+    {
+        public int ID { get; set; }
+        public bool? TaskStatus { get; set; }
+        public int[] SelectedCompetencyIds { get; set; } = [];
+    }
+}
---
+++ DigitalLearningSolutions.Web/ViewModels/CompetencyAssessments/SelectOptionalCompetenciesViewModel.cs
@@ -0,0 +1,27 @@
+﻿namespace DigitalLearningSolutions.Web.ViewModels.CompetencyAssessments
+{
+    using DigitalLearningSolutions.Data.Models.CompetencyAssessments;
+    using DigitalLearningSolutions.Web.Helpers;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class SelectOptionalCompetenciesViewModel : SelectOptionalCompetenciesFormData
+    {
+        public SelectOptionalCompetenciesViewModel(CompetencyAssessmentBase competencyAssessmentBase, IEnumerable<Competency> competencies, bool? taskStatus)
+        {
+            ID = competencyAssessmentBase.ID;
+            TaskStatus = taskStatus;
+            CompetencyAssessmentName = competencyAssessmentBase.CompetencyAssessmentName;
+            UserRole = competencyAssessmentBase.UserRole;
+            Competencies = competencies;
+            VocabularySingular = FrameworkVocabularyHelper.VocabularySingular(competencyAssessmentBase.Vocabulary);
+            VocabularyPlural = FrameworkVocabularyHelper.VocabularyPlural(competencyAssessmentBase.Vocabulary);
+            SelectedCompetencyIds = competencies.Where(c => c.Optional == true).Select(c => c.CompetencyID).ToArray();
+        }
+        public string CompetencyAssessmentName { get; set; }
+        public int UserRole { get; set; }
+        public string VocabularySingular { get; set; }
+        public string VocabularyPlural { get; set; }
+        public IEnumerable<Competency> Competencies { get; set; }
+    }
+}
