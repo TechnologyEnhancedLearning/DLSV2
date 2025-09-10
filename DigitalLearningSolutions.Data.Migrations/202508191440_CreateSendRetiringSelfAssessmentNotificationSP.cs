766149fc102a23b5875c0f31e33ab0339fc814ba
Auldrin-Possa <auldrin.possa@hee.nhs.uk>
2025-09-10 08:31:12 +0100

    TD-5552-Moved general TEL team details to config table.

--- DigitalLearningSolutions.Data.Migrations/202508191440_CreateSendRetiringSelfAssessmentNotificationSP.cs
+++ DigitalLearningSolutions.Data.Migrations/202508191440_CreateSendRetiringSelfAssessmentNotificationSP.cs
@@ -6,10 +6,25 @@
     {
         public override void Up()
         {
+            string generalTELTeam =
+                @"[
+                    { ""FirstName"":"""", ""LastName"":"""", ""Email"":""Support@dls.nhs.uk"" },
+                    { ""FirstName"":""Anna"", ""LastName"":""Athanasopoulou"", ""Email"":""anna.athanasopoulou@nhs.net"" },
+                    { ""FirstName"":""Benjamin"", ""LastName"":""Witton"", ""Email"":""Benjamin.witton1@nhs.net"" }
+                ]";
+
+            Execute.Sql(@$"IF NOT EXISTS (SELECT ConfigID FROM Config WHERE ConfigName = 'GeneralTELTeam')
+                        BEGIN
+                          INSERT INTO Config VALUES ('GeneralTELTeam', '{generalTELTeam}', 0,GETDATE(), GETDATE())
+                        END"
+               );
+
             Execute.Sql(Properties.Resources.TD_5552_SendRetiringNotification);
         }
         public override void Down()
         {
+            Execute.Sql(@"DELETE FROM Config WHERE ConfigName = N'GeneralTELTeam'");
+
             Execute.Sql("DROP PROCEDURE [dbo].[SendRetiringSelfAssessmentNotification]");
         }
     }
