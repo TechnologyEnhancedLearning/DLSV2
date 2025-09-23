385f464ef752fcf08335d8b0ec806cc76f400262
Auldrin-Possa <auldrin.possa@hee.nhs.uk>
2025-09-23 15:47:38 +0100

    TD-6149-Retiring confirmation page added, added/modified code in relevant files.

--- DigitalLearningSolutions.Web/ViewModels/Common/RetiringSelfAssessmentViewModel.cs
+++ DigitalLearningSolutions.Web/ViewModels/Common/RetiringSelfAssessmentViewModel.cs
@@ -1,4 +1,4 @@
-﻿namespace DigitalLearningSolutions.Web.ViewModels.Supervisor
+﻿namespace DigitalLearningSolutions.Web.ViewModels.Common
 {
     using DigitalLearningSolutions.Web.Attributes;
     using System;
@@ -6,7 +6,7 @@
     public class RetiringSelfAssessmentViewModel
     {
         public int SelfAssessmentID { get; set; }
-        public int SupervisorDelegateID { get; set; }
+        public int RouteID { get; set; }
         public DateTime? RetirementDate { get; set; }
         [BooleanMustBeTrue(ErrorMessage = "Please tick the checkbox to confirm you wish to perform this action")]
         public bool ActionConfirmed { get; set; }
