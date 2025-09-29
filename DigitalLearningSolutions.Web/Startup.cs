4c92b6abdf2d1e5d63c65114d81a477e2bc815e1
Phil-NHS <philip.tate@nhs.net>
2025-09-29 11:37:16 +0100

    TD-6277 Nonce console errors but not gmt.js
    Comment: May be desireable to consider a task to pass the nonce in header, base controller in viewbag and randomly generate it

--- DigitalLearningSolutions.Web/Startup.cs
+++ DigitalLearningSolutions.Web/Startup.cs
@@ -658,7 +658,7 @@ namespace DigitalLearningSolutions.Web
                     endpoints.MapControllerRoute("default", "{controller=Home}/{action=Index}")
             );
 
-            migrationRunner.MigrateUp();
+             migrationRunner.MigrateUp();
         }
 
         private Task RedirectToLogin(RedirectContext<CookieAuthenticationOptions> context)
