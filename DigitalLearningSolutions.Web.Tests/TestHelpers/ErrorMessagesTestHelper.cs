fca89d2a50dc17d84e55ca4455d68301d558409f
sherif-olaboye <123654949+sherif-olaboye@users.noreply.github.com>
2025-10-02 11:09:55 +0100

    TD-4624 Fixing the test that is failing

--- DigitalLearningSolutions.Web.Tests/TestHelpers/ErrorMessagesTestHelper.cs
+++ DigitalLearningSolutions.Web.Tests/TestHelpers/ErrorMessagesTestHelper.cs
@@ -5,14 +5,16 @@ namespace DigitalLearningSolutions.Web.Tests.TestHelpers
     {
         public const string InvalidFormatError =
             @"The format you entered isn’t recognised. Please check and try again.
-            Valid formats include
-            7 digits - example, 1234567
-            1-2 letters followed by 6 digits - example, AB123456
-            ‘P’ followed by 5-6 digits - example, P12345, P123456
-            ‘C’ or ‘P’ followed by 6 digits - example, C123456, P123456
-            Optional letter followed by 5-6 digits - example, A12345, B123456
-            ‘L’ followed by 4-6 digits - example, L1234, L123456
-            2 digits, hyphen, then 4-5 alphanumeric characters - example, 12-AB123";
+            <br>Valid formats include:
+            <ul>
+                <li>7 digits - example, 1234567</li>
+                <li>1–2 letters followed by 6 digits - example, AB123456</li>
+                <li>‘P’ followed by 5–6 digits - example, P12345, P123456</li>
+                <li>‘C’ or ‘P’ followed by 6 digits - example, C123456, P123456</li>
+                <li>Optional letter followed by 5–6 digits - example, A12345, B123456</li>
+                <li>‘L’ followed by 4–6 digits - example, L1234, L123456</li>
+                <li>2 digits, hyphen, then 4–5 alphanumeric characters - example, 12-AB123</li>
+            </ul>";
 
         public const string MissingNumberError = "Enter a professional registration number";
         public const string LengthError = "Professional registration number must be between 4 and 8 characters";
