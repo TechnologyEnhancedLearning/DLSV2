c25f0d7dd22c23e8a48cea4fa81e29ec1004ec8e
sherif-olaboye <123654949+sherif-olaboye@users.noreply.github.com>
2025-11-26 10:38:47 +0000

    TD-5307 Refactoring validation

--- DigitalLearningSolutions.Web/Controllers/CompetencyAssessmentsController/CompetencyAssessments.cs
+++ DigitalLearningSolutions.Web/Controllers/CompetencyAssessmentsController/CompetencyAssessments.cs
@@ -429,31 +429,40 @@
         {
             var adminId = GetAdminID();
             var competencyAssessmentId = model.CompetencyAssessmentId;
-            if (!ModelState.IsValid || (model.FrameworkId == 0 && model.ActionName == "AddFramework"))
+            if (!ModelState.IsValid)
             {
-                if (model.FrameworkId == 0 && model.ActionName == "AddFramework")
-                {
-                    ModelState.AddModelError(nameof(model.FrameworkId), "You must select the framework sources you wish to add to the competency assessment");
-                }
                 var frameworks = frameworkService.GetAllFrameworks(adminId);
                 var competencyAssessmentBase = competencyAssessmentService.GetCompetencyAssessmentBaseById(competencyAssessmentId, adminId);
+
                 if (competencyAssessmentBase == null)
                 {
-                    logger.LogWarning($"Failed to load Vocabulary page for competencyAssessmentId: {competencyAssessmentId} adminId: {adminId}");
+                    logger.LogWarning( $"Failed to load Vocabulary page for competencyAssessmentId: {competencyAssessmentId} adminId: {adminId}");
                     return StatusCode(500);
                 }
+
                 if (competencyAssessmentBase.UserRole < 2)
                 {
                     return StatusCode(403);
                 }
+
                 var primaryFrameworkId = competencyAssessmentService.GetPrimaryLinkedFrameworkId(competencyAssessmentId);
                 var additionalFrameworks = competencyAssessmentService.GetLinkedFrameworkIds(competencyAssessmentId);
-                var viewModel = new SelectFrameworkSourcesViewModel(competencyAssessmentBase, frameworks, additionalFrameworks, primaryFrameworkId, model.TaskStatus, model.ActionName);
+
+                var viewModel = new SelectFrameworkSourcesViewModel(
+                    competencyAssessmentBase,
+                    frameworks,
+                    additionalFrameworks,
+                    primaryFrameworkId,
+                    model.TaskStatus,
+                    model.ActionName
+                );
+
                 return View("SelectFrameworkSources", viewModel);
             }
+
             if (actionName == "AddFramework")
             {
-                competencyAssessmentService.InsertSelfAssessmentFramework(adminId, competencyAssessmentId, model.FrameworkId);
+                competencyAssessmentService.InsertSelfAssessmentFramework(adminId, competencyAssessmentId, model.FrameworkId.Value);
                 return RedirectToAction("SelectFrameworkSources", new { competencyAssessmentId, actionName = "Summary" });
             }
             else
