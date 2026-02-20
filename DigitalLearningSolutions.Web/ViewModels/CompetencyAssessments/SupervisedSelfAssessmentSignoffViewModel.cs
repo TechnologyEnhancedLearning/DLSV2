3cbb359b0d251d279c436b6826aac87ffbe2b3a0
Auldrin-Possa <auldrin.possa@hee.nhs.uk>
2026-02-20 08:22:19 +0000

    TD-5401-Supervisor roles updated in the featuer

--- DigitalLearningSolutions.Web/ViewModels/CompetencyAssessments/SupervisedSelfAssessmentSignoffViewModel.cs
+++ DigitalLearningSolutions.Web/ViewModels/CompetencyAssessments/SupervisedSelfAssessmentSignoffViewModel.cs
@@ -14,15 +14,15 @@
         {
             CompetencyAssessmentId = model.CompetencyAssessmentId;
             Confirm = model.Confirm;
-            Supervised = model.Supervised;
+            Signoff = model.Signoff;
             ActionName = model.ActionName;
         }
         public int CompetencyAssessmentId { get; set; }
         public string CompetencyAssessmentName { get; set; } = string.Empty;
-        public int? Supervised { get; set; }
+        public int? Signoff { get; set; }
         public int? Confirm { get; set; }
         public string? ActionName { get; set; }
-        public string SupervisedText => Supervised == 1 ? "Yes" : "No";
+        public string SignoffText => Signoff == 1 ? "Yes" : "No";
         public string ConfirmText => Confirm == 1 ? "Yes" : "No";
     }
 }
