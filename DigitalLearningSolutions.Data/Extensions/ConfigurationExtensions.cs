c2e692584301507859c975bfd0883ec0f5e3667d
sherif-olaboye <123654949+sherif-olaboye@users.noreply.github.com>
2026-06-11 10:00:20 +0100

    TD-7112 Remove the Log in button in DLS

--- DigitalLearningSolutions.Data/Extensions/ConfigurationExtensions.cs
+++ DigitalLearningSolutions.Data/Extensions/ConfigurationExtensions.cs
@@ -11,7 +11,6 @@
         private const string LearningHubOpenApiKey = "LearningHubOpenAPIKey";
         private const string LearningHubOpenApiBaseUrl = "LearningHubOpenAPIBaseUrl";
         private const string PricingPageEnabled = "FeatureManagement:PricingPage";
-        private const string ShowDlsLoginButton = "FeatureManagement:ShowDlsLoginButton";
         
         private const string LearningHubAuthBaseUrl = "BaseUrl";
         private const string LearningHubAuthLoginEndpoint = "LoginEndpoint";
@@ -117,11 +116,6 @@
         public static bool IsPricingPageEnabled(this IConfiguration config)
         {
             bool.TryParse(config[PricingPageEnabled], out bool isEnabled);
-            return isEnabled;
-        }
-        public static bool IsDlsLoginButtonShown(this IConfiguration config)
-        {
-            bool.TryParse(config[ShowDlsLoginButton], out bool isEnabled);
             return isEnabled;
         }
         public static int GetLearningHubSsoHashTolerance(this IConfiguration config)
