36f4f8240964572f5d24899f16f528784a4874f3
sherif-olaboye <123654949+sherif-olaboye@users.noreply.github.com>
2026-08-06 16:49:32 +0100

    TD-7596 Review and optimise SP StoreDiagScoreSCO

---
+++ DigitalLearningSolutions.Data.Migrations/202608051545_TD-7596_Alter_StoreDiagScoreSCO.cs
@@ -0,0 +1,19 @@
+﻿
+
+namespace DigitalLearningSolutions.Data.Migrations
+{
+    using FluentMigrator;
+
+    [Migration(202608061170)]
+    public class _202608051545_TD_7596_Alter_StoreDiagScoreSCO : Migration
+    {
+        public override void Up()
+        {
+            Execute.Sql(Properties.Resources.TD_7596_Alter_StoreDiagScoreSCO_Up);
+        }
+        public override void Down()
+        {
+            Execute.Sql(Properties.Resources.TD_7596_Alter_StoreDiagScoreSCO_Down);
+        }
+    }
+}
