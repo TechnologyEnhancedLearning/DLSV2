e2c269645a582f1a2b665a8ccaae7fc5560ac874
Rohit Shrivastava <rohit.shrivastava@hee.nhs.uk>
2026-08-12 15:59:05 +0100

    TD-7590 Optimise GetAssessmentResultByDelegate

---
+++ DigitalLearningSolutions.Data.Migrations/202608102300_Alter_GetAssessmentResultsByDelegate.cs
@@ -0,0 +1,123 @@
+﻿namespace DigitalLearningSolutions.Data.Migrations
+{
+    using FluentMigrator;
+    using FluentMigrator.SqlServer;
+
+    [Migration(202608102300)]
+    public class Alter_GetAssessmentResultByDelegate : Migration
+    {
+        public override void Up()
+        {
+            Create.Index("IX_SelfAssessmentResults_Assessment_Delegate_Question")
+                .OnTable("SelfAssessmentResults")
+                .OnColumn("SelfAssessmentID").Ascending()
+                .OnColumn("DelegateUserID").Ascending()
+                .OnColumn("CompetencyID").Ascending()
+                .OnColumn("AssessmentQuestionID").Ascending()
+                .WithOptions()
+                .NonClustered()
+                .Include("ID")
+                .Include("DateTime")
+                .Include("Result")
+                .Include("SupportingComments");
+
+            Create.Index("IX_CandidateAssessments_Active_Assessment_Delegate")
+                .OnTable("CandidateAssessments")
+                .OnColumn("SelfAssessmentID").Ascending()
+                .OnColumn("DelegateUserID").Ascending()
+                .WithOptions()
+                .NonClustered()
+                .Include("ID");
+
+            Execute.Sql(@"CREATE NONCLUSTERED INDEX IX_CandidateAssessments_Active_Assessment_Delegate ON dbo.CandidateAssessments (SelfAssessmentID, DelegateUserID) INCLUDE (ID) WHERE RemovedDate IS NULL;");
+
+            Create.Index("IX_SelfAssessmentStructure_Assessment_Competency")
+                .OnTable("SelfAssessmentStructure")
+                .OnColumn("SelfAssessmentID").Ascending()
+                .OnColumn("CompetencyID").Ascending()
+                .WithOptions()
+                .NonClustered()
+                .Include("CompetencyGroupID")
+                .Include("Ordering")
+                .Include("Optional");
+
+            Create.Index("IX_CompetencyAssessmentQuestions_Competency_Question")
+                .OnTable("CompetencyAssessmentQuestions")
+                .OnColumn("CompetencyID").Ascending()
+                .OnColumn("AssessmentQuestionID").Ascending()
+                .WithOptions()
+                .NonClustered()
+                .Include("Ordering")
+                .Include("Required");
+
+            Create.Index("IX_CandidateAssessmentOptionalCompetencies_Lookup")
+                .OnTable("CandidateAssessmentOptionalCompetencies")
+                .OnColumn("CandidateAssessmentID").Ascending()
+                .OnColumn("CompetencyID").Ascending()
+                .OnColumn("CompetencyGroupID").Ascending()
+                .WithOptions()
+                .NonClustered()
+                .Include("IncludedInSelfAssessment");
+
+            Create.Index("IX_SelfAssessmentResultSupervisorVerifications_Result_Active")
+                .OnTable("SelfAssessmentResultSupervisorVerifications")
+                .OnColumn("SelfAssessmentResultId").Ascending()
+                .OnColumn("Superceded").Ascending()
+                .WithOptions()
+                .NonClustered()
+                .Include("ID")
+                .Include("Requested")
+                .Include("Verified")
+                .Include("Comments")
+                .Include("SignedOff")
+                .Include("CandidateAssessmentSupervisorID");
+
+            Create.Index("IX_CompetencyAssessmentQuestionRoleRequirements_ResultRag")
+                .OnTable("CompetencyAssessmentQuestionRoleRequirements")
+                .OnColumn("SelfAssessmentID").Ascending()
+                .OnColumn("CompetencyID").Ascending()
+                .OnColumn("AssessmentQuestionID").Ascending()
+                .OnColumn("LevelValue").Ascending()
+                .WithOptions()
+                .NonClustered()
+                .Include("LevelRAG");
+
+            Create.Index("IX_FrameworkCompetencies_Competency_Framework")
+                .OnTable("FrameworkCompetencies")
+                .OnColumn("CompetencyID").Ascending()
+                .OnColumn("FrameworkID").Ascending()
+                .WithOptions()
+                .NonClustered();
+
+            Execute.Sql(Properties.Resources.TD_7273_Alter_GetAssessmentResultsByDelegate_Up);
+        }
+        public override void Down()
+        {
+            Delete.Index("IX_SelfAssessmentResults_Assessment_Delegate_Question")
+                .OnTable("SelfAssessmentResults");
+
+            Delete.Index("IX_CandidateAssessments_Active_Assessment_Delegate")
+                .OnTable("CandidateAssessments");
+
+            Delete.Index("IX_SelfAssessmentStructure_Assessment_Competency")
+                .OnTable("SelfAssessmentStructure");
+
+            Delete.Index("IX_CompetencyAssessmentQuestions_Competency_Question")
+                .OnTable("CompetencyAssessmentQuestions");
+
+            Delete.Index("IX_CandidateAssessmentOptionalCompetencies_Lookup")
+                .OnTable("CandidateAssessmentOptionalCompetencies");
+
+            Delete.Index("IX_SelfAssessmentResultSupervisorVerifications_Result_Active")
+                .OnTable("SelfAssessmentResultSupervisorVerifications");
+
+            Delete.Index("IX_CompetencyAssessmentQuestionRoleRequirements_ResultRag")
+                .OnTable("CompetencyAssessmentQuestionRoleRequirements");
+
+            Delete.Index("IX_FrameworkCompetencies_Competency_Framework")
+                .OnTable("FrameworkCompetencies");
+
+            Execute.Sql(Properties.Resources.TD_7273_Alter_GetAssessmentResultsByDelegate_Down);
+        }
+    }
+}
--- DigitalLearningSolutions.Data.Migrations/Properties/Resources.Designer.cs
+++ DigitalLearningSolutions.Data.Migrations/Properties/Resources.Designer.cs
@@ -19,7 +19,7 @@ namespace DigitalLearningSolutions.Data.Migrations.Properties {
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "17.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "18.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     internal class Resources {
@@ -3100,7 +3100,57 @@ namespace DigitalLearningSolutions.Data.Migrations.Properties {
                 return ResourceManager.GetString("TD_7592_Alter_GetNotificationsForAdminUser_Up", resourceCulture);
             }
         }
-        
+
+        /// <summary>
+        ///   Looks up a localized string similar to /****** Object:  StoredProcedure [dbo].[GetAssessmentResultsByDelegate]    Script Date: 03/08/2026 15:54:52 ******/
+        ///SET ANSI_NULLS ON
+        ///GO
+        ///
+        ///SET QUOTED_IDENTIFIER ON
+        ///GO
+        ///
+        ///
+        ///-- =============================================
+        ///-- Author:		Rohit Shrivastava
+        ///-- Create date: 10/08/2026
+        ///-- Description:	
+        ///-- =============================================
+        ///ALTER PROCEDURE [dbo].[GetAssessmentResultsByDelegate]
+        ///
+        /// </summary>
+        internal static string TD_7273_Alter_GetAssessmentResultsByDelegate_Down
+        {
+            get
+            {
+                return ResourceManager.GetString("TD_7273_Alter_GetAssessmentResultsByDelegate_Down", resourceCulture);
+            }
+        }
+
+        /// <summary>
+        ///   Looks up a localized string similar to /****** Object:  StoredProcedure [dbo].[GetAssessmentResultsByDelegate]    Script Date: 03/08/2026 15:54:52 ******/
+        ///SET ANSI_NULLS ON
+        ///GO
+        ///
+        ///SET QUOTED_IDENTIFIER ON
+        ///GO
+        ///
+        ///
+        ///-- =============================================
+        ///-- Author:		Rohit Shrivastava
+        ///-- Create date: 10/08/2026
+        ///-- Description:	
+        ///-- =============================================
+        ///ALTER PROCEDURE [dbo].[GetAssessmentResultsByDelegate]
+        ///	
+        /// </summary>
+        internal static string TD_7273_Alter_GetAssessmentResultsByDelegate_Up
+        {
+            get
+            {
+                return ResourceManager.GetString("TD_7273_Alter_GetAssessmentResultsByDelegate_Up", resourceCulture);
+            }
+        }
+
         /// <summary>
         ///   Looks up a localized string similar to /****** Object:  StoredProcedure [dbo].[GetActiveAvailableCustomisationsForCentreFiltered_V6]    Script Date: 29/09/2022 19:11:04 ******/
         ///SET ANSI_NULLS ON
