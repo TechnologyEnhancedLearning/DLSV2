1796c01e7deada2f47194d07cdfe5e34a94ed07f
Auldrin-Possa <auldrin.possa@hee.nhs.uk>
2026-09-18 15:48:25 +0100

    TD-7794-Removed CompletedDate filter from learning log items lookup.

--- DigitalLearningSolutions.Web/Services/RecommendedLearningService.cs
+++ DigitalLearningSolutions.Web/Services/RecommendedLearningService.cs
@@ -128,7 +128,7 @@
                 ll => ll.ArchivedDate == null && ll.LearningHubResourceReferenceId == rr.RefId
             ).ToList();
             var incompleteLearningLogItem =
-                learningLogItemsForResource.SingleOrDefault(ll => ll.CompletedDate == null);
+                learningLogItemsForResource.SingleOrDefault();
 
             var clrsForResource =
                 competencyLearningResources.Where(clr => clr.LearningHubResourceReferenceId == rr.RefId)
